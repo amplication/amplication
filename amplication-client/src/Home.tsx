fb2d65460f162b8434435e90adc452072384740f
Iddan Aaronsohn <mail@aniddan.com>
2020-05-06 20:15:46 +0300

    Add styles

--- amplication-client/src/Home.tsx
+++ amplication-client/src/Home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Icon } from "@rmwc/icon";
+import "./Home.css";
 
 const apps = [
   {
-    name: "",
+    id: "test",
+    name: "Test App",
+    description:
+      "App description - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    version: "V2",
   },
 ];
 
@@ -11,16 +17,27 @@ function Home() {
   return (
     <div>
       <h1>My Apps</h1>
-      <div className="create-new-app">
-        <Icon icon="add" /> Create New
+      <div className="apps">
+        <div className="create-new-app">
+          <Icon icon="add" /> Create New
+        </div>
+        {apps.map((app) => {
+          return (
+            <div key={app.id} className="app-preview">
+              <header>
+                <div className="icon"></div>
+                <h2>{app.name}</h2>
+              </header>
+              <p>{app.description}</p>
+              <hr />
+              <footer>
+                <span>App Version {app.version}</span>
+                <Link to={`/applications/${app.id}/history`}>Show History</Link>
+              </footer>
+            </div>
+          );
+        })}
       </div>
-      {apps.map((app, i) => {
-        return (
-          <div key={i} className="app-preview">
-            {app.name}
-          </div>
-        );
-      })}
     </div>
   );
 }
