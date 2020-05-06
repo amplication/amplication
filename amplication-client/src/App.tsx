04a31061bb5343b8129e89b37a8f62fb60341e3a
Iddan Aaronsohn <mail@aniddan.com>
2020-05-06 17:50:48 +0300

    Add rmwc and define top app bar

--- amplication-client/src/App.tsx
+++ amplication-client/src/App.tsx
@@ -1,8 +1,40 @@
 import React from "react";
+import {
+  TopAppBar,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+  TopAppBarActionItem,
+  TopAppBarFixedAdjust,
+} from "@rmwc/top-app-bar";
+import "@material/top-app-bar/dist/mdc.top-app-bar.css";
+import "@material/icon-button/dist/mdc.icon-button.css";
+import "@material/ripple/dist/mdc.ripple.css";
 import "./App.css";
 
+const data = {
+  organization: {
+    name: "First Organization",
+  },
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection alignStart>
+            <TopAppBarTitle>{data.organization.name}</TopAppBarTitle>
+          </TopAppBarSection>
+          <TopAppBarSection alignEnd>
+            <TopAppBarActionItem icon="search" />
+            <TopAppBarActionItem icon="notifications" />
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <TopAppBarFixedAdjust />
+    </div>
+  );
 }
 
 export default App;
