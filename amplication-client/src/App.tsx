e248bd8e199000545541aeb022020cbaff50700d
Iddan Aaronsohn <mail@aniddan.com>
2020-05-07 17:03:22 +0300

    Move all mock data to mock.json

--- amplication-client/src/App.tsx
+++ amplication-client/src/App.tsx
@@ -4,26 +4,16 @@ import { TopAppBarFixedAdjust } from "@rmwc/top-app-bar";
 import Header from "./Header";
 import Application from "./Application";
 import Home from "./Home";
+import { user, organization } from "./mock.json";
 import "@material/top-app-bar/dist/mdc.top-app-bar.css";
 import "@material/icon-button/dist/mdc.icon-button.css";
 import "@material/ripple/dist/mdc.ripple.css";
 import "./App.css";
 
-const data = {
-  organization: {
-    name: "First Organization",
-  },
-  user: {
-    image:
-      "https://247wallst.files.wordpress.com/2016/05/warren-buffett-square-e1462828190521.jpg?w=400",
-    name: "User Name",
-  },
-};
-
 function App() {
   return (
     <div>
-      <Header organization={data.organization} user={data.user} />
+      <Header organization={organization} user={user} />
       <TopAppBarFixedAdjust />
       <Switch>
         <Route exact path="/" component={Home} />
