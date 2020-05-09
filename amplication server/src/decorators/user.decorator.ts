0e010f7be824438796eca225e088972b9f37e1b8
Iddan Aaronsohn <mail@aniddan.com>
2020-05-09 18:56:41 +0300

    Fix user decorator

--- amplication server/src/decorators/user.decorator.ts
+++ amplication server/src/decorators/user.decorator.ts
@@ -1,10 +1,12 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { User } from '../models';
 
-export function getUser(ctx): User {
-  return ctx.req.user;
+export function getUser(executionContext: ExecutionContext): User {
+  const gqlExecutionContext = GqlExecutionContext.create(executionContext);
+  return gqlExecutionContext.getContext().req.user;
 }
 
-export const UserEntity = createParamDecorator(
-  (data, [root, args, ctx, info]) => getUser(ctx)
+export const UserEntity = createParamDecorator((data, ctx: ExecutionContext) =>
+  getUser(ctx)
 );
