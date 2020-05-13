39b62aa25d232c649d071939209356647204eb18
Yuval Hazaz <yuval@amplication.com>
2020-05-13 09:36:19 +0300

    add file appblock

---
+++ packages/amplication-server/src/models/appblock.ts
@@ -0,0 +1,14 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType({
+  isAbstract: true,
+  description: undefined
+})
+export class AppBlock {
+  @Field(_type => String, {
+    nullable: false,
+    description: undefined
+  })
+  test!: string;
+
+}
