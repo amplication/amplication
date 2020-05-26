c2a00cb762f5815224a40d02a55abb24e047063c
Iddan Aaronsohn <mail@aniddan.com>
2020-05-26 21:43:33 +0300

    Reformat jsonSchemaValidation

--- packages/amplication-server/src/services/jsonSchemaValidation.service.ts
+++ packages/amplication-server/src/services/jsonSchemaValidation.service.ts
@@ -1,15 +1,13 @@
 import { SchemaValidationResult } from '../dto/schemaValidationResult';
 import Ajv from 'ajv';
 
-export class JsonSchemaValidationService  {
-
- public async validateSchema(
+export class JsonSchemaValidationService {
+  public async validateSchema(
     schema: object,
     data: any
   ): Promise<SchemaValidationResult> {
-    
     try {
-      let ajv: Ajv.Ajv = new Ajv({ allErrors: true }); 
+      let ajv: Ajv.Ajv = new Ajv({ allErrors: true });
 
       let isValid = ajv.validate(schema, data);
 
@@ -18,11 +16,8 @@ export class JsonSchemaValidationService  {
         return new SchemaValidationResult(false, ajv.errorsText());
       }
       return new SchemaValidationResult(true);
-
     } catch (error) {
-        return new SchemaValidationResult(false, error);
+      return new SchemaValidationResult(false, error);
     }
-
-}
-
+  }
 }
