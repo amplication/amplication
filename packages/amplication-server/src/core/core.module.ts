83509fe98ede8be65e1103a9ec2be67e58102dbb
Iddan Aaronsohn <mail@aniddan.com>
2020-05-27 19:46:43 +0300

    Improve imports of core.module

--- packages/amplication-server/src/core/core.module.ts
+++ packages/amplication-server/src/core/core.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 
-import { AccountModule } from '../core/account/account.module';
-import { OrganizationModule } from '../core/organization/organization.module';
+import { ExceptionFiltersModule } from 'src/filters/exceptionFilters.module';
+
+import { AccountModule } from './account/account.module';
+import { OrganizationModule } from './organization/organization.module';
 import { AppModule } from './app/app.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { EntityModule } from './entity/entity.module';
 import { EntityFieldModule } from './entityField/entityField.module';
 import { PermissionsModule } from './permissions/permissions.module';
-import { ExceptionFiltersModule } from '../filters/exceptionFilters.module';
 
 @Module({
   imports: [
