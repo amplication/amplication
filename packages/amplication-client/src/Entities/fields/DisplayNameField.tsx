79008082b5dd95764e1d4286af5e7074035d5d26
Iddan Aaronsohn <mail@aniddan.com>
2020-06-06 00:43:15 +0300

    Auto generate display name

---
+++ packages/amplication-client/src/Entities/fields/DisplayNameField.tsx
@@ -0,0 +1,24 @@
+import React, { useRef, useEffect } from "react";
+import { useFormikContext } from "formik";
+import { capitalCase } from "capital-case";
+import { TextField, Props } from "./TextField";
+
+export const DisplayName = (props: Props) => {
+  const formik = useFormikContext<{ name: string }>();
+  const previousNameValue = useRef<string>();
+
+  useEffect(() => {
+    const nextNameValue = formik.values.name;
+    if (previousNameValue.current !== nextNameValue) {
+      const displayName = generateDisplayName(nextNameValue);
+      formik.setFieldValue(props.name, displayName);
+    }
+    previousNameValue.current = nextNameValue;
+  }, [formik, props.name]);
+
+  return <TextField {...props} />;
+};
+
+function generateDisplayName(name: string): string {
+  return capitalCase(name);
+}
