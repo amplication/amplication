95e26f534b643a93cd63ec88056482d2cc2609f8
Yuval Hazaz <yuval@amplication.com>
2020-06-11 12:18:41 +0300

    Extend FindMany From Block

--- packages/amplication-server/src/core/connectorRestApi/dto/ConnectorRestApiWhereInput.ts
+++ packages/amplication-server/src/core/connectorRestApi/dto/ConnectorRestApiWhereInput.ts
@@ -1,44 +1,8 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { WhereUniqueInput, DateTimeFilter, StringFilter } from 'src/dto';
+import { InputType } from '@nestjs/graphql';
+import { BlockWhereInput } from '../../block/dto/BlockWhereInput';
 
 @InputType({
   isAbstract: true,
   description: undefined
 })
-export class ConnectorRestApiWhereInput {
-  @Field(() => StringFilter, {
-    nullable: true,
-    description: undefined
-  })
-  id?: StringFilter | null;
-
-  @Field(() => DateTimeFilter, {
-    nullable: true,
-    description: undefined
-  })
-  createdAt?: DateTimeFilter | null;
-
-  @Field(() => DateTimeFilter, {
-    nullable: true,
-    description: undefined
-  })
-  updatedAt?: DateTimeFilter | null;
-
-  @Field(() => WhereUniqueInput, {
-    nullable: true,
-    description: undefined
-  })
-  app?: WhereUniqueInput | null;
-
-  @Field(() => StringFilter, {
-    nullable: true,
-    description: undefined
-  })
-  name?: StringFilter | null;
-
-  @Field(() => StringFilter, {
-    nullable: true,
-    description: undefined
-  })
-  description?: StringFilter | null;
-}
+export class ConnectorRestApiWhereInput extends BlockWhereInput {}
--- packages/amplication-server/src/core/connectorRestApiCall/dto/ConnectorRestApiCallOrderByInput.ts
+++ packages/amplication-server/src/core/connectorRestApiCall/dto/ConnectorRestApiCallOrderByInput.ts
@@ -1,38 +1,8 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { OrderByArg } from 'src/enums/OrderByArg';
+import { InputType } from '@nestjs/graphql';
+import { BlockOrderByInput } from '../../block/dto/BlockOrderByInput';
 
 @InputType({
   isAbstract: true,
   description: undefined
 })
-export class ConnectorRestApiCallOrderByInput {
-  @Field(() => OrderByArg, {
-    nullable: true,
-    description: undefined
-  })
-  id?: keyof typeof OrderByArg | null;
-
-  @Field(() => OrderByArg, {
-    nullable: true,
-    description: undefined
-  })
-  createdAt?: keyof typeof OrderByArg | null;
-
-  @Field(() => OrderByArg, {
-    nullable: true,
-    description: undefined
-  })
-  updatedAt?: keyof typeof OrderByArg | null;
-
-  @Field(() => OrderByArg, {
-    nullable: true,
-    description: undefined
-  })
-  name?: keyof typeof OrderByArg | null;
-
-  @Field(() => OrderByArg, {
-    nullable: true,
-    description: undefined
-  })
-  description?: keyof typeof OrderByArg | null;
-}
+export class ConnectorRestApiCallOrderByInput extends BlockOrderByInput {}
