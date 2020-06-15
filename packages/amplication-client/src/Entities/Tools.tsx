af99fe5463c948c12024c024e568c2c08b2d8513
Iddan Aaronsohn <mail@aniddan.com>
2020-06-15 22:19:47 +0300

    Style tools

--- packages/amplication-client/src/Entities/Tools.tsx
+++ packages/amplication-client/src/Entities/Tools.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@rmwc/button";
-import "@rmwc/button/styles";
+import { Ripple } from "@rmwc/ripple";
+import "@rmwc/ripple/styles";
 import { Icon } from "@rmwc/icon";
 import "@rmwc/icon/styles";
+import "./Tools.css";
 
 const Tool = ({
   title,
@@ -15,18 +16,18 @@ const Tool = ({
   link: string;
 }) => {
   return (
-    <Link to={link}>
-      <Button>
-        <Icon icon={icon} />
+    <Ripple>
+      <Link className="tool" to={link}>
+        <Icon className="icon" icon={icon} />
         {title}
-      </Button>
-    </Link>
+      </Link>
+    </Ripple>
   );
 };
 
 const Tools = () => {
   return (
-    <div>
+    <div className="tools">
       <Tool icon="search" title="Entity" link="entities/new" />
       <Tool icon="search" title="Option Set" link="entities/new" />
       <Tool icon="search" title="Structure" link="entities/new" />
