8ef44b6d89e43ae6d3c610ba7b1beecd5844ce8e
Yuval Hazaz <yuval@amplication.com>
2020-06-21 12:23:32 +0300

    add icons to application menu

--- packages/amplication-client/src/Application/ApplicationLayout.tsx
+++ packages/amplication-client/src/Application/ApplicationLayout.tsx
@@ -5,10 +5,18 @@ import { Drawer, DrawerContent } from "@rmwc/drawer";
 import "@rmwc/drawer/styles";
 import { List, ListItem } from "@rmwc/list";
 import "@rmwc/list/styles";
+import { Icon } from "@rmwc/icon";
 
 import ApplicationHome from "./ApplicationHome";
 import Entities from "../Entities/Entities";
 import "./ApplicationLayout.scss";
+import iconEntity from "../assets/icons/entity.svg";
+import iconPages from "../assets/icons/pages.svg";
+import iconFlow from "../assets/icons/flow.svg";
+import iconConnector from "../assets/icons/connector.svg";
+import iconApi from "../assets/icons/api.svg";
+import iconSettings from "../assets/icons/settings.svg";
+import iconApp from "../assets/icons/app-placeholder.svg"; /**@todo: replace app placeholder with a component that shows an automated icon or a custom icon logo */
 
 type Props = {
   match: match<{
@@ -16,34 +24,51 @@ type Props = {
   }>;
 };
 
+/**@todo: create a menu component to show the list of items based on input parameter including the selected item  */
+
 function ApplicationLayout({ match }: Props) {
   const { application } = match.params;
   return (
     <div className="application-layout">
-      <Drawer>
+      <Drawer className="application-menu">
+        {" "}
         <DrawerContent>
           <List>
-            <Link to={`/${application}`}>
-              <ListItem>App</ListItem>
-            </Link>
-            <Link to={`/${application}/entities/`}>
-              <ListItem>Entities</ListItem>
-            </Link>
-            <Link to={`/${application}/pages`}>
-              <ListItem>Pages</ListItem>
-            </Link>
-            <Link to={`/${application}/workflow`}>
-              <ListItem>Workflow</ListItem>
-            </Link>
-            <Link to={`/${application}/connectors`}>
-              <ListItem>Connectors</ListItem>
-            </Link>
-            <Link to={`/${application}/api`}>
-              <ListItem>API</ListItem>
-            </Link>
-            <Link to={`/${application}/settings`}>
-              <ListItem>Settings</ListItem>
-            </Link>
+            <ListItem>
+              <Link title="Application Home" to={`/${application}`}>
+                <Icon icon={iconApp} className="logo" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link title="Entities" to={`/${application}/entities/`}>
+                <Icon icon={iconEntity} className="logo" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link title="Pages" to={`/${application}/pages`}>
+                <Icon icon={iconPages} className="logo" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link title="Workflow" to={`/${application}/workflow`}>
+                <Icon icon={iconFlow} className="logo" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link title="Connectors" to={`/${application}/connectors`}>
+                <Icon icon={iconConnector} className="logo" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link title="API" to={`/${application}/api`}>
+                <Icon icon={iconApi} className="logo" />
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link title="Settings" to={`/${application}/settings`}>
+                <Icon icon={iconSettings} className="logo" />
+              </Link>
+            </ListItem>
           </List>
         </DrawerContent>
       </Drawer>
