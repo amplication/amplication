ce781d918bf9b7dab5243b1d000f075a55749fca
Iddan Aaronsohn <mail@aniddan.com>
2020-07-12 16:10:42 +0300

    Use babel in nest app module codegen

--- example-app-2/nest-app-module-codegen.ts
+++ example-app-2/nest-app-module-codegen.ts
@@ -1,8 +1,13 @@
+import template from "@babel/template";
+import generate from "@babel/generator";
+import * as t from "@babel/types";
 import { ImportableModule, createModuleFromTemplate } from "./module.util";
 import { removeExt } from "./path.util";
 
 const appModuleTemplatePath = require.resolve("./templates/app.module.ts");
 const APP_MODULE_PATH = "app.module.ts";
+
+const buildImport = template("import { %%name%% } from %%module%%");
 
 export async function createAppModule(
   resourceModules: ImportableModule[]
@@ -18,12 +23,19 @@ export async function createAppModule(
   const imports = nestModules
     .map((module) => {
       const importPath = removeExt(module.path);
-      return `import { ${module.exports[0]} } from "./${importPath}"`;
+      const ast = buildImport({
+        name: t.identifier(module.exports[0]),
+        module: "./" + importPath,
+      });
+      // @ts-ignore
+      return generate(ast).code;
     })
     .join("\n");
-  const modules = `[${nestModules
-    .map((module) => module.exports[0])
-    .join(", ")}]`;
+  const modulesAst = t.arrayExpression(
+    nestModules.map((module) => t.identifier(module.exports[0]))
+  );
+  const modules = generate(modulesAst).code;
+
   return createModuleFromTemplate(
     APP_MODULE_PATH,
     appModuleTemplatePath,
