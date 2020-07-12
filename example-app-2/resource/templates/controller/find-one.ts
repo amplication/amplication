f0f6c8fa33ba2715f7ccee9565f6c61025575d8c
Iddan Aaronsohn <mail@aniddan.com>
2020-07-12 16:15:32 +0300

    Add missing async to findOne

--- example-app-2/resource/templates/controller/find-one.ts
+++ example-app-2/resource/templates/controller/find-one.ts
@@ -1,6 +1,6 @@
 /** $$COMMENT$$ */
 @Get("$$PATH$$")
-findOne(@Query() query, @Param() params): Promise<$$ENTITY$$> {
+async findOne(@Query() query, @Param() params): Promise<$$ENTITY$$> {
     const entity =  await this.service.findOne({ ...query, where: params });
     if (entity === null) {
         throw new NotFoundException(`No entity was found for ${JSON.stringify(query)}`)
