3d18f2d50681c40b39df0eb35558bfb1ea71d507
Iddan Aaronsohn <mail@aniddan.com>
2020-07-13 11:06:49 +0300

    Fix import to prisma

--- example-app-2/dist/customer/customer.service.ts
+++ example-app-2/dist/customer/customer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "./prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { Customer } from "../dto/Customer";
 
 import {
--- example-app-2/resource/templates/service/service.ts
+++ example-app-2/resource/templates/service/service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "./prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { $$ENTITY$$ } from "$$ENTITY_DTO_MODULE$$";
 
 import {
