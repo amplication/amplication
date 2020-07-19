76fc50b91fb339186a29c02242f4772cee99dfbc
Iddan Aaronsohn <mail@aniddan.com>
2020-07-19 17:29:16 +0300

    Fix Prisma module

--- packages/amplication-code-generator/static/prisma/prisma.module.ts
+++ packages/amplication-code-generator/static/prisma/prisma.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 
 @Module({
-  imports: [],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
