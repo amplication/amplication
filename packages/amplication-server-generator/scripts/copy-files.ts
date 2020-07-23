dc1683fa29681e2042dfc553002f68b328f0610a
Iddan Aaronsohn <mail@aniddan.com>
2020-07-23 17:25:05 +0300

    Use absolute paths in copy files

--- packages/amplication-server-generator/scripts/copy-files.ts
+++ packages/amplication-server-generator/scripts/copy-files.ts
@@ -2,16 +2,23 @@
  * Copy static files to destination folder to be available
  */
 
+import * as path from "path";
 import * as fs from "fs";
 import fg from "fast-glob";
 
+const SRC_DIRECTORY = path.join(__dirname, "..", "src");
+const DIST_DIRECTORY = path.join(__dirname, "..", "dist");
+
 async function copyFiles() {
-  const templatesPaths = await fg("src/**/templates/*");
-  const staticPaths = await fg("src/static/**");
+  const templatesPaths = await fg(`${SRC_DIRECTORY}/**/templates/*`);
+  const staticPaths = await fg(`${SRC_DIRECTORY}/static/**`);
   const paths = [...templatesPaths, ...staticPaths];
   await Promise.all(
     paths.map((path) => {
-      const dest = path.replace(/^src/, "dist");
+      const dest = path.replace(
+        new RegExp("^" + SRC_DIRECTORY),
+        DIST_DIRECTORY
+      );
       return fs.promises.copyFile(path, dest);
     })
   );
