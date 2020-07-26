7a34d69c5b86c13540a66f02d82416199b88e759
Iddan Aaronsohn <mail@aniddan.com>
2020-07-26 18:06:48 +0300

    Stream server logs in e2e

--- packages/amplication-generator/scripts/run-e2e.ts
+++ packages/amplication-generator/scripts/run-e2e.ts
@@ -1,13 +1,16 @@
 import * as path from "path";
-
 import { Docker } from "docker-cli-js";
+import getPort from "get-port";
+import fetch, { Response } from "node-fetch";
+import sleep from "sleep-promise";
+import { spawn } from "child_process";
 import { generateExample } from "../example/src/generate";
-import { run } from "./docker.util";
 
 // Use when running the E2E multiple times to shorten build time
 const { NO_DELETE_IMAGE } = process.env;
 
 const EXAMPLE_DIST = path.join(__dirname, "..", "example", "dist");
+const SERVER_START_TIMEOUT = 30000;
 
 runE2E()
   .then(() => {
@@ -32,18 +35,57 @@ async function runE2E() {
   const { imageId } = await docker.command("build .");
 
   // Run with Docker
-  const container = await run(imageId, {
-    cwd: EXAMPLE_DIST,
+  console.info("Running Docker container...");
+  const port = await getPort();
+  const { containerId } = await docker.command(
+    `run -p ${port}:3000 -d ${imageId}`
+  );
+
+  streamLogs(containerId);
+
+  // Wait for the container to be ready
+  console.log("Waiting for server to be ready...");
+  await sleep(SERVER_START_TIMEOUT);
+
+  let res: Response;
+  console.log("POST /customers");
+  res = await fetch(`http://0.0.0.0:${port}/customers`, {
+    method: "POST",
+    body: JSON.stringify({
+      email: "alice@example.com",
+      firstName: "Alice",
+      lastName: "Appleseed",
+    }),
   });
+  console.log(await res.json());
+
+  console.log("GET /customers");
+  res = await fetch(`http://0.0.0.0:${port}/customers`);
+  const customers = await res.json();
+  console.log(customers);
+  const id = customers[0].id;
+
+  console.log(`GET /customer/${id}`);
+  res = await fetch(`http://0.0.0.0:${port}/customer/${id}`);
+  console.log(await res.json());
 
   console.info("Killing Docker container...");
-  await docker.command(`kill ${container}`);
+  await docker.command(`kill ${containerId}`);
 
   console.info("Removing Docker container...");
-  await docker.command(`rm ${container}`);
+  await docker.command(`rm ${containerId}`);
 
   if (!NO_DELETE_IMAGE) {
     // Remove the built Docker image
     await docker.command(`image rm ${imageId}`);
   }
+}
+
+function streamLogs(containerID: string) {
+  const logs = spawn("docker", ["logs", "--follow", containerID], {
+    stdio: "inherit",
+  });
+  logs.on("close", (code) => {
+    throw new Error(`Closed with ${code}`);
+  });
 }
