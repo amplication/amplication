1d3af110e4d3d9e5909ec0da3fd62d823dd17248
Iddan Aaronsohn <mail@aniddan.com>
2020-08-02 16:58:43 +0300

    Use namespace only

--- packages/amplication-prisma-generator/src/prisma-generator.ts
+++ packages/amplication-prisma-generator/src/prisma-generator.ts
@@ -1,6 +1,5 @@
 import * as PrismaSchemaDSL from "prisma-schema-dsl";
 import { Entity, EnumDataType, Field } from "./types";
-import { createScalarField, ScalarType } from "prisma-schema-dsl";
 
 export const HEADER = `generator client {
   provider = "prisma-client-js"
@@ -9,12 +8,17 @@ export const HEADER = `generator client {
 export const USER_MODEL = PrismaSchemaDSL.createModel("User", [
   PrismaSchemaDSL.createScalarField(
     "username",
-    ScalarType.String,
+    PrismaSchemaDSL.ScalarType.String,
     false,
     true,
     true
   ),
-  PrismaSchemaDSL.createScalarField("password", ScalarType.String, false, true),
+  PrismaSchemaDSL.createScalarField(
+    "password",
+    PrismaSchemaDSL.ScalarType.String,
+    false,
+    true
+  ),
 ]);
 
 export async function createPrismaSchema(
@@ -44,43 +48,108 @@ export function createPrismaField(
   const { dataType, name } = field;
   switch (dataType) {
     case EnumDataType.singleLineText: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.multiLineText: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.email: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.state: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.autoNumber: {
-      return createScalarField(name, ScalarType.Int, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.Int,
+        false,
+        true
+      );
     }
     case EnumDataType.wholeNumber: {
-      return createScalarField(name, ScalarType.Int, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.Int,
+        false,
+        true
+      );
     }
     case EnumDataType.dateTime: {
-      return createScalarField(name, ScalarType.DateTime, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.DateTime,
+        false,
+        true
+      );
     }
     case EnumDataType.decimalNumber: {
-      return createScalarField(name, ScalarType.Float, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.Float,
+        false,
+        true
+      );
     }
     case EnumDataType.file: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.image: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.boolean: {
-      return createScalarField(name, ScalarType.Boolean, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.Boolean,
+        false,
+        true
+      );
     }
     case EnumDataType.uniqueId: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     case EnumDataType.geographicAddress: {
-      return createScalarField(name, ScalarType.String, false, true);
+      return PrismaSchemaDSL.createScalarField(
+        name,
+        PrismaSchemaDSL.ScalarType.String,
+        false,
+        true
+      );
     }
     default: {
       throw new Error(`Unfamiliar data type: ${dataType}`);
