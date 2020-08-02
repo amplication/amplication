4e321aaab0282d7ab8b5569d8f8abc8b3179c52c
Gal Cegla <galcegla@gmail.com>
2020-08-02 18:35:28 +0300

    Format fixes

--- packages/amplication-server/src/core/user/user.service.spec.ts
+++ packages/amplication-server/src/core/user/user.service.spec.ts
@@ -4,13 +4,13 @@ import { PrismaService } from 'src/services/prisma.service';
 import { User, UserRole, Account } from 'src/models';
 import { Organization } from 'prisma/dal';
 
-const EXAMPLE_USER_ID = "exampleUserId"
+const EXAMPLE_USER_ID = 'exampleUserId';
 
 const EXAMPLE_USER: User = {
-	id : EXAMPLE_USER_ID,
-	createdAt : new Date(),
-	updatedAt : new Date(),
-  };
+  id: EXAMPLE_USER_ID,
+  createdAt: new Date(),
+  updatedAt: new Date()
+};
 
 const prismaUserFindOneMock = jest.fn(() => {
   return EXAMPLE_USER;
@@ -20,21 +20,23 @@ const prismaUserFindManyMock = jest.fn(() => {
   return [EXAMPLE_USER];
 });
 
-
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, {
-		provide: PrismaService,
-		useClass: jest.fn().mockImplementation(() => ({
-		  user: {
-			findOne: prismaUserFindOneMock,
-			findMany: prismaUserFindManyMock,
-		  },
-		}))
-	  }]
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useClass: jest.fn().mockImplementation(() => ({
+            user: {
+              findOne: prismaUserFindOneMock,
+              findMany: prismaUserFindManyMock
+            }
+          }))
+        }
+      ]
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -44,11 +46,13 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should find one', async() => {
-	  expect(await service.findUser({where: {id: EXAMPLE_USER_ID}})).toEqual(EXAMPLE_USER)
+  it('should find one', async () => {
+    expect(await service.findUser({ where: { id: EXAMPLE_USER_ID } })).toEqual(
+      EXAMPLE_USER
+    );
   });
 
-  it('should find many', async() => {
-	  expect(await service.findUsers({where: {}})).toEqual([EXAMPLE_USER])
+  it('should find many', async () => {
+    expect(await service.findUsers({ where: {} })).toEqual([EXAMPLE_USER]);
   });
 });
