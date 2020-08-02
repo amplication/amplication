392fb5c89c6a6dc80ec3a03687511b82ae40d7fd
Gal Cegla <galcegla@gmail.com>
2020-08-02 18:25:09 +0300

    Add unit tests for user.service.ts

--- packages/amplication-server/src/core/user/user.service.spec.ts
+++ packages/amplication-server/src/core/user/user.service.spec.ts
@@ -1,13 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/services/prisma.service';
+import { User, UserRole, Account } from 'src/models';
+import { Organization } from 'prisma/dal';
 
+//	example user model:
+const EXAMPLE_USER_ID = "exampleUserId"
+
+const EXAMPLE_USER: User = {
+	id : EXAMPLE_USER_ID,
+	createdAt : new Date(),
+	updatedAt : new Date(),
+  };
+
+//	mock fn.expr:
+const prismaUserFindOneMock = jest.fn(() => {
+  return EXAMPLE_USER;
+});
+
+const prismaUserFindManyMock = jest.fn(() => {
+  return [EXAMPLE_USER];
+});
+
+
+
+//	test:
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, PrismaService]
+      providers: [UserService, {
+		provide: PrismaService,
+		useClass: jest.fn().mockImplementation(() => ({
+		  user: {
+			findOne: prismaUserFindOneMock,
+			findMany: prismaUserFindManyMock,
+		  },
+		}))
+	  }]
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -16,4 +47,15 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should find one', async() => {
+	  expect(await service.findUser({where: {id: EXAMPLE_USER_ID}})).toEqual(EXAMPLE_USER)
+  });
+
+  it('should find many', async() => {
+	  expect(await service.findUsers({where: {}})).toEqual([EXAMPLE_USER])
+  });
+
+
+
 });
