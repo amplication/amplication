3680a00aeb0cb13acb544d5cacceb448e85fdc67
Iddan Aaronsohn <mail@aniddan.com>
2020-08-06 16:15:10 +0300

    Add missing import

--- packages/amplication-server/src/core/codeGeneration/codeGeneration.service.ts
+++ packages/amplication-server/src/core/codeGeneration/codeGeneration.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Queue } from 'bull';
 import { InjectQueue } from '@nestjs/bull';
 import { QUEUE_NAME } from './constants';
-import { CodeGenerationRequest } from './dto/code-generation-request';
+import { CodeGenerationRequest } from './dto/codeGenerationRequest';
 
 @Injectable()
 export class CodeGenerationService {
