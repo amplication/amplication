53c566b83a0976b7da46042dc900aef80d8e8ab5
Iddan Aaronsohn <mail@aniddan.com>
2020-08-12 12:11:42 +0300

    Save generated app to storage

--- packages/amplication-server/src/core/generatedApp/constants.ts
+++ packages/amplication-server/src/core/generatedApp/constants.ts
@@ -1,1 +1,2 @@
 export const QUEUE_NAME = 'appGeneration';
+export const STORAGE_DIRECTORY = 'generated-applications';
--- packages/amplication-server/src/core/generatedApp/generatedApp.consumer.ts
+++ packages/amplication-server/src/core/generatedApp/generatedApp.consumer.ts
@@ -2,22 +2,21 @@ import { Processor, Process } from '@nestjs/bull';
 import { Job } from 'bull';
 import { StorageService } from '@codebrew/nestjs-storage';
 import { createDataService, Entity } from 'amplication-data-service-generator';
-import { QUEUE_NAME } from './constants';
+import { QUEUE_NAME, STORAGE_DIRECTORY } from './constants';
 import { AppGenerationRequest } from './dto/AppGenerationRequest';
-
-const STORAGE_DIRECTORY = 'generated-applications';
 
 @Processor(QUEUE_NAME)
 export class GeneratedAppConsumer {
   constructor(private readonly storageService: StorageService) {}
   @Process()
   async generate(job: Job<AppGenerationRequest>) {
+    const disk = this.storageService.getDisk();
     const modules = await createDataService(job.data.entities as Entity[]);
     const directory = `${STORAGE_DIRECTORY}/${job.data.id}`;
-    await modules.map(module =>
-      this.storageService
-        .getDisk()
-        .put(`${directory}/${module.path}`, module.code)
+    await Promise.all(
+      modules.map(module =>
+        disk.put(`${directory}/${module.path}`, module.code)
+      )
     );
   }
 }
