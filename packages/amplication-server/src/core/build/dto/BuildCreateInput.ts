2e33644090100c68e908a04826bec67720644107
Iddan Aaronsohn <mail@aniddan.com>
2020-08-13 15:44:01 +0300

    Add app to create input

--- packages/amplication-server/src/core/build/dto/BuildCreateInput.ts
+++ packages/amplication-server/src/core/build/dto/BuildCreateInput.ts
@@ -1,33 +1,21 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { WhereParentIdInput } from 'src/dto';
 import { ConnectManyUniqueInput } from 'src/dto/ConnectManyUniqueInput';
+import { WhereParentIdInput } from 'src/dto';
 
 @InputType({
   isAbstract: true
 })
 export class BuildCreateInput {
-  @Field(() => String, {
-    nullable: true
-  })
-  id?: string | null | undefined;
-
-  @Field(() => Date, {
-    nullable: true
-  })
-  createdAt?: Date | null | undefined;
-
-  @Field(() => WhereParentIdInput, {
-    nullable: false
-  })
-  createdBy!: WhereParentIdInput;
+  @Field(() => WhereParentIdInput)
+  app!: WhereParentIdInput;
 
   @Field(() => ConnectManyUniqueInput, {
     nullable: true
   })
-  blockVersions?: ConnectManyUniqueInput;
+  blockVersions!: ConnectManyUniqueInput;
 
   @Field(() => ConnectManyUniqueInput, {
     nullable: false
   })
-  entityVersions: ConnectManyUniqueInput;
+  entityVersions!: ConnectManyUniqueInput;
 }
