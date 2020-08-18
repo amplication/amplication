cae7c74f74f3959dd3918288396b972a73896d17
Yuval Hazaz <yuval@amplication.com>
2020-08-18 14:36:10 +0300

    Fix copy files to work with Windows (@iddan)

--- packages/amplication-data-service-generator/scripts/copy-files.ts
+++ packages/amplication-data-service-generator/scripts/copy-files.ts
@@ -5,20 +5,29 @@
 import * as path from "path";
 import * as fs from "fs";
 import fg from "fast-glob";
+import normalize from "normalize-path";
 
 const SRC_DIRECTORY = path.join(__dirname, "..", "src");
 const DIST_DIRECTORY = path.join(__dirname, "..", "dist");
-const SRC_DIRECTORY_REGEXP = new RegExp("^" + SRC_DIRECTORY);
 
 async function copyFiles() {
-  const templatesPaths = await fg(`${SRC_DIRECTORY}/**/*.template.ts`);
-  const staticPaths = await fg(`${SRC_DIRECTORY}/static/**`);
+  const templatesPaths = await fg(
+    `${normalize(SRC_DIRECTORY)}/**/*.template.ts`
+  );
+  if (templatesPaths.length === 0) {
+    throw new Error("At least one template file must match");
+  }
+  const staticPaths = await fg(`${normalize(SRC_DIRECTORY)}/static/**`);
+  if (staticPaths.length === 0) {
+    throw new Error("At least one static file must match");
+  }
   const paths = [...templatesPaths, ...staticPaths];
   await Promise.all(
     paths.map(async (filePath) => {
-      const dest = filePath.replace(SRC_DIRECTORY_REGEXP, DIST_DIRECTORY);
+      const normalizedFilePath = path.normalize(filePath);
+      const dest = normalizedFilePath.replace(SRC_DIRECTORY, DIST_DIRECTORY);
       await fs.promises.mkdir(path.dirname(dest), { recursive: true });
-      await fs.promises.copyFile(filePath, dest);
+      await fs.promises.copyFile(normalizedFilePath, dest);
     })
   );
 }
