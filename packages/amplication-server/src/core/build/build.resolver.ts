ceb011f35e992133ea5e4cd267cdb6788fa7413d
Iddan Aaronsohn <mail@aniddan.com>
2020-08-20 21:11:22 +0300

    Add field resolvers to build resolver

--- packages/amplication-server/src/core/build/build.resolver.ts
+++ packages/amplication-server/src/core/build/build.resolver.ts
@@ -1,5 +1,12 @@
 import { UseGuards, UseFilters } from '@nestjs/common';
-import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Resolver,
+  Query,
+  Parent,
+  ResolveField
+} from '@nestjs/graphql';
 import { GqlResolverExceptionsFilter } from 'src/filters/GqlResolverExceptions.filter';
 import { GqlAuthGuard } from 'src/guards/gql-auth.guard';
 import { Build } from './dto/Build';
@@ -11,17 +18,34 @@ import { AuthorizeContext } from 'src/decorators/authorizeContext.decorator';
 import { AuthorizableResourceParameter } from 'src/enums/AuthorizableResourceParameter';
 import { InjectContextValue } from 'src/decorators/injectContextValue.decorator';
 import { InjectableResourceParameter } from 'src/enums/InjectableResourceParameter';
+import { App, User } from 'src/models';
+import { AppService } from '..';
+import { UserService } from '../user/user.service';
 
 @Resolver(() => Build)
 @UseFilters(GqlResolverExceptionsFilter)
 @UseGuards(GqlAuthGuard)
 export class BuildResolver {
-  constructor(private readonly service: BuildService) {}
+  constructor(
+    private readonly service: BuildService,
+    private readonly appService: AppService,
+    private readonly userService: UserService
+  ) {}
 
   @Query(() => [Build])
   @AuthorizeContext(AuthorizableResourceParameter.AppId, 'where.app.id')
   async builds(@Args() args: FindManyBuildArgs): Promise<Build[]> {
     return this.service.findMany(args);
+  }
+
+  @ResolveField()
+  async app(@Parent() build: Build): Promise<App> {
+    return this.appService.app({ where: { id: build.appId } });
+  }
+
+  @ResolveField()
+  async createdBy(@Parent() build: Build): Promise<User> {
+    return this.userService.user({ where: { id: build.userId } });
   }
 
   @Mutation(() => String)
