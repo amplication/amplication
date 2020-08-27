55647a074286b95af32c4421639ed4ab4eee6e6c
Iddan Aaronsohn <mail@aniddan.com>
2020-08-27 12:35:43 +0300

    Remove unnecessary import to ContextLoggerModule from exceptionFilters

--- packages/amplication-server/src/filters/exceptionFilters.module.ts
+++ packages/amplication-server/src/filters/exceptionFilters.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GqlResolverExceptionsFilter } from './GqlResolverExceptions.filter';
-import { ContextLoggerModule } from '../services/contextLogger.module';
 
 @Module({
-  imports: [ContextLoggerModule],
   providers: [GqlResolverExceptionsFilter],
-  exports: [GqlResolverExceptionsFilter, ContextLoggerModule]
+  exports: [GqlResolverExceptionsFilter]
 })
 export class ExceptionFiltersModule {}
