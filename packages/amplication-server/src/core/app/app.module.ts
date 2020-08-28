6474ed124ec16bc57306344016da4152fd1c4137
Iddan Aaronsohn <mail@aniddan.com>
2020-08-28 23:19:08 +0300

    Fix the import EntityModule in AppModule

--- packages/amplication-server/src/core/app/app.module.ts
+++ packages/amplication-server/src/core/app/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { EntityService } from '../entity/entity.service';
-import { AppResolver } from './app.resolver';
-import { CommitResolver } from './commit.resolver';
 import { PrismaModule } from 'src/services/prisma.module';
 import { PermissionsModule } from '../permissions/permissions.module';
 import { UserModule } from '../user/user.module';
+import { EntityModule } from '../entity/entity.module';
+import { AppService } from './app.service';
+import { AppResolver } from './app.resolver';
+import { CommitResolver } from './commit.resolver';
 
 @Module({
-  imports: [PrismaModule, PermissionsModule, UserModule],
-  providers: [AppService, AppResolver, EntityService, CommitResolver],
+  imports: [PrismaModule, PermissionsModule, UserModule, EntityModule],
+  providers: [AppService, AppResolver, CommitResolver],
   exports: [AppService, AppResolver, CommitResolver]
 })
 export class AppModule {}
