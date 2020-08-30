086692340a536861feba5c8c72c9c570746ff3eb
Iddan Aaronsohn <mail@aniddan.com>
2020-08-30 15:06:13 +0300

    Update packages/amplication-server/src/core/block/blockType.service.spec.ts

--- packages/amplication-server/src/core/block/blockType.service.spec.ts
+++ packages/amplication-server/src/core/block/blockType.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlockTypeService } from './blockType.service';
-import { async } from 'rxjs/internal/scheduler/async';
 import { BlockService } from './block.service';
 import { EnumBlockType } from 'src/enums/EnumBlockType';
 
