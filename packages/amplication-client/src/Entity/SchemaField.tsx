e635e12cea54dce5d0c136e7dbf108ec50f39c0c
Yuval Hazaz <yuval@amplication.com>
2020-09-01 17:36:42 +0300

    remove unused reference

--- packages/amplication-client/src/Entity/SchemaField.tsx
+++ packages/amplication-client/src/Entity/SchemaField.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { capitalCase } from "capital-case";
 import { TextField } from "../Components/TextField";
 import { ToggleField } from "../Components/ToggleField";
