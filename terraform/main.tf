928011953ca0137034fe782ccecc125799eb773f
Iddan Aaronsohn <mail@aniddan.com>
2020-09-02 14:34:24 +0300

    Fix Cloud Build  - Add Google APIs to Terraform  - Give Google Cloud Build default service account access to secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,55 @@ provider "google-beta" {
   region  = "us-east1"
 }
 
+
+resource "google_project_service" "cloud_build_api" {
+  service = "cloudbuild.googleapis.com"
+}
+
+resource "google_project_service" "google_cloud_memorystore_for_redis_api" {
+  service = "redis.googleapis.com"
+}
+
+resource "google_project_service" "cloud_run_admin_api" {
+  service = "run.googleapis.com"
+}
+
+resource "google_project_service" "secret_manager_api" {
+  service = "secretmanager.googleapis.com"
+}
+
+resource "google_project_service" "service_management_api" {
+  service = "servicemanagement.googleapis.com"
+}
+
+resource "google_project_service" "service_usage_api" {
+  service = "serviceusage.googleapis.com"
+}
+
+resource "google_project_service" "cloud_sql" {
+  service = "sql-component.googleapis.com"
+}
+
+resource "google_project_service" "cloud_sql_admin_api" {
+  service = "sqladmin.googleapis.com"
+}
+
+resource "google_project_service" "google_cloud_storage_json_api" {
+  service = "storage-api.googleapis.com"
+}
+
+resource "google_project_service" "cloud_storage" {
+  service = "storage-component.googleapis.com"
+}
+
+resource "google_project_service" "cloud_storage_api" {
+  service = "storage.googleapis.com"
+}
+
+resource "google_project_service" "serverless_vpc_access_api" {
+  service = "vpcaccess.googleapis.com"
+}
+
 # GitHub
 
 locals {
@@ -93,10 +142,23 @@ data "google_secret_manager_secret_version" "github_client_secret" {
 data "google_compute_default_service_account" "default" {
 }
 
-resource "google_secret_manager_secret_iam_member" "member" {
+resource "google_secret_manager_secret_iam_member" "compute_default_service_account" {
   secret_id = google_secret_manager_secret.github_client_secret.secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+}
+
+data "google_project" "project" {
+}
+
+locals {
+  google_cloud_build_service_account = "${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_secret_manager_secret_iam_member" "build_default_service_account" {
+  secret_id = google_secret_manager_secret.github_client_secret.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${local.google_cloud_build_service_account}"
 }
 
 # Cloud Run
@@ -170,7 +232,7 @@ resource "google_cloud_run_service" "default" {
   autogenerate_revision_name = true
 
   depends_on = [
-    google_secret_manager_secret_iam_member.member
+    google_secret_manager_secret_iam_member.compute_default_service_account
   ]
 }
 
