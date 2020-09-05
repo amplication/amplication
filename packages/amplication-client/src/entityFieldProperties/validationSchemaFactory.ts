f2afba29fa684f54ad67eab7648b07be2ba2b551
Iddan Aaronsohn <mail@aniddan.com>
2020-09-05 15:37:42 +0300

    Use amplication-data in client

--- packages/amplication-client/src/entityFieldProperties/validationSchemaFactory.ts
+++ packages/amplication-client/src/entityFieldProperties/validationSchemaFactory.ts
@@ -1,19 +1,5 @@
+import { schemas } from "amplication-data";
 import * as models from "../models";
-import singleLineText from "./schemas/singleLineText.json";
-import multiLineText from "./schemas/multiLineText.json";
-import email from "./schemas/email.json";
-import autoNumber from "./schemas/autoNumber.json";
-import wholeNumber from "./schemas/wholeNumber.json";
-import dateTime from "./schemas/dateTime.json";
-import decimalNumber from "./schemas/decimalNumber.json";
-import lookup from "./schemas/lookup.json";
-import optionSet from "./schemas/optionSet.json";
-import multiSelectOptionSet from "./schemas/multiSelectOptionSet.json";
-import boolean from "./schemas/boolean.json";
-import id from "./schemas/id.json";
-import createdAt from "./schemas/createdAt.json";
-import updatedAt from "./schemas/updatedAt.json";
-import geographicAddress from "./schemas/geographicAddress.json";
 
 export type SchemaProperty = {
   type: string;
@@ -34,34 +20,36 @@ export type Schema = {
   };
 };
 
-const schemas: {
+type DataTypeToSchema = {
   [dataType in models.EnumDataType]: Schema;
-} = {
-  [models.EnumDataType.SingleLineText]: singleLineText,
-  [models.EnumDataType.MultiLineText]: multiLineText,
-  [models.EnumDataType.Email]: email,
-  [models.EnumDataType.AutoNumber]: autoNumber,
+};
+
+const dataTypeToSchema: DataTypeToSchema = {
+  [models.EnumDataType.SingleLineText]: schemas.singleLineText,
+  [models.EnumDataType.MultiLineText]: schemas.multiLineText,
+  [models.EnumDataType.Email]: schemas.email,
+  [models.EnumDataType.AutoNumber]: schemas.autoNumber,
   /** @todo reference to minimumValue */
-  [models.EnumDataType.WholeNumber]: wholeNumber,
-  [models.EnumDataType.DateTime]: dateTime,
+  [models.EnumDataType.WholeNumber]: schemas.wholeNumber,
+  [models.EnumDataType.DateTime]: schemas.dateTime,
   /**
    * @todo reference to minimumValue
    * @todo Check for the right value for precision
    */
-  [models.EnumDataType.DecimalNumber]: decimalNumber,
+  [models.EnumDataType.DecimalNumber]: schemas.decimalNumber,
   /** @todo validate the actual selected entity */
-  [models.EnumDataType.Lookup]: lookup,
+  [models.EnumDataType.Lookup]: schemas.lookup,
   /** @todo validate the actual selected option set */
-  [models.EnumDataType.OptionSet]: optionSet,
+  [models.EnumDataType.OptionSet]: schemas.optionSet,
   /** @todo validate the actual selected option set */
-  [models.EnumDataType.MultiSelectOptionSet]: multiSelectOptionSet,
-  [models.EnumDataType.Boolean]: boolean,
-  [models.EnumDataType.Id]: id,
-  [models.EnumDataType.CreatedAt]: createdAt,
-  [models.EnumDataType.UpdatedAt]: updatedAt,
-  [models.EnumDataType.GeographicAddress]: geographicAddress,
+  [models.EnumDataType.MultiSelectOptionSet]: schemas.multiSelectOptionSet,
+  [models.EnumDataType.Boolean]: schemas.boolean,
+  [models.EnumDataType.Id]: schemas.id,
+  [models.EnumDataType.CreatedAt]: schemas.createdAt,
+  [models.EnumDataType.UpdatedAt]: schemas.updatedAt,
+  [models.EnumDataType.GeographicAddress]: schemas.geographicAddress,
 };
 
 export function getSchema(dataType: models.EnumDataType): Schema {
-  return schemas[dataType];
+  return dataTypeToSchema[dataType];
 }
