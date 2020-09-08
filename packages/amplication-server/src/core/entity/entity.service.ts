30dbcc519f8abb57f688cafe660f7edba7830e18
Yuval Hazaz <yuval@amplication.com>
2020-09-08 16:05:56 +0300

    fix test

--- packages/amplication-server/src/core/entity/entity.service.ts
+++ packages/amplication-server/src/core/entity/entity.service.ts
@@ -287,7 +287,6 @@ export class EntityService {
 
       return {
         resourceId: entity.id,
-        /**@todo: calc change type */
         action: action,
         resourceType: EnumPendingChangeResourceType.Entity,
         versionNumber: lastVersion.versionNumber + 1,
