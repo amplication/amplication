53a1b1185305019fd842d154edc33b69a99fd428
Iddan Aaronsohn <mail@aniddan.com>
2020-09-13 01:42:17 +0300

    Fix types for scalar fields in edit DTOs

--- packages/amplication-data-service-generator/src/resource/dto/create-dto.ts
+++ packages/amplication-data-service-generator/src/resource/dto/create-dto.ts
@@ -1,7 +1,11 @@
 import { print } from "recast";
 import { namedTypes, builders } from "ast-types";
+import { TSTypeKind } from "ast-types/gen/kinds";
 import { FullEntity } from "../../types";
+import { EntityField, EnumDataType } from "../../models";
 import { Module } from "../../util/module";
+import { createPrismaField } from "../../prisma/create-prisma-schema";
+import { FieldKind, ScalarType } from "prisma-schema-dsl";
 
 export function createDTOModules(entity: FullEntity): Module[] {
   const dtos = [
@@ -15,25 +19,30 @@ export function createDTOModules(entity: FullEntity): Module[] {
     return {
       code: print(program).code,
       /** @todo lower case entity directory */
-      path: `${entity.name}/${dto.id?.name}`,
+      path: `${entity.name}/${dto.id?.name}.ts`,
     };
   });
 }
 
 const UNEDITABLE_FIELDS = new Set<string>(["id", "createdAt", "updatedAt"]);
 
+const PRISMA_SCALAR_TO_TYPE: {
+  [scalar in ScalarType]: TSTypeKind;
+} = {
+  [ScalarType.Boolean]: builders.tsBooleanKeyword(),
+  [ScalarType.DateTime]: builders.tsTypeReference(builders.identifier("Date")),
+  [ScalarType.Float]: builders.tsNumberKeyword(),
+  [ScalarType.Int]: builders.tsNumberKeyword(),
+  [ScalarType.String]: builders.tsStringKeyword(),
+  [ScalarType.Json]: builders.tsUnknownKeyword(),
+};
+
 export function createCreateInput(
   entity: FullEntity
 ): namedTypes.ClassDeclaration {
   const properties = entity.fields
-    .filter((field) => !UNEDITABLE_FIELDS.has(field.name))
-    .map((field) => {
-      const type = builders.tsNullKeyword();
-      return builders.tsPropertySignature(
-        builders.identifier(field.name),
-        builders.tsTypeAnnotation(type)
-      );
-    });
+    .filter(isEditableField)
+    .map(createEditPropertySignature);
   return builders.classDeclaration(
     builders.identifier(`${entity.name}CreateInput`),
     builders.classBody(properties)
@@ -44,17 +53,29 @@ export function createUpdateInput(
   entity: FullEntity
 ): namedTypes.ClassDeclaration {
   const properties = entity.fields
-    .filter((field) => !UNEDITABLE_FIELDS.has(field.name))
-    .map((field) => {
-      const type = builders.tsNullKeyword();
-      return builders.tsPropertySignature(
-        builders.identifier(field.name),
-        builders.tsTypeAnnotation(type)
-      );
-    });
+    .filter(isEditableField)
+    .map(createEditPropertySignature);
   return builders.classDeclaration(
     builders.identifier(`${entity.name}UpdateInput`),
     builders.classBody(properties)
+  );
+}
+
+function isEditableField(field: EntityField): boolean {
+  return !UNEDITABLE_FIELDS.has(field.name);
+}
+
+function createEditPropertySignature(
+  field: EntityField
+): namedTypes.TSPropertySignature {
+  const prismaField = createPrismaField(field);
+  const type =
+    prismaField.kind === FieldKind.Scalar
+      ? PRISMA_SCALAR_TO_TYPE[prismaField.type]
+      : builders.tsTypeReference(builders.identifier(prismaField.type));
+  return builders.tsPropertySignature(
+    builders.identifier(field.name),
+    builders.tsTypeAnnotation(type)
   );
 }
 
@@ -79,7 +100,10 @@ export function createWhereInput(
 export function createWhereUniqueInput(
   entity: FullEntity
 ): namedTypes.ClassDeclaration {
-  const properties = entity.fields.map((field) => {
+  const uniqueFields = entity.fields.filter(
+    (field) => field.dataType === EnumDataType.Id
+  );
+  const properties = uniqueFields.map((field) => {
     const type = builders.tsNullKeyword();
     return builders.tsPropertySignature(
       builders.identifier(field.name),
