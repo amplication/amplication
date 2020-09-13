6fc0f37a05b6e6e40a935f6fe0012fe67a3b5660
Iddan Aaronsohn <mail@aniddan.com>
2020-09-13 16:58:43 +0300

    Write logs to DB

--- packages/amplication-server/src/core/build/build.consumer.ts
+++ packages/amplication-server/src/core/build/build.consumer.ts
@@ -8,10 +8,13 @@ import {
   OnGlobalQueueError
 } from '@nestjs/bull';
 import { Job } from 'bull';
+import { Inject } from '@nestjs/common';
 import { StorageService } from '@codebrew/nestjs-storage';
-import * as DataServiceGenerator from 'amplication-data-service-generator';
 import { PrismaService } from 'nestjs-prisma';
-import winston from 'winston';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { BuildLogLevel, InputJsonValue } from '@prisma/client';
+import * as DataServiceGenerator from 'amplication-data-service-generator';
 import { EntityService } from '..';
 import { QUEUE_NAME } from './constants';
 import { BuildRequest } from './dto/BuildRequest';
@@ -20,13 +23,21 @@ import { getBuildFilePath } from './storage';
 import { createZipFileFromModules } from './zip';
 import { AppRoleService } from '../appRole/appRole.service';
 
+const WINSTON_LEVEL_TO_BUILD_LOG_LEVEL: { [level: string]: BuildLogLevel } = {
+  error: 'Error',
+  warn: 'Warning',
+  info: 'Info',
+  debug: 'Debug'
+};
+
 @Processor(QUEUE_NAME)
 export class BuildConsumer {
   constructor(
     private readonly storageService: StorageService,
     private readonly prisma: PrismaService,
     private readonly entityService: EntityService,
-    private readonly appRoleService: AppRoleService
+    private readonly appRoleService: AppRoleService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger
   ) {}
 
   @OnQueueCompleted()
@@ -84,7 +95,10 @@ export class BuildConsumer {
     });
     const entities = await this.getBuildEntities(build);
     const roles = await this.appRoleService.getAppRoles({});
-    const logger = winston.createLogger({});
+    const logger = this.logger.child({});
+    logger.on('logging', (transport, level, message, meta) => {
+      this.writeLog(id, level, message, meta);
+    });
     const modules = await DataServiceGenerator.createDataService(
       entities,
       roles,
@@ -94,6 +108,26 @@ export class BuildConsumer {
     const disk = this.storageService.getDisk('local');
     const zip = await createZipFileFromModules(modules);
     await disk.put(filePath, zip);
+  }
+
+  private async writeLog(
+    id: string,
+    level: string,
+    message: string,
+    meta: InputJsonValue
+  ) {
+    await this.prisma.build.update({
+      where: { id },
+      data: {
+        logs: {
+          create: {
+            level: WINSTON_LEVEL_TO_BUILD_LOG_LEVEL[level],
+            message,
+            meta
+          }
+        }
+      }
+    });
   }
 
   private async updateStatus(
