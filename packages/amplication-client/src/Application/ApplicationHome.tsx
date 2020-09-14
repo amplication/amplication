783fd1d760faef3460f87b21317c1cf1f38125e7
Yuval Hazaz <yuval@amplication.com>
2020-09-14 12:42:16 +0300

    app home page

--- packages/amplication-client/src/Application/ApplicationHome.tsx
+++ packages/amplication-client/src/Application/ApplicationHome.tsx
@@ -1,54 +1,93 @@
 import React, { useState, useCallback } from "react";
 import { match } from "react-router-dom";
+import { Formik, Form } from "formik";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
-import { TabBar, Tab } from "@rmwc/tabs";
+import PageContent from "../Layout/PageContent";
+import FloatingToolbar from "../Layout/FloatingToolbar";
+import ApplicationBadge from "./ApplicationBadge";
+import * as models from "../models";
+import EditableTitleField from "../Components/EditableTitleField";
 import "@rmwc/tabs/styles";
-// import { Card } from "@rmwc/card";
-// import {
-//   DataTable,
-//   DataTableContent,
-//   DataTableHead,
-//   DataTableRow,
-//   DataTableHeadCell,
-//   DataTableBody,
-//   DataTableCell,
-// } from "@rmwc/data-table";
 import "./ApplicationHome.scss";
+import { EnumPanelStyle, Panel } from "../Components/Panel";
+import FormikAutoSave from "../util/formikAutoSave";
 
 type Props = {
   match: match<{ application: string }>;
 };
 
+type DType = {
+  updateApp: models.App;
+};
+
+const CLASS_NAME = "application-home";
+
+const INITIAL_VALUES = {
+  name: "",
+  description: "",
+};
+
 function ApplicationHome({ match }: Props) {
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
-  const handleActivate = useCallback(
-    (event) => {
-      setActiveTabIndex(event.detail.index);
-    },
-    [setActiveTabIndex]
-  );
-  const { data, loading } = useQuery(GET_APPLICATION, {
+  const { data, loading } = useQuery<{ app: models.App }>(GET_APPLICATION, {
     variables: {
       id: match.params.application,
     },
   });
+
+  const handleSubmit = useCallback((data: models.AppUpdateInput) => {
+    console.log(data);
+  }, []);
 
   if (loading) {
     return <span>Loading...</span>;
   }
 
   return (
-    <main className="application-home">
-      <header>
-        <h1>{data?.app.name}</h1>
-        <p>{data?.app.description}</p>
-        <TabBar activeTabIndex={activeTabIndex} onActivate={handleActivate}>
-          <Tab>Versions</Tab>
-          <Tab>Environment</Tab>
-        </TabBar>
-      </header>
-    </main>
+    <PageContent className={CLASS_NAME} withFloatingBar>
+      <main>
+        <FloatingToolbar />
+        <Panel
+          className={`${CLASS_NAME}__info`}
+          panelStyle={EnumPanelStyle.Transparent}
+        >
+          <div className={`${CLASS_NAME}__info__badge`}>
+            <ApplicationBadge
+              name={data?.app.name || ""}
+              expanded
+              large
+              hideFullName
+            />
+          </div>
+          <div className={`${CLASS_NAME}__info__name`}>
+            <Formik
+              initialValues={data?.app || INITIAL_VALUES}
+              enableReinitialize
+              onSubmit={handleSubmit}
+            >
+              {(formik) => {
+                return (
+                  <>
+                    <Form>
+                      <FormikAutoSave debounceMS={1000} />
+                      <EditableTitleField
+                        name="name"
+                        label="Application Name"
+                      />
+                      <EditableTitleField
+                        secondary
+                        name="description"
+                        label="Description"
+                      />
+                    </Form>
+                  </>
+                );
+              }}
+            </Formik>
+          </div>
+        </Panel>
+      </main>
+    </PageContent>
   );
 }
 
