36b65d8f0a8c85f76bf2a6fd236b59e3bb8b85a8
Gal Cegla <galcegla@gmail.com>
2020-09-15 16:00:59 +0300

    Added some tests for password service

--- packages/amplication-server/src/core/account/password.service.spec.ts
+++ packages/amplication-server/src/core/account/password.service.spec.ts
@@ -1,5 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasswordService } from './password.service';
+
+const EXAMPLE_PASSWORD = 'examplePassword';
+const EXAMPLE_HASHED_PASSWORD = 'exampleHashedPassword';
+
+const configServiceCompareMock = jest.fn(() => {
+  return true;
+});
+const configServiceHashMock = jest.fn(() => {
+  return EXAMPLE_HASHED_PASSWORD;
+});
 
 describe('PasswordService', () => {
   let service: PasswordService;
@@ -7,10 +18,30 @@ describe('PasswordService', () => {
   beforeEach(async () => {
     jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PasswordService]
+      providers: [
+        PasswordService,
+        {
+          provide: ConfigService,
+          useClass: jest.fn(() => ({
+            compare: configServiceCompareMock,
+            hash: configServiceHashMock
+          }))
+        }
+      ],
+      imports: []
     }).compile();
 
     service = module.get<PasswordService>(PasswordService);
+  });
+
+  it('should validate a password', () => {
+    const args = {
+      password: EXAMPLE_PASSWORD,
+      hashedPassword: EXAMPLE_HASHED_PASSWORD
+    };
+    expect(
+      service.validatePassword(args.password, args.hashedPassword)
+    ).toEqual(true);
   });
 
   //it('should throw an error', async () => {});
