1b51e5c7efb9d082d75b303531d1ba738248095a
Iddan Aaronsohn <mail@aniddan.com>
2020-09-15 22:09:44 +0300

    Fix inject variables and test it

--- scripts/inject-variables.js
+++ scripts/inject-variables.js
@@ -1,24 +1,30 @@
 const fs = require("fs");
 
 const HTML_FILE = "/packages/amplication-client/build/index.html";
+exports.HTML_FILE = HTML_FILE;
 const ENV_VARS_PLACEHOLDER = "ENVIRONMENT_VARIABLES";
+exports.ENV_VARS_PLACEHOLDER = ENV_VARS_PLACEHOLDER;
 const REACT_APP_ENV_REGEXP = /^REACT_APP/;
 
 if (require.main === module) {
-  injectVariables().catch((error) => {
+  injectVariables(process.env).catch((error) => {
     console.error(error);
     process.exit(1);
   });
 }
 
-async function injectVariables() {
-  const vars = Object.fromEntries(
-    Object.entries(process.env).filter(([key]) =>
+exports.injectVariables = async function injectVariables(environment) {
+  const vars = getReactAppEnv(environment);
+  const html = await fs.promises.readFile(HTML_FILE, "utf-8");
+  const updatedHTML = html.replace(ENV_VARS_PLACEHOLDER, JSON.stringify(vars));
+  await fs.promises.writeFile(HTML_FILE, updatedHTML);
+  console.info("Updated client environment variables");
+};
+
+exports.getReactAppEnv = function getReactAppEnv(environment) {
+  return Object.fromEntries(
+    Object.entries(environment).filter(([key]) =>
       key.match(REACT_APP_ENV_REGEXP)
     )
   );
-  const html = await fs.promises.readFile(HTML_FILE, "utf-8");
-  const updatedHTML = html.replace(ENV_VARS_PLACEHOLDER, JSON.stringify(vars));
-  console.info("Updated client environment variables");
-  fs.writeFile(HTML_FILE, updatedHTML);
-}
+};
---
+++ scripts/inject-variables.spec.js
@@ -0,0 +1,92 @@
+const fs = require("fs");
+const {
+  getReactAppEnv,
+  ENV_VARS_PLACEHOLDER,
+  HTML_FILE,
+  injectVariables,
+} = require("./inject-variables");
+
+const REACT_APP_EXAMPLE_VAR = "REACT_APP_EXAMPLE_VAR";
+const REACT_APP_OTHER_EXAMPLE_VAR = "REACT_APP_OTHER_EXAMPLE_VAR";
+const EXAMPLE_NON_REACT_APP_VAR = "EXAMPLE_NON_REACT_APP_VAR";
+const EXAMPLE_VALUE = "EXAMPLE_VALUE";
+const EXAMPLE_ENVIRONMENT = {
+  [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE,
+  [REACT_APP_OTHER_EXAMPLE_VAR]: EXAMPLE_VALUE,
+  [EXAMPLE_NON_REACT_APP_VAR]: EXAMPLE_VALUE,
+};
+const EXAMPLE_HTML = `<html>
+<body>
+  <script>
+    const envVars = ${ENV_VARS_PLACEHOLDER};
+  </script>
+</body>
+</html>`;
+const UPDATED_HTML = `<html>
+<body>
+  <script>
+    const envVars = ${JSON.stringify(EXAMPLE_ENVIRONMENT)};
+  </script>
+</body>
+</html>`;
+
+const readFileMock = jest.fn();
+const writeFileMock = jest.fn();
+
+jest.mock("fs");
+
+fs.promises = {
+  readFile: readFileMock,
+  writeFile: writeFileMock,
+};
+
+describe("getReactAppEnv", () => {
+  const cases = [
+    [
+      "Single react app env var",
+      { [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE },
+      { [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE },
+    ],
+    [
+      "Single react app env var and non react app env var",
+      {
+        [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE,
+        [EXAMPLE_NON_REACT_APP_VAR]: EXAMPLE_VALUE,
+      },
+      { [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE },
+    ],
+    [
+      "Single react app env var and non react app env var",
+      {
+        [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE,
+        [EXAMPLE_NON_REACT_APP_VAR]: EXAMPLE_VALUE,
+      },
+      { [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE },
+    ],
+    [
+      "Two react app env vars and non react app env var",
+      {
+        [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE,
+        [REACT_APP_OTHER_EXAMPLE_VAR]: EXAMPLE_VALUE,
+        [EXAMPLE_NON_REACT_APP_VAR]: EXAMPLE_VALUE,
+      },
+      {
+        [REACT_APP_EXAMPLE_VAR]: EXAMPLE_VALUE,
+        [REACT_APP_OTHER_EXAMPLE_VAR]: EXAMPLE_VALUE,
+      },
+    ],
+  ];
+  test.each(cases)("%s", (name, environment, expected) => {
+    expect(getReactAppEnv(environment)).toEqual(expected);
+  });
+});
+
+describe("injectVariables", async () => {
+  fs.promises.readFile.mockImplementation(() => EXAMPLE_HTML);
+  fs.promises.writeFile.mockImplementation(() => {});
+  expect(await injectVariables(EXAMPLE_ENVIRONMENT)).toBeUndefined();
+  expect(readFileMock).toBeCalledTimes(1);
+  expect(readFileMock).toBeCalledWith(HTML_FILE, "utf-8");
+  expect(writeFileMock).toBeCalledTimes(1);
+  expect(writeFileMock).toBeCalledWith(HTML_FILE, UPDATED_HTML);
+});
