c9b3d583133029d4effc133dcf7b03d078878653
Iddan Aaronsohn <mail@aniddan.com>
2020-09-16 10:04:50 +0300

    Update script to get index html as argument

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 set -e;
 
 # Run inject variables script
-node /packages/amplication-client/src/scripts/inject-variables.js;
+CLIENT_DIRECTORY=/packages/amplication-client;
+node $CLIENT_DIRECTORY/src/scripts/inject-variables.js $CLIENT_DIRECTORY/build/index.html;
 
 # Execute CMD
 exec "$@";
--- packages/amplication-client/src/scripts/inject-variables.js
+++ packages/amplication-client/src/scripts/inject-variables.js
@@ -1,28 +1,45 @@
+/**
+ * This module is written in JavaScript instead of TypeScript so it can be
+ * executed without being compiled.
+ */
+
 const fs = require("fs");
 
-const HTML_FILE = "/packages/amplication-client/build/index.html";
-exports.HTML_FILE = HTML_FILE;
 const ENV_VARS_PLACEHOLDER = "ENVIRONMENT_VARIABLES";
 exports.ENV_VARS_PLACEHOLDER = ENV_VARS_PLACEHOLDER;
 const REACT_APP_ENV_REGEXP = /^REACT_APP_/;
 
 if (require.main === module) {
-  injectVariables(process.env).catch((error) => {
+  const [, , htmlFile] = process.argv;
+  if (!htmlFile) {
+    throw new Error("Must pass one argument: HTML_FILE");
+  }
+  injectVariables(process.env, htmlFile).catch((error) => {
     console.error(error);
     process.exit(1);
   });
 }
 
-async function injectVariables(environment) {
+/**
+ * Inject REACT_APP_ environment variables to given htmlFile
+ * @param {Object} environment
+ * @param {string} htmlFile
+ */
+async function injectVariables(environment, htmlFile) {
   const vars = getReactAppEnv(environment);
-  const html = await fs.promises.readFile(HTML_FILE, "utf-8");
+  const html = await fs.promises.readFile(htmlFile, "utf-8");
   const updatedHTML = html.replace(ENV_VARS_PLACEHOLDER, JSON.stringify(vars));
-  await fs.promises.writeFile(HTML_FILE, updatedHTML);
+  await fs.promises.writeFile(htmlFile, updatedHTML);
   console.info("Updated client environment variables");
 }
 
 exports.injectVariables = injectVariables;
 
+/**
+ * Get only REACT_APP_ environment variables from given environment object
+ * @param {Object} environment
+ * @returns environment with REACT_APP_ environment variables only
+ */
 function getReactAppEnv(environment) {
   return Object.fromEntries(
     Object.entries(environment).filter(([key]) =>
