95bbe46a8e09fd12d24579a7baa7326bf1a6af3c
Iddan Aaronsohn <mail@aniddan.com>
2020-09-17 16:24:09 +0300

    Test gcs disk service when bucket is not defined

--- packages/amplication-server/src/core/storage/gcs.disk.service.spec.ts
+++ packages/amplication-server/src/core/storage/gcs.disk.service.spec.ts
@@ -1,11 +1,11 @@
 import { DriverType } from '@codebrew/nestjs-storage';
 import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
-import { GCSDiskService } from './gcs.disk.service';
+import { GCSDiskService, GCS_BUCKET_VAR } from './gcs.disk.service';
 
 const EXAMPLE_BUCKET_NAME = 'EXAMPLE_BUCKET_NAME';
 
-const configServiceGetMock = jest.fn(() => EXAMPLE_BUCKET_NAME);
+const configServiceGetMock = jest.fn();
 
 describe('GCSDiskService', () => {
   let service: GCSDiskService;
@@ -22,8 +22,10 @@ describe('GCSDiskService', () => {
       ]
     }).compile();
     service = module.get<GCSDiskService>(GCSDiskService);
+    jest.clearAllMocks();
   });
-  test('it gets disk', () => {
+  test('gets disk if bucket is defined', () => {
+    configServiceGetMock.mockImplementation(() => EXAMPLE_BUCKET_NAME);
     expect(service.getDisk()).toEqual({
       driver: DriverType.GCS,
       config: {
@@ -31,5 +33,13 @@ describe('GCSDiskService', () => {
         keyFilename: ''
       }
     });
+    expect(configServiceGetMock).toBeCalledTimes(1);
+    expect(configServiceGetMock).toBeCalledWith(GCS_BUCKET_VAR);
+  });
+  test('gets null if bucket is not defined', () => {
+    configServiceGetMock.mockImplementation(() => null);
+    expect(service.getDisk()).toEqual(null);
+    expect(configServiceGetMock).toBeCalledTimes(1);
+    expect(configServiceGetMock).toBeCalledWith(GCS_BUCKET_VAR);
   });
 });
--- packages/amplication-server/src/core/storage/gcs.disk.service.ts
+++ packages/amplication-server/src/core/storage/gcs.disk.service.ts
@@ -2,13 +2,16 @@ import { DriverType, GoogleGcsStorageDisk } from '@codebrew/nestjs-storage';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-const GCS_BUCKET_VAR = 'GCS_BUCKET';
+export const GCS_BUCKET_VAR = 'GCS_BUCKET';
 
 @Injectable()
 export class GCSDiskService {
   constructor(private readonly configService: ConfigService) {}
-  getDisk(): GoogleGcsStorageDisk {
+  getDisk(): GoogleGcsStorageDisk | null {
     const bucket = this.configService.get(GCS_BUCKET_VAR);
+    if (!bucket) {
+      return null;
+    }
     return {
       driver: DriverType.GCS,
       config: {
