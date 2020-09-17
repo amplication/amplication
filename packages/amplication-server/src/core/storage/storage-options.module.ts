c2e878e2b5d6cee4186b204471d255f22de00fd6
Iddan Aaronsohn <mail@aniddan.com>
2020-09-17 17:52:50 +0300

    Add missing import to storage options module

--- packages/amplication-server/src/core/storage/storage-options.module.ts
+++ packages/amplication-server/src/core/storage/storage-options.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GCSDiskService } from './gcs.disk.service';
 import { LocalDiskService } from './local.disk.service';
 import { StorageOptionsService } from './storage-options.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [GCSDiskService, LocalDiskService, StorageOptionsService],
   exports: [StorageOptionsService]
 })
