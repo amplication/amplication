5fcd7fec820392bd0a86bd9475c194a1f5c037e5
Iddan Aaronsohn <mail@aniddan.com>
2020-09-17 16:57:32 +0300

    Add storage module

---
+++ packages/amplication-server/src/core/storage/storage-options.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { GCSDiskService } from './gcs.disk.service';
+import { LocalDiskService } from './local.disk.service';
+import { StorageOptionsService } from './storage-options.service';
+
+@Module({
+  providers: [GCSDiskService, LocalDiskService, StorageOptionsService]
+})
+export class StorageOptionsModule {}
