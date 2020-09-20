910a491d025160a4eb927410f976255bf23fb568
Iddan <iddan@h-iMac-sl-User.local>
2020-09-20 23:37:28 +0300

    Statically validate JSON

--- packages/amplication-server/src/core/background/background.service.ts
+++ packages/amplication-server/src/core/background/background.service.ts
@@ -1,5 +1,6 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JsonValue } from 'type-fest';
 
 const HOST_VAR = 'HOST';
 
@@ -9,7 +10,10 @@ export class BackgroundService {
     private readonly httpService: HttpService,
     private readonly configService: ConfigService
   ) {}
-  async queue(path: string, payload: any): Promise<void> {
+  async queue(path: string, payload: JsonValue): Promise<void> {
+    return this.httpRequest(path, payload);
+  }
+  private async httpRequest(path: string, payload: JsonValue) {
     const host = this.configService.get(HOST_VAR);
     await this.httpService.post(`${host}${path}`, payload).toPromise();
   }
--- packages/amplication-server/src/core/build/dto/CreateGeneratedAppDTO.ts
+++ packages/amplication-server/src/core/build/dto/CreateGeneratedAppDTO.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { JsonObject, JsonValue } from 'type-fest';
 
-export class CreateGeneratedAppDTO {
+export class CreateGeneratedAppDTO implements JsonObject {
+  [key: string]: JsonValue;
   @IsNotEmpty()
   @IsString()
   buildId: string;
