903209288aaab014233a34215dd5d13e4b4b57e6
Gal Cegla <galcegla@gmail.com>
2020-09-21 12:02:42 +0300

    Added should not validate test to jsonSchemaValidation

--- packages/amplication-server/src/services/jsonSchemaValidation.service.spec.ts
+++ packages/amplication-server/src/services/jsonSchemaValidation.service.spec.ts
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { Test, TestingModule } from '@nestjs/testing';
 import Ajv from 'ajv';
+import { error } from 'console';
 import { SchemaValidationResult } from 'src/dto/schemaValidationResult';
 import { JsonSchemaValidationService } from './jsonSchemaValidation.service';
 
 jest.mock('ajv');
-Ajv.prototype.validate.mockImplementation(() => {
-  return true;
-});
 
 describe('JsonSchemaValidationService', () => {
   let service: JsonSchemaValidationService;
@@ -25,12 +23,28 @@ describe('JsonSchemaValidationService', () => {
   });
 
   it('should validate a schema', async () => {
+    Ajv.prototype.validate.mockImplementation(() => {
+      return true;
+    });
     const args = {
       schema: {},
       data: {}
     };
     expect(await service.validateSchema(args.schema, args.data)).toEqual(
       new SchemaValidationResult(true)
+    );
+  });
+
+  it('should not validate a schema', async () => {
+    Ajv.prototype.validate.mockImplementation(() => {
+      return false;
+    });
+    const args = {
+      schema: {},
+      data: {}
+    };
+    expect(await service.validateSchema(args.schema, args.data)).toEqual(
+      new SchemaValidationResult(false, Ajv.prototype.errorsText())
     );
   });
 });
