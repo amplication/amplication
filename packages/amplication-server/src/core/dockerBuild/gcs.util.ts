76432c91688354e392c934d246c4f5952e4ff7c2
Iddan Aaronsohn <mail@aniddan.com>
2020-09-29 15:01:53 +0300

    Fix parseGCSAuthenticatedURL

--- packages/amplication-server/src/core/dockerBuild/gcs.util.ts
+++ packages/amplication-server/src/core/dockerBuild/gcs.util.ts
@@ -4,6 +4,6 @@ export const parseGCSAuthenticatedURL = (
   authenticatedURL: string
 ): { bucket: string; object: string } => {
   const url = new URL(authenticatedURL);
-  const [bucket, ...rest] = url.pathname.split(PATH_DELIMITER);
+  const [, bucket, ...rest] = url.pathname.split(PATH_DELIMITER);
   return { bucket, object: rest.join(PATH_DELIMITER) };
 };
