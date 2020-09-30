924ceb4adedafd0eb83af26a478fc4a48b083845
Iddan Aaronsohn <mail@aniddan.com>
2020-09-30 10:45:42 +0300

    Add more checks to parseGCSAuthenticatedURL

--- packages/amplication-server/src/core/dockerBuild/gcs.util.spec.ts
+++ packages/amplication-server/src/core/dockerBuild/gcs.util.spec.ts
@@ -1,9 +1,18 @@
-import { parseGCSAuthenticatedURL } from './gcs.util';
+import {
+  GCS_HOST,
+  InvalidHostError,
+  NO_BUCKET_ERROR_MESSAGE,
+  NO_OBJECT_ERROR_MESSAGE,
+  parseGCSAuthenticatedURL
+} from './gcs.util';
 
-const EXAMPLE_AUTHENTICATED_URL =
-  'https://storage.cloud.google.com/amplication-artifacts/generated-applications/ckfc6hu0s31030ws6kz1cm5wc.zip';
-const EXAMPLE_BUCKET = 'amplication-artifacts';
-const EXAMPLE_OBJECT = 'generated-applications/ckfc6hu0s31030ws6kz1cm5wc.zip';
+const EXAMPLE_BUCKET = 'example-bucket';
+const EXAMPLE_OBJECT = 'example-directory/example-filename';
+const EXAMPLE_AUTHENTICATED_URL = `https://${GCS_HOST}/${EXAMPLE_BUCKET}/${EXAMPLE_OBJECT}`;
+const EXAMPLE_INVALID_HOST = 'example.com';
+const EXAMPLE_INVALID_HOST_URL = `https://${EXAMPLE_INVALID_HOST}`;
+const EXAMPLE_MISSING_BUCKET_URL = `https://${GCS_HOST}`;
+const EXAMPLE_MISSING_OBJECT_URL = `https://${GCS_HOST}/${EXAMPLE_BUCKET}`;
 
 describe('parseGCSAuthenticatedURL', () => {
   test('parses authenticated URL correctly', () => {
@@ -11,5 +20,20 @@ describe('parseGCSAuthenticatedURL', () => {
       bucket: EXAMPLE_BUCKET,
       object: EXAMPLE_OBJECT
     });
+  });
+  test('fails for invalid host', () => {
+    expect(() => parseGCSAuthenticatedURL(EXAMPLE_INVALID_HOST_URL)).toThrow(
+      new InvalidHostError(EXAMPLE_INVALID_HOST)
+    );
+  });
+  test('fails for missing bucket', () => {
+    expect(() => parseGCSAuthenticatedURL(EXAMPLE_MISSING_BUCKET_URL)).toThrow(
+      NO_BUCKET_ERROR_MESSAGE
+    );
+  });
+  test('fails for missing object', () => {
+    expect(() => parseGCSAuthenticatedURL(EXAMPLE_MISSING_OBJECT_URL)).toThrow(
+      NO_OBJECT_ERROR_MESSAGE
+    );
   });
 });
--- packages/amplication-server/src/core/dockerBuild/gcs.util.ts
+++ packages/amplication-server/src/core/dockerBuild/gcs.util.ts
@@ -1,9 +1,32 @@
+export const GCS_HOST = 'storage.cloud.google.com';
+export const NO_BUCKET_ERROR_MESSAGE =
+  'Google Cloud Storage Authenticated URL pathname first part must be a name of a bucket';
+export const NO_OBJECT_ERROR_MESSAGE =
+  'Google Cloud Storage Authenticated URL pathname second part must be a path of an object';
 const PATH_DELIMITER = '/';
+
+export class InvalidHostError extends Error {
+  constructor(host: string) {
+    super(
+      `Google Cloud Storage Authenticated URL host must be: ${GCS_HOST} instead received ${host}`
+    );
+  }
+}
 
 export const parseGCSAuthenticatedURL = (
   authenticatedURL: string
 ): { bucket: string; object: string } => {
   const url = new URL(authenticatedURL);
+  if (url.host !== GCS_HOST) {
+    throw new InvalidHostError(url.host);
+  }
   const [, bucket, ...rest] = url.pathname.split(PATH_DELIMITER);
-  return { bucket, object: rest.join(PATH_DELIMITER) };
+  if (!bucket) {
+    throw new Error(NO_BUCKET_ERROR_MESSAGE);
+  }
+  const object = rest.join(PATH_DELIMITER);
+  if (!object) {
+    throw new Error(NO_OBJECT_ERROR_MESSAGE);
+  }
+  return { bucket, object };
 };
