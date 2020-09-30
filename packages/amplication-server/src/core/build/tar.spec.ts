8e083bf1f5fe759a105c671e33f6ed4cdf5f4458
Iddan Aaronsohn <mail@aniddan.com>
2020-09-30 11:26:55 +0300

    Improve testing of tar and simplify implementation

--- packages/amplication-server/src/core/build/tar.spec.ts
+++ packages/amplication-server/src/core/build/tar.spec.ts
@@ -1,13 +1,55 @@
+import { Buffer } from 'buffer';
+import tar from 'tar-stream';
+import getStream from 'get-stream';
 import { createTarFileFromModules } from './tar';
+
+jest.mock('tar');
+const tarPackEntryEndMock = jest.fn();
+const tarPackEntryMock = jest.fn(() => ({
+  end: tarPackEntryEndMock
+}));
+const tarFinalizeMock = jest.fn();
+const MOCK_PACK = {
+  entry: tarPackEntryMock,
+  finalize: tarFinalizeMock
+};
+// eslint-disable-next-line
+// @ts-ignore
+tar.pack = jest.fn(() => MOCK_PACK);
+
+const EXAMPLE_BUFFER = Buffer.from([]);
+
+jest.mock('get-stream');
+// eslint-disable-next-line
+// @ts-ignore
+getStream.buffer.mockImplementation(() => Promise.resolve(EXAMPLE_BUFFER));
+
+const EXAMPLE_PATH = 'EXAMPLE_PATH';
+const EXAMPLE_CODE = 'EXAMPLE_CODE';
 
 describe('createTarFileFromModules', () => {
   test('it creates a tar file', async () => {
     const modules = [
       {
-        path: 'EXAMPLE_PATH',
-        code: 'EXAMPLE_CODE'
+        path: EXAMPLE_PATH,
+        code: EXAMPLE_CODE
       }
     ];
-    await expect(createTarFileFromModules(modules)).resolves.toMatchSnapshot();
+    await expect(createTarFileFromModules(modules)).resolves.toBe(
+      EXAMPLE_BUFFER
+    );
+    expect(tar.pack).toBeCalledTimes(1);
+    expect(tar.pack).toBeCalledWith();
+    expect(tarPackEntryEndMock).toBeCalledTimes(1);
+    expect(tarPackEntryEndMock).toBeCalledWith();
+    expect(tarFinalizeMock).toBeCalledTimes(1);
+    expect(tarFinalizeMock).toBeCalledWith();
+    expect(tarPackEntryMock).toBeCalledTimes(1);
+    expect(tarPackEntryMock).toBeCalledWith(
+      { name: EXAMPLE_PATH },
+      EXAMPLE_CODE
+    );
+    expect(getStream.buffer).toBeCalledTimes(1);
+    expect(getStream.buffer).toBeCalledWith(MOCK_PACK);
   });
 });
--- packages/amplication-server/src/core/build/tar.ts
+++ packages/amplication-server/src/core/build/tar.ts
@@ -1,26 +1,15 @@
 import tar from 'tar-stream';
-import { Buffer } from 'buffer';
+import getStream from 'get-stream';
 import * as DataServiceGenerator from 'amplication-data-service-generator';
 
 export function createTarFileFromModules(
   modules: DataServiceGenerator.Module[]
 ): Promise<Buffer> {
-  return new Promise((resolve, reject) => {
-    const pack = tar.pack();
-    for (const module of modules) {
-      const entry = pack.entry({ name: module.path }, module.code);
-      entry.end();
-    }
-    const chunks = [];
-    pack.on('data', chunk => {
-      chunks.push(chunk);
-    });
-    pack.on('end', () => {
-      resolve(Buffer.concat(chunks));
-    });
-    pack.on('error', error => {
-      reject(error);
-    });
-    pack.finalize();
-  });
+  const pack = tar.pack();
+  for (const module of modules) {
+    const entry = pack.entry({ name: module.path }, module.code);
+    entry.end();
+  }
+  pack.finalize();
+  return getStream.buffer(pack);
 }
