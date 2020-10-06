1bf7251f895fd4fbfc89208de9efdee38639d36f
Iddan Aaronsohn <mail@aniddan.com>
2020-10-06 21:04:24 +0300

    Create basic provider

--- packages/amplication-deployer/src/gcp/GCPProvider.spec.ts
+++ packages/amplication-deployer/src/gcp/GCPProvider.spec.ts
@@ -1,58 +1,102 @@
-// import { CloudBuildProvider } from "./CloudBuildProvider";
-// import { createConfig } from "./config";
-// import { GCS_HOST } from "./gcs.util";
+import { Buffer } from "buffer";
+import getStream from "get-stream";
+import { createConfig } from "./config";
+import { GCPProvider } from "./GCPProvider";
+import * as modules from "./modules";
+import * as hashUtil from "./hash.util";
 
-// const EXAMPLE_PROJECT_ID = "EXAMPLE_PROJECT_ID";
-// const EXAMPLE_REPOSITORY = "EXAMPLE_REPOSITORY";
-// const EXAMPLE_TAG = "EXAMPLE_TAG";
-// const EXAMPLE_BUCKET = "EXAMPLE_BUCKET";
-// const EXAMPLE_OBJECT = "EXAMPLE_OBJECT";
-// const EXAMPLE_GCS_CODE_URL = `https://${GCS_HOST}/${EXAMPLE_BUCKET}/${EXAMPLE_OBJECT}`;
-// const EXAMPLE_GCR_IMAGES = [`gcr.io/${EXAMPLE_BUCKET}/${EXAMPLE_OBJECT}`];
-// const EXAMPLE_BUILD_ARGS = {
-//   EXAMPLE_KEY: "EXAMPLE_VALUE",
-// };
-// const EXAMPLE_FINISHED_CLOUD_BUILD_BUILD = {
-//   images: EXAMPLE_GCR_IMAGES,
-// };
+const EXAMPLE_PROJECT_ID = "EXAMPLE_PROJECT_ID";
+const EXAMPLE_BUCKET = "EXAMPLE_BUCKET";
+const EXAMPLE_BUFFER = Buffer.from([]);
+const EXAMPLE_STREAM = {};
+const EXAMPLE_CONFIGURATION = { terraform: { backend: {} }, module: {} };
+const EXAMPLE_VARIABLES = { EXAMPLE_VARIABLE: "EXAMPLE_VARIABLE_VALUE" };
+const EXAMPLE_BACKEND_CONFIGURATION = {
+  EXAMPLE_BACKEND_CONFIGURATION_KEY: "EXAMPLE_BACKEND_CONFIGURATION_VALUE",
+};
+const EXAMPLE_HASH = "EXAMPLE_HASH";
+const EXAMPLE_ARCHIVE_FILE_NAME = `${EXAMPLE_HASH}.tar.gz`;
+const EXAMPLE_FINISHED_CLOUD_BUILD_BUILD = {};
 
-// const cloudBuildBuildPromiseMock = jest.fn(() => [
-//   EXAMPLE_FINISHED_CLOUD_BUILD_BUILD,
-// ]);
-// const cloudBuildCreateBuildMock = jest.fn(() => [
-//   {
-//     promise: cloudBuildBuildPromiseMock,
-//   },
-// ]);
-// const MOCK_CLOUD_BUILD_CLIENT = {
-//   createBuild: cloudBuildCreateBuildMock,
-// };
+const cloudBuildBuildPromiseMock = jest.fn(() => [
+  EXAMPLE_FINISHED_CLOUD_BUILD_BUILD,
+]);
+const cloudBuildCreateBuildMock = jest.fn(() => [
+  {
+    promise: cloudBuildBuildPromiseMock,
+  },
+]);
+const MOCK_CLOUD_BUILD_CLIENT = {
+  createBuild: cloudBuildCreateBuildMock,
+};
+const storageFileSaveMock = jest.fn(() => Promise.resolve());
+const storageBucketFileMock = jest.fn(() => ({
+  save: storageFileSaveMock,
+}));
+const storageBucketMock = jest.fn(() => ({
+  file: storageBucketFileMock,
+}));
+const MOCK_STORAGE = {
+  bucket: storageBucketMock,
+};
 
-// describe("CloudBuildProvider", () => {
-//   test("builds docker image using google cloud build", async () => {
-//     await expect(
-//       new CloudBuildProvider(
-//         // @ts-ignore
-//         MOCK_CLOUD_BUILD_CLIENT,
-//         EXAMPLE_PROJECT_ID
-//       ).deploy(
-//         EXAMPLE_REPOSITORY,
-//         EXAMPLE_TAG,
-//         EXAMPLE_GCS_CODE_URL,
-//         EXAMPLE_BUILD_ARGS
-//       )
-//     ).resolves.toEqual({ images: EXAMPLE_GCR_IMAGES });
-//     expect(cloudBuildCreateBuildMock).toBeCalledTimes(1);
-//     expect(cloudBuildCreateBuildMock).toBeCalledWith({
-//       projectId: EXAMPLE_PROJECT_ID,
-//       build: createConfig(
-//         EXAMPLE_REPOSITORY,
-//         EXAMPLE_TAG,
-//         EXAMPLE_GCS_CODE_URL,
-//         EXAMPLE_BUILD_ARGS
-//       ),
-//     });
-//     expect(cloudBuildBuildPromiseMock).toBeCalledTimes(1);
-//     expect(cloudBuildBuildPromiseMock).toBeCalledWith();
-//   });
-// });
+jest.mock("./modules");
+const modulesEntryEndMock = jest.fn();
+const modulesEntryMock = jest.fn(() => ({
+  end: modulesEntryEndMock,
+}));
+const modulesPipeMock = jest.fn();
+// @ts-ignore
+modules.createTar.mockImplementation(() => ({
+  entry: modulesEntryMock,
+  pipe: modulesPipeMock,
+}));
+
+jest.mock("get-stream");
+// @ts-ignore
+getStream.buffer.mockImplementation(() => EXAMPLE_BUFFER);
+
+jest.mock("./hash.util.ts");
+// @ts-ignore
+hashUtil.createHash.mockImplementation(() => EXAMPLE_HASH);
+
+describe("GCPProvider", () => {
+  test("builds docker image using google cloud build", async () => {
+    await expect(
+      new GCPProvider(
+        // @ts-ignore
+        MOCK_CLOUD_BUILD_CLIENT,
+        // @ts-ignore
+        MOCK_STORAGE,
+        EXAMPLE_PROJECT_ID,
+        EXAMPLE_BUCKET
+      ).deploy(
+        EXAMPLE_CONFIGURATION,
+        EXAMPLE_VARIABLES,
+        EXAMPLE_BACKEND_CONFIGURATION
+      )
+    ).resolves.toEqual({});
+    expect(cloudBuildCreateBuildMock).toBeCalledTimes(1);
+    expect(cloudBuildCreateBuildMock).toBeCalledWith({
+      projectId: EXAMPLE_PROJECT_ID,
+      build: createConfig(
+        EXAMPLE_BUCKET,
+        EXAMPLE_ARCHIVE_FILE_NAME,
+        EXAMPLE_BACKEND_CONFIGURATION
+      ),
+    });
+    expect(cloudBuildBuildPromiseMock).toBeCalledTimes(1);
+    expect(cloudBuildBuildPromiseMock).toBeCalledWith();
+    expect(storageFileSaveMock).toBeCalledTimes(1);
+    expect(storageFileSaveMock).toBeCalledWith(EXAMPLE_BUFFER);
+    expect(storageBucketFileMock).toBeCalledTimes(1);
+    expect(storageBucketFileMock).toBeCalledWith(EXAMPLE_ARCHIVE_FILE_NAME);
+    expect(storageBucketMock).toBeCalledTimes(1);
+    expect(storageBucketMock).toBeCalledWith(EXAMPLE_BUCKET);
+    expect(getStream.buffer).toBeCalledTimes(1);
+    expect(hashUtil.createHash).toBeCalledTimes(1);
+    expect(modules.createTar).toBeCalledTimes(1);
+    expect(modulesEntryEndMock).toBeCalledTimes(2);
+    expect(modulesEntryMock).toBeCalledTimes(2);
+  });
+});
