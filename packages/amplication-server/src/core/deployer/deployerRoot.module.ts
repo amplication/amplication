d489c059c70e897b1245383e51ec9540de001df8
Iddan Aaronsohn <mail@aniddan.com>
2020-10-06 15:57:39 +0300

    Throw meaningful error message for missing default

--- packages/amplication-server/src/core/deployer/deployerRoot.module.ts
+++ packages/amplication-server/src/core/deployer/deployerRoot.module.ts
@@ -14,11 +14,15 @@ export enum DeployerProvider {
 export const DEPLOYER_DEFAULT_VAR = 'DEPLOYER_DEFAULT';
 export const APPS_GCP_PROJECT_ID_VAR = 'APPS_GCP_PROJECT_ID';
 export const GCS_BUCKET_VAR = 'GCS_BUCKET';
+export const UNDEFINED_DEPLOYER_DEFAULT_ERROR_MESSAGE = `${DEPLOYER_DEFAULT_VAR} environment variable must be defined`;
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DeployerRootModule = DeployerModule.forRootAsync({
   useFactory: (configService: ConfigService) => {
     const deployerDefault = configService.get(DEPLOYER_DEFAULT_VAR);
+    if (!deployerDefault) {
+      throw new Error(UNDEFINED_DEPLOYER_DEFAULT_ERROR_MESSAGE);
+    }
     const appsGCPProjectId = configService.get(APPS_GCP_PROJECT_ID_VAR);
     const bucket = configService.get(GCS_BUCKET_VAR);
     const gcpProvider =
