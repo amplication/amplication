0dac8a0238167ebff20f8ec3c5a342290ba72add
Iddan Aaronsohn <mail@aniddan.com>
2020-10-06 14:31:48 +0300

    Remove tags

--- packages/amplication-deployer/src/cloud-build/modules/db/main.tf
+++ packages/amplication-deployer/src/cloud-build/modules/db/main.tf
@@ -6,7 +6,6 @@ provider "google" {
 resource "google_sql_database" "database" {
   name     = "${var.app_id}_database"
   instance = var.instance_name
-  tags     = [var.app_id]
 }
 
 resource "random_password" "app_database_password" {
@@ -19,7 +18,6 @@ resource "google_sql_user" "app_database_user" {
   name     = "${var.app_id}_database_user"
   instance = var.instance_name
   password = random_password.app_database_password.result
-  tags     = [var.app_id]
 }
 
 output "name" {
