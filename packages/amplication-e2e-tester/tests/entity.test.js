fab17dea06d378cd0c093d677ca3449eb83100a4
belkind27 <71218434+belkind27@users.noreply.github.com>
2020-10-07 15:28:14 +0300

    add failed entity test

---
+++ packages/amplication-e2e-tester/tests/entity.test.js
@@ -0,0 +1,57 @@
+const TITLE = "Amplication";
+const LOGIN_URL = "http://localhost:3001/login";
+const HOME_PAGE_URL = "http://localhost:3001/";
+const USER_EMAIL = "lisa@simpson.com";
+const USER_PASSWORD = "secret42";
+const APP_NAME = "my first app";
+const TIMEOUT = 60000;
+const ENTITY_NAME = "my first entity";
+const CREATE_ENTITY_BUTTON_CONTENT = "Create Entity";//starrt
+const CREATE_NEW_BUTTON_CONTENT = "Create New";
+describe("create new entity test", () => {
+  beforeAll(async () => {
+    await page.goto(LOGIN_URL);
+  }, TIMEOUT);
+  it(
+    "should create new entity",
+    async () => {
+      var RESULT_URL;
+      var RESULT_SPAN;
+      page.setDefaultTimeout(TIMEOUT);
+      await expect(page.title()).resolves.toMatch(TITLE);
+      await (await page.waitForXPath("//input[@name='email']")).type(
+        USER_EMAIL
+      );
+      await (await page.waitForXPath('//input[@name="password"]')).type(
+        USER_PASSWORD
+      );
+      await page.click("button.amp-button--primary");
+      await page.waitForNavigation();
+      RESULT_URL = await page.url();
+      await expect(RESULT_URL).toMatch(HOME_PAGE_URL);
+      (
+        await page.waitForXPath(
+          `//div[@class="application-badge__app-name" and contains(.,"${APP_NAME}")]`
+        )
+      ).click();
+      await page.waitForNavigation();
+      (await page.waitForXPath('//a[@title="Entities"]')).click();
+      await page.waitForNavigation();
+      await page.waitForXPath(`//button[contains(text(),'${CREATE_NEW_BUTTON_CONTENT}')]`);
+      await (await page.waitForXPath('//input[@name="displayName"]')).type(
+        ENTITY_NAME
+      );
+      (
+        await page.waitForXPath(`//button[contains(.,'${CREATE_ENTITY_BUTTON_CONTENT}')]`)
+      ).click();
+      await page.waitForNavigation();
+      (await page.waitForXPath('//a[@title="Entities"]')).click();
+      await page.waitForNavigation();
+      RESULT_SPAN = await page.waitForXPath(
+        `//span[@class="text-medium" and contains(.,"${ENTITY_NAME}")]`
+      );
+      await expect(RESULT_SPAN).toBeTruthy();
+    },
+    TIMEOUT
+  );
+});
