c041a9362137f3ff0dca5c25972c80d363897b27
Iddan Aaronsohn <mail@aniddan.com>
2020-10-12 13:15:28 +0300

    Fix export NoProviderDefinedError

--- packages/amplication-deployer/src/deployer/NoProviderDefinedError.ts
+++ packages/amplication-deployer/src/deployer/NoProviderDefinedError.ts
@@ -1,4 +1,4 @@
-class NoProviderDefinedError extends Error {
+export class NoProviderDefinedError extends Error {
   constructor() {
     super(
       "No provider defined. Either the providerName argument must be provided or the default option provider to be defined"
