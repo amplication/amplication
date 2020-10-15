079403cd03ba658e4685688e5dc29ee5337e6628
Iddan Aaronsohn <iddan@h-MacBook-Pro-sl-Iddan.local>
2020-10-15 18:22:13 +0300

    Add missing google project services

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -3,6 +3,15 @@ provider "google" {
   region  = var.region
 }
 
+resource "google_project_service" "cloud_resource_manager_api" {
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "cloud_sql_admin_api" {
+  service    = "dns.googleapis.com"
+  depends_on = [google_project_service.cloud_resource_manager_api]
+}
+
 resource "google_dns_managed_zone" "zone" {
   name     = var.name
   dns_name = var.dns_name
