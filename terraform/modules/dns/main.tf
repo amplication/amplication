33e4e1008d0be2b34ef131670feaff43e73c9f7c
Iddan Aaronsohn <iddan@h-MacBook-Pro-sl-Iddan.local>
2020-10-15 18:25:06 +0300

    Make zone dependant on dns api

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -7,12 +7,13 @@ resource "google_project_service" "cloud_resource_manager_api" {
   service = "cloudresourcemanager.googleapis.com"
 }
 
-resource "google_project_service" "cloud_sql_admin_api" {
+resource "google_project_service" "cloud_dns_api" {
   service    = "dns.googleapis.com"
   depends_on = [google_project_service.cloud_resource_manager_api]
 }
 
 resource "google_dns_managed_zone" "zone" {
-  name     = var.name
-  dns_name = var.dns_name
+  name       = var.name
+  dns_name   = var.dns_name
+  depends_on = [google_project_service.cloud_dns_api]
 }
