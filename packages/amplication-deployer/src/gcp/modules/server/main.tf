4c34b49c78d1c8cf0d801bf71aa49c8da5cc989c
Iddan Aaronsohn <iddan@h-MacBook-Pro-sl-Iddan.local>
2020-10-19 12:31:57 +0300

    Add gcloud build submit in terraform

--- packages/amplication-deployer/src/gcp/modules/server/main.tf
+++ packages/amplication-deployer/src/gcp/modules/server/main.tf
@@ -40,6 +40,31 @@ resource "google_cloud_run_service" "default" {
   autogenerate_revision_name = true
 }
 
+
+resource "null_resource" "local_gcloud" {
+  depends_on = ["google_cloudbuild_trigger.trigger"]
+  provisioner "local-exec" {
+    command = <<EOF
+cat <<EOT >> cloudbuild.yaml
+steps:
+ - name: gcr.io/cloud-builders/docker
+   args:
+    - run
+    - $_IMAGE_ID
+    - bash
+    - -c
+    - |
+      set -e;
+      wget -O /workspace/cloud_sql_proxy https://storage.googleapis.com/cloudsql-proxy/v1.16/cloud_sql_proxy.linux.386;
+      chmod +x /workspace/cloud_sql_proxy;
+      /workspace/cloud_sql_proxy -dir=/workspace -instances=$PROJECT_ID:$_REGION:$_DB_INSTANCE=tcp:5432 & sleep 2
+      POSTGRESQL_URL="postgresql://$_POSTGRESQL_USER:$_POSTGRESQL_PASSWORD@localhost:5432/$_POSTGRESQL_DB" npx @prisma/cli migrate up --create-db --auto-approve --experimental;
+EOT
+gcloud builds submit --no-source --config cloudbuild.yaml --substitutions _IMAGE_ID=var.image_id,_REGION=var.region,_DB_INSTANCE=var.database_instance,_POSTGRESQL_USER=var.database_user,_POSTGRESQL_PASSWORD=var.database_password,_POSTGRESQL_DB=var.database_name
+    EOF
+  }
+}
+
 resource "google_cloud_run_domain_mapping" "default" {
   location = var.region
   name     = var.domain
