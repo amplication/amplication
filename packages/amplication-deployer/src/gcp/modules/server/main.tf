7b4dee95c43138f5e26fdd24b66814370fb51d63
Iddan Aaronsohn <iddan@h-MacBook-Pro-sl-Iddan.local>
2020-10-19 13:59:32 +0300

    Use $HOME for aliasing bin

--- packages/amplication-deployer/src/gcp/modules/server/main.tf
+++ packages/amplication-deployer/src/gcp/modules/server/main.tf
@@ -66,8 +66,8 @@ steps:
       /workspace/cloud_sql_proxy -dir=/workspace -instances=$PROJECT_ID:$_REGION:$_DB_INSTANCE=tcp:5432 & sleep 2
       POSTGRESQL_URL="postgresql://$_POSTGRESQL_USER:$_POSTGRESQL_PASSWORD@localhost:5432/$_POSTGRESQL_DB" npx @prisma/cli migrate up --create-db --auto-approve --experimental;
 EOT
-curl -sSL https://sdk.cloud.google.com | bash;
-export PATH=$PATH:$PWD/google-cloud-sdk/bin;
+curl https://sdk.cloud.google.com | bash;
+export PATH=$PATH:$HOME/google-cloud-sdk/bin;
 gcloud builds submit --no-source --config cloudbuild.yaml --substitutions _IMAGE_ID=var.image_id,_REGION=var.region,_DB_INSTANCE=var.database_instance,_POSTGRESQL_USER=var.database_user,_POSTGRESQL_PASSWORD=var.database_password,_POSTGRESQL_DB=var.database_name
     EOF
   }
