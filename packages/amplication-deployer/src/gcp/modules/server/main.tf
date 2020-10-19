6e109bcddb56ee27bd8a6bdaf4e826deafffc807
Iddan Aaronsohn <iddan@h-MacBook-Pro-sl-Iddan.local>
2020-10-19 13:50:53 +0300

    Fix path of google cloud sdk bin

--- packages/amplication-deployer/src/gcp/modules/server/main.tf
+++ packages/amplication-deployer/src/gcp/modules/server/main.tf
@@ -67,7 +67,7 @@ steps:
       POSTGRESQL_URL="postgresql://$_POSTGRESQL_USER:$_POSTGRESQL_PASSWORD@localhost:5432/$_POSTGRESQL_DB" npx @prisma/cli migrate up --create-db --auto-approve --experimental;
 EOT
 curl -sSL https://sdk.cloud.google.com | bash;
-export PATH=$PATH:/root/google-cloud-sdk/bin;
+export PATH=$PATH:$PWD/google-cloud-sdk/bin;
 gcloud builds submit --no-source --config cloudbuild.yaml --substitutions _IMAGE_ID=var.image_id,_REGION=var.region,_DB_INSTANCE=var.database_instance,_POSTGRESQL_USER=var.database_user,_POSTGRESQL_PASSWORD=var.database_password,_POSTGRESQL_DB=var.database_name
     EOF
   }
