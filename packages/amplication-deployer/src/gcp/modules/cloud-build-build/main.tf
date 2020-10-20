a82373faad0ca64b3e8319686408351100f206c6
Iddan Aaronsohn <iddan@h-MacBook-Pro-sl-Iddan.local>
2020-10-20 17:32:50 +0300

    Use string values for triggers

--- packages/amplication-deployer/src/gcp/modules/cloud-build-build/main.tf
+++ packages/amplication-deployer/src/gcp/modules/cloud-build-build/main.tf
@@ -6,10 +6,15 @@ variable "substitutions" {
   type = list(string)
 }
 
+locals {
+  encoded_configuration = yamlencode(var.configuration)
+  encoded_substitutions = join(",", var.substitutions)
+}
+
 resource "null_resource" "local_gcloud" {
   triggers = {
-    configuration = var.configuration
-    substitutions = var.substitutions
+    configuration = var.encoded_configuration
+    substitutions = var.encoded_substitutions
   }
   provisioner "local-exec" {
     # Assumption: the module is running inside a terraform docker container (bashed on alpine linux)
@@ -21,13 +26,13 @@ apk add --update \
   bash;
 ln -sf python3 /usr/bin/python;
 cat <<'EOT' > cloudbuild.yaml
-${yamlencode(var.configuration)}
+${var.encoded_configuration}
 EOT
 echo "Installing Google Cloud SDK...";
 curl --silent --show-error https://sdk.cloud.google.com | bash > /dev/null 2>&1;
 export PATH=$PATH:$HOME/google-cloud-sdk/bin;
 echo "Submitting build...";
-gcloud builds submit --no-source --config cloudbuild.yaml --substitutions ${join(",", var.substitutions)};
+gcloud builds submit --no-source --config cloudbuild.yaml --substitutions ${var.encoded_substitutions};
 echo "Build is done";
     EOF
   }
