d9899724f40d754ddcec2af3d1f52c562edae603
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-10-20 19:15:25 +0300

    remove unused imports

--- packages/amplication-client/src/VersionControl/ActionLog.tsx
+++ packages/amplication-client/src/VersionControl/ActionLog.tsx
@@ -1,6 +1,4 @@
-import React, { useMemo, useEffect } from "react";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+import React, { useMemo } from "react";
 import { LazyLog } from "react-lazylog";
 import { isEmpty } from "lodash";
 import { Icon } from "@rmwc/icon";
@@ -9,8 +7,6 @@ import { CircularProgress } from "@rmwc/circular-progress";
 
 import { differenceInSeconds } from "date-fns";
 
-import { formatError } from "../util/error";
-import { Snackbar } from "@rmwc/snackbar";
 import "@rmwc/snackbar/styles";
 import chalk from "chalk";
 import * as models from "../models";
@@ -127,26 +123,3 @@ const ActionLog = ({ action }: Props) => {
 };
 
 export default ActionLog;
-
-// export const GET_ACTION_LOG = gql`
-//   query actionLog($actionId: String!) {
-//     action(where: { id: $actionId }) {
-//       id
-//       createdAt
-//       steps {
-//         id
-//         createdAt
-//         message
-//         status
-//         completedAt
-//         logs {
-//           id
-//           createdAt
-//           message
-//           meta
-//           level
-//         }
-//       }
-//     }
-//   }
-// `;
