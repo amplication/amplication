fdf65f797949e1db62f290e2e11b7f84ff620416
רועי בלקינד <belkind27@gmail.com>
2020-10-22 11:32:20 +0300

    add Create NewEntity Field function & update create random name function

--- packages/amplication-e2e-tester/tests/functions.ts
+++ packages/amplication-e2e-tester/tests/functions.ts
@@ -1,7 +1,8 @@
-import { Response } from "puppeteer";
+import { ElementHandle, Response } from "puppeteer";
 
 export function createRandomName(): string {
-  return Math.random().toString(32);
+  const randomString = Math.random().toString(32);
+  return "s" + randomString.slice(2, randomString.length);
 }
 export async function signUp(
   signUpBtn: string,
@@ -34,4 +35,43 @@ export async function signUp(
     await page.waitForXPath(`//button[contains(text(),'${continueBtn}')]`)
   ).click();
   return page.waitForNavigation();
+}
+export async function CreateNewEntityField(
+  fieldType: string
+): Promise<ElementHandle> {
+  const fieldsName = createRandomName();
+  await page.type(
+    "form > .text-input > .text-input__inner-wrapper > label > input",
+    fieldsName
+  );
+  await page.click(
+    ".amp-data-grid__toolbar > form > .text-input > .text-input__inner-wrapper > .ButtonBase-exshql-0"
+  );
+  await page.waitForSelector(
+    ".side-bar__inner-wrapper > .mdc-drawer__content > .amp-form > .ButtonBase-exshql-0 > .rmwc-icon"
+  );
+  await page.click(
+    ".side-bar__inner-wrapper > .mdc-drawer__content > .amp-form > .ButtonBase-exshql-0 > .rmwc-icon"
+  );
+  await page.waitForSelector(
+    ".amp-form > .text-input > .text-input__inner-wrapper > label > textarea"
+  );
+  await page.type(
+    ".amp-form > .text-input > .text-input__inner-wrapper > label > textarea",
+    createRandomName()
+  );
+  await (await page.waitForXPath(`//input[@name="required"]`)).click();
+  await (await page.waitForXPath(`//input[@name="searchable"]`)).click();
+  await page.click(
+    ".mdc-drawer__content > .amp-form > .select-field > label > .select-field__container"
+  );
+  await (
+    await page.waitForXPath(`//div[contains(text(),"${fieldType}")]`)
+  ).click();
+  await page.click(
+    ".side-bar__inner-wrapper > .side-bar__header > .Tooltip-h3c7f-0 > .ButtonBase-exshql-0 > .rmwc-icon"
+  );
+  return page.waitForXPath(
+    `//span[@class="text-medium" and contains(.,"${fieldsName}")]`
+  );
 }
