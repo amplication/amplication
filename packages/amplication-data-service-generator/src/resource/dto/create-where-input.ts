69a82d9a52a53a4d2668909832ff708cc2d09e54
Iddan Aaronsohn <mail@aniddan.com>
2020-10-25 14:22:21 +0200

    Remove accidentally added constants to create-where-input

--- packages/amplication-data-service-generator/src/resource/dto/create-where-input.ts
+++ packages/amplication-data-service-generator/src/resource/dto/create-where-input.ts
@@ -5,30 +5,6 @@ import { NamedClassDeclaration } from "../../util/ast";
 import { createFieldClassProperty } from "./create-field-class-property";
 import { isOneToOneRelationField, isScalarListField } from "./field.util";
 
-const EXAMPLE_ENTITY_ID = "EXAMPLE_ENTITY_ID";
-const EXAMPLE_ENTITY_NAME = "ExampleEntityName";
-const EXAMPLE_ENTITY_ID_TO_NAME: Record<string, string> = {
-  [EXAMPLE_ENTITY_ID]: EXAMPLE_ENTITY_NAME,
-};
-
-const EXAMPLE_ENTITY_FIELD_NAME = "exampleEntityFieldName";
-const EXAMPLE_ENTITY_FIELD: EntityField = {
-  name: EXAMPLE_ENTITY_FIELD_NAME,
-  displayName: "Example Entity Field Display Name",
-  description: "Example entity field description",
-  dataType: EnumDataType.Id,
-  required: true,
-  searchable: false,
-};
-const EXAMPLE_ENTITY: Entity = {
-  id: EXAMPLE_ENTITY_ID,
-  name: EXAMPLE_ENTITY_NAME,
-  displayName: "Example Entity",
-  pluralDisplayName: "Example Entities",
-  fields: [EXAMPLE_ENTITY_FIELD],
-  permissions: [],
-};
-
 export function createWhereInput(
   entity: Entity,
   entityIdToName: Record<string, string>
