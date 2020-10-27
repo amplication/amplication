6ba6cefced73ecaed262c1fd15b9de5fa0dc739d
meeroslava <sapphirenymph@gmail.com>
2020-10-27 16:19:22 +0200

    added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../packages/amplication-design-system/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+ 
+}
---
+++ packages/amplication-design-system/TextInput.stories.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+import { TextInput } from './TextInput';
+
+export default {
+  title: 'TextInput',
+  component: TextInput,
+} as Meta;
