2cfb0b7e119dbbd27e3c7a6b406fadeb7004b808
Iddan Aaronsohn <mail@aniddan.com>
2020-10-27 16:49:02 +0200

    Fix storybook insallation

---
+++ packages/amplication-design-system/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ["../src/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
--- packages/amplication-design-system/src/TextInput.stories.tsx
+++ packages/amplication-design-system/src/TextInput.stories.tsx
@@ -1,5 +1,3 @@
-
-import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { TextInput } from './TextInput';
 
