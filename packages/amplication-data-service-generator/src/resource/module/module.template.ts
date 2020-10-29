edbe565a49e4d41177f5fa8fad492865bbe94519
Iddan Aaronsohn <mail@aniddan.com>
2020-10-29 20:03:03 +0200

    Add explicit imports to module

--- packages/amplication-data-service-generator/src/resource/module/module.template.ts
+++ packages/amplication-data-service-generator/src/resource/module/module.template.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
+import { MorganModule } from "nest-morgan";
 import { PrismaModule } from "nestjs-prisma";
+import { ACLModule } from "../auth/acl.module";
+import { AuthModule } from "../auth/auth.module";
 
 declare class CONTROLLER {}
 declare class SERVICE {}
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ACLModule, AuthModule, MorganModule, PrismaModule],
   controllers: [CONTROLLER],
   providers: [SERVICE],
   exports: [SERVICE],
