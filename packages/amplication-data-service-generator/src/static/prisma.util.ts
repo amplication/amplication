171642a00708448526b1bccb25bf5c5ec4747550
Iddan Aaronsohn <mail@aniddan.com>
2020-11-03 16:56:27 +0200

    Correct prisma util

--- packages/amplication-data-service-generator/src/static/prisma.util.ts
+++ packages/amplication-data-service-generator/src/static/prisma.util.ts
@@ -1,7 +1,9 @@
-const PRISMA_QUERY_INTERPRETATION_ERROR = "P2016";
-const PRISMA_RECORD_NOT_FOUND = "RecordNotFound";
+export const PRISMA_QUERY_INTERPRETATION_ERROR = "P2016";
+export const PRISMA_RECORD_NOT_FOUND = "RecordNotFound";
 
-export function isRecordNotFoundError(error: Error): boolean {
+export function isRecordNotFoundError(
+  error: Error & { code?: string }
+): boolean {
   return (
     "code" in error &&
     error.code === PRISMA_QUERY_INTERPRETATION_ERROR &&
