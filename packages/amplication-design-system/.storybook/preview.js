e65b6bd29d2a5e2bdbabe60d2ab01edaca8211bd
meeroslava <sapphirenymph@gmail.com>
2020-11-03 15:17:08 +0200

    Update packages/amplication-design-system/.storybook/preview.js
    Co-authored-by: Iddan Aaronsohn <mail@aniddan.com>

--- packages/amplication-design-system/.storybook/preview.js
+++ packages/amplication-design-system/.storybook/preview.js
@@ -1,6 +1,6 @@
 
-import '!style-loader!css-loader!sass-loader!../src/index.scss';
-import '!style-loader!css-loader!sass-loader!../src/style/amplication-font.scss';
+import '../src/index.scss';
+import '../src/style/amplication-font.scss';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
