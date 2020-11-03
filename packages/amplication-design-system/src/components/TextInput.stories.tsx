f8411dc43cd0219118b2243dcddd520e8278f718
meeroslava <sapphirenymph@gmail.com>
2020-11-03 15:17:21 +0200

    Update packages/amplication-design-system/src/components/TextInput.stories.tsx
    Co-authored-by: Iddan Aaronsohn <mail@aniddan.com>

--- packages/amplication-design-system/src/components/TextInput.stories.tsx
+++ packages/amplication-design-system/src/components/TextInput.stories.tsx
@@ -21,7 +21,7 @@ export const Controlled = () => {
       <TextInput
         value={value}
         onChange={(e) => {
-          //@ts-ignore
+          // @ts-ignore
           const { value } = e.target;
           if (value.match(ALPHABET_ONLY)) {
             setValue(value);
