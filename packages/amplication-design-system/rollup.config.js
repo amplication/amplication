1805c76bd2e191b0da99a5fa96ec835fa0b126e4
Iddan Aaronsohn <mail@aniddan.com>
2020-11-08 16:55:51 +0200

    Make rollup run in production unless stated otherwise

--- packages/amplication-design-system/rollup.config.js
+++ packages/amplication-design-system/rollup.config.js
@@ -4,6 +4,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
 import pkg from "./package.json";
 
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
+
 const extensions = [".ts", ".tsx", ".js", ".jsx"];
 
 export default [
