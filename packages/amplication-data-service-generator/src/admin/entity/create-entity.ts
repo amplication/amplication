bd2a6d9cc00679ba3ece5ca292337faf9e681456
Iddan Aaronsohn <mail@aniddan.com>
2020-11-10 15:44:58 +0200

    Extract entity list to template file

--- packages/amplication-data-service-generator/src/admin/entity/create-entity.ts
+++ packages/amplication-data-service-generator/src/admin/entity/create-entity.ts
@@ -1,17 +1,22 @@
+import * as path from "path";
+import { print } from "recast";
+import { builders } from "ast-types";
 import { Entity } from "../../types";
+import { interpolate, removeTSVariableDeclares } from "../../util/ast";
 import { Module, readFile } from "../../util/module";
 
+const entityListTemplate = path.resolve(__dirname, "entity-list.template.tsx");
+
 export async function createEntity(entity: Entity): Promise<Module> {
+  const file = await readFile(entityListTemplate);
+  const listComponentName = `${entity.name}List`;
+  interpolate(file, {
+    ENTITY_LIST: builders.identifier(listComponentName),
+    ENTITY_NAME: builders.stringLiteral(listComponentName),
+  });
+  removeTSVariableDeclares(file);
   return {
-    path: `admin/src/${entity.name}.tsx`,
-    code: `
-    import React from "react";
-    
-    export const ${entity.name} = () => (
-        <>
-            <h1>${entity.name}</h1>
-        </>
-    )
-    `,
+    path: `admin/src/${listComponentName}.tsx`,
+    code: print(file).code,
   };
 }
---
+++ packages/amplication-data-service-generator/src/admin/entity/entity-list.template.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+declare const ENTITY_NAME: string;
+
+export const ENTITY_LIST = () => (
+  <>
+    <h1>{ENTITY_NAME}</h1>
+  </>
+);
