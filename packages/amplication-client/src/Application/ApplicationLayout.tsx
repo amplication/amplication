8499293b96c5efcac9896c9b6898bb8bf0a888a6
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-11-20 13:41:34 +0200

    feat(client):fixed menu panel collapse

--- packages/amplication-client/src/Application/ApplicationLayout.tsx
+++ packages/amplication-client/src/Application/ApplicationLayout.tsx
@@ -16,6 +16,7 @@ import "./ApplicationLayout.scss";
 import * as models from "../models";
 
 import MenuItem from "../Layout/MenuItem";
+import MenuItemWithFixedPanel from "../Layout/MenuItemWithFixedPanel";
 import MainLayout from "../Layout/MainLayout";
 import ApplicationIcon from "./ApplicationIcon";
 import PendingChangesContext, {
@@ -26,6 +27,11 @@ import { track } from "../util/analytics";
 import { SHOW_UI_ELEMENTS } from "../feature-flags";
 import ScreenResolutionMessage from "../Layout/ScreenResolutionMessage";
 import PendingChangesBar from "../VersionControl/PendingChangesBar";
+
+enum enumFixedPanelKeys {
+  None = "None",
+  PendingChanges = "PendingChanges",
+}
 
 export type ApplicationData = {
   app: models.App;
@@ -47,6 +53,21 @@ function ApplicationLayout({ match }: Props) {
   const { application } = match.params;
 
   const [pendingChanges, setPendingChanges] = useState<PendingChangeItem[]>([]);
+
+  const [selectedFixedPanel, setSelectedFixedPanel] = useState<string>(
+    enumFixedPanelKeys.PendingChanges
+  );
+
+  const handleMenuItemWithFixedPanelClicked = useCallback(
+    (panelKey: string) => {
+      if (selectedFixedPanel === panelKey) {
+        setSelectedFixedPanel(enumFixedPanelKeys.None);
+      } else {
+        setSelectedFixedPanel(panelKey);
+      }
+    },
+    [selectedFixedPanel]
+  );
 
   const { data: pendingChangesData, refetch } = useQuery<
     PendingChangeStatusData
@@ -137,13 +158,16 @@ function ApplicationLayout({ match }: Props) {
               {applicationData?.app.name}
             </span>
           </MenuItem>
-          <MenuItem
-            className={`${CLASS_NAME}__pending-changes`}
-            title="Pending Changes"
+          <MenuItemWithFixedPanel
+            tooltip="Pending Changes"
+            icon="pending_changes"
+            isOpen={selectedFixedPanel === enumFixedPanelKeys.PendingChanges}
+            panelKey={enumFixedPanelKeys.PendingChanges}
+            onClick={handleMenuItemWithFixedPanelClicked}
           >
             <PendingChangesBar applicationId={application} />
-          </MenuItem>
-
+          </MenuItemWithFixedPanel>
+          <div className={`${CLASS_NAME}__menu-group`} />
           <MenuItem
             title="Entities"
             to={`/${application}/entities`}
--- packages/amplication-client/src/VersionControl/PendingChangesBar.tsx
+++ packages/amplication-client/src/VersionControl/PendingChangesBar.tsx
@@ -1,47 +1,17 @@
-import React, { useCallback, useContext, useState } from "react";
-import { Icon } from "@rmwc/icon";
+import React from "react";
 import "./PendingChangesBar.scss";
-import PendingChangesContext from "../VersionControl/PendingChangesContext";
-import { MenuFixedPanel } from "../util/teleporter";
-import { Button, EnumButtonStyle } from "../Components/Button";
 import PendingChanges from "./PendingChanges";
 
 const CLASS_NAME = "pending-changes-bar";
-const ICON_SIZE = "xlarge";
 
 type Props = {
   applicationId: string;
 };
 
 const PendingChangesBar = ({ applicationId }: Props) => {
-  const pendingChangesContext = useContext(PendingChangesContext);
-
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleClick = useCallback(() => {
-    setIsOpen((isOpen) => {
-      return !isOpen;
-    });
-  }, [setIsOpen]);
-
   return (
     <div className={CLASS_NAME}>
-      <Button buttonStyle={EnumButtonStyle.Clear} onClick={handleClick}>
-        <Icon
-          icon={{
-            icon: "pending_changes",
-            size: ICON_SIZE,
-          }}
-        />
-      </Button>
-      <span className={`${CLASS_NAME}__counter`}>
-        {pendingChangesContext.pendingChanges.length.toString()}
-      </span>
-      {isOpen && (
-        <MenuFixedPanel.Source>
-          <PendingChanges applicationId={applicationId} />
-        </MenuFixedPanel.Source>
-      )}
+      <PendingChanges applicationId={applicationId} />
     </div>
   );
 };
