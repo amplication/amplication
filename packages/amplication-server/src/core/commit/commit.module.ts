d5ff77f7e63780c3259862b87a138bbb75ae9e82
Gal Cegla <galcegla@gmail.com>
2020-11-22 15:25:19 +0200

    Add Commit Module

---
+++ packages/amplication-server/src/core/commit/commit.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'nestjs-prisma';
+import { CommitResolver } from '../app/commit.resolver';
+import { CommitService } from './commit.service';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [CommitService, CommitResolver],
+  exports: [CommitService, CommitResolver]
+})
+export class CommitModule {}
