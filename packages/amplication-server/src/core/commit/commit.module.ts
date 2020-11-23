a6ce246e047e8aba3ae34855c0b079dd381fa3a1
Gal Cegla <galcegla@gmail.com>
2020-11-23 15:17:41 +0200

    Add PermissionModule to CommitModule

--- packages/amplication-server/src/core/commit/commit.module.ts
+++ packages/amplication-server/src/core/commit/commit.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'nestjs-prisma';
+import { PermissionsModule } from '../permissions/permissions.module';
 import { UserModule } from '../user/user.module';
 import { CommitResolver } from './commit.resolver';
 import { CommitService } from './commit.service';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, PermissionsModule],
   providers: [CommitService, CommitResolver],
   exports: [CommitService, CommitResolver]
 })
