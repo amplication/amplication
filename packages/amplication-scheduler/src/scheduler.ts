ba13e8d9ed57d3db55a8e773bb308dbf0e8e05da
Iddan Aaronsohn <mail@aniddan.com>
2020-11-23 12:44:40 +0200

    Add scheduler package

---
+++ packages/amplication-scheduler/src/scheduler.ts
@@ -0,0 +1,22 @@
+import { CronJob } from "cron";
+import fetch from "node-fetch";
+import { Config } from "./config";
+
+export function createJob(config: Config): CronJob {
+  return new CronJob(
+    config.schedule,
+    () =>
+      fetch(config.request.url, config.request)
+        .then((res) => {
+          const { method = "GET" } = config.request;
+          const log = `${method} ${config.request.url} - ${res.status}`;
+          console.info(log);
+        })
+        .catch((error) => {
+          console.error(error.message);
+        }),
+    null,
+    true,
+    config.timezone
+  );
+}
