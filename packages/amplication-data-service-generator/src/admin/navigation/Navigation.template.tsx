8e46beade5434b812d30c0c020384b7b12c8bea2
Iddan Aaronsohn <mail@aniddan.com>
2020-12-01 19:43:18 +0200

    Add signout

--- packages/amplication-data-service-generator/src/admin/navigation/Navigation.template.tsx
+++ packages/amplication-data-service-generator/src/admin/navigation/Navigation.template.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+// @ts-ignore
+import { removeCredentials } from "./auth";
 
 declare const ITEMS: React.ReactElement[];
 
-const Navigation = (): React.ReactElement => <ul>{ITEMS}</ul>;
+const Navigation = (): React.ReactElement => {
+  const history = useHistory();
+  const signOut = useCallback(() => {
+    removeCredentials();
+    history.push("/login");
+  }, [history]);
+  return (
+    <ul>
+      {ITEMS}
+      <li>
+        <button onClick={signOut}>Sign Out</button>
+      </li>
+    </ul>
+  );
+};
 
 export default Navigation;
 
--- packages/amplication-data-service-generator/src/admin/static/src/auth.ts
+++ packages/amplication-data-service-generator/src/admin/static/src/auth.ts
@@ -3,14 +3,23 @@ export type Credentials = {
   password: string;
 };
 
+const CREDENTIALS_LOCAL_STORAGE_ITEM = "credentials";
+
 export function setCredentials(credentials: Credentials) {
-  localStorage.setItem("credentials", JSON.stringify(credentials));
+  localStorage.setItem(
+    CREDENTIALS_LOCAL_STORAGE_ITEM,
+    JSON.stringify(credentials)
+  );
 }
 
 export function getCredentials(): Credentials | null {
-  const raw = localStorage.getItem("credentials");
+  const raw = localStorage.getItem(CREDENTIALS_LOCAL_STORAGE_ITEM);
   if (raw === null) {
     return null;
   }
   return JSON.parse(raw);
+}
+
+export function removeCredentials(): void {
+  localStorage.removeItem(CREDENTIALS_LOCAL_STORAGE_ITEM);
 }
