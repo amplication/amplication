cc9478715414d94328836bc7491571446ad7a2b6
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-12-02 14:31:35 +0200

    Add SelectMenu

--- packages/amplication-design-system/src/index.ts
+++ packages/amplication-design-system/src/index.ts
@@ -10,4 +10,12 @@ export {
   default as SearchField,
   Props as SearchFieldProps,
 } from "./components/SearchField/SearchField";
+export {
+  SelectMenu,
+  SelectMenuModal,
+  SelectMenuItem,
+  SelectMenuList,
+  Props as SelectMenuProps,
+} from "./components/SelectMenu/SelectMenu";
+
 export { default as Provider } from "./components/Provider";
