1b1adf8b0ce5fe947cf6e9bccf34be83da3f0d5e
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-12-02 13:36:50 +0200

    feat(design-system): add SearchField

--- packages/amplication-design-system/src/index.ts
+++ packages/amplication-design-system/src/index.ts
@@ -6,4 +6,8 @@ export {
 } from "./components/Button";
 export { default as CircleIcon } from "./components/CircleIcon";
 export { TextInput, Props as TextInputProps } from "./components/TextInput";
+export {
+  default as SearchField,
+  Props as SearchFieldProps,
+} from "./components/SearchField";
 export { default as Provider } from "./components/Provider";
