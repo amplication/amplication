da0f2f79d1c04901b9610f9ed2e17b56a9756053
Iddan Aaronsohn <mail@aniddan.com>
2020-12-03 12:41:04 +0200

    Move dtos out dtos directory

--- packages/amplication-data-service-generator/src/admin/create-dto-name-to-path.ts
+++ packages/amplication-data-service-generator/src/admin/create-dto-name-to-path.ts
@@ -1,15 +1,13 @@
 import { camelCase } from "camel-case";
 import { DTOs } from "../resource/create-dtos";
-import { SRC_DIRECTORY } from "./constants";
-
-const API_DIRECTORY = `${SRC_DIRECTORY}/api`;
+import { API_DIRECTORY } from "./constants";
 
 export function createDTONameToPath(dtos: DTOs): Record<string, string> {
   return Object.fromEntries(
     Object.entries(dtos).flatMap(([entityName, entityDTOs]) =>
       Object.values(entityDTOs).map((dto) => [
         dto.id.name,
-        `${API_DIRECTORY}/${camelCase(entityName)}/dtos/${dto.id.name}.ts`,
+        `${API_DIRECTORY}/${camelCase(entityName)}/${dto.id.name}.ts`,
       ])
     )
   );
