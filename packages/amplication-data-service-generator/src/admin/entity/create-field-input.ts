0c67b1ea10511fd8ebd63ad846b285e3be9e1c01
Iddan Aaronsohn <mail@aniddan.com>
2020-12-03 13:29:24 +0200

    Move mapping to the bottom of the file

--- packages/amplication-data-service-generator/src/admin/entity/create-field-input.ts
+++ packages/amplication-data-service-generator/src/admin/entity/create-field-input.ts
@@ -6,6 +6,33 @@ const DIV_ID = builders.jsxIdentifier("div");
 const LABEL_ID = builders.jsxIdentifier("label");
 const SINGLE_SPACE_STRING_LITERAL = builders.stringLiteral(" ");
 
+/**
+ * Creates an input element to be placed inside a Formik form for editing the given entity field
+ * @param field the entity field to create input for
+ * @returns the input element AST representation
+ */
+export function createFieldInput(field: EntityField): namedTypes.JSXElement {
+  const createDataTypeFieldInput = DATA_TYPE_TO_FIELD_INPUT[field.dataType];
+  if (!createDataTypeFieldInput) {
+    throw new Error(
+      `Can not display field ${field.name} with data type ${field.dataType}`
+    );
+  }
+  return builders.jsxElement(
+    builders.jsxOpeningElement(DIV_ID),
+    builders.jsxClosingElement(DIV_ID),
+    [
+      builders.jsxElement(
+        builders.jsxOpeningElement(LABEL_ID),
+        builders.jsxClosingElement(LABEL_ID),
+        [builders.jsxText(field.displayName)]
+      ),
+      builders.jsxExpressionContainer(SINGLE_SPACE_STRING_LITERAL),
+      createDataTypeFieldInput(field),
+    ]
+  );
+}
+
 const jsxElement = typedExpression(namedTypes.JSXElement);
 
 const DATA_TYPE_TO_FIELD_INPUT: {
@@ -70,30 +97,3 @@ const DATA_TYPE_TO_FIELD_INPUT: {
       field.name
     )} textarea />`,
 };
-
-/**
- * Creates an input element to be placed inside a Formik form for editing the given entity field
- * @param field the entity field to create input for
- * @returns the input element AST representation
- */
-export function createFieldInput(field: EntityField): namedTypes.JSXElement {
-  const createDataTypeFieldInput = DATA_TYPE_TO_FIELD_INPUT[field.dataType];
-  if (!createDataTypeFieldInput) {
-    throw new Error(
-      `Can not display field ${field.name} with data type ${field.dataType}`
-    );
-  }
-  return builders.jsxElement(
-    builders.jsxOpeningElement(DIV_ID),
-    builders.jsxClosingElement(DIV_ID),
-    [
-      builders.jsxElement(
-        builders.jsxOpeningElement(LABEL_ID),
-        builders.jsxClosingElement(LABEL_ID),
-        [builders.jsxText(field.displayName)]
-      ),
-      builders.jsxExpressionContainer(SINGLE_SPACE_STRING_LITERAL),
-      createDataTypeFieldInput(field),
-    ]
-  );
-}
