8b41680bb5b5ab0dd206e2795875ff2693bc447c
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-12-05 22:01:33 +0200

    feat(design-system):MainLayout and Page

--- packages/amplication-design-system/src/components/PanelCollapsible/PanelCollapsible.stories.tsx
+++ packages/amplication-design-system/src/components/PanelCollapsible/PanelCollapsible.stories.tsx
@@ -11,26 +11,40 @@ export default {
 
 export const Default = (props: any) => {
   return (
-    <Provider>
-      <PanelCollapsible headerContent="Header">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </PanelCollapsible>
-    </Provider>
+    <PanelCollapsible headerContent="Header">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </PanelCollapsible>
   );
 };
 
 export const InitiallyOpen = (props: any) => {
   return (
-    <Provider>
-      <PanelCollapsible initiallyOpen headerContent="Header">
+    <PanelCollapsible initiallyOpen headerContent="Header">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </PanelCollapsible>
+  );
+};
+
+export const manualCollapseDisabled = (props: any) => {
+  return (
+    <>
+      <PanelCollapsible manualCollapseDisabled headerContent="Header">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -41,41 +55,21 @@ export const InitiallyOpen = (props: any) => {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </PanelCollapsible>
-    </Provider>
-  );
-};
-
-export const manualCollapseDisabled = (props: any) => {
-  return (
-    <Provider>
-      <>
-        <PanelCollapsible manualCollapseDisabled headerContent="Header">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </PanelCollapsible>
-        <PanelCollapsible
-          initiallyOpen
-          manualCollapseDisabled
-          headerContent="Header"
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </PanelCollapsible>
-      </>
-    </Provider>
+      <PanelCollapsible
+        initiallyOpen
+        manualCollapseDisabled
+        headerContent="Header"
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </PanelCollapsible>
+    </>
   );
 };
