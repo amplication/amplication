532f1fc74d6b1f39b1a632c9d4402664a9ecb7b5
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-12-06 12:43:15 +0200

    feat(dsg): Admin UI List View

--- packages/amplication-data-service-generator/src/admin/entity/create-field-value.ts
+++ packages/amplication-data-service-generator/src/admin/entity/create-field-value.ts
@@ -2,6 +2,9 @@ import { namedTypes } from "ast-types";
 import { EntityField } from "../../types";
 import { jsxFragment } from "../util";
 
+const FIELD_NAME_CREATED_AT = "createdAt";
+const FIELD_NAME_UPDATED_AT = "updatedAt";
+
 /**
  * Creates a node for displaying given entity field value
  * @param field the entity field to create value view for
@@ -11,5 +14,11 @@ export function createFieldValue(
   field: EntityField,
   dataId: namedTypes.Identifier
 ): namedTypes.JSXFragment {
+  if (
+    field.name === FIELD_NAME_CREATED_AT ||
+    field.name === FIELD_NAME_UPDATED_AT
+  ) {
+    return jsxFragment`<><TimeSince time={${dataId}.${field.name}} /></>`;
+  }
   return jsxFragment`<>{JSON.stringify(${dataId}.${field.name})}</>`;
 }
