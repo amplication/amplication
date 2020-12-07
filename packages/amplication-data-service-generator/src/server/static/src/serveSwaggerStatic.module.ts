97c2ffa6dd96c6042c89dc325d36a2dbb394cd20
Iddan Aaronsohn <mail@aniddan.com>
2020-12-07 23:58:31 +0200

    Allow providing static files path to generated server

--- packages/amplication-data-service-generator/src/server/static/src/serveSwaggerStatic.module.ts
+++
@@ -1,8 +0,0 @@
-import { ServeStaticModule } from "@nestjs/serve-static";
-import * as path from "path";
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export const ServeSwaggerStaticModule = ServeStaticModule.forRoot({
-  serveRoot: "/swagger",
-  rootPath: path.join(__dirname, "swagger"),
-});
