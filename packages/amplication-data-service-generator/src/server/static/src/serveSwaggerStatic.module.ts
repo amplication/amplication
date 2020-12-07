6a63334ae45cbb0d56f4f4a773ff360482438464
Iddan Aaronsohn <mail@aniddan.com>
2020-12-07 20:21:18 +0200

    Fix serve swagger static module

--- packages/amplication-data-service-generator/src/server/static/src/serveSwaggerStatic.module.ts
+++ packages/amplication-data-service-generator/src/server/static/src/serveSwaggerStatic.module.ts
@@ -3,8 +3,6 @@ import * as path from "path";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const ServeSwaggerStaticModule = ServeStaticModule.forRoot({
+  serveRoot: "/swagger",
   rootPath: path.join(__dirname, "swagger"),
-  serveStaticOptions: {
-    index: false,
-  },
 });
