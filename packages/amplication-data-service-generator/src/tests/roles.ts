1228dc935d7c1aa0e41cd09da63dfc780bcc97f2
Yuval Hazaz <hazaz.yuval@gmail.com>
2020-12-09 11:17:53 +0200

    add test data

--- packages/amplication-data-service-generator/src/tests/roles.ts
+++ packages/amplication-data-service-generator/src/tests/roles.ts
@@ -6,6 +6,20 @@ const roles = [
     displayName: "User",
     name: "user",
   },
+  {
+    id: "4da76cb1-46d0-4fd0-b81b-930c0ae5ec41",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    displayName: "Admin",
+    name: "admin",
+  },
+  {
+    id: "4da76cb1-46d0-4fd0-b81b-930c0ae5ec42",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    displayName: "Area Manager",
+    name: "areaManager",
+  },
 ];
 
 export default roles;
