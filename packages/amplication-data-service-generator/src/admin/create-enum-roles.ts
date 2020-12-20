80445ed507d42e08ebbc975fc553bf786094ed5d
Iddan Aaronsohn <mail@aniddan.com>
2020-12-20 14:14:53 +0200

    Create enum roles spec

--- packages/amplication-data-service-generator/src/admin/create-enum-roles.ts
+++ packages/amplication-data-service-generator/src/admin/create-enum-roles.ts
@@ -1,15 +1,26 @@
-import { builders } from "ast-types";
+import { builders, namedTypes } from "ast-types";
 import { pascalCase } from "pascal-case";
 import { print } from "recast";
 import { createDTOFile } from "../server/resource/dto/create-dto-module";
 import { Role, Module } from "../types";
 import { SRC_DIRECTORY } from "./constants";
 
-const ENUM_ROLES_ID = builders.identifier("EnumRoles");
-const MODULE_PATH = `${SRC_DIRECTORY}/user/${ENUM_ROLES_ID.name}.ts`;
+export const ENUM_ROLES_ID = builders.identifier("EnumRoles");
+export const MODULE_PATH = `${SRC_DIRECTORY}/user/${ENUM_ROLES_ID.name}.ts`;
 
 export function createEnumRolesModule(roles: Role[]): Module {
-  const enumDeclaration = builders.tsEnumDeclaration(
+  const enumDeclaration = createRolesEnumDeclaration(roles);
+  const file = createDTOFile(enumDeclaration, MODULE_PATH, {});
+  return {
+    path: MODULE_PATH,
+    code: print(file).code,
+  };
+}
+
+export function createRolesEnumDeclaration(
+  roles: Role[]
+): namedTypes.TSEnumDeclaration {
+  return builders.tsEnumDeclaration(
     ENUM_ROLES_ID,
     roles.map((role) =>
       builders.tsEnumMember(
@@ -18,9 +29,4 @@ export function createEnumRolesModule(roles: Role[]): Module {
       )
     )
   );
-  const file = createDTOFile(enumDeclaration, MODULE_PATH, {});
-  return {
-    path: MODULE_PATH,
-    code: print(file).code,
-  };
 }
