4f43c6bffad08e754204e2581a1bc3c7804ac7cb
Iddan Aaronsohn <mail@aniddan.com>
2020-12-21 09:58:19 +0200

    Implement to many field resolvers

--- packages/amplication-data-service-generator/src/server/resource/resolver/create-resolver.ts
+++ packages/amplication-data-service-generator/src/server/resource/resolver/create-resolver.ts
@@ -1,7 +1,7 @@
 import { print } from "recast";
 import { builders, namedTypes } from "ast-types";
 import { camelCase } from "camel-case";
-import { Entity, Module } from "../../../types";
+import { Entity, EntityLookupField, Module } from "../../../types";
 import { readFile, relativeImportPath } from "../../../util/module";
 import {
   interpolate,
@@ -10,23 +10,23 @@ import {
   removeTSVariableDeclares,
   removeTSInterfaceDeclares,
   removeTSClassDeclares,
-  // getClassDeclarationById,
-  // isConstructor,
+  getClassDeclarationById,
   removeESLintComments,
   importContainedIdentifiers,
   removeImportsTSIgnoreComments,
+  NamedClassDeclaration,
+  getMethods,
 } from "../../../util/ast";
-// import { isOneToOneRelationField, isRelationField } from "../../../util/field";
+import { isToManyRelationField } from "../../../util/field";
 import { SRC_DIRECTORY } from "../../constants";
 import { DTOs, getDTONameToPath } from "../create-dtos";
 import { getImportableDTOs } from "../dto/create-dto-module";
 import { createServiceId } from "../service/create-service";
 import { createSelect } from "../controller/create-select";
 
-// const TO_MANY_MIXIN_ID = builders.identifier("Mixin");
-
+const TO_MANY_MIXIN_ID = builders.identifier("Mixin");
 const templatePath = require.resolve("./resolver.template.ts");
-// const toManyTemplatePath = require.resolve("./to-many.template.ts");
+const toManyTemplatePath = require.resolve("./to-many.template.ts");
 
 export async function createResolverModule(
   entityName: string,
@@ -65,55 +65,25 @@ export async function createResolverModule(
     FIND_ONE_ARGS: dtos[entity.name].findOneArgs.id,
   });
 
-  // const classDeclaration = getClassDeclarationById(file, id);
+  const classDeclaration = getClassDeclarationById(file, id);
+  const toManyRelationFields = entity.fields.filter(isToManyRelationField);
+  const toManyRelationMethods = (
+    await Promise.all(
+      toManyRelationFields.map((field) =>
+        createToManyRelationMethods(
+          field,
+          entityDTO,
+          entityType,
+          dtos,
+          entityIdToName,
+          entitiesByName,
+          serviceId
+        )
+      )
+    )
+  ).flat();
 
-  // const toManyRelationships: EntityLookupField[] = entity.fields.filter(
-  //   (field): field is EntityLookupField =>
-  //     isRelationField(field) && !isOneToOneRelationField(field)
-  // );
-
-  // for (const field of toManyRelationships) {
-  //   const toManyFile = await readFile(toManyTemplatePath);
-  //   const relatedEntityId = field.properties.relatedEntityId;
-  //   const relatedEntityName = entityIdToName[relatedEntityId];
-  //   const relatedEntity = entitiesByName[relatedEntityName];
-  //   const relatedEntityDTO = dtos[relatedEntityName].entity;
-  //   const relatedEntityWhereUniqueInput =
-  //     dtos[relatedEntityName].whereUniqueInput;
-  //   const relatedEntityWhereInput = dtos[relatedEntityName].whereInput;
-  //   interpolate(toManyFile, {
-  //     RELATED_ENTITY_WHERE_UNIQUE_INPUT: relatedEntityWhereUniqueInput.id,
-  //     RELATED_ENTITY_WHERE_INPUT: relatedEntityWhereInput.id,
-  //     RELATED_ENTITY: builders.identifier(relatedEntityName),
-  //     RELATED_ENTITY_NAME: builders.stringLiteral(relatedEntityName),
-  //     WHERE_UNIQUE_INPUT: entityDTOs.whereUniqueInput.id,
-  //     SERVICE: serviceId,
-  //     ENTITY_NAME: builders.stringLiteral(entityType),
-  //     PROPERTY: builders.identifier(field.name),
-  //     FIND_MANY: builders.identifier(camelCase(`findMany ${field.name}`)),
-  //     FIND_MANY_PATH: builders.stringLiteral(`/:id/${field.name}`),
-  //     CREATE: builders.identifier(camelCase(`create ${field.name}`)),
-  //     CREATE_PATH: builders.stringLiteral(`/:id/${field.name}`),
-  //     DELETE: builders.identifier(camelCase(`delete ${field.name}`)),
-  //     DELETE_PATH: builders.stringLiteral(`/:id/${field.name}`),
-  //     UPDATE: builders.identifier(camelCase(`update ${field.name}`)),
-  //     UPDATE_PATH: builders.stringLiteral(`/:id/${field.name}`),
-  //     SELECT: createSelect(relatedEntityDTO, relatedEntity),
-  //   });
-  //   const mixinClassDeclaration = findClassDeclarationById(
-  //     toManyFile,
-  //     TO_MANY_MIXIN_ID
-  //   );
-  //   if (!mixinClassDeclaration) {
-  //     throw new Error(`Could not find ${TO_MANY_MIXIN_ID.name}`);
-  //   }
-  //   classDeclaration.body.body.push(
-  //     ...mixinClassDeclaration.body.body.filter(
-  //       (member) =>
-  //         namedTypes.ClassMethod.check(member) && !isConstructor(member)
-  //     )
-  //   );
-  // }
+  classDeclaration.body.body.push(...toManyRelationMethods);
 
   const serviceImport = importNames(
     [serviceId],
@@ -141,4 +111,34 @@ export async function createResolverModule(
 
 export function createResolverId(entityType: string): namedTypes.Identifier {
   return builders.identifier(`${entityType}Resolver`);
+}
+
+async function createToManyRelationMethods(
+  field: EntityLookupField,
+  entityDTO: NamedClassDeclaration,
+  entityType: string,
+  dtos: DTOs,
+  entityIdToName: Record<string, string>,
+  entitiesByName: Record<string, Entity>,
+  serviceId: namedTypes.Identifier
+) {
+  const toManyFile = await readFile(toManyTemplatePath);
+  const { relatedEntityId } = field.properties;
+  const relatedEntityName = entityIdToName[relatedEntityId];
+  const relatedEntity = entitiesByName[relatedEntityName];
+  const relatedEntityDTOs = dtos[relatedEntityName];
+
+  interpolate(toManyFile, {
+    SERVICE: serviceId,
+    ENTITY: entityDTO.id,
+    ENTITY_NAME: builders.stringLiteral(entityType),
+    RELATED_ENTITY: builders.identifier(relatedEntityName),
+    RELATED_ENTITY_NAME: builders.stringLiteral(relatedEntityName),
+    PROPERTY: builders.identifier(field.name),
+    FIND_MANY: builders.identifier(camelCase(relatedEntity.pluralDisplayName)),
+    ARGS: relatedEntityDTOs.findManyArgs.id,
+    SELECT: createSelect(relatedEntityDTOs.entity, relatedEntity),
+  });
+
+  return getMethods(getClassDeclarationById(toManyFile, TO_MANY_MIXIN_ID));
 }
