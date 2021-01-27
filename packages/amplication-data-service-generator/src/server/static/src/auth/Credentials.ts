9578d259dae3c2c5261ea4ffc364a6aa30aa0eca
Petar Vujović <petarvujovic98@gmail.com>
2021-01-27 17:51:57 +0100

    Add missing imports

--- packages/amplication-data-service-generator/src/server/static/src/auth/Credentials.ts
+++ packages/amplication-data-service-generator/src/server/static/src/auth/Credentials.ts
@@ -1,5 +1,6 @@
-import { InputType } from "@nestjs/graphql";
-import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { InputType, Field } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
 
 @InputType()
 export class Credentials {
