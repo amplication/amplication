fa1cf3bc26301e97a5277890121df887fbd6f724
Yuval Hazaz <hazaz.yuval@gmail.com>
2021-01-31 14:26:29 +0200

    fix(client):theme provider useLocalStorage

--- packages/amplication-client/src/Layout/ThemeProvider.tsx
+++ packages/amplication-client/src/Layout/ThemeProvider.tsx
@@ -1,21 +1,23 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useCallback, useMemo, useEffect } from "react";
 import ThemeContext from "./ThemeContext";
+import useLocalStorage from "react-use-localstorage";
 
 type Props = {
   children: React.ReactNode;
 };
+const LOCAL_STORAGE_KEY = "amplicationTheme";
 
 function ThemeProvider({ children }: Props) {
-  const [currentTheme, setCurrentTheme] = useState<string>("");
+  const [currentTheme, setCurrentTheme] = useLocalStorage(
+    LOCAL_STORAGE_KEY,
+    "dark"
+  );
 
   const setTheme = useCallback(
     (theme: string) => {
       const currentThemeClass = `amp-theme-${currentTheme}`;
       document.body.classList.remove(currentThemeClass);
       setCurrentTheme(theme);
-      const nextThemeClass = `amp-theme-${theme}`;
-
-      document.body.classList.add(nextThemeClass);
     },
     [setCurrentTheme, currentTheme]
   );
@@ -27,6 +29,11 @@ function ThemeProvider({ children }: Props) {
     }),
     [currentTheme, setTheme]
   );
+
+  useEffect(() => {
+    const nextThemeClass = `amp-theme-${currentTheme}`;
+    document.body.classList.add(nextThemeClass);
+  }, [currentTheme]);
 
   return (
     <ThemeContext.Provider value={ThemeContextValue}>
