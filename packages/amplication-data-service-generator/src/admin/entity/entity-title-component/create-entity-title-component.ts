0efeef35e7d8adbca5f852b26ee595acd26faa49
Yuval Hazaz <hazaz.yuval@gmail.com>
2021-03-30 09:50:35 +0300

    remove unused imports

--- packages/amplication-data-service-generator/src/admin/entity/entity-title-component/create-entity-title-component.ts
+++ packages/amplication-data-service-generator/src/admin/entity/entity-title-component/create-entity-title-component.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import { builders } from "ast-types";
 import { Entity, EnumDataType } from "../../../types";
-import { addImports, importNames, interpolate } from "../../../util/ast";
+import { addImports, interpolate } from "../../../util/ast";
 import { readFile, relativeImportPath } from "../../../util/module";
 import { DTOs } from "../../../server/resource/create-dtos";
 import { EntityComponent } from "../../types";
