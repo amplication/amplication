cd8c7c235c65dc1a115ed801d9bcbd91a54873c4
yuval-hazaz <hazaz.yuval@gmail.com>
2021-05-09 15:19:41 +0300

    test(dsg): test where input with non-searchable fields

--- packages/amplication-data-service-generator/src/server/resource/dto/create-where-input.spec.ts
+++ packages/amplication-data-service-generator/src/server/resource/dto/create-where-input.spec.ts
@@ -1,7 +1,10 @@
 import { builders } from "ast-types";
 import { print } from "recast";
 import { Entity } from "../../../types";
-import { EXAMPLE_ID_FIELD } from "../util/test-data";
+import {
+  EXAMPLE_ID_FIELD,
+  EXAMPLE_NON_SEARCHABLE_SINGLE_LINE_TEXT_FIELD,
+} from "../util/test-data";
 import { createWhereInput, createWhereInputID } from "./create-where-input";
 import { createInput } from "./create-input";
 
@@ -16,9 +19,34 @@ const EXAMPLE_ENTITY: Entity = {
   permissions: [],
 };
 
+const EXAMPLE_ENTITY_WITH_NON_SEARCHABLE_FIELD: Entity = {
+  id: EXAMPLE_ENTITY_ID,
+  name: EXAMPLE_ENTITY_NAME,
+  displayName: "Example Entity",
+  pluralDisplayName: "Example Entities",
+  fields: [EXAMPLE_ID_FIELD, EXAMPLE_NON_SEARCHABLE_SINGLE_LINE_TEXT_FIELD],
+  permissions: [],
+};
+
 describe("createWhereInput", () => {
   test("creates input", () => {
     expect(print(createWhereInput(EXAMPLE_ENTITY)).code).toEqual(
+      print(
+        createInput(
+          createWhereInputID(EXAMPLE_ENTITY_NAME),
+          [EXAMPLE_ID_FIELD],
+          EXAMPLE_ENTITY,
+          true,
+          true
+        )
+      ).code
+    );
+  });
+
+  test("creates input only for searchable fields", () => {
+    expect(
+      print(createWhereInput(EXAMPLE_ENTITY_WITH_NON_SEARCHABLE_FIELD)).code
+    ).toEqual(
       print(
         createInput(
           createWhereInputID(EXAMPLE_ENTITY_NAME),
