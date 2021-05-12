b92a7a49d378010ed4d7cf64904104d200c04640
yuval-hazaz <hazaz.yuval@gmail.com>
2021-05-12 12:23:30 +0300

    add static components

---
+++ packages/amplication-data-service-generator/src/admin/static/src/Components/Pagination.tsx
@@ -0,0 +1,9 @@
+import { Pagination as RAPagination, PaginationProps } from "react-admin";
+
+const PAGINATION_OPTIONS = [10, 25, 50, 100, 200];
+
+const Pagination = (props: PaginationProps) => (
+  <RAPagination rowsPerPageOptions={PAGINATION_OPTIONS} {...props} />
+);
+
+export default Pagination;
