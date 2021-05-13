8f571bb24a8cfd74b7176199d59bda8d718c019f
yuval-hazaz <hazaz.yuval@gmail.com>
2021-05-13 22:32:53 +0300

    feat(dsg): add pagination component

--- packages/amplication-data-service-generator/src/admin/entity/entity-list-component/entity-list-component.template.tsx
+++ packages/amplication-data-service-generator/src/admin/entity/entity-list-component/entity-list-component.template.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { List, Datagrid, ListProps } from "react-admin";
+//@ts-ignore
+import Pagination from "../Components/Pagination";
 
 declare const ENTITY_PLURAL_DISPLAY_NAME: string;
 declare const CELLS: React.ReactElement[];
@@ -11,6 +13,7 @@ export const ENTITY_LIST = (props: ListProps): React.ReactElement => {
       bulkActionButtons={false}
       title={ENTITY_PLURAL_DISPLAY_NAME}
       perPage={50}
+      pagination={<Pagination />}
     >
       <Datagrid rowClick="show">{CELLS}</Datagrid>
     </List>
