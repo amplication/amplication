997501a9657388ac6ccc8c527fb18069a9be826a
yuval-hazaz <hazaz.yuval@gmail.com>
2021-05-17 10:26:42 +0300

    style(client): create from excel sidebar style

--- packages/amplication-client/src/Application/CreateAppFromExcelForm.tsx
+++ packages/amplication-client/src/Application/CreateAppFromExcelForm.tsx
@@ -1,3 +1,5 @@
+import { Panel } from "@amplication/design-system";
+import { Icon } from "@rmwc/icon";
 import { Formik } from "formik";
 import React from "react";
 import { Button, EnumButtonStyle } from "../Components/Button";
@@ -30,40 +32,30 @@ export const CreateAppFromExcelForm = ({
       render={({ values, handleSubmit }) => (
         <Form className={`${CLASS_NAME}__layout__body`}>
           <div className={`${CLASS_NAME}__layout__body__side`}>
+            <h2>App Details</h2>
             <h3>{fileName}</h3>
-            <div className={`${CLASS_NAME}__notice`}>
+            <DisplayNameField
+              name="app.name"
+              label="Give your app a descriptive name"
+              required
+            />
+            <Panel className={`${CLASS_NAME}__notice`}>
+              <div className={`${CLASS_NAME}__notice__title`}>
+                <Icon icon={{ size: "xsmall", icon: "info_circle" }} />
+                <span>Good to know</span>
+              </div>
               <ul className={`${CLASS_NAME}__layout__body__side__message`}>
-                <li>
-                  You can change the name and the data type of your fields.
-                </li>
-                <li>
-                  You can create additional entities and move fields between
-                  entities to normalize your data model.
-                </li>
                 <li>
                   All relations are created as one-to-many by default. You can
                   change that later if needed.
                 </li>
-                <li>
-                  Even after the app is created, you can always update your data
-                  models.
-                </li>
+
                 <li>
                   Once your app is ready, you can simply download the generated
                   code or push it directly to a GitHub repository.
                 </li>
-                <li>
-                  Give your app a descriptive name and click on "Create App"
-                  below.
-                </li>
               </ul>
-            </div>
-
-            <DisplayNameField
-              name="app.name"
-              label="Application Name"
-              required
-            />
+            </Panel>
 
             <Button
               buttonStyle={EnumButtonStyle.Primary}
