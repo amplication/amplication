cd1bc8bdd3179a39e411a5734752d65a6e51df82
yuval-hazaz <hazaz.yuval@gmail.com>
2021-07-14 15:21:06 +0300

    feat(client): cancel subscription

--- packages/amplication-client/src/Workspaces/Subscription.tsx
+++ packages/amplication-client/src/Workspaces/Subscription.tsx
@@ -2,10 +2,12 @@ import "@rmwc/snackbar/styles";
 import React, { useMemo, useCallback, useEffect, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { CircularProgress } from "@rmwc/circular-progress";
-
+import { useTracking } from "../util/analytics";
 import "./Subscription.scss";
 import * as models from "../models";
 import PlanList from "../Plans/PlanList";
+import { Button, EnumButtonStyle } from "../Components/Button";
+import { cancelSubscription as cancelPaddleSubscription } from "../util/paddle";
 
 const CLASS_NAME = "subscription";
 
@@ -16,27 +18,19 @@ type TSubscriptionsData = {
 const POLL_INTERVAL = 2000;
 
 function Subscription() {
-  const [purchaseCompleted, setPurchaseCompleted] = useState<Boolean>(false);
+  const [transactionCompleted, setTransactionCompleted] = useState<Boolean>(
+    false
+  );
+  const { trackEvent } = useTracking();
 
   const { data: subscriptionsData, startPolling, stopPolling } = useQuery<
     TSubscriptionsData
   >(GET_ACTIVE_SUBSCRIPTIONS);
 
-  const onPurchaseSuccess = useCallback(() => {
-    setPurchaseCompleted(true);
+  const onTransactionSuccess = useCallback(() => {
+    setTransactionCompleted(true);
     startPolling(POLL_INTERVAL);
   }, [startPolling]);
-
-  //cleanup polling
-  useEffect(() => {
-    if (subscriptionsData && subscriptionsData.subscriptions.length > 0) {
-      stopPolling();
-      setPurchaseCompleted(false);
-    }
-    return () => {
-      stopPolling();
-    };
-  }, [stopPolling, subscriptionsData]);
 
   const currentSubscription = useMemo(() => {
     if (!subscriptionsData || subscriptionsData.subscriptions.length === 0)
@@ -44,12 +38,39 @@ function Subscription() {
     return subscriptionsData.subscriptions[0];
   }, [subscriptionsData]);
 
+  const handleCancelSubscription = useCallback(() => {
+    if (currentSubscription && currentSubscription.cancelUrl) {
+      trackEvent({
+        eventName: "cancelSubscription",
+        productId: currentSubscription.workspaceId,
+        cancelUrl: currentSubscription.cancelUrl,
+      });
+
+      cancelPaddleSubscription(
+        currentSubscription.cancelUrl,
+        currentSubscription.workspaceId,
+        onTransactionSuccess
+      );
+    }
+  }, [trackEvent, onTransactionSuccess, currentSubscription]);
+
+  //cleanup polling
+  useEffect(() => {
+    if (subscriptionsData && subscriptionsData.subscriptions.length > 0) {
+      stopPolling();
+      setTransactionCompleted(false);
+    }
+    return () => {
+      stopPolling();
+    };
+  }, [stopPolling, subscriptionsData]);
+
   return (
     <div className={CLASS_NAME}>
       <div className={`${CLASS_NAME}__header`}>
         <h2>Workspace Plan</h2>
       </div>
-      {purchaseCompleted ? (
+      {transactionCompleted ? (
         <div className={`${CLASS_NAME}__processing`}>
           <div>Processing...please wait...</div>
           <CircularProgress />
@@ -63,12 +84,19 @@ function Subscription() {
           </div>
           {currentSubscription?.status} <br />
           <br />
-          {currentSubscription?.cancelUrl}
+          <Button
+            className={`${CLASS_NAME}__cancel`}
+            buttonStyle={EnumButtonStyle.Secondary}
+            onClick={handleCancelSubscription}
+            icon="trash_2"
+          >
+            Cancel Subscription
+          </Button>
           <br />
           {currentSubscription?.updateUrl}
         </div>
       ) : (
-        <PlanList onPurchaseSuccess={onPurchaseSuccess} />
+        <PlanList onPurchaseSuccess={onTransactionSuccess} />
       )}
     </div>
   );
@@ -80,6 +108,7 @@ export const GET_ACTIVE_SUBSCRIPTIONS = gql`
   query getActiveSubscriptions {
     subscriptions(where: { status: { not: Deleted } }) {
       id
+      workspaceId
       subscriptionPlan
       status
       nextBillDate
