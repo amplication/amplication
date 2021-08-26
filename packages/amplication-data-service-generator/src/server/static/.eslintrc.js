84be4b3f1958a3032b381c04298aecd6602ec725
tupe12334 <ofek.gabay.he@gmail.com>
2021-08-26 13:06:43 +0300

    adding comment for static server

--- packages/amplication-data-service-generator/src/server/static/.eslintrc.js
+++ packages/amplication-data-service-generator/src/server/static/.eslintrc.js
@@ -1,17 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: '@typescript-eslint/parser', // A parser that converts TypeScript into an ESTree-compatible form so it can be used in ESLint.
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'tsconfig.json', // This option allows you to provide a path to your project's tsconfig.json. This setting is required if you want to use rules which require type information. Relative paths are interpreted relative to the current working directory if tsconfigRootDir is not set.
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import' // linting of ES2015+ (ES6+) import/export syntax
+  ],
   extends: [
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/configs
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+    // https://github.com/import-js/eslint-plugin-import#installation
+    'plugin:import/recommended',
     'plugin:import/typescript',
   ],
   root: true,
@@ -27,12 +31,12 @@ module.exports = {
     },
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    'import/no-cycle': 'error',
-    '@typescript-eslint/camelcase': 'off',
+    // '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off', // Require explicit return types on functions and class methods
+    '@typescript-eslint/no-explicit-any': 'off', // Disallow usage of the any type
+    '@typescript-eslint/ban-ts-comment': 'off', // Bans @ts-<directive> comments from being used or requires descriptions after directive
+    'import/no-cycle': 'error', // Forbid a module from importing a module with a dependency path back to itself
+    // '@typescript-eslint/camelcase': 'off', // This rule has been deprecated in favour of the naming-convention rule.
     '@typescript-eslint/naming-convention': [
       'error',
       {
@@ -52,8 +56,15 @@ module.exports = {
         format: ['PascalCase'],
       },
       { selector: 'enumMember', format: ['PascalCase'] },
-    ],
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-    '@typescript-eslint/no-floating-promises': 'error',
+      {
+        selector: "interface", "format": ["PascalCase"],
+        "custom": {
+          "regex": "^I[A-Z]",
+          "match": true
+        }
+      }
+    ], // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }], // Disallow unused variables
+    '@typescript-eslint/no-floating-promises': 'error', // Requires Promise-like values to be handled appropriately
   },
 };
