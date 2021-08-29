a42baa14ae181dc127a675f44c19c8456c507aef
tupe12334 <ofek.gabay.he@gmail.com>
2021-08-29 03:47:43 +0300

    add static files

--- packages/amplication-data-service-generator/src/server/static/src/auth/auth.module.ts
+++ packages/amplication-data-service-generator/src/server/static/src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module, forwardRef } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
 // @ts-ignore
 // eslint-disable-next-line
 import { UserModule } from "../user/user.module";
@@ -8,10 +9,27 @@ import { AuthResolver } from "./auth.resolver";
 import { AuthService } from "./auth.service";
 import { BasicStrategy } from "./basic.strategy";
 import { PasswordService } from "./password.service";
+import { jwtConstants } from "./jwt/constants";
+import { JwtStrategy } from "./jwt/jwt.strategy";
 
+// const JWT_SECRET = "JWT_SECRET";
 @Module({
-  imports: [forwardRef(() => UserModule), PassportModule],
-  providers: [AuthService, BasicStrategy, PasswordService, AuthResolver],
+  imports: [
+    forwardRef(() => UserModule),
+    PassportModule,
+    JwtModule.register({
+      //TODO add connection to env file
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "2d" },
+    }),
+  ],
+  providers: [
+    AuthService,
+    BasicStrategy,
+    PasswordService,
+    AuthResolver,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
   exports: [AuthService, PasswordService],
 })
---
+++ packages/amplication-data-service-generator/src/server/static/src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { jwtConstants } from "./constants";
+import { UserInfo } from "../UserInfo";
+// @ts-ignore
+// eslint-disable-next-line
+import { UserService } from "../../user/user.service";
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: UserInfo): Promise<UserInfo> {
+    const { username } = payload;
+    const user = await this.userService.findOne({
+      where: { username },
+    });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
