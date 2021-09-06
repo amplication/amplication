bf7df97a4ff87e1763745ef63f3002d3bcdeb343
tupe12334 <ofek.gabay.he@gmail.com>
2021-09-07 02:50:37 +0300

    update

--- packages/amplication-data-service-generator/src/server/auth/guards/defaultAuth.guard.template.ts
+++ packages/amplication-data-service-generator/src/server/auth/guards/defaultAuth.guard.template.ts
@@ -1,15 +1,3 @@
-import { ExecutionContext } from "@nestjs/common";
-import { GqlExecutionContext } from "@nestjs/graphql";
-import type { Request } from "express";
-
 declare class GUARD {}
 
 export class DefaultAuthGuard extends GUARD {}
-
-export class GqlDefaultAuthGuard extends DefaultAuthGuard {
-  // This method is required for the interface - do not delete it.
-  getRequest(context: ExecutionContext): Request {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext<{ req: Request }>().req;
-  }
-}
