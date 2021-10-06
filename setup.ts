97e8d0c4100f7dc2466934c42ef8034bd1fdb964
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-06 15:10:30 +0300

    starting the pre validation section

--- setup.ts
+++ setup.ts
@@ -1,7 +1,30 @@
+import { satisfies } from "semver";
+import { execSync } from "child_process";
 async function main() {
   console.log("Setting up the project");
-  const { engines } = require("./package.json");
-  const { node, npm } = engines;
+  preValidate();
+  console.log("Finish pre validation successfully!");
+
+  function preValidate() {
+    const nodeVersion = process.versions.node;
+    const { engines } = require("./package.json");
+    const { node, npm } = engines;
+    if (isValidNodeVersion(nodeVersion, node)) {
+      console.info(`Pass node version with version: ${nodeVersion}`);
+    } else {
+      throw new Error("Invalid node version");
+    }
+    function isValidNodeVersion(
+      nodeVersion: string,
+      nodeRange: string
+    ): boolean {
+      if (!satisfies(nodeVersion, node)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 }
 if (require.main === module) {
   main();
