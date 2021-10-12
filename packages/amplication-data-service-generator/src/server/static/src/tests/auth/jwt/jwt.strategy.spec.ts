5534620a7bd6bbeef1b0f242b16e5ef97958b854
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-12 08:44:35 +0300

    update the jwt stategy spec

--- packages/amplication-data-service-generator/src/server/static/src/tests/auth/jwt/jwt.strategy.spec.ts
+++ packages/amplication-data-service-generator/src/server/static/src/tests/auth/jwt/jwt.strategy.spec.ts
@@ -1,3 +1,4 @@
+import { UnauthorizedException } from "@nestjs/common";
 import { mock } from "jest-mock-extended";
 import { JwtStrategy } from "../../../auth/jwt/jwt.strategy";
 import { SecretsManagerService } from "../../../providers/secrets/secretsManager.service";
@@ -6,7 +7,25 @@ import { SecretsManagerService } from "../../../providers/secrets/secretsManager
 import { UserService } from "../../../user/user.service";
 
 describe("Testing the JWT strategy class", () => {
+  const JWT_SECRET_KEY = "JWT_SECRET_KEY";
   const secretsService = mock<SecretsManagerService>();
+  secretsService.getSecret.mockReturnValue(Promise.resolve(JWT_SECRET_KEY));
   const userService = mock<UserService>();
   const jwtStrategy = new JwtStrategy(userService, secretsService);
+  beforeEach(() => {
+    userService.findOne.mockClear();
+  });
+  test("When there is no user throw error", async () => {
+    //ARRANGE
+    userService.findOne.mockReturnValue(Promise.resolve(null));
+
+    //ACT
+    //TODO finish the test
+    expect(
+      await jwtStrategy.validate({
+        username: "ofek",
+        roles: ["User"],
+      })
+    ).toThrowError(UnauthorizedException);
+  });
 });
