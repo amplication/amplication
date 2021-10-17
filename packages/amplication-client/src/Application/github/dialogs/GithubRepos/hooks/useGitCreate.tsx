1e2e42fea66f19d4cdc4021c12688d3ae220871d
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-18 00:11:31 +0300

    feat(client): add git create hook

--- packages/amplication-client/src/Application/github/dialogs/GithubRepos/hooks/useGitCreate.tsx
+++ packages/amplication-client/src/Application/github/dialogs/GithubRepos/hooks/useGitCreate.tsx
@@ -1,5 +1,68 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useCallback, useState } from "react";
+import {
+  EnumSourceControlService,
+  GitRepo,
+  RepoCreateInput,
+} from "../../../../../models";
 
-export default function useGitCreate() {
-  return <div></div>;
+type Props = {
+  appId: string;
+  sourceControlService: EnumSourceControlService;
+  cb: (repo: GitRepo) => any;
+};
+
+export default function useGitCreate({
+  appId,
+  sourceControlService,
+  cb,
+}: Props) {
+  const [error, setError] = useState("");
+  const [triggerCreation, { called, loading }] = useMutation(CREATE_REPO);
+
+  const handleCreation = useCallback(
+    (data: RepoCreateInput) => {
+      triggerCreation({
+        variables: {
+          name: data.name,
+          appId,
+          sourceControlService,
+          public: data.public,
+        },
+      })
+        .then((value) => {
+          const data: GitRepo = value.data.createRepoInOrg;
+          cb(data);
+        })
+        .catch((error) => {
+          setError(error.graphQLErrors[0].message);
+        });
+      // trackEvent({
+      //   eventName: "updateAppSettings",
+      // }); //TODO what is that
+    },
+    [appId, sourceControlService, triggerCreation]
+  );
+  return { called, loading, error, handleCreation };
 }
+
+const CREATE_REPO = gql`
+  mutation createRepoInOrg(
+    $sourceControlService: EnumSourceControlService!
+    $appId: String!
+    $name: String!
+    $public: Boolean!
+  ) {
+    createRepoInOrg(
+      appId: $appId
+      sourceControlService: $sourceControlService
+      input: { name: $name, public: $public }
+    ) {
+      name
+      url
+      private
+      fullName
+      admin
+    }
+  }
+`;
