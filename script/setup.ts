eb92398f9f1adcead91d0d03b91c2a57a6e68788
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-20 20:00:44 +0300

    refactor(script): loging

--- script/setup.ts
+++ script/setup.ts
@@ -1,14 +1,10 @@
 import { exec } from "child_process";
+import { satisfies } from "semver";
 import { createLogger, format, Logger, transports } from "winston";
-import { satisfies, minVersion, clean, valid } from "semver";
 const { combine, colorize, simple } = format;
 
 class Task {
-  constructor(
-    public command: string,
-    public label: string,
-    public finishMessage: string
-  ) {}
+  constructor(public command: string, public label: string) {}
 }
 const logger = createLogger({
   transports: [new transports.Console()],
@@ -17,28 +13,29 @@ const logger = createLogger({
 function preValidate() {
   const { engines } = require("../package.json");
   const { node: nodeRange, npm } = engines;
-  // const currentNpmVersion = process.env.npm_package_engines_npm?.trim();
-  // console.log(process);
   const npm_config_user_agent = process.env.npm_config_user_agent;
   const currentNpmVersionArray = npm_config_user_agent?.match(
     /npm\/[\^*\~*]*[\d\.]+/
   );
-  if (!currentNpmVersionArray) {
-    throw new Error("Npm error");
-  }
-  const currentNpmVersion = currentNpmVersionArray[0].substr(4);
-  if (!currentNpmVersion) {
-    throw new Error("Npm error");
-  }
-  if (!satisfies(process.versions.node, nodeRange)) {
-    throw new Error(
-      `Invalid node version, please use the specified node version ${nodeRange}`
+  const currentNpmVersion = currentNpmVersionArray[0]?.substr(4);
+  if (!currentNpmVersionArray || !currentNpmVersion) {
+    logger.error(
+      "Mmmmm... it seems like you don't have permission to run the script. Try to run it as an administrator."
     );
+    process.exit(1);
+  }
+  const currentNode = process.versions.node;
+  if (!satisfies(currentNode, nodeRange)) {
+    logger.error(
+      `This system seems to use an incompatible Node.js version, current version: ${currentNode} required version: ${nodeRange}`
+    );
+    process.exit(1);
   }
   if (!satisfies(currentNpmVersion, npm)) {
-    throw new Error(
-      `Invalid npm version, please use the specified npm version ${npm}`
+    logger.error(
+      `This system seems to use an incompatible NPM version, current version: ${currentNpmVersion} required version: ${npm}`
     );
+    process.exit(1);
   }
 }
 async function runFunction(task: Task, logger: Logger): Promise<string> {
@@ -47,7 +44,7 @@ async function runFunction(task: Task, logger: Logger): Promise<string> {
     exec(task.command, (error, stdout, stderr) => {
       error && reject(error);
       if (stdout) {
-        logger.info(`Finish ${task.finishMessage}`);
+        logger.info(`Finish ${task.label}`);
         stdout && resolve(task.label);
       }
     });
@@ -56,7 +53,6 @@ async function runFunction(task: Task, logger: Logger): Promise<string> {
 const bootstrap: Task[] = [
   {
     command: "npm run bootstrap",
-    finishMessage: "npm run bootstrap",
     label: "Bootstrap",
   },
 ];
@@ -64,42 +60,36 @@ const clientStep: Task[] = [
   {
     command:
       "npm run build -- --scope @amplication/client --include-dependencies",
-    finishMessage: "the build of all the client dependencies",
     label: "Client build",
   },
 ];
 const serverBuild: Task[] = [
   {
     command: "npm run prisma:generate",
-    finishMessage: "prisma generation in the sever",
     label: "prisma generation",
   },
   {
     command:
       "npm run build -- --scope @amplication/server --include-dependencies",
-    finishMessage: "the build of all the server dependencies",
     label: "server build",
   },
 ];
 const graphqlGeneration: Task[] = [
   {
     command: "npm run generate",
-    finishMessage: "all of the graphql schemas generation",
-    label: "graphql generation",
+    label: "generation graphql schema",
   },
 ];
 const docker: Task[] = [
   {
     command: "cd packages/amplication-server && npm run docker",
-    finishMessage: "the docker compose",
-    label: "docker compose startup",
+    label: "running docker compose up",
   },
 ];
 
 const dockerInit: Task[] = [
   {
     command: "cd packages/amplication-server && npm run start:db",
-    finishMessage: "the seeding of the db",
     label: "db seeding",
   },
 ];
@@ -114,15 +104,22 @@ const tasks: Task[][] = [
 if (require.main === module) {
   (async () => {
     preValidate();
-    logger.info(`Starting the script!`);
+    logger.info(`Welcome to Amplication installer!`);
+    logger.info(
+      "This script will help you easily set up a running amplication server"
+    );
+    console.log("");
+
     for (let i = 0; i < tasks.length; i++) {
       const step = tasks[i];
-      logger.info(`Starting step ${i + 1}`);
+
+      logger.info(`Starting step ${i + 1}/${tasks.length}`);
       const tasksPromises = step.map((task) => {
         return runFunction(task, logger);
       });
       await Promise.all(tasksPromises);
+      console.log("");
     }
-    logger.info("Finish all the process for the setup, have fun hacking 🎉");
+    logger.info("Finish all the process for the setup, have fun hacking");
   })();
 }
