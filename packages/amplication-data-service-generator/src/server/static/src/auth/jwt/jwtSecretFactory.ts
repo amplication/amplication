3a2ddcb5dbb6194bdaaab9db3a1f3df96f3245b2
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-20 19:23:18 +0300

    refactor(dsg): validation of secret

--- packages/amplication-data-service-generator/src/server/static/src/auth/jwt/jwtSecretFactory.ts
+++ packages/amplication-data-service-generator/src/server/static/src/auth/jwt/jwtSecretFactory.ts
@@ -3,8 +3,14 @@ import { SecretsManagerService } from "../../providers/secrets/secretsManager.se
 
 export const jwtSecretFactory = {
   provide: JWT_SECRET_KEY,
-  useFactory: async (secretsService: SecretsManagerService) => {
-    return await secretsService.getSecret<string>(JWT_SECRET_KEY);
+  useFactory: async (
+    secretsService: SecretsManagerService
+  ): Promise<string> => {
+    const secret = await secretsService.getSecret<string>(JWT_SECRET_KEY);
+    if (secret) {
+      return secret;
+    }
+    throw new Error("jwtSecretFactory missing secret");
   },
   inject: [SecretsManagerService],
 };
