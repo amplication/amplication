0dd3282feef4f358782bee14b8bba9aac699db61
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-24 11:23:38 +0300

    tests(dsg): adding tests, not ready

--- packages/amplication-data-service-generator/src/server/static/src/auth/auth.service.spec.ts
+++ packages/amplication-data-service-generator/src/server/static/src/auth/auth.service.spec.ts
@@ -4,6 +4,10 @@ import { AuthService } from "./auth.service";
 // eslint-disable-next-line
 import { UserService, User } from "../user/user.service";
 import { PasswordService } from "./password.service";
+import { Credentials } from "./Credentials";
+import { MockUser } from "../tests/__mocks__/MockUser";
+import { JwtService } from "@nestjs/jwt";
+import { mock } from "jest-mock-extended";
 
 const VALID_USER: User = {
   username: "Valid User",
@@ -13,6 +17,8 @@ const INVALID_USER: User = {
   username: "Invalid User",
   password: "Invalid User Password",
 };
+
+const signToken = "signToken";
 
 const userService = {
   findOne(args: { where: { username: string } }): User | null {
@@ -30,8 +36,8 @@ const passwordService = {
 };
 
 describe("AuthService", () => {
+  //ARRANGE
   let service: AuthService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -42,6 +48,10 @@ describe("AuthService", () => {
         {
           provide: PasswordService,
           useValue: passwordService,
+        },
+        {
+          provide: JwtService,
+          useValue: mock<JwtService>(),
         },
         AuthService,
       ],
@@ -54,17 +64,55 @@ describe("AuthService", () => {
     expect(service).toBeDefined();
   });
 
-  it("should validate a valid user", async () => {
-    await expect(
-      service.validateUser(VALID_USER.username, VALID_USER.password)
-    ).resolves.toEqual({
-      username: VALID_USER.username,
+  describe("Testing the authService.validateUser()", () => {
+    it("should validate a valid user", async () => {
+      await expect(
+        service.validateUser(VALID_USER.username, VALID_USER.password)
+      ).resolves.toEqual({
+        username: VALID_USER.username,
+      });
+    });
+
+    it("should not validate a invalid user", async () => {
+      await expect(
+        service.validateUser(INVALID_USER.username, INVALID_USER.password)
+      ).resolves.toBe(null);
     });
   });
 
-  it("should not validate a invalid user", async () => {
-    await expect(
-      service.validateUser(INVALID_USER.username, INVALID_USER.password)
-    ).resolves.toBe(null);
+  describe("Testing the authService.login()", () => {
+    const validCredentials: Credentials = {
+      password: "gabay",
+      username: MockUser.username,
+    };
+    let passwordService = mock<PasswordService>();
+    let jwtService = mock<JwtService>();
+
+    beforeEach(() => {
+      //ARRANGE
+      const userService = mock<UserService>();
+      userService.findOne
+        .calledWith({
+          where: { username: validCredentials.username },
+        })
+        .mockReturnValue(
+          Promise.resolve({ ...MockUser, password: "hashedPassword" })
+        );
+      passwordService = mock<PasswordService>();
+      jwtService = mock<JwtService>();
+
+      // authService = new AuthService(userService, passwordService, jwtService);
+    });
+    it("should return userInfo object for correct username and password", async () => {
+      passwordService.compare.mockReturnValue(Promise.resolve(true));
+      jwtService.signAsync.mockReturnValue(Promise.resolve(signToken));
+      const loginResult = await service.login(validCredentials);
+      expect(loginResult).toBe({
+        username: validCredentials.username,
+        roles: MockUser.roles,
+        accessToken: signToken,
+      });
+    });
   });
 });
+//TODO not ready
