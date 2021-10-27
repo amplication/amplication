578e7ab6419042511cf48471dcf72cccdf3c315f
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-27 18:32:22 +0300

    tests(dsg): add params tests

--- packages/amplication-data-service-generator/src/server/static/src/auth/jwt/jwtToken.service.ts
+++ packages/amplication-data-service-generator/src/server/static/src/auth/jwt/jwtToken.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { INVALID_PASSWORD_ERROR, INVALID_USERNAME_ERROR } from "../constants";
 import { ITokenService } from "../ITokenService";
 
 @Injectable()
 export class JwtTokenService implements ITokenService {
   constructor(protected readonly jwtService: JwtService) {}
   createToken(username: string, password: string): Promise<string> {
+    if (!username) return Promise.reject(INVALID_USERNAME_ERROR);
+    if (!password) return Promise.reject(INVALID_PASSWORD_ERROR);
     return this.jwtService.signAsync({ username });
   }
 }
