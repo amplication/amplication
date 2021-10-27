6503969ef0baaf0a25ffb152bd563846aa778d87
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-27 17:41:07 +0300

    fix(dsg): fix the base64 token

--- packages/amplication-data-service-generator/src/server/static/src/auth/basic/basicToken.service.ts
+++ packages/amplication-data-service-generator/src/server/static/src/auth/basic/basicToken.service.ts
@@ -4,6 +4,8 @@ import { ITokenService } from "../ITokenService";
 @Injectable()
 export class BasicTokenService implements ITokenService {
   createToken(username: string, password: string): Promise<string> {
-    return Promise.resolve(btoa(`${username}:${password}`));
+    return Promise.resolve(
+      Buffer.from(`${username}:${password}`).toString("base64")
+    );
   }
 }
