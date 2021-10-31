3c3f72bab2cb89517fa0587d817ced41eae0a0c9
tupe12334 <ofek.gabay.he@gmail.com>
2021-10-31 17:48:20 +0200

    refactor(dsg): add comments

--- packages/amplication-data-service-generator/src/server/auth/token/templates/basicToken.service.ts
+++ packages/amplication-data-service-generator/src/server/auth/token/templates/basicToken.service.ts
@@ -4,9 +4,17 @@ import { Injectable } from "@nestjs/common";
 import { INVALID_PASSWORD_ERROR, INVALID_USERNAME_ERROR } from "../constants";
 //@ts-ignore
 import { ITokenService } from "../ITokenService";
-
+/**
+ * TokenServiceBase is a basic http implementation of ITokenService
+ */
 @Injectable()
 export class TokenServiceBase implements ITokenService {
+  /**
+   *
+   * @param username
+   * @param password
+   * @returns a base64 string of the username and password
+   */
   createToken(username: string, password: string): Promise<string> {
     if (!username) return Promise.reject(INVALID_USERNAME_ERROR);
     if (!password) return Promise.reject(INVALID_PASSWORD_ERROR);
--- packages/amplication-data-service-generator/src/server/auth/token/templates/jwtToken.service.ts
+++ packages/amplication-data-service-generator/src/server/auth/token/templates/jwtToken.service.ts
@@ -5,10 +5,18 @@ import { JwtService } from "@nestjs/jwt";
 import { INVALID_PASSWORD_ERROR, INVALID_USERNAME_ERROR } from "../constants";
 //@ts-ignore
 import { ITokenService } from "../ITokenService";
-
+/**
+ * TokenServiceBase is a jwt bearer implementation of ITokenService
+ */
 @Injectable()
 export class TokenServiceBase implements ITokenService {
   constructor(protected readonly jwtService: JwtService) {}
+  /**
+   *
+   * @param username
+   * @param password
+   * @returns a jwt token sign with the username
+   */
   createToken(username: string, password: string): Promise<string> {
     if (!username) return Promise.reject(INVALID_USERNAME_ERROR);
     if (!password) return Promise.reject(INVALID_PASSWORD_ERROR);
