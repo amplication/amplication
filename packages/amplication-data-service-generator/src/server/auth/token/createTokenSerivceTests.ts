326645d60ce38f202533afa88d0263c61c41779b
tupe12334 <ofek.gabay.he@gmail.com>
2021-11-01 13:42:30 +0200

    fix(dsg): make sure that ts ignore comment remain

--- packages/amplication-data-service-generator/src/server/auth/token/createTokenSerivceTests.ts
+++ packages/amplication-data-service-generator/src/server/auth/token/createTokenSerivceTests.ts
@@ -1,7 +1,6 @@
 import { print } from "recast";
 import { EnumAuthProviderType } from "../../../models";
 import { Module } from "../../../types";
-import { removeTSIgnoreComments } from "../../../util/ast";
 import { readFile } from "../../../util/module";
 
 export async function createTokenServiceTests(
@@ -15,8 +14,6 @@ export async function createTokenServiceTests(
   );
   const file = await readFile(templatePath);
   const filePath = `${authTestsDir}/token.service.spec.ts`;
-
-  removeTSIgnoreComments(file);
 
   return { code: print(file).code, path: filePath };
 }
