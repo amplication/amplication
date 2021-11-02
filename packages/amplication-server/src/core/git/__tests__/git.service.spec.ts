ef2d8a99598d5d8d51470eb1b2c04d19ff8d9672
tupe12334 <ofek.gabay.he@gmail.com>
2021-11-02 13:24:10 +0200

    test(server): git service tests

--- packages/amplication-server/src/core/git/__tests__/git.service.spec.ts
+++ packages/amplication-server/src/core/git/__tests__/git.service.spec.ts
@@ -1,3 +1,45 @@
-describe('Testing the git.service', () => {
-  // const gitService = new GitService()
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { mock } from 'jest-mock-extended';
+import { AppService } from '../../app/app.service';
+import { APP_ID, TEST_APP_MOCK } from '../../app/__mocks__/App';
+import { EnumSourceControlService } from '../dto/enums/EnumSourceControlService';
+import { GitService } from '../git.service';
+import { TEST_GIT_REPO } from '../__mocks__/GitRepo';
+import { TEST_GIT_REPOS } from '../__mocks__/GitRepos';
+import { githubService } from '../__mocks__/GitService.mock';
+
+describe('GitService', () => {
+  let gitService: GitService;
+  beforeEach(() => {
+    const appService = mock<AppService>();
+    appService.app.mockReturnValue(Promise.resolve(TEST_APP_MOCK));
+    gitService = new GitService(githubService, appService);
+  });
+  it('should be defined', () => {
+    expect(gitService).toBeDefined();
+  });
+  //#region github
+  {
+    const sourceControlService = EnumSourceControlService.Github;
+    describe('GitService.getReposOfUser()', () => {
+      it('should return GitRepo[]', async () => {
+        const repos = await gitService.getReposOfUser({
+          appId: APP_ID,
+          sourceControlService
+        });
+        expect(repos).toBe(TEST_GIT_REPOS);
+      });
+    });
+    describe('GitService.createRepo()', () => {
+      it('should return GitRepo', async () => {
+        const repo = await gitService.createRepo({
+          appId: APP_ID,
+          sourceControlService,
+          input: { name: 'repo', public: true }
+        });
+        expect(repo).toBe(TEST_GIT_REPO);
+      });
+    });
+  }
+  //#endregion
 });
