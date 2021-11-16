3c1e9ec5845ce11a8c08f4f93cb26d2badd60306
tupe12334 <ofek.gabay.he@gmail.com>
2021-11-16 15:59:42 +0200

    refactor(server): moving to token factory model

--- packages/amplication-server/src/core/git/git.service.ts
+++ packages/amplication-server/src/core/git/git.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { isEmpty } from 'lodash';
 import { AppService } from '..';
-import { INVALID_APP_ID } from '../app/app.service';
 import { GithubService } from '../github/github.service';
 import { BaseGitArgs } from './dto/args/BaseGitArgs';
 import { CreateRepoArgs } from './dto/args/CreateRepoArgs';
@@ -10,36 +8,31 @@ import { EnumSourceControlService } from './dto/enums/EnumSourceControlService';
 import { GitRepo } from './dto/objects/GitRepo';
 import { GitUser } from './dto/objects/GitUser';
 import { INVALID_SOURCE_CONTROL_ERROR } from './errors/InvalidSourceControlError';
+import { TokenFactory } from './utils/TokenFactory';
 
 @Injectable()
 export class GitService {
+  private tokenFactory: TokenFactory;
   constructor(
     private readonly githubService: GithubService,
-    private readonly appService: AppService
-  ) {}
+    appService: AppService
+  ) {
+    this.tokenFactory = new TokenFactory(appService);
+  }
+
   async getReposOfUser(args: GetReposListArgs): Promise<GitRepo[]> {
-    const app = await this.appService.app({ where: { id: args.appId } });
+    const githubToken = await this.tokenFactory.getToken(args.appId);
 
-    if (isEmpty(app)) {
-      throw new Error(INVALID_APP_ID);
-    }
-
-    if (isEmpty(app.githubToken)) {
-      throw new Error(
-        `Sync cannot be enabled since this app is not authorized with any GitHub repo. You should first complete the authorization process`
-      );
-    }
     switch (args.sourceControlService) {
       case EnumSourceControlService.Github:
-        return await this.githubService.getUserRepos(app.githubToken);
+        return await this.githubService.getUserRepos(githubToken);
       default:
         throw INVALID_SOURCE_CONTROL_ERROR;
     }
   }
   async createRepo(args: CreateRepoArgs): Promise<GitRepo> {
     const { input, appId, sourceControlService } = args;
-    const app = await this.appService.app({ where: { id: appId } });
-    const { githubToken } = app;
+    const githubToken = await this.tokenFactory.getToken(appId);
 
     switch (sourceControlService) {
       case EnumSourceControlService.Github:
@@ -54,8 +47,8 @@ export class GitService {
 
   async getUsername(args: BaseGitArgs): Promise<string> {
     const { appId, sourceControlService } = args;
-    const app = await this.appService.app({ where: { id: appId } });
-    const { githubToken } = app;
+    const githubToken = await this.tokenFactory.getToken(appId);
+
     switch (sourceControlService) {
       case EnumSourceControlService.Github:
         return (await this.githubService.getUser(githubToken)).username;
@@ -65,8 +58,8 @@ export class GitService {
   }
   async getUser(args: BaseGitArgs): Promise<GitUser> {
     const { appId, sourceControlService } = args;
-    const app = await this.appService.app({ where: { id: appId } });
-    const { githubToken } = app;
+    const githubToken = await this.tokenFactory.getToken(appId);
+
     switch (sourceControlService) {
       case EnumSourceControlService.Github:
         return await this.githubService.getUser(githubToken);
---
+++ packages/amplication-server/src/core/git/utils/TokenFactory.ts
@@ -0,0 +1,21 @@
+import { INVALID_APP_ID } from '../../../core/app/app.service';
+import { AppService } from '../../../core';
+import { isEmpty } from 'lodash';
+
+export class TokenFactory {
+  constructor(private appService: AppService) {}
+  async getToken(appId: string): Promise<string> {
+    const app = await this.appService.app({ where: { id: appId } });
+
+    if (isEmpty(app)) {
+      throw new Error(INVALID_APP_ID);
+    }
+
+    if (isEmpty(app.githubToken)) {
+      throw new Error(
+        `App Missing a Github token. You should first complete the authorization process`
+      );
+    }
+    return app.githubToken;
+  }
+}
