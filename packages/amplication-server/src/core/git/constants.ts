9547e979e8b67224569624287af32b0ba499e8ee
tupe12334 <ofek.gabay.he@gmail.com>
2021-11-17 22:01:58 +0200

    refactor(server): typo

--- packages/amplication-server/src/core/git/constants.ts
+++ packages/amplication-server/src/core/git/constants.ts
@@ -1,3 +1,3 @@
-export const REPO_NAME_TAKEN_ERROR_MESSAGE = "'Repository is already exist'";
+export const REPO_NAME_TAKEN_ERROR_MESSAGE = 'Repository is already exist';
 export const INVALID_SOURCE_CONTROL_ERROR_MESSAGE =
   'Invalid source control service';
