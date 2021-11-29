41ba31dcebdca3461789ea85eace26b026e44350
tupe12334 <ofek.gabay.he@gmail.com>
2021-11-29 14:35:25 +0200

    fix(server): linter

--- packages/amplication-server/src/core/github/github.module.ts
+++ packages/amplication-server/src/core/github/github.module.ts
@@ -2,7 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
 import { GoogleSecretsManagerModule } from 'src/services/googleSecretsManager.module';
-import { AppModule } from '../app/app.module';
+import { AppModule } from '../app/app.module'; // eslint-disable-line import/no-cycle
 import { PermissionsModule } from '../permissions/permissions.module';
 import { GithubService } from './github.service';
 import { GithubTokenExtractor } from './utils/tokenExtractor/githubTokenExtractor';
