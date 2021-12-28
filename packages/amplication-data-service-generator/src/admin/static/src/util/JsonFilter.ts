9c0eb5e879afd35915899b6f027a47e8ba6d70c1
tupe12334 <ofek.gabay.he@gmail.com>
2021-12-28 13:13:57 +0200

    fix(dsg): update json filter

--- packages/amplication-data-service-generator/src/admin/static/src/util/JsonFilter.ts
+++ packages/amplication-data-service-generator/src/admin/static/src/util/JsonFilter.ts
@@ -1,31 +1,5 @@
 import { JsonValue } from "type-fest";
-import { Field, InputType } from "@nestjs/graphql";
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional } from "class-validator";
-import { GraphQLJSONObject } from "graphql-type-json";
-
-@InputType({
-  isAbstract: true,
-  description: undefined,
-})
 export class JsonFilter {
-  @ApiProperty({
-    required: false,
-    type: GraphQLJSONObject,
-  })
-  @IsOptional()
-  @Field(() => GraphQLJSONObject, {
-    nullable: true,
-  })
   equals?: Omit<JsonValue, "null">;
-
-  @ApiProperty({
-    required: false,
-    type: GraphQLJSONObject,
-  })
-  @IsOptional()
-  @Field(() => GraphQLJSONObject, {
-    nullable: true,
-  })
   not?: Omit<JsonValue, "null">;
 }
