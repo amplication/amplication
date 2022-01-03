2b64ea40eb54862b500616b4b401c38aac66de55
tupe12334 <ofek.gabay.he@gmail.com>
2022-01-03 16:45:12 +0200

    fix(data): typo

--- packages/amplication-data/scripts/__tests__/type-generation.spec.ts
+++ packages/amplication-data/scripts/__tests__/type-generation.spec.ts
@@ -4,12 +4,9 @@ import { DATA_TYPES } from "../../src/constants";
 import { camelCase } from "lodash";
 describe("Testing the generation of new type", () => {
   const schemasFolder = join(__dirname, "../..", "src/schemas");
-  test.each(DATA_TYPES)(
-    "getSchemaForDataType(EnumDataType.%s)",
-    async (dataType) => {
-      const name = `${camelCase(dataType)}.json`;
-      const path = join(schemasFolder, name);
-      expect(await generateTypeFile(path, name)).toMatchSnapshot();
-    }
-  );
+  test.each(DATA_TYPES)("generateTypeFile(%s)", async (dataType) => {
+    const name = `${camelCase(dataType)}.json`;
+    const path = join(schemasFolder, name);
+    expect(await generateTypeFile(path, name)).toMatchSnapshot();
+  });
 });
