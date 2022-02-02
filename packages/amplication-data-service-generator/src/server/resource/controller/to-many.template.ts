6936c60ce7ab64936049c8a8af58e8950108fb40
tupe12334 <ofek.gabay.he@gmail.com>
2022-02-02 17:11:09 +0200

    fix(dsg): using unique insted of regular

--- packages/amplication-data-service-generator/src/server/resource/controller/to-many.template.ts
+++ packages/amplication-data-service-generator/src/server/resource/controller/to-many.template.ts
@@ -142,7 +142,7 @@ export class Mixin {
   })
   async UPDATE(
     @common.Param() params: WHERE_UNIQUE_INPUT,
-    @common.Body() body: RELATED_ENTITY_WHERE_INPUT[],
+    @common.Body() body: RELATED_ENTITY_WHERE_UNIQUE_INPUT[],
     @nestAccessControl.UserRoles() userRoles: string[]
   ): Promise<void> {
     const data = {
