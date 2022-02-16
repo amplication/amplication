baed3e474b26b1e5e9553c3e5f95e0a5e41f8ddc
tupe12334 <ofek.gabay.he@gmail.com>
2022-02-16 12:02:34 +0200

    feat(dsg): api property

--- packages/amplication-data-service-generator/src/server/resource/dto/create-field-class-property.ts
+++ packages/amplication-data-service-generator/src/server/resource/dto/create-field-class-property.ts
@@ -17,7 +17,7 @@ import {
   createEnumName,
   createPrismaFields,
 } from "../../prisma/create-prisma-schema";
-import { ApiPropertyDecoratorBuilder } from "./api-property-decorator-builder";
+import { ApiPropertyDecoratorBuilder } from "./api-property-decorator";
 import * as classTransformerUtil from "./class-transformer.util";
 import {
   IS_BOOLEAN_ID,
