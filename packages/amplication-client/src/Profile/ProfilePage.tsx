3c533c665ac052861ed44740e778988380b381ae
molaycule <moh.agboola@gmail.com>
2022-02-19 07:36:03 +0100

    fix(client): profile button doesn't lead to anywhere

---
+++ packages/amplication-client/src/Profile/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileForm from "./ProfileForm";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default ProfilePage;
