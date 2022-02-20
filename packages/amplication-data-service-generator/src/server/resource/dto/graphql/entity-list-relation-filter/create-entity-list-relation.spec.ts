4874d411fec4f85080b3f9edb329a98af584d266
Amit Barletz <amitbarletz@MBP-ORENB.tikalk.com>
2022-02-20 14:14:27 +0200

    fix(dsg): change to snapshot test

--- packages/amplication-data-service-generator/src/server/resource/dto/graphql/entity-list-relation-filter/create-entity-list-relation.spec.ts
+++ packages/amplication-data-service-generator/src/server/resource/dto/graphql/entity-list-relation-filter/create-entity-list-relation.spec.ts
@@ -1,71 +1,69 @@
-import { print } from "recast";
-import { Entity } from "../../../../../types";
-import {
-  EXAMPLE_ID_FIELD,
-  EXAMPLE_SINGLE_LINE_TEXT_FIELD,
-} from "../../../util/test-data";
+import { EntityField } from "../../../../../types";
 import { createWhereInput } from "../../create-where-input";
 import {
   createEntityListRelationFilter,
   createEntityListRelationFilterID,
 } from "./create-entity-list-relation-filter";
+import { printTypescript } from "../../../../../tests/server/resource/dto/utils";
+import cloneDeep from "lodash.clonedeep";
+import defaultEntity from "../../../../../tests/server/resource/dto/constants/default-entity";
+import defaultJsonField from "../../../../../tests/server/resource/dto/constants/json-field";
 
-const EXAMPLE_ENTITY: Entity = {
-  id: "EXAMPLE_ENTITY_ID",
-  name: "ExampleEntity",
-  displayName: "Example Entity",
-  pluralDisplayName: "Example Entities",
-  fields: [EXAMPLE_ID_FIELD, EXAMPLE_SINGLE_LINE_TEXT_FIELD],
-  permissions: [],
-};
-const EXAMPLE_WHERE_INPUT = createWhereInput(EXAMPLE_ENTITY);
-
-describe("createEntityListRelation", () => {
-  test("create list relation", async () => {
-    expect(
-      print(
-        await createEntityListRelationFilter(
-          EXAMPLE_ENTITY,
-          EXAMPLE_WHERE_INPUT
-        )
-      ).code
-    ).toEqual(`@InputType()
-class ${createEntityListRelationFilterID(EXAMPLE_ENTITY.name).name} {
+describe("Testing the creation of createEntityListRelation", () => {
+  const entity = cloneDeep(defaultEntity);
+  test("Testing entity json field with all true", async () => {
+    const trueJsonFiled: EntityField = {
+      ...cloneDeep(defaultJsonField),
+      searchable: true,
+      required: true,
+      unique: true,
+    };
+    entity.fields = [trueJsonFiled];
+    const whereInput = createWhereInput(entity);
+    const code = printTypescript(
+      await createEntityListRelationFilter(entity, whereInput)
+    );
+    // eslint-disable-next-line jest/no-interpolation-in-snapshots
+    expect(code).toMatchInlineSnapshot(`
+"@InputType()
+class ${createEntityListRelationFilterID(entity.name).name} {
   @ApiProperty({
     required: false,
-    type: () => ${EXAMPLE_WHERE_INPUT.id.name},
+    type: () => ${whereInput.id.name},
   })
   @ValidateNested()
-  @Type(() => ${EXAMPLE_WHERE_INPUT.id.name})
+  @Type(() => ${whereInput.id.name})
   @IsOptional()
-  @Field(() => ${EXAMPLE_WHERE_INPUT.id.name}, {
+  @Field(() => ${whereInput.id.name}, {
     nullable: true,
   })
-  every?: ${EXAMPLE_WHERE_INPUT.id.name};
-
-  @ApiProperty({
-    required: false,
-    type: () => ${EXAMPLE_WHERE_INPUT.id.name},
-  })
-  @ValidateNested()
-  @Type(() => ${EXAMPLE_WHERE_INPUT.id.name})
-  @IsOptional()
-  @Field(() => ${EXAMPLE_WHERE_INPUT.id.name}, {
-    nullable: true,
-  })
-  some?: ${EXAMPLE_WHERE_INPUT.id.name};
+  every?: ${whereInput.id.name};
 
   @ApiProperty({
     required: false,
-    type: () => ${EXAMPLE_WHERE_INPUT.id.name},
+    type: () => ${whereInput.id.name},
   })
   @ValidateNested()
-  @Type(() => ${EXAMPLE_WHERE_INPUT.id.name})
+  @Type(() => ${whereInput.id.name})
   @IsOptional()
-  @Field(() => ${EXAMPLE_WHERE_INPUT.id.name}, {
+  @Field(() => ${whereInput.id.name}, {
     nullable: true,
   })
-  none?: ${EXAMPLE_WHERE_INPUT.id.name};
-}`);
+  some?: ${whereInput.id.name};
+
+  @ApiProperty({
+    required: false,
+    type: () => ${whereInput.id.name},
+  })
+  @ValidateNested()
+  @Type(() => ${whereInput.id.name})
+  @IsOptional()
+  @Field(() => ${whereInput.id.name}, {
+    nullable: true,
+  })
+  none?: ${whereInput.id.name};
+}
+"
+`);
   });
 });
