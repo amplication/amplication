e9149c9cc5a565f3ad87c2019044d6aaa62c0679
tupe12334 <ofek.gabay.he@gmail.com>
2022-02-23 16:24:01 +0200

    feat(client): seperating the gitsync notes

---
+++ packages/amplication-client/src/Application/git/GitSyncNotes.tsx
@@ -0,0 +1,22 @@
+import { Icon } from "@amplication/design-system";
+import React from "react";
+import { CLASS_NAME } from "./AuthAppWithGithub";
+
+export default function GitSyncNotes() {
+  return (
+    <div className={`${CLASS_NAME}__notice`}>
+      Please note:
+      <ul>
+        <li>
+          <Icon icon="check_circle" />
+          The changes will be pushed to the root of the selected repository,
+          using Pull Requests.
+        </li>
+        <li>
+          <Icon icon="check_circle" />
+          The selected repository must not be empty.
+        </li>
+      </ul>
+    </div>
+  );
+}
