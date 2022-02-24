8222d89a2104c14a2a96fd8cf69540693d3651f8
Amit Barletz <amitbarletz@MBP-ORENB.tikalk.com>
2022-02-24 10:51:18 +0200

    fix(dsg): fix source to be plural

--- packages/amplication-data-service-generator/src/admin/entity/create-field-input.ts
+++ packages/amplication-data-service-generator/src/admin/entity/create-field-input.ts
@@ -8,6 +8,7 @@ import {
 } from "../../types";
 import { jsxElement } from "../util";
 import { isToManyRelationField } from "../../util/field";
+import pluralize from "pluralize";
 
 /**
  * Creates an input element to be placed inside a Formik form for editing the given entity field
@@ -47,7 +48,9 @@ const DATA_TYPE_TO_FIELD_INPUT: {
   [EnumDataType.Lookup]: (field) => {
     const { relatedEntity } = field.properties as LookupResolvedProperties;
     if (isToManyRelationField(field)) {
-      return jsxElement`<ReferenceArrayInput source="${relatedEntity.name.toLowerCase()}" 
+      return jsxElement`<ReferenceArrayInput source="${pluralize(
+        relatedEntity.name.toLowerCase()
+      )}" 
       reference="${relatedEntity.name}"
       parse={(value: any) => value && value.map((v: any) => ({ id: v }))}
       format={(value: any) => value && value.map((v: any) => v.id)}
