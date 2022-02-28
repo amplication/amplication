f635f351c777b03fb937e667f64a03b9e48e594c
tupe12334 <ofek.gabay.he@gmail.com>
2022-02-28 11:21:18 +0200

    refactor(clint)

--- packages/amplication-client/src/Application/git/AuthAppWithGithubCallback.tsx
+++ packages/amplication-client/src/Application/git/AuthAppWithGithubCallback.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-//import { match } from "react-router-dom";
 import { gql, useMutation } from "@apollo/client";
 import { useTracking } from "../../util/analytics";
 
