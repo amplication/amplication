c2b09cb47ef8aada4066a83f5a02e84399aa0412
tupe12334 <ofek.gabay.he@gmail.com>
2022-03-01 12:29:45 +0200

    sql: merging all sql commands

--- packages/amplication-server/prisma/migrations/20220221141316_21022022/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `code` on the `GitOrganization` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "GitOrganization" DROP COLUMN "code";
