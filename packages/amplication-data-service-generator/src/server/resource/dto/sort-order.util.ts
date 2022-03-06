3a0100a6a5b7f7e18859196bca84b7033ffe0326
tupe12334 <ofek.gabay.he@gmail.com>
2022-03-06 17:28:28 +0200

    fix(dsg): renaming the Asc & Desc

--- packages/amplication-data-service-generator/src/server/resource/dto/sort-order.util.ts
+++ packages/amplication-data-service-generator/src/server/resource/dto/sort-order.util.ts
@@ -2,5 +2,5 @@ import { builders } from "ast-types";
 
 export const SORT_ORDER_MODULE = "../../util/SortOrder";
 export const SORT_ORDER_ID = builders.identifier("SortOrder");
-export const SORT_ORDER_ASC_LITERAL = builders.literal("Asc");
-export const SORT_ORDER_DESC_LITERAL = builders.literal("Desc");
+export const SORT_ORDER_ASC_LITERAL = builders.literal("asc");
+export const SORT_ORDER_DESC_LITERAL = builders.literal("desc");
