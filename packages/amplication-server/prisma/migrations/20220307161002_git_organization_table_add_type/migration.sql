46c3331094dd14f2d9dce6db059160724445728f
Mor Hagbi <mor@amplication.com>
2022-03-07 19:24:43 +0200

    update type field to nullable

--- packages/amplication-server/prisma/migrations/20220307161002_git_organization_table_add_type/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "EnumGitOrganizationType" AS ENUM ('User', 'Organization');
-
--- AlterTable
-ALTER TABLE "GitOrganization" ADD COLUMN     "type" "EnumGitOrganizationType" NOT NULL DEFAULT E'User';
