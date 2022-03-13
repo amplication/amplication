21825754f038367bedbea6ed1399709fe5c3c018
Mor Hagbi <mor@amplication.com>
2022-03-13 17:25:05 +0200

    fix(dg):revert change

--- packages/amplication-data-service-generator/src/models.ts
+++ packages/amplication-data-service-generator/src/models.ts
@@ -634,7 +634,6 @@ export type Entity = {
   name: Scalars["String"];
   permissions?: Maybe<Array<EntityPermission>>;
   pluralDisplayName: Scalars["String"];
-  pluralName: Scalars["String"];
   updatedAt: Scalars["DateTime"];
   versions?: Maybe<Array<EntityVersion>>;
 };
