286eac591ac078b372c23a78782df80a9f9be3cd
Mor Hagbi <mor@amplication.com>
2022-03-22 21:12:27 +0200

    add git.service.spec

--- packages/amplication-git-service/src/__tests__/services/git.service.spec.ts
+++ packages/amplication-git-service/src/__tests__/services/git.service.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EnumGitProvider } from '../../Dto/enums/EnumGitProvider';
 import { RemoteGitRepository } from '../..//Dto/entities/RemoteGitRepository';
-import { GitModule } from '../../git.module';
-import { GithubService } from '../..//providers/github.service';
 import { GitService } from '../../services/git.service';
 import { GitServiceFactory } from '../../utils/GitServiceFactory';
+import { MOCK_GIT_SERVICE_FACTORY } from '../../__mocks__/GitServiceFactory.mock';
+import { EnumGitOrganizationType } from '../../Dto/enums/EnumGitOrganizationType';
 
 const TEST_GIT_REPOS: RemoteGitRepository[] = [
   {
@@ -28,8 +28,13 @@ describe('GitService', () => {
   beforeEach(async () => {
     jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GitService, GithubService, GitServiceFactory],
-      imports: [GitModule],
+      providers: [
+        GitService,
+        {
+          provide: GitServiceFactory,
+          useValue: MOCK_GIT_SERVICE_FACTORY,
+        },
+      ],
     }).compile();
 
     gitService = module.get<GitService>(GitService);
@@ -39,9 +44,23 @@ describe('GitService', () => {
     expect(gitService).toBeDefined();
   });
   {
+    describe('GitService.createRepo()', () => {
+      it('should return App', async () => {
+        expect(
+          await gitService.createGitRepository(
+            'repoName',
+            EnumGitProvider.Github,
+            EnumGitOrganizationType.Organization,
+            'gitOrganizationName',
+            '123456',
+          ),
+        ).toEqual(RemoteGitRepository);
+      });
+    });
+
     describe('GitService.getReposOfOrganization()', () => {
       it('should return RemoteGitRepositories[]', async () => {
-        const installationId = '24297005';
+        const installationId = '123456';
         const gitProvider = EnumGitProvider.Github;
         const remoteGitRepositories = await gitService.getReposOfOrganization(
           gitProvider,
