ff517ea5622288981779c23dcc9a8a8c191849a6
tupe12334 <ofek.gabay.he@gmail.com>
2022-04-04 10:30:20 +0300

    feat(server): add a tamplate for queue service

---
+++ packages/amplication-server/src/core/queue/queue.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { QueueService, QUEUE_SERVICE_NAME } from './queue.service';
+
+export const BROKER_IP_ENV_KEY = 'BROKER_IP_ENV_KEY';
+
+@Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: QUEUE_SERVICE_NAME,
+        useFactory: (configService: ConfigService) => {
+          const envBrokerIp = configService.get<string>(BROKER_IP_ENV_KEY);
+          if (!envBrokerIp) {
+            throw new Error('Missing broker ip in env file');
+          }
+          return {
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: 'server-queue-client',
+                brokers: [envBrokerIp]
+              }
+            }
+          };
+        },
+        inject: [ConfigService]
+      }
+    ])
+  ],
+  providers: [QueueService],
+  exports: [QueueService]
+})
+export class QueueModule {}
---
+++ packages/amplication-server/src/core/queue/queue.service.ts
@@ -0,0 +1,15 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+
+export const QUEUE_SERVICE_NAME = 'QUEUE_SERVICE';
+
+@Injectable()
+export class QueueService {
+  constructor(
+    @Inject(QUEUE_SERVICE_NAME)
+    private readonly kafkaService: ClientKafka
+  ) {}
+  emitCreateGitPullRequest(data: any) {
+    return this.kafkaService.emit('git_pr_generate', data);
+  }
+}
