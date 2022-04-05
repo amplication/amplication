8ed4cc95e13668b946b852dc8c7fee9eb7e29bdc
tupe12334 <ofek.gabay.he@gmail.com>
2022-04-05 16:52:05 +0300

    fix(pr): send all for first time

--- packages/amplication-pull-request/src/core/diff/diff.service.ts
+++ packages/amplication-pull-request/src/core/diff/diff.service.ts
@@ -3,6 +3,7 @@ import { compare, Options } from 'dir-compare';
 import { join } from 'path';
 import { ChangedFile } from '@amplication/common/src/dto/ChangedFile';
 import { BuildPathFactory } from './utils/BuildPathFactory';
+import { sync } from 'fast-glob';
 
 @Injectable()
 export class DiffService {
@@ -24,22 +25,28 @@ export class DiffService {
     if (oldBuildPath === newBuildPath) {
       throw new Error('Cant get the same build id');
     }
-
-    const res = await compare(oldBuildPath, newBuildPath, compareOptions);
-    const changedFiles = res.diffSet.filter((diff) => {
-      if (diff.state !== 'equal') {
-        return true;
-      }
-      return false;
-    });
-    return (
-      changedFiles
-        // Remove all the old deleted files
-        .filter((diff) => diff.name2)
-        .map((diff) => ({
-          path: join(diff.relativePath, diff.name2),
-        }))
-    );
+    try {
+      const res = await compare(oldBuildPath, newBuildPath, compareOptions);
+      const changedFiles = res.diffSet.filter((diff) => {
+        if (diff.state !== 'equal') {
+          return true;
+        }
+        return false;
+      });
+      return (
+        changedFiles
+          // Remove all the old deleted files
+          .filter((diff) => diff.name2)
+          .map((diff) => ({
+            path: join(diff.relativePath, diff.name2),
+          }))
+      );
+    } catch (error) {
+      const files = sync(`${newBuildPath}/**`).map((file) => ({
+        path: file,
+      }));
+      return files;
+    }
   }
 }
 
