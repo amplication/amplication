253fbca145a173c36c6379ff3127ab042b031b17
tupe12334 <ofek.gabay.he@gmail.com>
2022-04-05 14:35:26 +0300

    class validator

--- packages/amplication-common/src/dto/sendPullRequest.ts
+++ packages/amplication-common/src/dto/sendPullRequest.ts
@@ -1,10 +1,15 @@
 import { EnumGitProvider } from "../types";
-
+import { IsString } from "class-validator";
 export class SendPullRequestArgs {
+  @IsString()
   amplicationAppId!: string;
+  @IsString()
   oldBuildId!: string;
+  @IsString()
   newBuildId!: string;
+  @IsString()
   installationId!: string;
+  @IsString()
   gitProvider!: EnumGitProvider;
   // @Field(() => GitOrganization, { nullable: false })
   // gitOrganization: GitOrganization;
