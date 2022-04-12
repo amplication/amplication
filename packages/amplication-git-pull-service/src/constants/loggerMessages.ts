b1d8f0af50f3a36d89169cbbf457404d7a2c41e0
Amit Barletz <amit@amplication.com>
2022-04-12 14:03:06 +0300

    feat(git-pull-service) fix code review issues

--- packages/amplication-git-pull-service/src/constants/loggerMessages.ts
+++ packages/amplication-git-pull-service/src/constants/loggerMessages.ts
@@ -5,6 +5,8 @@ export const LoggerMessages = {
     FOUND_PREVIOUS_READY_COMMIT: "found previous ready commit",
     CLONE_SUCCESS: "clone successfully",
     PULL_COPY_SUCCESS: "pull and copy successfully",
+    DELETE_SUCCESSFULLY:
+      "update record status to Deleted and delete from storage",
   },
   error: {
     CATCH_ERROR_MESSAGE: "error from:",
--- packages/amplication-git-pull-service/src/providers/storage/storage.service.ts
+++ packages/amplication-git-pull-service/src/providers/storage/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { IStorage } from "../../contracts/interfaces/storage.interface";
 import * as fse from "fs-extra";
+import fs from "fs";
 import { CustomError } from "../../errors/CustomError";
 
 @Injectable()
@@ -8,8 +9,12 @@ export class StorageService implements IStorage {
   async copyDir(srcDir: string, destDir: string): Promise<void> {
     try {
       await fse.copy(srcDir, destDir);
-    } catch (err) {
+    } catch (err: any) {
       throw new CustomError("failed to copy files from srcDir to destDir", err);
     }
+  }
+
+  deleteDir(dir: string): void {
+    fs.rmSync(dir, { recursive: true, force: true });
   }
 }
