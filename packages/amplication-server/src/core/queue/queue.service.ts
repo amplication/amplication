6c72211a47e909ac53cbc67bd31bda51af6c2735
tupe12334 <ofek.gabay.he@gmail.com>
2022-04-12 13:06:39 +0300

    fix(server): get topic from env

--- packages/amplication-server/src/core/queue/queue.service.ts
+++ packages/amplication-server/src/core/queue/queue.service.ts
@@ -1,23 +1,36 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
 import {
+  ResultMessage,
   SendPullRequestArgs,
-  GENERATE_PULL_REQUEST_MESSAGE,
-  SendPullRequestResponse,
-  ResultMessage
+  SendPullRequestResponse
 } from '@amplication/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientKafka } from '@nestjs/microservices';
 import { plainToClass } from 'class-transformer';
+import { GENERATE_PULL_REQUEST_TOPIC } from 'src/constants';
+import assert from 'assert';
 
 export const QUEUE_SERVICE_NAME = 'QUEUE_SERVICE';
 
 @Injectable()
 export class QueueService implements OnModuleInit {
+  generatePullRequestTopic: string;
   constructor(
     @Inject(QUEUE_SERVICE_NAME)
-    private readonly kafkaService: ClientKafka
-  ) {}
+    private readonly kafkaService: ClientKafka,
+    configService: ConfigService
+  ) {
+    const envGeneratePullRequestTopic = configService.get<string>(
+      GENERATE_PULL_REQUEST_TOPIC
+    );
+    assert(
+      envGeneratePullRequestTopic,
+      'Missing env for generate pull request topics'
+    );
+    this.generatePullRequestTopic = envGeneratePullRequestTopic;
+  }
   onModuleInit(): void {
-    this.kafkaService.subscribeToResponseOf(GENERATE_PULL_REQUEST_MESSAGE);
+    this.kafkaService.subscribeToResponseOf(this.generatePullRequestTopic);
   }
 
   emitCreateGitPullRequest(
@@ -25,7 +38,7 @@ export class QueueService implements OnModuleInit {
   ): Promise<SendPullRequestResponse | null> {
     return new Promise(res => {
       this.kafkaService
-        .send(GENERATE_PULL_REQUEST_MESSAGE, data)
+        .send(this.generatePullRequestTopic, data)
         .subscribe((response: ResultMessage<SendPullRequestResponse>) => {
           try {
             const validClass = plainToClass(
