00724d7fea67772913f7f65d2241197952c5fd00
Gal_Rami <galrami6@gmail.com>
2022-04-14 11:25:44 +0300

    python test

--- devlopment_scripts/update_helm_versions.py
+++ devlopment_scripts/update_helm_versions.py
@@ -4,50 +4,53 @@ import io,sys,os,shutil
 build_number = sys.argv[1]
 services_list = sys.argv[2]
 branch_name = sys.argv[3]
-print(f'services list: {services_list}')
-# service_chart = f'helm/helm_chart/charts/services/{service_name}/Chart.yaml'
-# umbrella_chart = 'helm/helm_chart/charts/amplication/Chart.yaml'
-print(f'branch : {branch_name}')
+branch_name_test = sys.argv[4]
 print(f'build number : {build_number}')
-
+print(f'services list: {services_list}')
+print(f'branch : {branch_name}')
+print(f'branch_name_test : {branch_name_test}')
+# umbrella_chart = 'helm/helm_chart/charts/amplication/Chart.yaml'
 # def my_function(version):
 #   splited_version = version.split('.')
 #   return f'{splited_version[0]}.{splited_version[1]}.{build_number}'
+# for service in services_list:
 
-# #update service chart:
-# with open(service_chart, 'r') as stream:
-#     data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
-#     version = my_function(data_loaded['version'])
-#     data_loaded['version'] = version
-# with io.open(service_chart, 'w', encoding='utf8') as outfile:
-#     ruamel.yaml.round_trip_dump(data_loaded, outfile, explicit_start=True)
+#     service_chart = f'helm/helm_chart/charts/services/{service}/Chart.yaml'
+#     #update service chart:
+#     with open(service_chart, 'r') as stream:
+#         data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
+#         version = my_function(data_loaded['version'])
+#         data_loaded['version'] = version
+#         data_loaded['appVersion'] = version
+#     with io.open(service_chart, 'w', encoding='utf8') as outfile:
+#         ruamel.yaml.round_trip_dump(data_loaded, outfile, explicit_start=True)
 
 
-# #update umbrella chart:
-# if service_name == 'amplication-client':
-#     num = 0
-# else: 
-#     num = 1
+#     #update umbrella chart:
+#     if service_name == 'amplication-client':
+#         num = 0
+#     else: 
+#         num = 1
 
-# with open(umbrella_chart, 'r') as stream:
-#     data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
-#     version = my_function(data_loaded['dependencies'][num]['version'])
-#     old_version = data_loaded['dependencies'][num]['version']
-#     new_version = version
-#     data_loaded['dependencies'][num]['version'] = version
-#     version = my_function(data_loaded['version'])
-#     data_loaded['version'] = version
-# with io.open(umbrella_chart, 'w', encoding='utf8') as outfile:
-#     ruamel.yaml.round_trip_dump(data_loaded, outfile, explicit_start=True)
+#     with open(umbrella_chart, 'r') as stream:
+#         data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
+#         version = my_function(data_loaded['dependencies'][num]['version'])
+#         old_version = data_loaded['dependencies'][num]['version']
+#         new_version = version
+#         data_loaded['dependencies'][num]['version'] = version
+#         version = my_function(data_loaded['version'])
+#         data_loaded['version'] = version
+#     with io.open(umbrella_chart, 'w', encoding='utf8') as outfile:
+#         ruamel.yaml.round_trip_dump(data_loaded, outfile, explicit_start=True)
 
-# #build new package:
-# print("")
-# print(os.getcwd())
-# os.remove(f'./../helm/helm_chart/charts/amplication/charts/{service_name}-{old_version}.tgz')
-# os.system(f'helm package ./../helm/helm_chart/charts/services/{service_name}')
+# # #build new package:
+# # print("")
+# # print(os.getcwd())
+# # os.remove(f'./../helm/helm_chart/charts/amplication/charts/{service_name}-{old_version}.tgz')
+# # os.system(f'helm package ./../helm/helm_chart/charts/services/{service_name}')
 
-# source_dir = './'
-# target_dir = 'helm/helm_chart/charts/amplication/charts/'
-# file_name = f'{service_name}-{new_version}.tgz'
+# # source_dir = './'
+# # target_dir = 'helm/helm_chart/charts/amplication/charts/'
+# # file_name = f'{service_name}-{new_version}.tgz'
 
-# shutil.move(os.path.join(source_dir, file_name), target_dir)
+# # shutil.move(os.path.join(source_dir, file_name), target_dir)
