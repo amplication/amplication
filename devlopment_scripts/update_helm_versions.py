4e7da46b5927390343f36b6ff6be5506307fc07b
Gal_Rami <galrami6@gmail.com>
2022-04-14 11:31:07 +0300

    test py

--- devlopment_scripts/update_helm_versions.py
+++ devlopment_scripts/update_helm_versions.py
@@ -4,17 +4,15 @@ import io,sys,os,shutil
 build_number = sys.argv[1]
 services_list = sys.argv[2]
 branch_name = sys.argv[3]
-branch_name_test = sys.argv[4]
 print(f'build number : {build_number}')
 print(f'services list: {services_list}')
 print(f'branch : {branch_name}')
-print(f'branch_name_test : {branch_name_test}')
-# umbrella_chart = 'helm/helm_chart/charts/amplication/Chart.yaml'
-# def my_function(version):
-#   splited_version = version.split('.')
-#   return f'{splited_version[0]}.{splited_version[1]}.{build_number}'
-# for service in services_list:
-
+umbrella_chart = 'helm/helm_chart/charts/amplication/Chart.yaml'
+def my_function(version):
+  splited_version = version.split('.')
+  return f'{splited_version[0]}.{splited_version[1]}.{build_number}'
+for service in services_list:
+    print(f'service : {service}')
 #     service_chart = f'helm/helm_chart/charts/services/{service}/Chart.yaml'
 #     #update service chart:
 #     with open(service_chart, 'r') as stream:
@@ -35,11 +33,11 @@ print(f'branch_name_test : {branch_name_test}')
 #     with open(umbrella_chart, 'r') as stream:
 #         data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
 #         version = my_function(data_loaded['dependencies'][num]['version'])
-#         old_version = data_loaded['dependencies'][num]['version']
-#         new_version = version
 #         data_loaded['dependencies'][num]['version'] = version
 #         version = my_function(data_loaded['version'])
 #         data_loaded['version'] = version
+#         version = my_function(data_loaded['appVersion'])
+#         data_loaded['appVersion'] = version
 #     with io.open(umbrella_chart, 'w', encoding='utf8') as outfile:
 #         ruamel.yaml.round_trip_dump(data_loaded, outfile, explicit_start=True)
 
