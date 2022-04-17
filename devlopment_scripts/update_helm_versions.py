b2eefa1a3d9bb3e1cd97c892b00aebfdfaa3ff31
Gal_Rami <galrami6@gmail.com>
2022-04-17 10:11:19 +0300

    test change image tag

--- devlopment_scripts/update_helm_versions.py
+++ devlopment_scripts/update_helm_versions.py
@@ -9,21 +9,20 @@ print(f'services : {services_list}')
 print(f'branch : {branch_name}')
 tmp = services_list[1:-1]
 services = tmp.split(", ")
-
 umbrella_chart = 'helm/helm_chart/charts/amplication/Chart.yaml'
-def my_function(version):
+
+def create_version(version):
   splited_version = version.split('.')
   return f'{splited_version[0]}.{splited_version[1]}.{build_number}'
-for service in services:
-    print(f'service : {service}')
-#     # print(f'first {services_list[0]}')
-#     # print(f'second {services_list[1]}')
 
-    service_chart = f'helm/helm_chart/charts/services/{service}/Chart.yaml'
+def update_helm(service, chart_path):
+    print(f'service : {service}')
+
+    service_chart = chart_path
     #update service chart:
     with open(service_chart, 'r') as stream:
         data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
-        version = my_function(data_loaded['version'])
+        version = create_version(data_loaded['version'])
         data_loaded['version'] = version
         data_loaded['appVersion'] = version
     with io.open(service_chart, 'w', encoding='utf8') as outfile:
@@ -38,21 +37,27 @@ for service in services:
 
     with open(umbrella_chart, 'r') as stream:
         data_loaded = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
-        version = my_function(data_loaded['dependencies'][num]['version'])
+        version = create_version(data_loaded['dependencies'][num]['version'])
         data_loaded['dependencies'][num]['version'] = version
-        version = my_function(data_loaded['version'])
+        version = create_version(data_loaded['version'])
         data_loaded['version'] = version
-        version = my_function(data_loaded['appVersion'])
+        version = create_version(data_loaded['appVersion'])
         data_loaded['appVersion'] = version
     with io.open(umbrella_chart, 'w', encoding='utf8') as outfile:
         ruamel.yaml.round_trip_dump(data_loaded, outfile, explicit_start=True)
 
+for service in services:
+    chart_path = f'helm/helm_chart/charts/services/{service}/Chart.yaml'
+    update_helm(service, chart_path)
+    
 if 'amplication-client' not in services:
     print("amplication-client package has no updates")
-    #pull from s3 
-    #load its chart yaml and give its version to umbrella 
+    service = 'amplication-client'
+    chart_path = f'/downloaded_helms/{service}/Chart.yaml'
+    update_helm(service, chart_path)
 
 if 'amplication-server' not in services:
     print("amplication-server package has no updates")
-    #pull from s3 
-    #load its chart yaml and give its version to umbrella 
+    service = 'amplication-server'
+    chart_path = f'/downloaded_helms/{service}/Chart.yaml'
+    update_helm(service, chart_path)
