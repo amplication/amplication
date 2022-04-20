06edf1411e2c5b55f37dc9c246812e3c52ef5637
Mor Hagbi <mor@amplication.com>
2022-04-20 12:34:57 +0300

    remove test

--- packages/amplication-git-push-webhook-service/src/app.controller.spec.ts
+++ packages/amplication-git-push-webhook-service/src/app.controller.spec.ts
@@ -2,15 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { QueueService } from './queue.service';
 
-describe('AppController', () => {
-  let appController: AppController;
+// describe('AppController', () => {
+//   let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [QueueService],
-    }).compile();
+//   beforeEach(async () => {
+//     const app: TestingModule = await Test.createTestingModule({
+//       controllers: [AppController],
+//       providers: [QueueService],
+//     }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-});
+//     appController = app.get<AppController>(AppController);
+//   });
+// });
