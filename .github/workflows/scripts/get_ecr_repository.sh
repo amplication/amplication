754e13db642256596f4685a7ab261c8dabae7be7
Ilia <iliag@sela.co.il>
2022-04-20 14:58:52 +0300

    fixed repo detection

--- .github/workflows/scripts/get_ecr_repository.sh
+++ .github/workflows/scripts/get_ecr_repository.sh
@@ -1,16 +1,15 @@
-REPO_NAME_TEMP=${GITHUB_REPOSITORY}
-REPO_NAME=${REPO_NAME##*/}
+REPO_NAME=${GITHUB_REPOSITORY##*/}
 echo "REPO_NAME: $REPO_NAME"
 if [ "$REPO_NAME" = "amplication" ]; then
-if [[ $GITHUB_REF != 'refs/heads/master' ]]; then
-    ECR_REPOSITORY_PREFIX="dev-staging-os"
+    if [[ "$GITHUB_REF" != 'refs/heads/master' ]]; then
+        ECR_REPOSITORY_PREFIX="dev-staging-os"
+    else
+        ECR_REPOSITORY_PREFIX="-staging-os"
+    fi
 else
-    ECR_REPOSITORY_PREFIX="-staging-os"
+    if [[ "$GITHUB_REF" != 'refs/heads/master' ]]; then
+        ECR_REPOSITORY_PREFIX="dev"
+    fi  
 fi
-else
-if [[ $GITHUB_REF != 'refs/heads/master' ]]; then
-    ECR_REPOSITORY_PREFIX="dev"
-fi  
-fi
-echo "ECR_REPOSITORY=${{ matrix.service }}${ECR_REPOSITORY_PREFIX}" >> $GITHUB_ENV
+echo "ECR_REPOSITORY=${SERVICE_NAME}${ECR_REPOSITORY_PREFIX}" >> $GITHUB_ENV
 echo "ECR_REPOSITORY: $ECR_REPOSITORY"
