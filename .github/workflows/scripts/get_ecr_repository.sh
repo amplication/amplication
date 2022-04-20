054e6c6693c49031127dc3700b4b2cd7875e0221
Ilia <iliag@sela.co.il>
2022-04-20 15:02:57 +0300

    fixed docker file

--- .github/workflows/scripts/get_ecr_repository.sh
+++ .github/workflows/scripts/get_ecr_repository.sh
@@ -1,13 +1,13 @@
 REPO_NAME=${GITHUB_REPOSITORY##*/}
 echo "REPO_NAME: $REPO_NAME"
 if [ "$REPO_NAME" = "amplication" ]; then
-    if [[ "$GITHUB_REF" != 'refs/heads/master' ]]; then
+    if [ "$GITHUB_REF" != 'refs/heads/master' ]; then
         ECR_REPOSITORY_PREFIX="dev-staging-os"
     else
         ECR_REPOSITORY_PREFIX="-staging-os"
     fi
 else
-    if [[ "$GITHUB_REF" != 'refs/heads/master' ]]; then
+    if [ "$GITHUB_REF" != 'refs/heads/master' ]; then
         ECR_REPOSITORY_PREFIX="dev"
     fi  
 fi
