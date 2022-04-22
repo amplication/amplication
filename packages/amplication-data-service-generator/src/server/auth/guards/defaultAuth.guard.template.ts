6c85d876b7b30add002b3a388e83ff13a063eebb
Amit Barletz <amit@amplication.com>
2022-04-22 23:12:53 +0300

    refactor guard

--- packages/amplication-data-service-generator/src/server/auth/guards/defaultAuth.guard.template.ts
+++ packages/amplication-data-service-generator/src/server/auth/guards/defaultAuth.guard.template.ts
@@ -1,5 +1,5 @@
 import { Reflector } from "@nestjs/core";
-import { ExecutionContext } from "@nestjs/common";
+import { ExecutionContext, Injectable } from "@nestjs/common";
 // @ts-ignore
 import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
@@ -7,25 +7,22 @@ declare class GUARD {
   canActivate(context: ExecutionContext): boolean | Promise<boolean>;
 }
 
+@Injectable()
 export class DefaultAuthGuard extends GUARD {
   constructor(private readonly reflector: Reflector) {
     super();
   }
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    let parentCanActivate = false;
-
-    try {
-      parentCanActivate = (await super.canActivate(context)) as boolean;
-    } catch (err) {
-      parentCanActivate = false;
-    }
-
-    const isPublic = this.reflector?.get<boolean>(
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.get<boolean>(
       IS_PUBLIC_KEY,
       context.getHandler()
     );
 
-    return isPublic || parentCanActivate;
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
   }
 }
