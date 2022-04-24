50e32472d11ad78213bcf9c9d0ae530dcadf8db3
Ilia <iliag@sela.co.il>
2022-04-24 11:55:08 +0300

    dixed retag command

--- .github/workflows/scripts/ecr_retag.sh
+++ .github/workflows/scripts/ecr_retag.sh
@@ -6,9 +6,7 @@ echo "MANIFEST: $MANIFEST"
 for tag in $(echo $TAGS | sed "s/,/ /g")
 do
     fixed_tag=$(echo $tag | sed 's/[^a-zA-Z0-9]/-/g')
-    command="aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag $fixed_tag --image-manifest $MANIFEST"
-    echo "$command"
-    $command
+    aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag $fixed_tag --image-manifest "$MANIFEST"
 done
 
 
