43801d1b9e8762c4f8227fe053b1185f49bb166e
Ilia <iliag@sela.co.il>
2022-04-24 11:04:38 +0300

    fixed tags loop

--- .github/workflows/scripts/ecr_retag.sh
+++ .github/workflows/scripts/ecr_retag.sh
@@ -3,11 +3,12 @@ echo "TAGS=$TAGS"
 echo "ECR_REPOSITORY=$ECR_REPOSITORY"
 MANIFEST=$(aws ecr batch-get-image --repository-name $ECR_REPOSITORY --image-ids imageTag=master --output json | jq --raw-output --join-output '.images[0].imageManifest')
 
-for tag in ${TAGS//,/ }
+for tag in $(echo $TAGS | sed "s/,/ /g")
 do
     fixed_tag=$(echo $tag | sed 's/[^a-zA-Z0-9]/-/g')
     command="aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag $fixed_tag --image-manifest $MANIFEST"
     echo "$command"
     $command
 done
+
 
