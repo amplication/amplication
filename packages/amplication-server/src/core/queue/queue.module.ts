fb6ef27138994fb1fe823d95c38e9b5e0fb86b64
tupe12334 <ofek.gabay.he@gmail.com>
2022-04-25 12:52:17 +0300

    refactor(server)

--- packages/amplication-server/src/core/queue/queue.module.ts
+++ packages/amplication-server/src/core/queue/queue.module.ts
@@ -1,33 +1,14 @@
+import { createNestjsKafkaConfig } from '@amplication/kafka';
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientsModule } from '@nestjs/microservices';
 import { QueueService, QUEUE_SERVICE_NAME } from './queue.service';
 
-export const KAFKA_BROKER_URL_VAR = 'KAFKA_BROKER_URL_VAR';
-const clientId = 'server-queue-client';
 @Module({
   imports: [
     ClientsModule.registerAsync([
       {
         name: QUEUE_SERVICE_NAME,
-        useFactory: (configService: ConfigService) => {
-          const envBrokerIp = configService.get<string>(KAFKA_BROKER_URL_VAR);
-          if (!envBrokerIp) {
-            throw new Error('Missing broker ip in env file');
-          }
-          const brokers = envBrokerIp.split(',');
-          return {
-            transport: Transport.KAFKA,
-            options: {
-              client: {
-                clientId,
-                brokers
-              },
-              consumer: { groupId: 'main' }
-            }
-          };
-        },
-        inject: [ConfigService]
+        useFactory: createNestjsKafkaConfig
       }
     ])
   ],
