c0a112a0b5af00d72795925db4e7fc5d6877d971
Ilia <iliag@sela.co.il>
2022-04-29 23:25:59 +0300

    fixed syntax

--- .github/workflows/scripts/ecr_retag.sh
+++ .github/workflows/scripts/ecr_retag.sh
@@ -10,17 +10,15 @@ do
     fixed_tag=$(echo $tag | sed 's/[^a-zA-Z0-9]/-/g')
     echo "fixed_tag: $fixed_tag"
     echo "aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag $fixed_tag --image-manifest $MANIFEST"
-    (aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag $fixed_tag --image-manifest "$MANIFEST")&>tag_result
-    retag_response=$(cat tag_result)
+    (aws ecr put-image --repository-name $ECR_REPOSITORY --image-tag $fixed_tag --image-manifest "$MANIFEST")&>$GITHUB_WORKSPACE/tag_result
+    retag_response=$(cat $GITHUB_WORKSPACE/tag_result)
     echo "retag_response: $retag_response"
-    if [ "$retag_response" == *"already exists in the repository with name"* ]; then
+    if [[ $retag_response == *"already exists in the repository with name"* ]]; then
         echo "Already exists in the repository with name: $fixed_tag"
     else
-        if [ "$retag_response" == *"error"* ] || [ "$retag_response" == *"ERROR"* ]; then
+        if [[ $retag_response == *"error"* ]] || [[ $retag_response == *"ERROR"* ]]; then
             echo "Failed to re tag docker $fixed_tag \n $retag_response"
             exit -1
         fi
     fi
 done
-
-
