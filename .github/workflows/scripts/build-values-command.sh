22aceb1ef7f86a8cbe328875879a34f15a1faf07
Ilia <iliag@sela.co.il>
2022-05-02 23:42:38 +0300

    adde dlogs

--- .github/workflows/scripts/build-values-command.sh
+++ .github/workflows/scripts/build-values-command.sh
@@ -12,8 +12,10 @@ do
     SERVICE_NAME="$(basename $dir)"
     REPO_NAME="$SERVICE_NAME-$TARGET_ENV"
     echo "SERVICE_NAME: $SERVICE_NAME"
-    tag_list=$(aws ecr describe-images --repository-name $REPO_NAME --image-ids imageTag=$IMAGE_TAG_ANCHOR --region us-east-1 2>&1 | jq -r '.imageDetails[0].imageTags' | jq -r '.[]')
+    tag_list="$(aws ecr describe-images --repository-name=$REPO_NAME --region us-east-1 --image-ids=imageTag=$IMAGE_TAG_ANCHOR 2> /dev/null )"
+    image_tags=$(echo $tag_list | jq -r '.imageDetails[0].imageTags' 2>&1)
     echo "tag_list: $tag_list"
+    echo "image_tags: $image_tags"
     VERSIONS=""
     
     echo "$IMAGE_TAG_ANCHOR" > found_tag
@@ -23,7 +25,7 @@ do
         if [[ $line =~ $re ]] ; then
           echo "$line" > found_tag
         fi
-    done <<< "$tag_list"
+    done <<< "$image_tags"
     
     FOUND_TAG=$(cat found_tag)
     if [ -z "$FOUND_TAG" ]
