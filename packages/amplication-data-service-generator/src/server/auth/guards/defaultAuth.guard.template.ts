59442431e7a87e2b5850d8c1e0696e883e519297
Amit Barletz <amit@amplication.com>
2022-05-03 11:52:40 +0300

    feat(dsg) check is public on default guard

--- packages/amplication-data-service-generator/src/server/auth/guards/defaultAuth.guard.template.ts
+++ packages/amplication-data-service-generator/src/server/auth/guards/defaultAuth.guard.template.ts
@@ -1,3 +1,29 @@
-declare class GUARD {}
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+// @ts-ignore
+import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
-export class DefaultAuthGuard extends GUARD {}
+declare class GUARD {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean>;
+  }
+
+@Injectable()
+export class DefaultAuthGuard extends GUARD {
+    constructor(private readonly reflector: Reflector) {
+        super();
+      }
+    
+      canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    
+        const isPublic = this.reflector.get<boolean>(
+          IS_PUBLIC_KEY,
+          context.getHandler()
+        );
+    
+        if (isPublic) {
+            return true;
+        }
+
+        return super.canActivate(context);
+      }
+}
