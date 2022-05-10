931d3975e854f11125093eedf1680b87b859159e
yuval-hazaz <hazaz.yuval@gmail.com>
2022-05-10 08:55:45 +0300

    design-system: run format

--- packages/amplication-design-system/.storybook/main.js
+++ packages/amplication-design-system/.storybook/main.js
@@ -1,8 +1,13 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions","@storybook/preset-scss"],
-  "framework": "@storybook/react",
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
+  ],
+  framework: "@storybook/react",
   core: {
-    builder: "webpack5"
+    builder: "webpack5",
   },
 };
