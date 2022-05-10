927e006129072d6e1976b6fe519b38844e493e1f
Mor Hagbi <mor@amplication.com>
2022-05-10 14:58:41 +0300

    get the correct tree files object

--- packages/amplication-client/src/Application/code-view/CommitFilesMenu.tsx
+++ packages/amplication-client/src/Application/code-view/CommitFilesMenu.tsx
@@ -1,19 +1,44 @@
-import React from "react";
 import { RichObjectTreeView } from "@amplication/design-system";
+import React from "react";
+//import axios from "axios";
 //import * as models from "../../models";
-
-// type TData = {
-//   pendingChanges: models.PendingChange[];
-// };
 
 type Props = {
   applicationId: string;
+  buildId?: string;
+  path: string;
+  onFileSelected: (nodeId: string) => void;
 };
 
-const CommitFilesMenu = ({ applicationId }: Props) => {
+const CommitFilesMenu = ({
+  applicationId,
+  buildId,
+  path = "",
+  onFileSelected,
+}: Props) => {
+  //const [files, setFilesTree] = useState<Files>();
+
+  const handleSelect = (event: React.SyntheticEvent, nodeId: string) => {
+    if (nodeId.includes("file")) {
+      onFileSelected(nodeId);
+    }
+  };
+
+  // axios
+  //   .get(`http://storage/${applicationId}/${buildId}/list?${path}`)
+  //   .then((res) => {
+  //     const treeFiles = res.data;
+  //     setFilesTree(treeFiles);
+  //     console.log(files);
+  //   });
+
   return (
     <div>
-      <RichObjectTreeView />
+      <RichObjectTreeView
+        onNodeSelect={(event, nodeId) => {
+          handleSelect(event, nodeId);
+        }}
+      />
     </div>
   );
 };
--- packages/amplication-design-system/src/components/TreeView/TreeView.tsx
+++ packages/amplication-design-system/src/components/TreeView/TreeView.tsx
@@ -3,36 +3,50 @@ import TreeView from "@mui/lab/TreeView";
 import TreeItem from "@mui/lab/TreeItem";
 import { Icon } from "../..";
 
+type Props = {
+  onNodeSelect: (event: React.SyntheticEvent, nodeId: string) => void;
+};
 interface RenderTree {
   id: string;
+  type: string;
   name: string;
   children?: readonly RenderTree[];
 }
 
 const data: RenderTree = {
   id: "root",
+  type: "",
   name: "Parent",
   children: [
     {
       id: "1",
-      name: "Child - 1",
+      type: "file",
+      name: "src",
+    },
+    {
+      id: "2",
+      type: "file",
+      name: "src2",
     },
     {
       id: "3",
-      name: "Child - 3",
-      children: [
-        {
-          id: "4",
-          name: "Child - 4",
-        },
-      ],
+      type: "folder",
+      name: "package.json",
     },
   ],
 };
 
-export function RichObjectTreeView() {
+export function RichObjectTreeView({ onNodeSelect }: Props) {
+  //{ files }: Props
+  //const [expanded, setExpanded] = useState<string[]>([]);
+  //const [selected, setSelected] = useState<string[]>([]);
+
   const renderTree = (nodes: RenderTree) => (
-    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+    <TreeItem
+      key={nodes.name}
+      nodeId={nodes.id + nodes.type}
+      label={nodes.name}
+    >
       {Array.isArray(nodes.children)
         ? nodes.children.map((node) => renderTree(node))
         : null}
@@ -45,6 +59,9 @@ export function RichObjectTreeView() {
       defaultCollapseIcon={<Icon icon="folder" size="small" />}
       defaultExpanded={["root"]}
       defaultExpandIcon={<Icon icon="file" size="small" />}
+      onNodeSelect={(event: React.SyntheticEvent, nodeId: string) =>
+        onNodeSelect(event, nodeId)
+      }
       sx={{ height: 110, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
     >
       {renderTree(data)}
