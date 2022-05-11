9904166bb82820852db931be0b8a68a1536638e2
Matan Shidlov <matan@amplication.com>
2022-05-11 17:03:58 +0300

    add login logging and reference redirect

--- packages/amplication-server/src/core/auth/auth.controller.ts
+++ packages/amplication-server/src/core/auth/auth.controller.ts
@@ -5,17 +5,19 @@ import {
   Get,
   Res,
   Req,
-  UseFilters
+  UseFilters, Logger, Inject
 } from '@nestjs/common';
 import { MorganInterceptor } from 'nest-morgan';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
 import { AuthService, AuthUser } from './auth.service';
 import { GithubAuthExceptionFilter } from 'src/filters/github-auth-exception.filter';
+import {WINSTON_MODULE_PROVIDER} from "nest-winston";
 
 @Controller('/')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,
+              @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
 
   @UseInterceptors(MorganInterceptor('combined'))
   @Get('/github')
@@ -29,11 +31,12 @@ export class AuthController {
   @Get('/github/callback')
   @UseGuards(AuthGuard('github'))
   async githubCallback(@Req() request: Request, @Res() response: Response) {
-    const token = await this.authService.prepareToken(request.user as AuthUser);
-
-    const host =
-      request.hostname === 'localhost' ? 'http://localhost:3001' : '';
-
-    response.redirect(301, `${host}/?token=${token}`);
+    const user:AuthUser = request.user as AuthUser;
+    this.logger.log({
+      level: 'info',
+      message: `receive login callback from github account_id=${user.account.id}`,
+    })
+    const token = await this.authService.prepareToken(user);
+    response.redirect(301, `${request.headers.referer}?token=${token}`);
   }
 }
