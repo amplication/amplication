f6d0a80fa2a7b3e37e73eb0c81e047d4e831f3f4
Ilia <iliag@sela.co.il>
2022-05-16 10:58:24 +0300

    fixed builds list

--- .github/workflows/scripts/get-service-build-list.py
+++ .github/workflows/scripts/get-service-build-list.py
@@ -16,7 +16,7 @@ services_retag_output_file=os.getenv('SERVICES_RETAG_OUPTUT_PATH',os.path.join(r
 helm_services_folder=os.getenv('HELM_SERVICES_FOLDER',os.path.join(root_folder,'helm/charts/services'))
 packages_folder=os.getenv('PACKAGES_FOLDER',os.path.join(root_folder,'packages'))
 changed_folders=[]
-changed_folders=["amplication-cli", "amplication-client", "amplication-container-builder", "amplication-data", "amplication-data-service-generator", "amplication-deployer", "amplication-design-system", "amplication-scheduler", "amplication-server"]
+#changed_folders=["amplication-cli", "amplication-client", "amplication-container-builder", "amplication-data", "amplication-data-service-generator", "amplication-deployer", "amplication-design-system", "amplication-scheduler", "amplication-server"]
 changed_files=os.getenv('CHANGED_FILES_PR') or os.getenv('CHANGED_FILES_NOT_PR')
 
 package_build_list=[]
@@ -127,7 +127,7 @@ f.close()
 
 print(f"Will build the follwoing services: {service_build_list}")
 with open(services_output_file, 'w') as outfile:
-    service_build_list_fixed = json.dumps(list(service_build_list.keys()))
+    service_build_list_fixed = json.dumps(service_build_list)
     json.dump(service_build_list_fixed, outfile)
 print(f"Will build the follwoing pcakges: {package_build_list}")
 with open(packages_output_file, 'w', encoding='utf-8') as outfile:
