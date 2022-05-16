8b27741175d847f090ea49d0c24a469d1db10617
Ilia <iliag@sela.co.il>
2022-05-16 14:18:33 +0300

    disbale log

--- .github/workflows/scripts/get-service-build-list.py
+++ .github/workflows/scripts/get-service-build-list.py
@@ -16,7 +16,7 @@ services_retag_output_file=os.getenv('SERVICES_RETAG_OUPTUT_PATH',os.path.join(r
 helm_services_folder=os.getenv('HELM_SERVICES_FOLDER',os.path.join(root_folder,'helm/charts/services'))
 packages_folder=os.getenv('PACKAGES_FOLDER',os.path.join(root_folder,'packages'))
 changed_folders=[]
-changed_folders=["amplication-cli", "amplication-client", "amplication-container-builder", "amplication-data", "amplication-data-service-generator", "amplication-deployer", "amplication-design-system", "amplication-scheduler", "amplication-server"]
+#changed_folders=["amplication-cli", "amplication-client", "amplication-container-builder", "amplication-data", "amplication-data-service-generator", "amplication-deployer", "amplication-design-system", "amplication-scheduler", "amplication-server"]
 changed_files=os.getenv('CHANGED_FILES_PR') or os.getenv('CHANGED_FILES_NOT_PR')
 
 package_build_list=[]
