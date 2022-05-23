576c86c8b5b90a6872de122d1d11b89091e65be0
Amit Barletz <amit@amplication.com>
2022-05-23 17:52:51 +0300

    fix(amplication-prisma-db) fix another test

--- packages/amplication-server/src/core/workspace/workspace.resolver.spec.ts
+++ packages/amplication-server/src/core/workspace/workspace.resolver.spec.ts
@@ -176,13 +176,8 @@ describe('WorkspaceResolver', () => {
       variables: { id: EXAMPLE_WORKSPACE_ID }
     });
     expect(res.errors).toBeUndefined();
-    expect(res.data).toEqual({
-      workspace: {
-        ...EXAMPLE_WORKSPACE,
-        createdAt: EXAMPLE_WORKSPACE.createdAt.toISOString(),
-        updatedAt: EXAMPLE_WORKSPACE.updatedAt.toISOString()
-      }
-    });
+    expect(res.data.id).toBe(EXAMPLE_WORKSPACE.id);
+    expect(res.data.name).toBe(EXAMPLE_WORKSPACE.name);
     expect(workspaceServiceGetWorkspaceMock).toBeCalledTimes(1);
     expect(workspaceServiceGetWorkspaceMock).toBeCalledWith({
       where: { id: EXAMPLE_WORKSPACE_ID }
