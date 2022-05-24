ab0ff13fce39e1d570dc6af793871477b06dfc65
yuval-hazaz <hazaz.yuval@gmail.com>
2022-05-24 14:16:45 +0300

    style(design-system): add button style

--- packages/amplication-client/src/VersionControl/BuildSummary.tsx
+++ packages/amplication-client/src/VersionControl/BuildSummary.tsx
@@ -226,7 +226,7 @@ const BuildSummary = ({ generating, build, onError }: Props) => {
           stepDeploy?.status === models.EnumActionStepStatus.Running ? (
             <Link to={`/${build.appId}/builds/${build.id}`}>
               <Button
-                buttonStyle={EnumButtonStyle.Secondary}
+                buttonStyle={EnumButtonStyle.Clear}
                 eventData={{
                   eventName: "BuildSandboxViewDetailsClick",
                 }}
@@ -241,7 +241,7 @@ const BuildSummary = ({ generating, build, onError }: Props) => {
             stepDeploy?.status === models.EnumActionStepStatus.Success ? (
             <a href={deployment.environment.address} target="app">
               <Button
-                buttonStyle={EnumButtonStyle.Secondary}
+                buttonStyle={EnumButtonStyle.Clear}
                 icon="link_2"
                 eventData={{
                   eventName: "openPreviewApp",
@@ -256,7 +256,7 @@ const BuildSummary = ({ generating, build, onError }: Props) => {
               {stepDeploy ? (
                 <Link to={`/${build.appId}/builds/${build.id}`}>
                   <Button
-                    buttonStyle={EnumButtonStyle.Secondary}
+                    buttonStyle={EnumButtonStyle.Clear}
                     eventData={{
                       eventName: "buildFailedViewDetails",
                     }}
