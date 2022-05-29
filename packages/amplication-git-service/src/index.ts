ec89ee94b06671d0243530c99ad0677bffe20c97
tupe12334 <ofek.gabay.he@gmail.com>
2022-05-29 18:54:49 +0300

    format

--- packages/amplication-git-service/src/index.ts
+++ packages/amplication-git-service/src/index.ts
@@ -3,4 +3,4 @@ export { GitService } from './services/git.service';
 export { GithubService } from './providers/github.service';
 export { GitServiceFactory } from './utils/GitServiceFactory';
 export { EnumGitOrganizationType } from './Dto/enums/EnumGitOrganizationType';
-export { EnumGitProvider } from './Dto/enums/EnumGitProvider'; 
+export { EnumGitProvider } from './Dto/enums/EnumGitProvider';
