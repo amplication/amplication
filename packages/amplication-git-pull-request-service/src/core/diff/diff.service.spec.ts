8e8d14c8f65afbc788dbf6d16e1ee63e74377449
tupe12334 <ofek.gabay.he@gmail.com>
2022-06-01 14:06:56 +0300

    fix(pr): test

--- packages/amplication-git-pull-request-service/src/core/diff/diff.service.spec.ts
+++ packages/amplication-git-pull-request-service/src/core/diff/diff.service.spec.ts
@@ -3,10 +3,20 @@ import { MOCK_BUILD_PATH_FACTORY } from './utils/BuildPathFactory.mock';
 
 const amplicationAppId = 'appId';
 
+const logger = {
+  debug: jest.fn(),
+  log: jest.fn(),
+  info: jest.fn(),
+  error: jest.fn(),
+  format: Symbol('EXAMPLE_LOGGER_FORMAT'),
+};
+
 describe('Testing the diff service', () => {
   let diffService: DiffService;
   beforeEach(() => {
-    diffService = new DiffService(MOCK_BUILD_PATH_FACTORY);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    diffService = new DiffService(MOCK_BUILD_PATH_FACTORY, logger);
   });
   it('should throw error if the builds id is the same', () => {
     return expect(
