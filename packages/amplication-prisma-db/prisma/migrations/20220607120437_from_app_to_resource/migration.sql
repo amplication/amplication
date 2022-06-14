9bf363196251c0b7c73a2f4ed4ddab56ed7f6489
tupe12334 <ofek.gabay.he@gmail.com>
2022-06-14 18:24:12 +0300

    fix(sql): add index

--- packages/amplication-prisma-db/prisma/migrations/20220607120437_from_app_to_resource/migration.sql
+++ packages/amplication-prisma-db/prisma/migrations/20220607120437_from_app_to_resource/migration.sql
@@ -10,6 +10,7 @@ ALTER INDEX "App.workspaceId_name_unique" RENAME TO "Resource.workspaceId_name_u
 ALTER TABLE "GitRepository"
 RENAME "appId" TO "resourceId";
 ALTER TABLE "GitRepository" RENAME CONSTRAINT  "GitRepository_appId_fkey" TO "GitRepository_resourceId_fkey";
+ALTER INDEX "GitRepository_appId_key" RENAME TO "GitRepository_resourceId_key";
 
 -- Commit work
 ALTER TABLE "Commit"
