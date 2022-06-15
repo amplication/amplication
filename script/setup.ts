568dced92a22a9413cf7a92d3110a384239a42c2
Amit Barletz <amit@amplication.com>
2022-06-15 12:46:59 +0300

    updated amplication server readme with env environment added ignore to prettier and add info log to setup

--- script/setup.ts
+++ script/setup.ts
@@ -148,6 +148,7 @@ if (require.main === module) {
         console.log("");
       }
       logger.info("Finish all the process for the setup, have fun hacking 👾");
+      logger.info("✋ To run a specific service, go to its README file and make sure you set all necessary environment variables");
     } catch (error) {
       spinner.fail(error.message);
     }
