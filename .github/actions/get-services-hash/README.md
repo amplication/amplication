f5ba5b0afadd261604a62a3bed2bb2a0556653e8
Matan Shidlov <matan@amplication.com>
2022-06-19 15:24:18 +0300

    add setup script to `get-services-hash` github action + clean aws-ci

---
+++ .github/actions/get-services-hash/README.md
@@ -0,0 +1,16 @@
+# Create services hash action
+
+This action prints create code hash fingerprint for each service in Amplication helm charts.
+
+## Outputs
+
+## `service_hashes`
+
+A list of services and services metadata including code hash.
+
+### Service Hash Element
+
+
+## Example usage
+
+uses: ./.github/actions/get-services-hash
---
+++ .github/actions/get-services-hash/scripts/setup.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+
+console.log("running npm install");
+
+exec("npm install", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
