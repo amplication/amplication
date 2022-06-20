60dc280ba5839d2d02e7fa3b823f5d95944e1dce
Shahar <shaharb@sela.co.il>
2022-06-20 09:46:19 +0200

    delete folders

--- .github/workflows/scripts/get-service-build-list.py
+++ .github/workflows/scripts/get-service-build-list.py
@@ -19,7 +19,6 @@ packages_folder = os.getenv(
 ee_packages_folder = os.getenv(
     'EE_PACKAGES_FOLDER', os.path.join(root_folder, 'ee/packages'))
 changed_folders = []
-changed_folders=["amplication-cli", "amplication-client", "amplication-container-builder", "amplication-data", "amplication-data-service-generator", "amplication-deployer", "amplication-design-system", "amplication-scheduler", "amplication-server"]
 changed_files = os.getenv('CHANGED_FILES_PR') or os.getenv(
     'CHANGED_FILES_NOT_PR')
 
