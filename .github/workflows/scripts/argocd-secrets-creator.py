b49f81a4cdce739c008ab6126ac3e0fd908f8af4
Shahar <shaharb@sela.co.il>
2022-06-23 12:53:49 +0200

    rollback

--- .github/workflows/scripts/argocd-secrets-creator.py
+++ .github/workflows/scripts/argocd-secrets-creator.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 root_folder=os.getenv('GITHUB_WORKSPACE',Path(__file__).parents[3])
 output_folder=os.getenv('OUTPUT_FOLDER',os.path.join(root_folder,'secrets'))
-secrets_file=os.getenv('SECRETS_FILE_PATH',os.path.join("/Users/iliagerman/Work/Sela/Clients/amplication/amplication/helm/charts/amplication/values-secrets-staging.json"))
+secrets_file=os.getenv('SECRETS_FILE_PATH',os.path.join("/Users/iliagerman/Work/Sela/Clients/amplication/amplication/helm/charts/amplication/values-secrets-staging-os.json"))
 secrets_template=os.getenv('SECRETS_TEMPLATE_FILE_PATH',os.path.join("/Users/iliagerman/Work/Sela/Clients/amplication/amplication/.github/workflows/scripts/secret_template.yaml"))
 
 print(f"root_folder: {root_folder}")
