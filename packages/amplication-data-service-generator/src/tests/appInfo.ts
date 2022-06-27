7090da47fd07e2dc7b7d15c9b61507290d03b964
barshimi <barshimi@gmail.com>
2022-06-27 13:52:31 +0300

    add new custom apps to snapshot

--- packages/amplication-data-service-generator/src/tests/appInfo.ts
+++ packages/amplication-data-service-generator/src/tests/appInfo.ts
@@ -1,29 +1,115 @@
 import { EnumAuthProviderType } from "../models";
 import { AppInfo } from "../types";
 
-const appInfo: AppInfo = {
-  name: "Sample Application",
-  description: "Sample application for testing",
-  version: "0.1.3",
-  id: "ckl0ow1xj00763cjnch10k6mc",
-  url: "https://app.amplication.com/ckl0ow1xj00763cjnch10k6mc",
-  settings: {
-    dbHost: "localhost",
-    dbName: "db-name",
-    dbPort: 5433,
-    dbPassword: "1234",
-    dbUser: "testUsername",
-    authProvider: EnumAuthProviderType.Http,
-    serverSettings: {
-      generateGraphQL: true,
-      generateRestApi: true,
-      serverPath: "",
+const appInfo: [string, AppInfo][] = [
+  [
+    "RegularApp",
+    {
+      name: "Sample Application",
+      description: "Sample application for testing",
+      version: "0.1.3",
+      id: "ckl0ow1xj00763cjnch10k6mc",
+      url: "https://app.amplication.com/ckl0ow1xj00763cjnch10k6mc",
+      settings: {
+        dbHost: "localhost",
+        dbName: "db-name",
+        dbPort: 5433,
+        dbPassword: "1234",
+        dbUser: "testUsername",
+        authProvider: EnumAuthProviderType.Http,
+        serverSettings: {
+          generateGraphQL: true,
+          generateRestApi: true,
+          serverPath: "",
+        },
+        adminUISettings: {
+          generateAdminUI: true,
+          adminUIPath: "",
+        },
+      },
     },
-    adminUISettings: {
-      generateAdminUI: true,
-      adminUIPath: "",
+  ],
+  [
+    "WithoutGraphQL",
+    {
+      name: "Sample Application",
+      description: "Sample application for testing",
+      version: "0.1.3",
+      id: "ckl0ow1xj00763cjnch10k6mc",
+      url: "https://app.amplication.com/ckl0ow1xj00763cjnch10k6mc",
+      settings: {
+        dbHost: "localhost",
+        dbName: "db-name",
+        dbPort: 5433,
+        dbPassword: "1234",
+        dbUser: "testUsername",
+        authProvider: EnumAuthProviderType.Http,
+        serverSettings: {
+          generateGraphQL: false,
+          generateRestApi: true,
+          serverPath: "",
+        },
+        adminUISettings: {
+          generateAdminUI: true,
+          adminUIPath: "",
+        },
+      },
     },
-  },
-};
+  ],
+  [
+    "WithoutRestApi",
+    {
+      name: "Sample Application",
+      description: "Sample application for testing",
+      version: "0.1.3",
+      id: "ckl0ow1xj00763cjnch10k6mc",
+      url: "https://app.amplication.com/ckl0ow1xj00763cjnch10k6mc",
+      settings: {
+        dbHost: "localhost",
+        dbName: "db-name",
+        dbPort: 5433,
+        dbPassword: "1234",
+        dbUser: "testUsername",
+        authProvider: EnumAuthProviderType.Http,
+        serverSettings: {
+          generateGraphQL: true,
+          generateRestApi: false,
+          serverPath: "",
+        },
+        adminUISettings: {
+          generateAdminUI: true,
+          adminUIPath: "",
+        },
+      },
+    },
+  ],
+  [
+    "WithCustomPath",
+    {
+      name: "Sample Application",
+      description: "Sample application for testing",
+      version: "0.1.3",
+      id: "ckl0ow1xj00763cjnch10k6mc",
+      url: "https://app.amplication.com/ckl0ow1xj00763cjnch10k6mc",
+      settings: {
+        dbHost: "localhost",
+        dbName: "db-name",
+        dbPort: 5433,
+        dbPassword: "1234",
+        dbUser: "testUsername",
+        authProvider: EnumAuthProviderType.Http,
+        serverSettings: {
+          generateGraphQL: true,
+          generateRestApi: true,
+          serverPath: "test",
+        },
+        adminUISettings: {
+          generateAdminUI: true,
+          adminUIPath: "test-ui",
+        },
+      },
+    },
+  ],
+];
 
 export default appInfo;
--- packages/amplication-data-service-generator/src/tests/create-data-service.spec.ts
+++ packages/amplication-data-service-generator/src/tests/create-data-service.spec.ts
@@ -14,7 +14,7 @@ const MODULE_EXTENSIONS_TO_SNAPSHOT = [
 jest.setTimeout(100000);
 
 describe("createDataService", () => {
-  test("creates app as expected", async () => {
+  test.each(appInfo)("%s", async (name, appInfo) => {
     const modules = await createDataService(entities, roles, appInfo);
     const modulesToSnapshot = modules.filter((module) =>
       MODULE_EXTENSIONS_TO_SNAPSHOT.some((extension) =>
