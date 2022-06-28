eec0540f32f837b6470b3084036f6092c47206f9
tupe12334 <ofek.gabay.he@gmail.com>
2022-06-28 14:39:04 +0300

    refactor(dsg)

--- packages/amplication-client/src/models.ts
+++ packages/amplication-client/src/models.ts
@@ -75,39 +75,6 @@ export type ApiTokenCreateInput = {
   name: Scalars["String"];
 };
 
-export type AppSettings = IBlock & {
-  __typename?: "AppSettings";
-  authProvider: EnumAuthProviderType;
-  blockType: EnumBlockType;
-  createdAt: Scalars["DateTime"];
-  dbHost: Scalars["String"];
-  dbName: Scalars["String"];
-  dbPassword: Scalars["String"];
-  dbPort: Scalars["Int"];
-  dbUser: Scalars["String"];
-  description: Scalars["String"];
-  displayName: Scalars["String"];
-  id: Scalars["String"];
-  inputParameters: Array<BlockInputOutput>;
-  lockedAt?: Maybe<Scalars["DateTime"]>;
-  lockedByUserId?: Maybe<Scalars["String"]>;
-  outputParameters: Array<BlockInputOutput>;
-  parentBlock?: Maybe<Block>;
-  updatedAt: Scalars["DateTime"];
-  versionNumber: Scalars["Float"];
-};
-
-export type AppSettingsUpdateInput = {
-  authProvider: EnumAuthProviderType;
-  dbHost: Scalars["String"];
-  dbName: Scalars["String"];
-  dbPassword: Scalars["String"];
-  dbPort: Scalars["Int"];
-  dbUser: Scalars["String"];
-  description?: InputMaybe<Scalars["String"]>;
-  displayName?: InputMaybe<Scalars["String"]>;
-};
-
 export type Auth = {
   __typename?: "Auth";
   /** JWT Bearer token */
@@ -870,7 +837,6 @@ export enum EnumAuthProviderType {
 }
 
 export enum EnumBlockType {
-  AppSettings = "AppSettings",
   CanvasPage = "CanvasPage",
   ConnectorFile = "ConnectorFile",
   ConnectorRestApi = "ConnectorRestApi",
@@ -883,6 +849,7 @@ export enum EnumBlockType {
   Flow = "Flow",
   FlowApi = "FlowApi",
   Layout = "Layout",
+  ServiceSettings = "ServiceSettings",
 }
 
 export type EnumBlockTypeFilter = {
@@ -1163,7 +1130,6 @@ export type Mutation = {
   setCurrentWorkspace: Auth;
   signup: Auth;
   updateAccount: Account;
-  updateAppSettings?: Maybe<AppSettings>;
   updateConnectorRestApi: ConnectorRestApi;
   updateConnectorRestApiCall: ConnectorRestApiCall;
   updateEntity?: Maybe<Entity>;
@@ -1174,6 +1140,7 @@ export type Mutation = {
   updateEntityPermissionRoles: EntityPermission;
   updateResource?: Maybe<Resource>;
   updateResourceRole?: Maybe<ResourceRole>;
+  updateServiceSettings?: Maybe<ServiceSettings>;
   updateWorkspace?: Maybe<Workspace>;
 };
 
@@ -1346,11 +1313,6 @@ export type MutationUpdateAccountArgs = {
   data: UpdateAccountInput;
 };
 
-export type MutationUpdateAppSettingsArgs = {
-  data: AppSettingsUpdateInput;
-  where: WhereUniqueInput;
-};
-
 export type MutationUpdateConnectorRestApiArgs = {
   data: BlockUpdateInput;
   where: WhereUniqueInput;
@@ -1398,6 +1360,11 @@ export type MutationUpdateResourceArgs = {
 
 export type MutationUpdateResourceRoleArgs = {
   data: ResourceRoleUpdateInput;
+  where: WhereUniqueInput;
+};
+
+export type MutationUpdateServiceSettingsArgs = {
+  data: ServiceSettingsUpdateInput;
   where: WhereUniqueInput;
 };
 
@@ -1453,7 +1420,6 @@ export type Query = {
   __typename?: "Query";
   account: Account;
   action: Action;
-  appSettings: AppSettings;
   block: Block;
   blocks: Array<Block>;
   build: Build;
@@ -1481,6 +1447,7 @@ export type Query = {
   resourceRoles: Array<ResourceRole>;
   resources: Array<Resource>;
   resourceValidateBeforeCommit: ResourceValidationResult;
+  serviceSettings: ServiceSettings;
   userApiTokens: Array<ApiToken>;
   workspace?: Maybe<Workspace>;
   workspaceMembers?: Maybe<Array<WorkspaceMember>>;
@@ -1488,10 +1455,6 @@ export type Query = {
 };
 
 export type QueryActionArgs = {
-  where: WhereUniqueInput;
-};
-
-export type QueryAppSettingsArgs = {
   where: WhereUniqueInput;
 };
 
@@ -1629,6 +1592,10 @@ export type QueryResourceValidateBeforeCommitArgs = {
   where: WhereUniqueInput;
 };
 
+export type QueryServiceSettingsArgs = {
+  where: WhereUniqueInput;
+};
+
 export type QueryWorkspaceArgs = {
   where: WhereUniqueInput;
 };
@@ -1666,6 +1633,7 @@ export type Resource = {
   gitRepositoryId?: Maybe<Scalars["String"]>;
   id: Scalars["String"];
   name: Scalars["String"];
+  type: EnumResourceType;
   updatedAt: Scalars["DateTime"];
   workspace: Workspace;
 };
@@ -1791,6 +1759,39 @@ export enum Role {
   ProjectAdmin = "ProjectAdmin",
   User = "User",
 }
+
+export type ServiceSettings = IBlock & {
+  __typename?: "ServiceSettings";
+  authProvider: EnumAuthProviderType;
+  blockType: EnumBlockType;
+  createdAt: Scalars["DateTime"];
+  dbHost: Scalars["String"];
+  dbName: Scalars["String"];
+  dbPassword: Scalars["String"];
+  dbPort: Scalars["Int"];
+  dbUser: Scalars["String"];
+  description: Scalars["String"];
+  displayName: Scalars["String"];
+  id: Scalars["String"];
+  inputParameters: Array<BlockInputOutput>;
+  lockedAt?: Maybe<Scalars["DateTime"]>;
+  lockedByUserId?: Maybe<Scalars["String"]>;
+  outputParameters: Array<BlockInputOutput>;
+  parentBlock?: Maybe<Block>;
+  updatedAt: Scalars["DateTime"];
+  versionNumber: Scalars["Float"];
+};
+
+export type ServiceSettingsUpdateInput = {
+  authProvider: EnumAuthProviderType;
+  dbHost: Scalars["String"];
+  dbName: Scalars["String"];
+  dbPassword: Scalars["String"];
+  dbPort: Scalars["Int"];
+  dbUser: Scalars["String"];
+  description?: InputMaybe<Scalars["String"]>;
+  displayName?: InputMaybe<Scalars["String"]>;
+};
 
 export type SignupInput = {
   email: Scalars["String"];
