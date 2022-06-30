6807f7535e572f61ded89c04d0f56395c93aba50
Shahar <shaharb@sela.co.il>
2022-06-30 14:05:49 +0300

    delete deployer

--- packages/amplication-deployer/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export { Configuration } from "./Configuration";
-export { Variables } from "./Variables";
-export { DeployResult, EnumDeployStatus } from "./DeployResult";
-export { IProvider } from "./IProvider";
-export { BackendConfiguration } from "./BackendConfiguration";
