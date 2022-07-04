91d682837838d336ae50edae3f78fca1f8f5f7cd
Amit Barletz <amit@amplication.com>
2022-07-04 20:14:17 +0300

    work still in progress

--- packages/amplication-server/src/core/project/dto/update-project.args.ts
+++ packages/amplication-server/src/core/project/dto/update-project.args.ts
@@ -1,12 +1,8 @@
 import { ArgsType, Field } from '@nestjs/graphql';
 import { ProjectUpdateInput } from './project-update.input';
-import { ProjectWhereInput } from './project-where-input.input';
 
 @ArgsType()
 export class UpdateProjectArgs {
   @Field(() => ProjectUpdateInput, { nullable: false })
   data!: ProjectUpdateInput;
-
-  @Field(() => ProjectWhereInput, { nullable: false })
-  where!: ProjectWhereInput;
 }
--- packages/amplication-server/src/core/project/project.resolver.ts
+++ packages/amplication-server/src/core/project/project.resolver.ts
@@ -1,5 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { FindOneArgs } from 'src/dto';
 import { Project } from 'src/models';
+import { ProjectService } from './project.service';
 
 @Resolver(() => Project)
-export class ProjectResolver {}
+export class ProjectResolver {
+  constructor(private projectService: ProjectService) {}
+
+  @Query(() => Project, { nullable: true })
+  async project(@Args() args: FindOneArgs): Promise<Project> {
+    return this.projectService.findProject(args);
+  }
+}
