7b52bf7e3068f3fca31b2c5b2d7926f82481c3b3
tupe12334 <ofek.gabay.he@gmail.com>
2022-07-06 12:44:21 +0300

    fix(client): remove the description

--- packages/amplication-client/src/Application/appSettings/DirectoriesSettingsForm.tsx
+++ packages/amplication-client/src/Application/appSettings/DirectoriesSettingsForm.tsx
@@ -16,7 +16,10 @@ import { match } from "react-router-dom";
 import PendingChangesContext from "../../VersionControl/PendingChangesContext";
 import "./GenerationSettingsForm.scss";
 import useSettingsHook from "../useSettingsHook";
-import { GET_APP_SETTINGS, UPDATE_APP_SETTINGS } from "./GenerationSettingsForm";
+import {
+  GET_APP_SETTINGS,
+  UPDATE_APP_SETTINGS,
+} from "./GenerationSettingsForm";
 
 type Props = {
   match: match<{ application: string }>;
@@ -48,14 +51,14 @@ function GenerationSettingsForm({ match }: Props) {
     {
       onCompleted: (data) => {
         pendingChangesContext.addBlock(data.updateAppSettings.id);
-      }
+      },
     }
   );
 
-  const {handleSubmit, FORM_SCHEMA} = useSettingsHook({
+  const { handleSubmit, FORM_SCHEMA } = useSettingsHook({
     trackEvent,
     updateAppSettings,
-    applicationId
+    applicationId,
   });
 
   return (
@@ -75,10 +78,7 @@ function GenerationSettingsForm({ match }: Props) {
                 <div className={`${CLASS_NAME}__header`}>
                   <h3>Base directories</h3>
                 </div>
-                <p className={`${CLASS_NAME}__description`}>
-                  Enter the Server and Admin-UI directories, if required. This will override the default directories.
-                </p>
-                <hr />
+
                 <FormikAutoSave debounceMS={1000} />
                 <Panel panelStyle={EnumPanelStyle.Transparent}>
                   <h2>Server</h2>
@@ -92,7 +92,6 @@ function GenerationSettingsForm({ match }: Props) {
                     labelType="normal"
                   />
                 </Panel>
-                <hr />
                 <Panel panelStyle={EnumPanelStyle.Transparent}>
                   <h2>Admin UI</h2>
                   <TextField
