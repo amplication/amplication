1a57dc733b4e0e8258e21cac53d2fffe99905936
Amit Barletz <amit@amplication.com>
2022-07-11 11:57:44 +0300

    add role

--- packages/amplication-server/src/core/project/project.resolver.ts
+++ packages/amplication-server/src/core/project/project.resolver.ts
@@ -6,25 +6,26 @@ import { ProjectFindManyArgs } from './dto/project-find-many.args';
 import { ProjectService } from './project.service';
 import { InjectContextValue } from 'src/decorators/injectContextValue.decorator';
 import { InjectableResourceParameter } from 'src/enums/InjectableResourceParameter';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Resolver(() => Project)
 export class ProjectResolver {
   constructor(private projectService: ProjectService) {}
 
   @Query(() => [Project], { nullable: true })
+  @Roles('ORGANIZATION_ADMIN')
   async projects(@Args() args: ProjectFindManyArgs): Promise<Project[]> {
     return this.projectService.findProjects(args);
   }
 
   @Query(() => Project, { nullable: true })
+  @Roles('ORGANIZATION_ADMIN')
   async project(@Args() args: FindOneArgs): Promise<Project> {
     return this.projectService.findProject(args);
   }
 
-  @Mutation(() => Project, {
-    nullable: true,
-    description: undefined
-  })
+  @Mutation(() => Project, { nullable: true })
+  @Roles('ORGANIZATION_ADMIN')
   @InjectContextValue(
     InjectableResourceParameter.WorkspaceId,
     'data.workspace.connect.id'
