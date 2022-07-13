d311596935acc23c917841698e910d11f1688eba
Mor Hagbi <mor@amplication.com>
2022-07-13 10:58:38 +0300

    update circale badge component

--- packages/amplication-design-system/src/components/CircleBadge/CircleBadge.tsx
+++ packages/amplication-design-system/src/components/CircleBadge/CircleBadge.tsx
@@ -5,12 +5,22 @@ import "./CircleBadge.scss";
 export type Props = {
   name: string;
   color?: string;
+  size?: "size24" | "size32" | "size40" | "size54" | "size64" | "size80";
 };
 
-export function CircleBadge({ name, color }: Props) {
+export const CircleBadge: React.FC<Props> = ({
+  name,
+  color,
+  children,
+  size,
+}) => {
   return (
-    <div className="circle-badge" style={{ backgroundColor: color }}>
+    <div
+      className={`circle-badge${size ? ` ${size}` : ""}`}
+      style={{ backgroundColor: color }}
+    >
       {name && name.slice(0, 1).toUpperCase()}
+      {children}
     </div>
   );
-}
+};
