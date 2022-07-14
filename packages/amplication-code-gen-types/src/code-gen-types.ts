64410bad07c67660bc5f5a8d60ab1cb3c84351e1
tupe12334 <ofek.gabay.he@gmail.com>
2022-07-14 12:46:01 +0300

    fix(cgt): fix crash

--- packages/amplication-code-gen-types/src/code-gen-types.ts
+++ packages/amplication-code-gen-types/src/code-gen-types.ts
@@ -1,8 +1,5 @@
 import * as models from "./models";
-import { Lookup } from "./types/lookup";
-import { MultiSelectOptionSet } from "./types/multiSelectOptionSet";
-import { OptionSet } from "./types/optionSet";
-export * from "./types";
+import { Lookup, MultiSelectOptionSet, OptionSet } from "./types";
 
 export {
   EnumEntityPermissionType,
