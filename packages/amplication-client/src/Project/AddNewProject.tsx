7a9c257ab7553edf998401490eacdc6c13750173
Amit Barletz <amit@amplication.com>
2022-07-14 20:36:22 +0300

    add new project btn + dialog

--- packages/amplication-client/src/Project/AddNewProject.tsx
+++ packages/amplication-client/src/Project/AddNewProject.tsx
@@ -1,17 +1,23 @@
-import { Button, EnumButtonStyle, Icon } from "@amplication/design-system";
+import { Button, EnumButtonStyle, EnumIconPosition } from "@amplication/design-system";
 import React from "react";
 import "./AddNewProject.scss";
 
 type Props = {
-  onAddNewProject: () => void;
+  onNewProjectClick: () => void;
 };
 
-const AddNewProject = ({ onAddNewProject }: Props) => {
+const AddNewProject = ({ onNewProjectClick }: Props) => {
   return (
-      <Button onClick={onAddNewProject} buttonStyle={EnumButtonStyle.Text}>
-        <Icon icon="plus" />
-        Add Description
-      </Button>
+    <Button
+      onClick={onNewProjectClick}
+      type="button"
+      buttonStyle={EnumButtonStyle.Text}
+      icon="plus"
+      iconPosition={EnumIconPosition.Left}
+      iconSize="xsmall"
+    >
+      Add New Project
+    </Button>
   );
 };
 
