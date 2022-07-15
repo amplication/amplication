899bd400a9f2bcd8077dd1f2f257c3bb1465d006
Amit Barletz <amit@amplication.com>
2022-07-15 09:33:16 +0300

    workspace selector init

--- packages/amplication-client/src/appRoutes.tsx
+++ packages/amplication-client/src/appRoutes.tsx
@@ -270,7 +270,7 @@ export const OldRoutes: RouteDef[] = [
   },
   {
     path: "/project",
-    Component: lazy(() => import("./Project/ProjectSideBar")),
+    Component: lazy(() => import("./Project/WorkspaceSelectorNew")),
     moduleName: "Project",
     routeTrackType: "project",
     moduleClass: "project-page",
