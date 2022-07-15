a03c4480ed3e1463aa122898de5a69d18afd8ad9
Amit Barletz <amit@amplication.com>
2022-07-15 11:46:34 +0300

    sidebar footer style

--- packages/amplication-client/src/Project/ProjectSideBarFooter.tsx
+++ packages/amplication-client/src/Project/ProjectSideBarFooter.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import InnerTabLink from "../Layout/InnerTabLink";
 import "./ProjectSideBarFooter.scss";
 
-const CLASS_NAME = 'side-bar-layout';
+const CLASS_NAME = 'sidebar-footer ';
 
 const ProjectSideBarFooter = () => {
     return (
-        <div className={`${CLASS_NAME}__tabs`}>
+        <div className={`${CLASS_NAME}`}>
           <InnerTabLink to={`/workspace/settings`} icon="settings">
             Workspace Settings
           </InnerTabLink>
