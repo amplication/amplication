c96a2c7aa276396b12275b81b18dc1736db6e5b5
Amit Barletz <amit@amplication.com>
2022-07-16 09:15:52 +0300

    fix class name

--- packages/amplication-client/src/Project/ProjectSideBarFooter.tsx
+++ packages/amplication-client/src/Project/ProjectSideBarFooter.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import InnerTabLink from "../Layout/InnerTabLink";
 import "./ProjectSideBarFooter.scss";
 
-const CLASS_NAME = 'sidebar-footer ';
+const CLASS_NAME = "sidebar-footer ";
 
 const ProjectSideBarFooter = () => {
-    return (
-        <div className={`${CLASS_NAME}`}>
-          <InnerTabLink to={`/workspace/settings`} icon="settings">
-            Workspace Settings
-          </InnerTabLink>
-          <InnerTabLink to={`/workspace/members`} icon="users">
-            Workspace Members
-          </InnerTabLink>
-        </div>
-      );
-}
+  return (
+    <div className={CLASS_NAME}>
+      <InnerTabLink to={`/workspace/settings`} icon="settings">
+        Workspace Settings
+      </InnerTabLink>
+      <InnerTabLink to={`/workspace/members`} icon="users">
+        Workspace Members
+      </InnerTabLink>
+    </div>
+  );
+};
 
 export default ProjectSideBarFooter;
