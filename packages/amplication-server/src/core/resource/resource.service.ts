27d3e222c4ab17dd52f5e3555a1e4223afffc5fa
Amit Barletz <amit@amplication.com>
2022-07-16 12:26:58 +0300

    fix format

--- packages/amplication-server/src/core/resource/resource.service.ts
+++ packages/amplication-server/src/core/resource/resource.service.ts
@@ -576,7 +576,6 @@ export class ResourceService {
   }
 
   async project(resourceId: string): Promise<Project> {
-    return this.prisma.project
-      .findUnique({ where: { id: resourceId } })
+    return this.prisma.project.findUnique({ where: { id: resourceId } });
   }
 }
