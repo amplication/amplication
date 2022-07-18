112729f278c954742cf34a9c7d0ad816efd5eabd
tupe12334 <ofek.gabay.he@gmail.com>
2022-07-18 14:53:51 +0300

    fix(server): update module

--- packages/amplication-server/src/core/projectConfigurationSettings/projectConfigurationSettings.module.ts
+++ packages/amplication-server/src/core/projectConfigurationSettings/projectConfigurationSettings.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BlockModule } from '../block/block.module';
+import { PermissionsModule } from '../permissions/permissions.module';
 import { ProjectConfigurationSettingsResolver } from './projectConfigurationSettings.resolver';
 import { ProjectConfigurationSettingsService } from './projectConfigurationSettings.service';
 
 @Module({
+  imports: [BlockModule,PermissionsModule],
   providers: [
     ProjectConfigurationSettingsService,
     ProjectConfigurationSettingsResolver
