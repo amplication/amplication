dee2b0a4fb4afd1b27c019823ee7005c696661be
Mor Hagbi <mor@amplication.com>
2022-07-20 12:40:31 +0300

    (client:) => fix warnings

--- packages/amplication-client/src/Workspaces/WorkspaceFooter.tsx
+++ packages/amplication-client/src/Workspaces/WorkspaceFooter.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import "./WorkspaceFooter.scss"
+import "./WorkspaceFooter.scss";
 
-
-const WorkspaceFooter: React.FC<{}> = ({}) => {
-  return (
-    <div className="page_footer">
-      Amplication footer
-    </div>
-  )
+const WorkspaceFooter: React.FC<{}> = () => {
+  return <div className="page_footer">Amplication footer</div>;
 };
 
 export default WorkspaceFooter;
--- packages/amplication-client/src/Workspaces/WorkspaceHeader.tsx
+++ packages/amplication-client/src/Workspaces/WorkspaceHeader.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import "./WorkspaceHeader.scss"
+import "./WorkspaceHeader.scss";
 
-
-const WorkspaceHeader: React.FC<{}> = ({}) => {
-  return (
-    <div className="page_header">
-      Amplication header
-    </div>
-  )
+const WorkspaceHeader: React.FC<{}> = () => {
+  return <div className="page_header">Amplication header</div>;
 };
 
 export default WorkspaceHeader;
