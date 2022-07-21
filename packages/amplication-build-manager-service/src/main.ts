f1b4d76c127a28930fe6aa65018f72024b286497
Eugene Tseitlin <eugene@amplication.com>
2022-07-21 18:57:14 +0300

    add port to config

--- packages/amplication-build-manager-service/src/main.ts
+++ packages/amplication-build-manager-service/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.connectMicroservice(createNestjsKafkaConfig());
 
   await app.startAllMicroservices();
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
