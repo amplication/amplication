8f474dde51a3e8f24de9f97455e85a14ffd2fe2e
Amit Barletz <amit@amplication.com>
2022-07-24 15:32:45 +0300

    fix(client) fix project list check on sidebar

--- packages/amplication-client/src/Project/ProjectSideBar.tsx
+++ packages/amplication-client/src/Project/ProjectSideBar.tsx
@@ -29,12 +29,10 @@ const ProjectSideBar = ({ currentWorkspace, projectsList }: Props) => {
       <span className={`${CLASS_NAME}__label`}>Workspace</span>
       <WorkspaceSelector />
       <hr className={`${CLASS_NAME}__divider`} />
-      {ProjectList && (
-        <ProjectList
+      <ProjectList
           projects={projectsList}
           workspaceId={currentWorkspace?.id}
         />
-      )}
       <AddNewProject onProjectCreated={handleProjectCreated} />
       <hr className={`${CLASS_NAME}__divider`} />
       <ProjectSideBarFooter />
