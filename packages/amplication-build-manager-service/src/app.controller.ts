cd2d0d3a7e8cd2f838a9378dc9e11e92fc39ff7a
Eugene Tseitlin <eugene@amplication.com>
2022-07-24 07:43:16 +0300

    remove line breaks

--- packages/amplication-build-manager-service/src/app.controller.ts
+++ packages/amplication-build-manager-service/src/app.controller.ts
@@ -14,9 +14,7 @@ export class AppController {
   @EventPattern('build.internal.generate-resource.event.0')
   async receiveCodeGenRequest(@Payload() message: any) {
     const gr: GenerateResource = message.value;
-
     const path = await this.buildContextStorage.saveBuildContextSource(gr);
-
     await this.codeBuildService.runBuild(path);
   }
 }
