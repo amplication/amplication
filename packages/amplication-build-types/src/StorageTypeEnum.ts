e134242fb168b1b02c900288947cc72f36a47417
Eugene Tseitlin <eugene@amplication.com>
2022-07-27 09:32:27 +0300

    created package with types for code build

---
+++ packages/amplication-build-types/src/StorageTypeEnum.ts
@@ -0,0 +1,5 @@
+export enum StorageTypeEnum {
+  FS = 'FS',
+  S3 = 'S3',
+  GCS = 'GCS',
+}
---
+++ packages/amplication-build-types/src/index.ts
@@ -0,0 +1,5 @@
+export { BuildStatusEvent } from './BuildStatusEvent';
+export { BuildStatusEnum } from './BuildStatusEnum';
+export { FileLocation } from './FileLocation';
+export { GenerateResource } from './GenerateResource';
+export { StorageTypeEnum } from './StorageTypeEnum';
