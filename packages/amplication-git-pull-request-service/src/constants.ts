240c5e564e6b5828fde978abc7ff7b5c533cf9e3
tupe12334 <ofek.gabay.he@gmail.com>
2022-07-28 13:00:27 +0300

    feat: add default path

--- packages/amplication-git-pull-request-service/src/constants.ts
+++ packages/amplication-git-pull-request-service/src/constants.ts
@@ -1,2 +1,11 @@
+import { join } from 'path';
+
 export const BUILDS_FOLDER_PATH_ENV_KEY = 'BASE_BUILDS_FOLDER';
 export const GENERATE_PULL_REQUEST_TOPIC = 'GENERATE_PULL_REQUEST_TOPIC';
+export const DOT_AMPLICATION_FOLDER = join(
+  process.cwd(),
+  '..',
+  '..',
+  '.amplication'
+);
+export const DEFAULT_BUILDS_FOLDER = join(DOT_AMPLICATION_FOLDER, 'builds');
--- packages/amplication-storage-gateway/src/constants.ts
+++ packages/amplication-storage-gateway/src/constants.ts
@@ -1,3 +1,12 @@
+import { join } from "path";
+
 export const JWT_SECRET_KEY = "JWT_SECRET_KEY";
 export const BASE_BUILDS_FOLDER = "BASE_BUILDS_FOLDER";
 export const CHECK_USER_ACCESS_TOPIC = "CHECK_USER_ACCESS_TOPIC";
+export const DOT_AMPLICATION_FOLDER = join(
+  process.cwd(),
+  "..",
+  "..",
+  ".amplication"
+);
+export const DEFAULT_BUILDS_FOLDER = join(DOT_AMPLICATION_FOLDER, "builds");
