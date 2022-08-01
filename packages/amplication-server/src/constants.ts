78c3f44c9721b8615a595d93739057d50f7d2cc0
Ofek Gabay <61761153+tupe12334@users.noreply.github.com>
2022-08-01 12:48:24 +0300

    Release 0.14.9 (#3277)
    * fix: create temp folders for packages
    
    * chore: rename temp folder
    
    * WIP
    
    * feat(.amplication): add ignore file
    
    * feat: update the .env files
    
    * refactor: chagne readme text
    
    * feat: add default path
    
    * feat(git pull): update git pull service
    
    * fix: README files
    
    * fix: storage insted of .amplication
    
    * fix: vscode cspell
    
    * feat(logger): finish logger setup
    
    * feat(logger): add prettier
    
    * refactor(logger): change the folder name
    
    * fix(logger): dont format js files
    
    * fix(logger): tests
    
    * feat(logger): pritter ignore
    
    * format
    
    * refactor(logger): rename
    
    * fix(logger): typo
    
    * refactor
    
    * refactor(logger)
    
    * refactor(logger): remove comments
    
    * v0.14.9
    
    Co-authored-by: Michael Solati <mkslt04@gmail.com>

--- packages/amplication-server/src/constants.ts
+++ packages/amplication-server/src/constants.ts
@@ -1,3 +1,16 @@
+import { join } from 'path';
+
 export const BASE_BUILDS_FOLDER = 'BASE_BUILDS_FOLDER';
 export const GENERATE_PULL_REQUEST_TOPIC = 'GENERATE_PULL_REQUEST_TOPIC';
 export const CHECK_USER_ACCESS_TOPIC = 'CHECK_USER_ACCESS_TOPIC';
+export const DOT_AMPLICATION_FOLDER = join(
+  process.cwd(),
+  '..',
+  '..',
+  '.amplication'
+);
+export const DEFAULT_BUILDS_FOLDER = join(
+  DOT_AMPLICATION_FOLDER,
+  'storage',
+  'builds'
+);
