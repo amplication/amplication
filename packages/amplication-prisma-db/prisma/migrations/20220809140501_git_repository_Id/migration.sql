de7c87b9b14280f42166c5102ce29028fb747d71
tupe12334 <ofek.gabay.he@gmail.com>
2022-08-16 14:28:53 +0300

    fix(sql): missing semi colon

--- packages/amplication-prisma-db/prisma/migrations/20220809140501_git_repository_Id/migration.sql
+++ packages/amplication-prisma-db/prisma/migrations/20220809140501_git_repository_Id/migration.sql
@@ -1,6 +1,6 @@
 -- Add gitRepositoryId to Resource
 ALTER TABLE "Resource" ADD COLUMN     "gitRepositoryId" TEXT;
-ALTER TABLE "Resource" ADD COLUMN     "gitRepositoryOverride" BOOLEAN NOT NULL DEFAULT false
+ALTER TABLE "Resource" ADD COLUMN     "gitRepositoryOverride" BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE "Resource" ADD CONSTRAINT "Resource_gitRepositoryId_fkey" FOREIGN KEY ("gitRepositoryId") REFERENCES "GitRepository"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- Extract the data from GitRepository and insert it into the Resource table fkeys
