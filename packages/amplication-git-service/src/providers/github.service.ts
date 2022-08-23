e03f0827ecf89228113fa836bba996469eedf379
tupe12334 <ofek.gabay.he@gmail.com>
2022-08-23 17:07:05 +0300

    refactor(git service): from string to regex objects

--- packages/amplication-git-service/src/providers/github.service.ts
+++ packages/amplication-git-service/src/providers/github.service.ts
@@ -201,11 +201,11 @@ export class GithubService implements IGitClient {
     //do not override files in 'server/src/[entity]/[entity].[controller/resolver/service/module].ts'
     //do not override server/scripts/customSeed.ts
     const doNotOverride = [
-      /^server\/src\/[^\/]+\/.+\.controller.ts$/,
-      /^server\/src\/[^\/]+\/.+\.resolver.ts$/,
-      /^server\/src\/[^\/]+\/.+\.service.ts$/,
-      /^server\/src\/[^\/]+\/.+\.module.ts$/,
-      /^server\/scripts\/customSeed.ts$/
+      new RegExp(`^server\/src\/[^\/]+\/.+\.controller.ts$`),
+      new RegExp(`^server\/src\/[^\/]+\/.+\.resolver.ts$`),
+      new RegExp(`^server\/src\/[^\/]+\/.+\.service.ts$`),
+      new RegExp(`^server\/src\/[^\/]+\/.+\.module.ts$`),
+      new RegExp(`^server\/scripts\/customSeed.ts$`)
     ];
 
     const authFolder = 'server/src/auth';
