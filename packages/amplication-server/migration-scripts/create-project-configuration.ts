cda3ebf8ec7afdc69cd6d42f59963aba139140dc
Eugene Tseitlin <eugene@amplication.com>
2022-08-25 13:18:26 +0200

    run format

--- packages/amplication-server/migration-scripts/create-project-configuration.ts
+++ packages/amplication-server/migration-scripts/create-project-configuration.ts
@@ -1,7 +1,10 @@
-import { PrismaClient, EnumResourceType, EnumBlockType } from '@amplication/prisma-db';
+import {
+  PrismaClient,
+  EnumResourceType,
+  EnumBlockType
+} from '@amplication/prisma-db';
 
 import { DEFAULT_RESOURCE_COLORS } from '../src/core/resource/constants';
-
 
 const DEFAULT_PROJECT_CONFIGURATION_NAME = 'Project Configuration';
 const DEFAULT_PROJECT_CONFIGURATION_DESCRIPTION =
@@ -12,14 +15,13 @@ const DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME =
 const DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION =
   'This block is used to store project configuration settings.';
 
-const blockVersionSettings = { 
-  baseDirectory: "/"
+const blockVersionSettings = {
+  baseDirectory: '/'
 };
-
 
 async function main() {
   const client = new PrismaClient();
-  
+
   const projects = await client.project.findMany({
     where: {
       resources: {
@@ -31,39 +33,37 @@ async function main() {
     include: {
       resources: true
     }
-  })
+  });
 
   const promises = projects.map(async project => {
-    const newProjectConfiguration = await client.resource.create({
-      data: {
-        color: DEFAULT_RESOURCE_COLORS.projectConfiguration,
-        resourceType: EnumResourceType.ProjectConfiguration,
-        description: DEFAULT_PROJECT_CONFIGURATION_DESCRIPTION,
-        name: DEFAULT_PROJECT_CONFIGURATION_NAME,
-        project: { connect: { id: project.id } }
-      }
-    });
-
-    const block = await client.block.create({
-      data: {
-        blockType: EnumBlockType.ProjectConfigurationSettings,
-        description: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION,
-        displayName: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME,
-        resource: { connect : { id: newProjectConfiguration.id } },
-      }
-    });
-
-    const blockVersion = await client.blockVersion.create({
-      data: {
-        versionNumber: 0,
-        displayName: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME,
-        description: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION,
-        inputParameters: {},
-        outputParameters: {},
-        settings: blockVersionSettings,
-        block: { connect: { id: block.id } },
-      }
-    });
+    client.$transaction([
+      client.blockVersion.create({
+        data: {
+          versionNumber: 0,
+          displayName: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME,
+          description: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION,
+          inputParameters: {},
+          outputParameters: {},
+          settings: blockVersionSettings,
+          block: {
+            create: {
+              blockType: EnumBlockType.ProjectConfigurationSettings,
+              description: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION,
+              displayName: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME,
+              resource: {
+                create: {
+                  color: DEFAULT_RESOURCE_COLORS.projectConfiguration,
+                  resourceType: EnumResourceType.ProjectConfiguration,
+                  description: DEFAULT_PROJECT_CONFIGURATION_DESCRIPTION,
+                  name: DEFAULT_PROJECT_CONFIGURATION_NAME,
+                  project: { connect: { id: project.id } }
+                }
+              }
+            }
+          }
+        }
+      })
+    ]);
   });
 
   await Promise.all(promises);
