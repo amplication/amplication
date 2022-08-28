17a82d76b2aaec1377e2e64b51241638dd5be668
Amit Barletz <amit@amplication.com>
2022-08-28 20:19:37 +0300

    fix(client) last commit text and status fix ui issue

--- packages/amplication-client/src/VersionControl/LastCommit.tsx
+++ packages/amplication-client/src/VersionControl/LastCommit.tsx
@@ -82,9 +82,12 @@ const LastCommit = ({ projectId }: Props) => {
     >
       <hr className={`${CLASS_NAME}__divider`} />
       <div className={`${CLASS_NAME}__content`}>
-        <p className={`${CLASS_NAME}__title`}>Last Commit</p>
-        <div className={`${CLASS_NAME}__status`}>
+        <p className={`${CLASS_NAME}__title`}>
+          Last Commit
           {build && <BuildStatusIcons build={build} showIcon={false} />}
+          </p>
+        
+        <div className={`${CLASS_NAME}__status`}>
           <SkeletonWrapper
             showSkeleton={generating}
             className={`${CLASS_NAME}__skeleton`}
