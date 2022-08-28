20c869f4b5770481ab13ebc6d3c0a64af739c156
Amit Barletz <amit@amplication.com>
2022-08-28 19:42:24 +0300

    feat(client) use ellipsis with css

--- packages/amplication-client/src/Components/CommitSelectorItem.tsx
+++ packages/amplication-client/src/Components/CommitSelectorItem.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Commit } from "../models";
-import { truncateWithEllipsis } from "../util/truncatedWithEllipsis";
 import "./CommitSelectorItem.scss";
 import UserBadge from "./UserBadge";
 
@@ -10,18 +9,11 @@ type Props = {
 const CLASS_NAME = "commit-selector-item";
 
 export const CommitSelectorItem = ({ commit }: Props) => {
-  const truncateCommitMessage = useMemo(() => {
-    return truncateWithEllipsis(
-      commit?.message.trim(),
-      15,
-      "No commit message"
-    );
-  }, [commit?.message]);
 
   return (
     <div className={CLASS_NAME}>
       <UserBadge />
-      <div className={`${CLASS_NAME}__title`}>{truncateCommitMessage}</div>
+      <div className={`${CLASS_NAME}__title`}>{commit?.message}</div>
     </div>
   );
 };
--- packages/amplication-client/src/Components/ResourceSelectorItem.tsx
+++ packages/amplication-client/src/Components/ResourceSelectorItem.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Resource } from "../models";
-import { truncateWithEllipsis } from "../util/truncatedWithEllipsis";
 import "./CommitSelectorItem.scss";
 import ResourceCircleBadge from "./ResourceCircleBadge";
 
@@ -10,17 +9,12 @@ type Props = {
 const CLASS_NAME = "commit-selector-item";
 
 export const ResourceSelectorItem = ({ resource }: Props) => {
-
-  const truncateServiceName = useMemo(() => {
-    return truncateWithEllipsis(resource?.name.trim(), 15, "Service Name");
-  }, [resource?.name]);
-
   return (
     <div className={CLASS_NAME}>
       {resource && (
         <>
           <ResourceCircleBadge type={resource.resourceType} size={"xsmall"} />
-          <div className={`${CLASS_NAME}__title`}>{truncateServiceName}</div>
+          <div className={`${CLASS_NAME}__title`}>{resource?.name}</div>
         </>
       )}
     </div>
