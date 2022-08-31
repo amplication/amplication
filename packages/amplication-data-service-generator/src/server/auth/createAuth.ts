9abc5c1bbe77d374fd64b0b1e2c85fb5dd3d11ea
yuval-hazaz <hazaz.yuval@gmail.com>
2022-08-31 16:14:48 +0300

    fix(dsg):move auth static files

--- packages/amplication-data-service-generator/src/server/auth/createAuth.ts
+++ packages/amplication-data-service-generator/src/server/auth/createAuth.ts
@@ -1,7 +1,11 @@
+import * as path from "path";
 import { AppInfo, Module } from "@amplication/code-gen-types";
+import { readStaticModules } from "../../read-static-modules";
 import { createDefaultGuard } from "./guards/createDefaultGuard";
 import { createTokenServiceTests } from "./token/createTokenSerivceTests";
 import { createTokenService } from "./token/createTokenService";
+
+const STATIC_DIRECTORY = path.resolve(__dirname, "static");
 
 export async function createAuthModules(
   srcDir: string,
@@ -11,8 +15,15 @@ export async function createAuthModules(
   const authTestsDir = `${srcDir}/tests/auth`;
   const { settings } = appInfo;
   const { authProvider } = settings;
+  
+  const staticModules = await readStaticModules(
+    STATIC_DIRECTORY,
+    srcDir
+  );
+
   const defaultGuardFile = await createDefaultGuard(authProvider, authDir);
   return Promise.all([
+    ...staticModules,
     defaultGuardFile,
     createTokenService(authDir, authProvider),
     createTokenServiceTests(authTestsDir, authProvider),
