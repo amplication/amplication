825a53b94a09f9734ece404adff69ff1a1bdcf77
tupe12334 <ofek.gabay.he@gmail.com>
2022-09-01 15:30:29 +0300

    refactor(pr): remove const

--- packages/amplication-git-pull-request-service/src/core/diff/visitors/mocks.ts
+++ packages/amplication-git-pull-request-service/src/core/diff/visitors/mocks.ts
@@ -1,10 +1,8 @@
 import { Difference } from 'dir-compare';
 import { mock } from 'jest-mock-extended';
 
-export const name1PlaceHolder = 'name1';
-
 export const missingFolderMock = mock<Difference>({
-  name1: name1PlaceHolder,
+  name1: 'name1',
   type1: 'directory',
   type2: 'missing',
   state: 'left',
