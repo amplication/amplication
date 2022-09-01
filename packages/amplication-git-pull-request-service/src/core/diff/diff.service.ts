b4989edaec61d6243d5f0ed4eb1ac7b8e32009d1
tupe12334 <ofek.gabay.he@gmail.com>
2022-09-01 14:27:05 +0300

    feat(pr): moving to the visitors way

--- packages/amplication-git-pull-request-service/src/core/diff/diff.service.ts
+++ packages/amplication-git-pull-request-service/src/core/diff/diff.service.ts
@@ -4,12 +4,14 @@ import {
 } from '@amplication/nest-logger-module';
 import { Inject, Injectable } from '@nestjs/common';
 import assert from 'assert';
-import { compare, DiffSet } from 'dir-compare';
+import { compare } from 'dir-compare';
 import { sync } from 'fast-glob';
 import { existsSync, readFileSync } from 'fs';
-import { join, normalize } from 'path';
+import { normalize } from 'path';
 import { PrModule } from '../../constants';
+import { mapDiffSetToPrModule } from './diffset-mapper';
 import { BuildPathFactory } from './utils/BuildPathFactory';
+import { deleteFilesVisitor } from './visitors/delete-files';
 
 @Injectable()
 export class DiffService {
@@ -29,7 +31,7 @@ export class DiffService {
     );
     // return all the new files if an old build folder dont exist
     if (!previousAmplicationBuildId) {
-      return this.firstBuild(newBuildPath);
+      return this.allNewModules(newBuildPath);
     }
     const oldBuildPath = this.buildsPathFactory.get(
       resourceId,
@@ -56,56 +58,25 @@ export class DiffService {
       compareDate: false,
       compareSize: false,
       compareSymlink: false,
+      skipEmptyDirs: true,
     });
 
     this.logger.debug('Finish the dir-compare lib process');
-    if (!res?.diffSet) {
-      throw new Error('');
-    }
-    // const changedFiles = res?.diffSet?.filter((diff) => {
-    //   if (diff.state !== 'equal' && diff.type2 === 'file') {
-    //     //make sure that only new files enter and ignore old files
-    //     if (diff.state !== 'left') {
-    //       return true;
-    //     }
-    //   }
-    //   return false;
-    // });
-    const removedFiles = DiffService.removedFiles(res.diffSet);
-    // this.logger.info('The list of the changed files', { changedFiles });
-    // if (!changedFiles) {
-    //   throw new Error("Didn't have changed files");
-    // }
-    const removedModules = removedFiles.map(({ name1, relativePath }) => {
-      if (!name1) {
-        throw new Error("Didn't have name1");
-      }
-      const path = join(relativePath, name1);
 
-      return {
-        path: path,
-        code: null,
-      };
-    });
-    // const modules = changedFiles.map(async ({ name2, path2, relativePath }) => {
-    //   if (!name2 || !path2) {
-    //     throw new Error("Didn't got valid props");
-    //   }
-    //   const path = join(relativePath, name2);
-    //   const code = await readFileSync(join(path2, name2)).toString('utf8');
-    //   return {
-    //     path,
-    //     code,
-    //   };
-    // });
+    if (!res?.diffSet) {
+      throw new Error('Error in creating a diff set');
+    }
+
+    const modules = mapDiffSetToPrModule(res.diffSet, [deleteFilesVisitor]);
+
     const resultModule = await Promise.all([
-      /*...modules,*/ ...(await this.firstBuild(newBuildPath)),
-      ...removedModules,
+      ...(await this.allNewModules(newBuildPath)),
+      ...modules,
     ]);
     return resultModule;
   }
 
-  private async firstBuild(newBuildPath: string) {
+  private async allNewModules(newBuildPath: string) {
     const basePathLength = newBuildPath.length;
     const files = sync(`${newBuildPath}/**`, { dot: true }).map(
       async (fullPath) => {
@@ -122,18 +93,5 @@ export class DiffService {
 
   private static assertBuildExist(buildPath) {
     assert(existsSync(buildPath));
-  }
-
-  private static removedFiles(diff: DiffSet): DiffSet {
-    return diff.filter((diff) => {
-      if (
-        diff.state === 'left' &&
-        diff.type2 === 'missing' &&
-        diff.type1 !== 'directory'
-      ) {
-        return true;
-      }
-      return false;
-    });
   }
 }
