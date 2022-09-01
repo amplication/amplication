263cf8f53a15690c5a2cc1f97f54cc836ccb9edd
tupe12334 <ofek.gabay.he@gmail.com>
2022-09-01 15:42:39 +0300

    test(pr): add test

--- packages/amplication-git-pull-request-service/src/core/diff/diffset-mapper.spec.ts
+++ packages/amplication-git-pull-request-service/src/core/diff/diffset-mapper.spec.ts
@@ -9,4 +9,34 @@ describe('Testing the diffset module mapper', () => {
     const result = mapDiffSetToPrModule(diffSet, visitors);
     expect(result).toEqual([]);
   });
+
+  it('should send the diffset to the visitors functions', () => {
+    const visitor = jest.fn();
+    const diffSet: DiffSet = [
+      {
+        level: 0,
+        permissionDeniedState: 'access-ok',
+        reason: 'different-content',
+        relativePath: '/',
+        state: 'equal',
+        type1: 'file',
+        type2: 'missing',
+      },
+      {
+        level: 0,
+        permissionDeniedState: 'access-ok',
+        reason: 'different-content',
+        relativePath: '/',
+        state: 'equal',
+        type1: 'file',
+        type2: 'missing',
+      },
+    ];
+    const visitors: DiffVisitorFn[] = [visitor];
+    mapDiffSetToPrModule(diffSet, visitors);
+
+    expect(visitor).toBeCalledTimes(2);
+    expect(visitor).toBeCalledWith(diffSet.at(0));
+    expect(visitor).toBeCalledWith(diffSet.at(1));
+  });
 });
