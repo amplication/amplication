370d0ebb14a280bbb77e6a9419202ebd967dccfe
Ofek Gabay <61761153+tupe12334@users.noreply.github.com>
2022-09-01 15:20:17 +0300

    Update packages/amplication-git-pull-request-service/src/core/diff/diffset-mapper.ts
    Co-authored-by: Ariel Weinberger <Weinberger.Ariel@gmail.com>

--- packages/amplication-git-pull-request-service/src/core/diff/diffset-mapper.ts
+++ packages/amplication-git-pull-request-service/src/core/diff/diffset-mapper.ts
@@ -2,6 +2,14 @@ import { DiffSet } from 'dir-compare';
 import { PrModule } from '../../constants';
 import { DiffVisitorFn } from './visitors/types';
 
+/**
+ * This method takes the diffSets and visitor functions and returns an array of modules.
+ * We use the visitor pattern here (https://en.wikipedia.org/wiki/Visitor_pattern), which means
+ * this function can be provided with any amount of "visitors". Each visitor function will be executed
+ * and provided with each diffset.
+ * 
+ * Empty results (null/undefined) will not be included in the final result.
+ */
 export function mapDiffSetToPrModule(
   diffSet: DiffSet,
   visitors: DiffVisitorFn[] = []
