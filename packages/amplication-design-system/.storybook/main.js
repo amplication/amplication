20ca8c55431600714762921d0d1fed13490178b8
Eugene Tseitlin <eugene@amplication.com>
2022-09-05 18:15:33 +0200

    returned back imports to make sass work

--- packages/amplication-design-system/.storybook/main.js
+++ packages/amplication-design-system/.storybook/main.js
@@ -6,24 +6,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/preset-scss",
   ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
-  },
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    });
-
-    // Return the altered config
-    return config;
   },
 };
--- packages/amplication-design-system/.storybook/preview.js
+++ packages/amplication-design-system/.storybook/preview.js
@@ -1,3 +1,10 @@
+import "../icons/amplicationicon.css";
+
+import "../src/index.scss";
+import "../src/style/icon.scss";
+import "../src/style/css-variables.scss";
+import "../icons";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
