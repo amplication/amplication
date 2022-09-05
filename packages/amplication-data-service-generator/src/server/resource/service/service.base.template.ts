7e94c4d008ed4f0e9ac6d69c8bbf160cf2ac8c5f
Mingyang-Li <mingyangli118@gmail.com>
2022-09-05 17:20:24 +1200

    fix: added await for db operations template

--- packages/amplication-data-service-generator/src/server/resource/service/service.base.template.ts
+++ packages/amplication-data-service-generator/src/server/resource/service/service.base.template.ts
@@ -15,34 +15,34 @@ export class SERVICE_BASE {
   async count<T extends Prisma.FIND_MANY_ARGS>(
     args: Prisma.SelectSubset<T, Prisma.FIND_MANY_ARGS>
   ): Promise<number> {
-    return this.prisma.DELEGATE.count(args);
+    return await this.prisma.DELEGATE.count(args);
   }
 
   async findMany<T extends Prisma.FIND_MANY_ARGS>(
     args: Prisma.SelectSubset<T, Prisma.FIND_MANY_ARGS>
   ): Promise<ENTITY[]> {
-    return this.prisma.DELEGATE.findMany(args);
+    return await this.prisma.DELEGATE.findMany(args);
   }
   async findOne<T extends Prisma.FIND_ONE_ARGS>(
     args: Prisma.SelectSubset<T, Prisma.FIND_ONE_ARGS>
   ): Promise<ENTITY | null> {
-    return this.prisma.DELEGATE.findUnique(args);
+    return await this.prisma.DELEGATE.findUnique(args);
   }
   async create<T extends Prisma.CREATE_ARGS>(
     args: Prisma.SelectSubset<T, Prisma.CREATE_ARGS>
   ): Promise<ENTITY> {
     // @ts-ignore
-    return this.prisma.DELEGATE.create<T>(CREATE_ARGS_MAPPING);
+    return await this.prisma.DELEGATE.create<T>(CREATE_ARGS_MAPPING);
   }
   async update<T extends Prisma.UPDATE_ARGS>(
     args: Prisma.SelectSubset<T, Prisma.UPDATE_ARGS>
   ): Promise<ENTITY> {
     // @ts-ignore
-    return this.prisma.DELEGATE.update<T>(UPDATE_ARGS_MAPPING);
+    return await this.prisma.DELEGATE.update<T>(UPDATE_ARGS_MAPPING);
   }
   async delete<T extends Prisma.DELETE_ARGS>(
     args: Prisma.SelectSubset<T, Prisma.DELETE_ARGS>
   ): Promise<ENTITY> {
-    return this.prisma.DELEGATE.delete(args);
+    return await this.prisma.DELEGATE.delete(args);
   }
 }
