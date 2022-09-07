73f0baa20e89fbbd7a0add65f24236acac5e0a48
tupe12334 <ofek.gabay.he@gmail.com>
2022-09-07 12:38:37 +0300

    fix(client): inner tab circle

--- packages/amplication-client/src/ServiceConnections/ServiceConnectionsList.tsx
+++ packages/amplication-client/src/ServiceConnections/ServiceConnectionsList.tsx
@@ -3,11 +3,11 @@ import { isEmpty } from "lodash";
 import React, { useContext, useEffect, useMemo } from "react";
 import { useHistory } from "react-router-dom";
 import { AppContext } from "../context/appContext";
-import InnerTabLink from "../Layout/InnerTabLink";
 import * as models from "../models";
 import { formatError } from "../util/error";
-import "./ServiceConnectionsList.scss";
 import useServiceConnection from "./hooks/useServiceConnection";
+import "./ServiceConnectionsList.scss";
+import ServiceConnectionsListItem from "./ServiceConnectionsListItem";
 
 type MessageBrokerListItem = {
   resource: models.Resource;
@@ -75,20 +75,25 @@ export const ServiceConnectionsList = React.memo(
         <div className={`${CLASS_NAME}__header`}>Message Brokers</div>
         {loading && <CircularProgress />}
         <div className={`${CLASS_NAME}__list`}>
-          {messageBrokerList.map((connection) => (
-            <div
-              key={connection.resource.id}
-              className={`${CLASS_NAME}__list__item`}
-            >
-              <InnerTabLink
-                icon="connection"
-                to={`/${currentWorkspace?.id}/${currentProject?.id}/${resourceId}/service-connections/${connection.resource.id}`}
-              >
-                <span>{connection.resource.name}</span>
-                <span>{connection.connection?.enabled ? "yes" : "no"}</span>
-              </InnerTabLink>
-            </div>
-          ))}
+          {messageBrokerList.map((connection, index) => {
+            if (
+              !currentProject?.id ||
+              !currentWorkspace?.id ||
+              connection?.connection?.enabled === undefined
+            ) {
+              return null;
+            }
+            return (
+              <ServiceConnectionsListItem
+                key={index}
+                currentProjectId={currentProject.id}
+                currentWorkspaceId={currentWorkspace.id}
+                enabled={connection.connection.enabled}
+                resourceId={resourceId}
+                connectionResource={connection.resource}
+              />
+            );
+          })}
         </div>
 
         <Snackbar open={Boolean(error)} message={errorMessage} />
---
+++ packages/amplication-client/src/ServiceConnections/ServiceConnectionsListItem.tsx
@@ -0,0 +1,47 @@
+import React, { useMemo } from "react";
+import InnerTabLink from "../Layout/InnerTabLink";
+import { Resource } from "../models";
+
+type Props = {
+  resourceId: string;
+  currentProjectId: string;
+  currentWorkspaceId: string;
+  enabled: boolean;
+  connectionResource: Resource;
+};
+
+const CLASS_NAME = "service-connections-list__list__item";
+
+export default function ServiceConnectionsListItem({
+  resourceId,
+  currentProjectId,
+  currentWorkspaceId,
+  enabled,
+  connectionResource,
+}: Props) {
+  const circle = useMemo(() => {
+    return (
+      <svg height="8" width="8">
+        <circle
+          cx="4"
+          cy="4"
+          r="3"
+          stroke-width="2"
+          fill={enabled ? "#31c587" : "#f85b6e"}
+        />
+      </svg>
+    );
+  }, [enabled]);
+  return (
+    <div key={connectionResource.id} className={`${CLASS_NAME}`}>
+      <InnerTabLink
+        to={`/${currentWorkspaceId}/${currentProjectId}/${resourceId}/service-connections/${connectionResource.id}`}
+      >
+        <div className={`${CLASS_NAME}__inner`}>
+          <span>{connectionResource.name}</span>
+          {circle}
+        </div>
+      </InnerTabLink>
+    </div>
+  );
+}
