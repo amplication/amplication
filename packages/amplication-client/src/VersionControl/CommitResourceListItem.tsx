fec11d95b97d90bb17f50ac5885433bc7651f998
Amit Barletz <amit@amplication.com>
2022-09-09 17:11:56 +0300

    fix(client) update commit card UI

--- packages/amplication-client/src/VersionControl/CommitResourceListItem.tsx
+++ packages/amplication-client/src/VersionControl/CommitResourceListItem.tsx
@@ -1,6 +1,6 @@
-import { EnumPanelStyle, Icon, Panel } from "@amplication/design-system";
+import { EnumPanelStyle, Panel } from "@amplication/design-system";
 import React, { useCallback, useContext, useMemo } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { ClickableId } from "../Components/ClickableId";
 import ResourceCircleBadge from "../Components/ResourceCircleBadge";
 import { AppContext } from "../context/appContext";
@@ -31,54 +31,51 @@ const CommitResourceListItem = ({ build }: Props) => {
   }, [build.commitId, build.resourceId, commitChangesByResource]);
 
   return (
-    <Panel className={CLASS_NAME} panelStyle={EnumPanelStyle.Bordered}>
-      <div className={`${CLASS_NAME}__row`}>
-        {build && build.resource && (
-          <>
-            <div className={`${CLASS_NAME}__title`}>
-              {build.resource && (
-                <ResourceCircleBadge type={build.resource.resourceType} />
-              )}
-              <p>{build.resource?.name}</p>
+    
+      
+        <NavLink
+          to={`/${currentWorkspace?.id}/${currentProject?.id}/${build?.resourceId}`}
+        >
+          {build && build.resource && (
+          <Panel
+            className={CLASS_NAME}
+            clickable
+            panelStyle={EnumPanelStyle.Bordered}
+          >
+            <div className={`${CLASS_NAME}__row`}>
+              <ResourceCircleBadge type={build.resource.resourceType} />
+
+              <span className={`${CLASS_NAME}__title`}>
+                {build.resource.name}
+              </span>
+
+              <span className="spacer" />
               <Link
-                to={`/${currentWorkspace?.id}/${currentProject?.id}/${build.resourceId}`}
+                to={`/${currentWorkspace?.id}/${currentProject?.id}/${build.resourceId}/changes/${build.commitId}`}
+                className={`${CLASS_NAME}__changes-count`}
               >
-                <Icon icon="link" size="xsmall" />
+                {resourceChangesCount && resourceChangesCount > 0
+                  ? resourceChangesCount
+                  : 0}{" "}
+                changes
               </Link>
             </div>
-
-            <ClickableId
-              label="Build ID"
-              to={`/${currentWorkspace?.id}/${currentProject?.id}/${build.resourceId}/builds/${build.id}`}
-              id={build.id}
-              onClick={handleBuildLinkClick}
-              eventData={{
-                eventName: "commitListBuildIdClick",
-              }}
-            />
-          </>
-        )}
-      </div>
-      <hr className={`${CLASS_NAME}__divider`} />
-      <div className={`${CLASS_NAME}__row`}>
-        <Link
-          to={`/${currentWorkspace?.id}/${currentProject?.id}/${build.resourceId}/changes/${build.commitId}`}
-          className={`${CLASS_NAME}__changes-count`}
-        >
-          {resourceChangesCount && resourceChangesCount > 0
-            ? resourceChangesCount
-            : 0}{" "}
-          changes
-        </Link>
-
-        <Link
-          to={`/${currentWorkspace?.id}/${currentProject?.id}/${build.resourceId}/builds/${build.id}`}
-        >
-          view log
-        </Link>
-        <BuildStatusIcons build={build} />
-      </div>
-    </Panel>
+            <hr className={`${CLASS_NAME}__divider`} />
+            <div className={`${CLASS_NAME}__row`}>
+              <BuildStatusIcons build={build} showIcon={false} />
+              <ClickableId
+                label="Build ID"
+                to={`/${currentWorkspace?.id}/${currentProject?.id}/${build.resourceId}/builds/${build.id}`}
+                id={build.id}
+                onClick={handleBuildLinkClick}
+                eventData={{
+                  eventName: "commitListBuildIdClick",
+                }}
+              />
+            </div>
+          </Panel>
+      )}
+        </NavLink>
   );
 };
 
