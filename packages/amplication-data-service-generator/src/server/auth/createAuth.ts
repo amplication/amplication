39b02348e8f3d082d766b219286a7e79a12f5254
Mor Hagbi <mor@amplication.com>
2022-09-11 18:19:39 +0300

    refactor createAuthModules pluginWrapper

--- packages/amplication-data-service-generator/src/server/auth/createAuth.ts
+++ packages/amplication-data-service-generator/src/server/auth/createAuth.ts
@@ -1,28 +1,38 @@
-import { EventNames, Module } from "@amplication/code-gen-types";
+import {
+  CreateAuthModulesParams,
+  EventNames,
+  Module,
+} from "@amplication/code-gen-types";
 import pluginWrapper from "../../plugin-wrapper";
 import DsgContext from "../../dsg-context";
 import { createDefaultGuard } from "./guards/createDefaultGuard";
 import { createTokenServiceTests } from "./token/createTokenSerivceTests";
 import { createTokenService } from "./token/createTokenService";
 
-export const createAuthModules = (eventParams: { srcDir: string }): Module[] =>
+export const createAuthModules = (
+  eventParams: CreateAuthModulesParams["before"]
+): Module[] =>
   pluginWrapper(
-    async (eventParams: { srcDir: string }): Promise<Module[]> => {
-      const context = DsgContext.getInstance;
-      const authDir = `${eventParams.srcDir}/auth`;
-      const authTestsDir = `${eventParams.srcDir}/tests/auth`;
-
-      const {
-        settings: { authProvider },
-      } = context.appInfo;
-      const defaultGuardFile = await createDefaultGuard(authProvider, authDir);
-
-      return Promise.all([
-        defaultGuardFile,
-        createTokenService(authDir, authProvider),
-        createTokenServiceTests(authTestsDir, authProvider),
-      ]);
-    },
+    createAuthModulesInternal,
     EventNames.CreateAuthModules,
     eventParams
   );
+
+async function createAuthModulesInternal({
+  srcDir,
+}: CreateAuthModulesParams["before"]): Promise<Module[]> {
+  const context = DsgContext.getInstance;
+  const authDir = `${srcDir}/auth`;
+  const authTestsDir = `${srcDir}/tests/auth`;
+
+  const {
+    settings: { authProvider },
+  } = context.appInfo;
+  const defaultGuardFile = await createDefaultGuard(authProvider, authDir);
+
+  return Promise.all([
+    defaultGuardFile,
+    createTokenService(authDir, authProvider),
+    createTokenServiceTests(authTestsDir, authProvider),
+  ]);
+}
