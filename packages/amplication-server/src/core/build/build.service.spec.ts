c1f66b5802a0304cd5308ff9f1d02aa50e4175a8
barshimi <barshimi@gmail.com>
2022-09-12 12:27:12 +0300

    plugin test update

--- packages/amplication-server/src/core/build/build.service.spec.ts
+++ packages/amplication-server/src/core/build/build.service.spec.ts
@@ -104,6 +104,7 @@ const EXAMPLE_PLUGIN_INSTALLATION: PluginInstallation = {
   parentBlock: null,
   versionNumber: 0,
   enabled: true,
+  npm: '@amplication/example-plugin',
   pluginId: '@amplication/example-plugin',
   blockType: EnumBlockType.PluginInstallation
 };
