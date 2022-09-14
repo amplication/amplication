af92a7d28bdfe4c16b4782cc138152465722cefb
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-09-14 15:02:34 +0200

    chore(dsg): remove unused import

--- packages/amplication-code-gen-types/src/code-gen-types.ts
+++ packages/amplication-code-gen-types/src/code-gen-types.ts
@@ -1,7 +1,6 @@
 import * as models from "./models";
 import { Lookup, MultiSelectOptionSet, OptionSet } from "./types";
 import { namedTypes } from "ast-types";
-import { DSGResourceData } from "./dsg-resource-data";
 
 export {
   EnumEntityPermissionType,
