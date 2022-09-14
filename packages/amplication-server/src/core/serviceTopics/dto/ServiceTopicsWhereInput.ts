9c8c49a7419a99330d5a1f50bab5cb057e89b714
Eugene Tseitlin <eugene@amplication.com>
2022-09-14 09:53:14 +0200

    remove unused field

--- packages/amplication-server/src/core/serviceTopics/dto/ServiceTopicsWhereInput.ts
+++ packages/amplication-server/src/core/serviceTopics/dto/ServiceTopicsWhereInput.ts
@@ -1,13 +1,8 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { StringFilter } from 'src/dto';
+import { InputType } from '@nestjs/graphql';
 import { BlockTypeWhereInput } from '../../block/dto';
 
 @InputType({
   isAbstract: true
 })
 export class ServiceTopicsWhereInput extends BlockTypeWhereInput {
-  @Field(() => StringFilter, {
-    nullable: true
-  })
-  messageBrokerId?: StringFilter | null;
 }
