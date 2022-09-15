04acb84c089f2fdb72addf876c4e2ed55c8f4763
Amit Barletz <amit@amplication.com>
2022-09-15 22:10:33 +0300

    fix deep merge in prepare YAML file

--- packages/amplication-data-service-generator/src/util/prepare-yaml-file.ts
+++ packages/amplication-data-service-generator/src/util/prepare-yaml-file.ts
@@ -1,11 +1,12 @@
 import YAML from "yaml";
+import { merge } from "lodash";
 
 export function prepareYamlFile(
   yamlFileContent: string,
   updateProperties: { [key: string]: any }
 ): string {
   const parsed = YAML.parse(yamlFileContent);
-  const updated = Object.assign(parsed, updateProperties);
+  const updated = merge(parsed, updateProperties);
 
   return YAML.stringify(updated, { nullStr: "~" });
 }
