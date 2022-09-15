735f579b3896bac046022ef3a82c07be8bb284c0
Amit Barletz <amit@amplication.com>
2022-09-15 22:06:54 +0300

    remove postgres default env variables from constants

--- packages/amplication-data-service-generator/src/server/constants.ts
+++ packages/amplication-data-service-generator/src/server/constants.ts
@@ -6,15 +6,6 @@ export const SCRIPTS_DIRECTORY = `${SERVER_BASE_DIRECTORY}/scripts`;
 export const AUTH_PATH = `${SRC_DIRECTORY}/auth`;
 
 export const ENV_VARIABLES: VariableDictionary = [
-  { POSTGRESQL_USER: "${dbUser}" },
-  { POSTGRESQL_PASSWORD: "${dbPassword}" },
-  { POSTGRESQL_DB_NAME: "${dbName}" },
-  { POSTGRESQL_PORT: "${dbPort}" },
-  { POSTGRESQL_HOST: "${dbHost}" },
-  {
-    POSTGRESQL_URL:
-      "postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}${dbName}",
-  },
   { BCRYPT_SALT: "10" },
   { COMPOSE_PROJECT_NAME: "amp_${resourceId}" },
   { JWT_SECRET_KEY: "Change_ME!!!" },
