228484cd40bcf0fe36728faa152ee839a9ee251b
yuval-hazaz <hazaz.yuval@gmail.com>
2022-09-17 21:41:06 +0300

    fix(dsg): add logger to context

--- packages/amplication-data-service-generator/src/create-data-service-impl.ts
+++ packages/amplication-data-service-generator/src/create-data-service-impl.ts
@@ -70,6 +70,7 @@ export async function createDataServiceImpl(
   const normalizedEntities = resolveLookupFields(entitiesWithPluralName);
 
   const context = DsgContext.getInstance;
+  context.logger = logger;
   context.appInfo = appInfo;
   context.roles = roles;
   context.entities = normalizedEntities;
