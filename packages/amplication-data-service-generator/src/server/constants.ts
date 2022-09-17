eaffaccd7966eb540ccd7b04531e163cc1daab9f
Amit Barletz <amit@amplication.com>
2022-09-17 23:40:30 +0300

    clean up postgres env and docker compose values

--- packages/amplication-data-service-generator/src/server/constants.ts
+++ packages/amplication-data-service-generator/src/server/constants.ts
@@ -6,13 +6,6 @@ export const SCRIPTS_DIRECTORY = `${SERVER_BASE_DIRECTORY}/scripts`;
 export const AUTH_PATH = `${SRC_DIRECTORY}/auth`;
 
 export const ENV_VARIABLES: VariableDictionary = [
-  { POSTGRESQL_USER: "${dbUser}" },
-  { POSTGRESQL_PASSWORD: "${dbPassword}" },
-  { POSTGRESQL_PORT: "${dbPort}" },
-  {
-    POSTGRESQL_URL:
-      "postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}${dbName}",
-  },
   { BCRYPT_SALT: "10" },
   { COMPOSE_PROJECT_NAME: "amp_${resourceId}" },
   { JWT_SECRET_KEY: "Change_ME!!!" },
