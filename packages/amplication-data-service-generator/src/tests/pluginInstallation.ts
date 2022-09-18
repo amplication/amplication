5413433cc0e3c68b7defc42fa9968042ec71ce3a
yuval-hazaz <hazaz.yuval@gmail.com>
2022-09-18 08:53:24 +0300

    revert snapshot test data

--- packages/amplication-data-service-generator/src/tests/pluginInstallation.ts
+++ packages/amplication-data-service-generator/src/tests/pluginInstallation.ts
@@ -4,10 +4,4 @@ export const POSTGRESQL_PLUGIN_ID = "db-postgres";
 export const MYSQL_PLUGIN_ID = "db-mysql";
 export const POSTGRESQL_NPM = "@amplication/plugin-db-postgres";
 
-export const installedPlugins: Plugin[] = [
-  {
-    pluginId: POSTGRESQL_PLUGIN_ID,
-    npm: POSTGRESQL_NPM,
-    enabled: true,
-  },
-];
+export const installedPlugins: Plugin[] = [];
