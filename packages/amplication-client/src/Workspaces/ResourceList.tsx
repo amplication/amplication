c230b51d9c79729de4d303f964d26e7b2f539302
Mahesh <maheshmahi19@outlook.com>
2022-09-20 01:28:51 +0530

    fix: loading state while navigating between projects

--- packages/amplication-client/src/Workspaces/ResourceList.tsx
+++ packages/amplication-client/src/Workspaces/ResourceList.tsx
@@ -89,7 +89,7 @@ function ResourceList() {
       <div className={`${CLASS_NAME}__title`}>Project Settings</div>
 
       <div className={`${CLASS_NAME}__settings`}>
-        {projectConfigurationResource && (
+        {!loadingResources && projectConfigurationResource && (
           <ResourceListItem resource={projectConfigurationResource} />
         )}
       </div>
@@ -104,15 +104,14 @@ function ResourceList() {
             image={EnumImages.AddResource}
           />
         ) : (
-          <>
-            {resources.map((resource) => (
-              <ResourceListItem
-                key={resource.id}
-                resource={resource}
-                onDelete={handleDelete}
-              />
-            ))}
-          </>
+          !loadingResources &&
+          resources.map((resource) => (
+            <ResourceListItem
+              key={resource.id}
+              resource={resource}
+              onDelete={handleDelete}
+            />
+          ))
         )}
       </div>
 
