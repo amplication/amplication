e3250bf728db6d2765ba9f08a704d3e7c490fa41
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-09-19 15:54:35 +0200

    Finalize next automated deployment

--- packages/amplication-client/src/Project/WorkspaceSettingsNavigation.tsx
+++ packages/amplication-client/src/Project/WorkspaceSettingsNavigation.tsx
@@ -10,7 +10,7 @@ const WorkspaceSettingsNavigation = () => {
     <div className={CLASS_NAME}>
       <div className={`${CLASS_NAME}__links`}>
         <InnerTabLink to={`/${currentWorkspace?.id}/settings`} icon="settings">
-          Workspace Settings (revision 1)
+          Workspace Settings
         </InnerTabLink>
         <InnerTabLink to={`/${currentWorkspace?.id}/members`} icon="users">
           Workspace Members
