2ee04db6eccb936935481f37335cb79281f35872
Matan Shidlov <matan@amplication.com>
2022-09-28 17:55:11 +0300

    fix compilation error TS2415: Class 'HealthController' incorrectly extends base class 'HealthControllerBase'.

--- packages/amplication-storage-gateway/src/health/health.controller.ts
+++ packages/amplication-storage-gateway/src/health/health.controller.ts
@@ -7,7 +7,7 @@ import {AMPLICATION_LOGGER_PROVIDER, AmplicationLogger} from "@amplication/nest-
 export class HealthController extends HealthControllerBase {
   constructor(
     protected readonly healthService: HealthService,
-    @Inject(AMPLICATION_LOGGER_PROVIDER) private logger: AmplicationLogger
+    @Inject(AMPLICATION_LOGGER_PROVIDER) protected logger: AmplicationLogger
   ) {
     super(healthService, logger);
   }
