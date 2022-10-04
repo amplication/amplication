67c353784cbed8320419d573ad2aae82d58bc40e
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-04 16:33:07 +0200

    Remove backup of amplication-code-gen-types

--- packages/amplication-code-gen-types-old/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { Schema, getSchemaForDataType } from "./get-schema-for-data-type";
-export * as types from "./types";
-export * from "./code-gen-types";
-export * from "./plugins-types";
-export * from "./plugin-events-params";
-export * from "./plugin-events";
-export * from "./dsg-resource-data";
