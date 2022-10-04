89a859061d955a61b16f75f52c1a1d54984252f5
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-04 21:22:39 +0200

    Delete old amplication git service

--- packages/amplication-git-service-old/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { GitModule } from './git.module';
-export { GitService } from './services/git.service';
-export { GithubService } from './providers/github.service';
-export { GitServiceFactory } from './utils/GitServiceFactory';
-export { EnumGitOrganizationType } from './Dto/enums/EnumGitOrganizationType';
-export { EnumGitProvider } from './Dto/enums/EnumGitProvider';
