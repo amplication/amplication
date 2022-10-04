8e830750eeb478247c7d70067297f81a00acebf7
ahlavorato <alinehaxkarlavorato@gmail.com>
2022-10-04 21:37:38 +0100

    use amplification nest logger module in amplification-git-service

--- packages/amplication-git-service/src/git.module.ts
+++ packages/amplication-git-service/src/git.module.ts
@@ -1,8 +1,10 @@
+import { AmplicationLoggerModule } from '@amplication/nest-logger-module';
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GitService } from './services/git.service';
 import { GitServiceFactory } from './utils/GitServiceFactory';
 import { GithubService } from './providers/github.service';
+import { SERVICE_NAME } from "./utils/constants";
 
 @Global()
 @Module({
@@ -11,7 +13,10 @@ import { GithubService } from './providers/github.service';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env.local', '.env']
-    })
+    }),
+    AmplicationLoggerModule.register({
+      metadata: { service: SERVICE_NAME },
+    }),
   ],
   providers: [GitService, GithubService, GitServiceFactory],
   exports: [GitService, GitServiceFactory, GithubService]
