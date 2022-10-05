136b1ba26987a9bc4f83444f24524b095e67bb14
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-05 13:40:43 +0200

    amplication-git-push-webook-service: nx migration

---
+++ ee/packages/amplication-git-push-webhook-service/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'amplication-git-push-webhook-service',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/amplication-git-push-webhook-service',
+};
