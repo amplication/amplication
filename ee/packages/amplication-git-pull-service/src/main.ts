01339ecca0c846bdbd370d8b346e0b966dfa3c62
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-07 00:04:26 +0200

    Fix

--- ee/packages/amplication-git-pull-service/src/main.ts
+++ ee/packages/amplication-git-pull-service/src/main.ts
@@ -1,4 +1,3 @@
-import { createNestjsKafkaConfig } from "@amplication/kafka";
 import { Module } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { MicroserviceOptions } from "@nestjs/microservices";
@@ -8,8 +7,7 @@ class AppModule {}
 
 async function main() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    createNestjsKafkaConfig()
+    AppModule
   );
 
   app.listen();
