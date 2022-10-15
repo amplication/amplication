023c2308322de5267064bd1f483124a95c43a1ff
Amit Barletz <amit@amplication.com>
2022-10-15 20:05:19 +0300

    fix lint better way

--- packages/amplication-data-service-generator/src/admin/static/src/reportWebVitals.ts
+++ packages/amplication-data-service-generator/src/admin/static/src/reportWebVitals.ts
@@ -1,10 +1,8 @@
 import { ReportHandler } from "web-vitals";
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    void import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
