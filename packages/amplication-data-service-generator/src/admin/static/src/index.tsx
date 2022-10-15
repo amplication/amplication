8218357cdcea5ebe05ea78b42cf28772fdc48099
Amit Barletz <amit@amplication.com>
2022-10-15 19:48:25 +0300

    fix lint

--- packages/amplication-data-service-generator/src/admin/static/src/index.tsx
+++ packages/amplication-data-service-generator/src/admin/static/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // @ts-ignore
+// eslint-disable-next-line import/no-unresolved
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
--- packages/amplication-data-service-generator/src/admin/static/src/reportWebVitals.ts
+++ packages/amplication-data-service-generator/src/admin/static/src/reportWebVitals.ts
@@ -1,7 +1,9 @@
 import { ReportHandler } from "web-vitals";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
