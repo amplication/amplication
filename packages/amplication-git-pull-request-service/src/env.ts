a53771e88ac75dc5f1d298186251f14afe8794bb
Eugene Tseitlin <eugene@amplication.com>
2022-10-23 13:46:38 +0200

    change env

--- packages/amplication-git-pull-request-service/src/env.ts
+++ packages/amplication-git-pull-request-service/src/env.ts
@@ -1,10 +1,10 @@
 export class Env {
-  static readonly PORT: 'PORT';
+  static readonly PORT = 'PORT';
 
-  static readonly CREATE_PR_REQUEST_TOPIC: 'CREATE_PR_REQUEST_TOPIC';
-  static readonly CREATE_PR_SUCCESS_TOPIC: 'CREATE_PR_SUCCESS_TOPIC';
-  static readonly CREATE_PR_FAILURE_TOPIC: 'CREATE_PR_FAILURE_TOPIC';
+  static readonly CREATE_PR_REQUEST_TOPIC = 'CREATE_PR_REQUEST_TOPIC';
+  static readonly CREATE_PR_SUCCESS_TOPIC = 'CREATE_PR_SUCCESS_TOPIC';
+  static readonly CREATE_PR_FAILURE_TOPIC = 'CREATE_PR_FAILURE_TOPIC';
 
-  static readonly BASE_BUILDS_FOLDER: 'BASE_BUILDS_FOLDER';
-  static readonly BUILD_OUTPUT_FOLDER: 'BUILD_OUTPUT_FOLDER';
+  static readonly BASE_BUILDS_FOLDER = 'BASE_BUILDS_FOLDER';
+  static readonly BUILD_OUTPUT_FOLDER = 'BUILD_OUTPUT_FOLDER';
 }
