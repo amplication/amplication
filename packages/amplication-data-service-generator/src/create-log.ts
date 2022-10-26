cc340537764142b88e3303c2f37134f909f413a0
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-26 19:29:28 +0200

    Temporarily comment axios call

--- packages/amplication-data-service-generator/src/create-log.ts
+++ packages/amplication-data-service-generator/src/create-log.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { LogEntry } from "winston";
 
 export const createLog = async (log: LogEntry): Promise<void> => {
-  await axios.post(process.env.CREATE_LOG_URL || "", {
-    buildId: process.env.BUILD_ID,
-    ...log,
-  });
+  // await axios.post(process.env.CREATE_LOG_URL || "", {
+  //   buildId: process.env.BUILD_ID,
+  //   ...log,
+  // });
 };
