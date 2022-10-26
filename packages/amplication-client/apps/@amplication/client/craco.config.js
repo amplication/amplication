9bba5a1746eca591044a4bf83667cdc988e7e0c2
barshimi <barshimi@gmail.com>
2022-10-26 15:26:22 +0300

    npx cra-to-nx

---
+++ packages/amplication-client/apps/@amplication/client/craco.config.js
@@ -0,0 +1,44 @@
+
+  const path = require('path');
+  const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+  const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+  module.exports = {
+    webpack: {
+      configure: (config) => {
+        // Remove guard against importing modules outside of `src`.
+        // Needed for workspace projects.
+        config.resolve.plugins = config.resolve.plugins.filter(
+          (plugin) => !(plugin instanceof ModuleScopePlugin)
+        );
+        // Add support for importing workspace projects.
+        config.resolve.plugins.push(
+          new TsConfigPathsPlugin({
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+            extensions: ['.ts', '.tsx', '.js', '.jsx'],
+            mainFields: ['module', 'main'],
+          })
+        );
+        
+        // Replace include option for babel loader with exclude
+        // so babel will handle workspace projects as well.
+        config.module.rules.forEach((r) => {
+          if (r.oneOf) {
+            const babelLoader = r.oneOf.find(
+              (rr) => rr.loader.indexOf('babel-loader') !== -1
+            );
+            babelLoader.exclude = /node_modules/;
+            delete babelLoader.include;
+          }
+        });
+        
+        return config;
+      },
+    },
+    jest: {
+      configure: (config) => {
+        config.resolver = '@nrwl/jest/plugins/resolver';
+        return config;
+      },
+    },
+  };
+  
