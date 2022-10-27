3bca9d3a71b98f164c7e889156f15bbdc196b0b8
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-27 12:51:02 +0200

    Remove kafka topic name from .env

--- ee/packages/amplication-git-pull-service/src/git-pull-event/git-pull-event.controller.ts
+++ ee/packages/amplication-git-pull-service/src/git-pull-event/git-pull-event.controller.ts
@@ -1,15 +1,13 @@
 import { Controller } from "@nestjs/common";
 import { MessagePattern, Payload } from "@nestjs/microservices";
-import { EnvironmentVariables } from "../utils/environment-variables";
 import { GitPullEventService } from "./git-pull-event.service";
-
-const KAFKA_REPOSITORY_PUSH_QUEUE = "KAFKA_REPOSITORY_PUSH_QUEUE";
+import { KafkaTopics } from "./git-pull-event.types";
 
 @Controller()
 export class GitPullEventController {
   constructor(protected readonly gitPullEventService: GitPullEventService) {}
 
-  @MessagePattern(EnvironmentVariables.getStrict(KAFKA_REPOSITORY_PUSH_QUEUE))
+  @MessagePattern(KafkaTopics.GitExternalPush)
   async processRepositoryPushEvent(@Payload() message: any) {
     await this.gitPullEventService.handlePushEvent(message.value);
   }
