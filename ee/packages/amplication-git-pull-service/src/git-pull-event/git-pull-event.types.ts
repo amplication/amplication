75fde151756a9533260ddd1ff25fc4d35a969bf2
Amit Barletz <amit@amplication.com>
2022-10-27 15:15:52 +0300

    refactor: removed undused interfaces

--- ee/packages/amplication-git-pull-service/src/git-pull-event/git-pull-event.types.ts
+++ ee/packages/amplication-git-pull-service/src/git-pull-event/git-pull-event.types.ts
@@ -50,42 +50,7 @@ export interface PushEventMessage extends EventData {
   installationId: string;
 }
 
-export interface GitClient {
-  clone: (
-    pushEventMessage: PushEventMessage,
-    baseDir: string,
-    accessToken: string
-  ) => Promise<void>;
-  pull: (
-    pushEventMessage: PushEventMessage,
-    baseDir: string,
-    accessToken: string
-  ) => Promise<void>;
-}
-
-export interface GitHostProviderFactory {
-  getHostProvider: (provider: GitProviderEnum) => GitProvider;
-}
 export interface GitProvider {
   createInstallationAccessToken: (installationId: string) => Promise<string>;
-}
-export interface GitPullEvent {
-  handlePushEvent: (pushEventMessage: PushEventMessage) => Promise<void>;
-}
-export interface GitPullEventRepository {
-  create: (eventData: EventData) => Promise<{ id: bigint }>;
-  update: (id: bigint, status: GitPullEventStatusEnum) => Promise<boolean>;
-  findByPreviousReadyCommit: (
-    provider: keyof typeof GitProviderEnum,
-    repositoryOwner: string,
-    repositoryName: string,
-    branch: string,
-    pushedAt: Date,
-    skip: number
-  ) => Promise<EventData | undefined>;
-}
-export interface Storage {
-  copyDir: (srcDir: string, destDir: string) => Promise<void>;
-  deleteDir: (dir: string) => void;
 }
 // #endregion
