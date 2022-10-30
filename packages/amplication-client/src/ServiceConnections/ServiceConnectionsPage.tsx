6b31255f03d3fd6407c888ac15b1c9dd67fd66e5
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2022-10-30 22:11:31 +0700

    fix(client): add message broker empty state

--- packages/amplication-client/src/ServiceConnections/ServiceConnectionsPage.tsx
+++ packages/amplication-client/src/ServiceConnections/ServiceConnectionsPage.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import { match } from "react-router-dom";
+import * as models from "../models";
+import { EmptyState } from "../Components/EmptyState";
+import { EnumImages } from "../Components/SvgThemeImage";
+import { AppContext } from "../context/appContext";
 import PageContent from "../Layout/PageContent";
 import { AppRouteProps } from "../routes/routesUtil";
+
+import useServiceConnection from "./hooks/useServiceConnection";
 import { ServiceConnectionsList } from "./ServiceConnectionsList";
+
+type MessageBrokerListItem = {
+  resource: models.Resource;
+  connection: models.ServiceTopics | undefined;
+};
 
 type Props = AppRouteProps & {
   match: match<{
@@ -13,15 +24,59 @@ type Props = AppRouteProps & {
 const pageTitle = "Topics";
 
 const TopicsPage: React.FC<Props> = ({ match, innerRoutes }: Props) => {
-  const { resource } = match.params;
+  const { resource: resourceId } = match.params;
+  const {
+    serviceTopicsList: data,
+    loadingServiceTopics: loading,
+    errorServiceTopics: error,
+  } = useServiceConnection(resourceId);
+
+  const { resources } = useContext(AppContext);
+
+  const messageBrokerList = useMemo((): MessageBrokerListItem[] => {
+    return resources
+      .filter(
+        (resource) =>
+          resource.resourceType === models.EnumResourceType.MessageBroker
+      )
+      .map(
+        (resource): MessageBrokerListItem => {
+          return {
+            resource,
+            connection: data?.ServiceTopicsList.find(
+              (connection) => connection.messageBrokerId === resource.id
+            ),
+          };
+        }
+      );
+  }, [resources, data?.ServiceTopicsList]);
+
+  const hasServiceTopicList = messageBrokerList.length > 0;
 
   return (
     <PageContent
       pageTitle={pageTitle}
       className="topics"
-      sideContent={<ServiceConnectionsList resourceId={resource} selectFirst />}
+      sideContent={
+        hasServiceTopicList ? (
+          <ServiceConnectionsList
+            resourceId={resourceId}
+            messageBrokerList={messageBrokerList}
+            loading={loading}
+            error={error}
+            selectFirst
+          />
+        ) : null
+      }
     >
-      {innerRoutes}
+      {hasServiceTopicList ? (
+        innerRoutes
+      ) : (
+        <EmptyState
+          message="There is no message broker to show."
+          image={EnumImages.MessageBrokerEmptyState}
+        />
+      )}
     </PageContent>
   );
 };
