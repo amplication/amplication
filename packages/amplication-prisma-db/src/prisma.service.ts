5ac1e5b634cf4def12d080bf7c458cf709c1d0b1
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-10-31 11:01:24 +0100

    Revert "Updating the client generation target (#4261)"
    This reverts commit 9577a698d93524c531bf03331a9302ba850bbde3.

--- packages/amplication-prisma-db/src/prisma.service.ts
+++ packages/amplication-prisma-db/src/prisma.service.ts
@@ -1,5 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../prisma/generated/prisma-client';
+import { PrismaClient } from '@amplication/prisma-clients/amplication-prisma-db';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
