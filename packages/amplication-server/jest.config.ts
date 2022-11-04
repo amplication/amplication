83142ab8f101c247de8518ad6305b04e51fd9d15
Amit Barletz <amit@amplication.com>
2022-11-04 13:04:10 +0200

    undo config file changes

--- packages/amplication-server/jest.config.ts
+++ packages/amplication-server/jest.config.ts
@@ -11,7 +11,6 @@ export default {
   transform: {
     '^.+\\.[tj]s$': 'ts-jest',
   },
-  extensionsToTreatAsEsm: ['.ts'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/amplication-server',
 };
