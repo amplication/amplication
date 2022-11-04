04ec5975de8fe150fa1be0958bdc27d851b38bbc
Amit Barletz <amit@amplication.com>
2022-11-04 16:34:28 +0200

    remove undued module import

--- packages/amplication-git-utils/src/app.module.ts
+++ packages/amplication-git-utils/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { GitModule } from "./git/git.module";
-import { GithubModule } from "./github/github.module";
 
 @Global()
 @Module({
   imports: [
     GitModule,
-    GithubModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [".env.local", ".env"],
