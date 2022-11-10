8def438657cfdaa9fc8686d99e82fd5df7727616
Eugene Tseitlin <eugene@amplication.com>
2022-11-10 10:45:54 +0100

    enable logs

--- packages/amplication-data-service-generator/src/create-log.ts
+++ packages/amplication-data-service-generator/src/create-log.ts
@@ -1,9 +1,9 @@
-// import axios from "axios";
+import axios from "axios";
 import { LogEntry } from "winston";
 
 export const createLog = async (log: LogEntry): Promise<void> => {
-  // await axios.post(process.env.CREATE_LOG_URL || "", {
-  //   buildId: process.env.BUILD_ID,
-  //   ...log,
-  // });
+  await axios.post(new URL('build-logger/create-log', process.env.BUILD_MANAGER_URL).href, {
+    buildId: process.env.BUILD_ID,
+    ...log,
+  });
 };
