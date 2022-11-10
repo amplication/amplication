1f60a0d6d769a0e4b56f15660ecb14e859c5bfdc
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-11-10 11:46:17 +0100

    Create React app with Nx

---
+++ packages/amplication-client/src/app/app.tsx
@@ -0,0 +1,13 @@
+import "./app.scss";
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="amplication-client" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ packages/amplication-client/src/main.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from "react-dom/client";
+
+import App from "./app/app";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App />);
