a61293d37e5d51957686a59202c15d5f4ec6d7d7
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-11-10 21:57:59 +0100

    Fix react compound timer using internal package

--- packages/amplication-client/src/Components/Timer.tsx
+++ packages/amplication-client/src/Components/Timer.tsx
@@ -1,6 +1,6 @@
-// import React, { useMemo } from "react";
-// import { differenceInMilliseconds } from "date-fns";
-// import TimerComponent from "react-compound-timer";
+import React, { useMemo } from "react";
+import { differenceInMilliseconds } from "date-fns";
+import TimerComponent from "@amplication/react-compound-timer";
 
 const CLASS_NAME = "amp-timer";
 const EMPTY_TIMER = "0m 0s";
@@ -12,34 +12,33 @@ type Props = {
 };
 
 function Timer({ startTime, endTime, runTimer }: Props) {
-  // const initialTime = useMemo(() => {
-  //   const endDate = endTime ? new Date(endTime) : new Date();
-  //   return differenceInMilliseconds(endDate, new Date(startTime));
-  // }, [startTime, endTime]);
+  const initialTime = useMemo(() => {
+    const endDate = endTime ? new Date(endTime) : new Date();
+    return differenceInMilliseconds(endDate, new Date(startTime));
+  }, [startTime, endTime]);
 
-  // const startImmediately = !endTime && runTimer;
+  const startImmediately = !endTime && runTimer;
 
   // use a unique key to force re-render of Timer when startTime or endTime change
-  // const timerKey =
-  //   (startTime ? startTime.toString() : "") +
-  //   (endTime ? endTime.toString() : "");
+  const timerKey =
+    (startTime ? startTime.toString() : "") +
+    (endTime ? endTime.toString() : "");
 
   return (
     <span className={`${CLASS_NAME}`}>
       {!startTime ? (
         EMPTY_TIMER
       ) : (
-        <></>
-        // <TimerComponent
-        //   key={timerKey}
-        //   initialTime={initialTime}
-        //   startImmediately={startImmediately}
-        // >
-        //   <TimerComponent.Hours
-        //     formatValue={(value) => (value ? `${value}h ` : "")}
-        //   />
-        //   <TimerComponent.Minutes />m <TimerComponent.Seconds />s
-        // </TimerComponent>
+        <TimerComponent
+          key={timerKey}
+          initialTime={initialTime}
+          startImmediately={startImmediately}
+        >
+          <TimerComponent.Hours
+            formatValue={(value) => (value ? `${value}h ` : "")}
+          />
+          <TimerComponent.Minutes />m <TimerComponent.Seconds />s
+        </TimerComponent>
       )}
     </span>
   );
