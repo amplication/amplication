c041fae171fd8ddc223e76f6ea7e61a897ba1b6a
barshimi <barshimi@gmail.com>
2022-11-13 16:27:11 +0200

    fix bug

--- packages/amplication-data-service-generator/src/server/resource/resolver/create-resolver.ts
+++ packages/amplication-data-service-generator/src/server/resource/resolver/create-resolver.ts
@@ -131,7 +131,7 @@ export async function createResolverModules(
       createResolverBaseModule,
       EventNames.CreateEntityResolverBase,
       {
-        templateBase,
+        template: templateBase,
         entityName,
         entityType,
         entityServiceModule,
