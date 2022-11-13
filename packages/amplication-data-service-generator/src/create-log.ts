01b7d68cd5b6a22f4031055ad974ad3ca4c65668
Eugene Tseitlin <eugene@amplication.com>
2022-11-13 13:44:15 +0100

    add remote env variable

--- packages/amplication-data-service-generator/src/create-log.ts
+++ packages/amplication-data-service-generator/src/create-log.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 import { LogEntry } from "winston";
 
 export const createLog = async (log: LogEntry): Promise<void> => {
+  if (!process.env.REMOTE_ENV) {
+    return;
+  }
+  
   await axios.post(new URL('build-logger/create-log', process.env.BUILD_MANAGER_URL).href, {
     buildId: process.env.BUILD_ID,
     ...log,
