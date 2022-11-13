6dc271b71154a2a734213388f6cff107b24ef475
Mor Hagbi <mor@amplication.com>
2022-11-13 11:10:10 +0200

    change react memo to react function

--- packages/amplication-client/src/ServiceConnections/ServiceConnectionsList.tsx
+++ packages/amplication-client/src/ServiceConnections/ServiceConnectionsList.tsx
@@ -20,81 +20,83 @@ type Props = {
   resourceId: string;
   selectFirst?: boolean;
 };
+const ServiceConnectionsList: React.FC<Props> = ({
+  resourceId,
+  selectFirst = false,
+}: Props) => {
+  const { currentWorkspace, currentProject, resources } = useContext(
+    AppContext
+  );
 
-export const ServiceConnectionsList = React.memo(
-  ({ resourceId, selectFirst = false }: Props) => {
-    const { currentWorkspace, currentProject, resources } = useContext(
-      AppContext
-    );
+  const history = useHistory();
 
-    const history = useHistory();
+  const {
+    serviceTopicsList: data,
+    loadingServiceTopics: loading,
+    errorServiceTopics: error,
+  } = useServiceConnection(resourceId);
 
-    const {
-      serviceTopicsList: data,
-      loadingServiceTopics: loading,
-      errorServiceTopics: error,
-    } = useServiceConnection(resourceId);
+  const errorMessage = formatError(error);
 
-    const errorMessage = formatError(error);
+  const messageBrokerList = useMemo((): MessageBrokerListItem[] => {
+    return resources
+      .filter(
+        (resource) =>
+          resource.resourceType === models.EnumResourceType.MessageBroker
+      )
+      .map(
+        (resource): MessageBrokerListItem => {
+          return {
+            resource,
+            connection: data?.ServiceTopicsList.find(
+              (connection) => connection.messageBrokerId === resource.id
+            ),
+          };
+        }
+      );
+  }, [resources, data?.ServiceTopicsList]);
 
-    const messageBrokerList = useMemo((): MessageBrokerListItem[] => {
-      return resources
-        .filter(
-          (resource) =>
-            resource.resourceType === models.EnumResourceType.MessageBroker
-        )
-        .map(
-          (resource): MessageBrokerListItem => {
-            return {
-              resource,
-              connection: data?.ServiceTopicsList.find(
-                (connection) => connection.messageBrokerId === resource.id
-              ),
-            };
+  useEffect(() => {
+    if (selectFirst && !isEmpty(messageBrokerList)) {
+      const resource = messageBrokerList[0].resource;
+      const connectionUrl = `/${currentWorkspace?.id}/${currentProject?.id}/${resourceId}/service-connections/${resource.id}`;
+      history.push(connectionUrl);
+    }
+  }, [
+    messageBrokerList,
+    selectFirst,
+    resourceId,
+    history,
+    currentWorkspace,
+    currentProject,
+  ]);
+
+  return (
+    <div className={CLASS_NAME}>
+      <div className={`${CLASS_NAME}__header`}>Message Brokers</div>
+      {loading && <CircularProgress />}
+      <div className={`${CLASS_NAME}__list`}>
+        {messageBrokerList.map((connection, index) => {
+          if (!currentProject?.id || !currentWorkspace?.id) {
+            return null;
           }
-        );
-    }, [resources, data?.ServiceTopicsList]);
 
-    useEffect(() => {
-      if (selectFirst && !isEmpty(messageBrokerList)) {
-        const resource = messageBrokerList[0].resource;
-        const connectionUrl = `/${currentWorkspace?.id}/${currentProject?.id}/${resourceId}/service-connections/${resource.id}`;
-        history.push(connectionUrl);
-      }
-    }, [
-      messageBrokerList,
-      selectFirst,
-      resourceId,
-      history,
-      currentWorkspace,
-      currentProject,
-    ]);
-
-    return (
-      <div className={CLASS_NAME}>
-        <div className={`${CLASS_NAME}__header`}>Message Brokers</div>
-        {loading && <CircularProgress />}
-        <div className={`${CLASS_NAME}__list`}>
-          {messageBrokerList.map((connection, index) => {
-            if (!currentProject?.id || !currentWorkspace?.id) {
-              return null;
-            }
-
-            return (
-              <ServiceConnectionsListItem
-                key={index}
-                currentProjectId={currentProject.id}
-                currentWorkspaceId={currentWorkspace.id}
-                enabled={connection.connection?.enabled || false}
-                resourceId={resourceId}
-                connectionResource={connection.resource}
-              />
-            );
-          })}
-        </div>
-
-        <Snackbar open={Boolean(error)} message={errorMessage} />
+          return (
+            <ServiceConnectionsListItem
+              key={index}
+              currentProjectId={currentProject.id}
+              currentWorkspaceId={currentWorkspace.id}
+              enabled={connection.connection?.enabled || false}
+              resourceId={resourceId}
+              connectionResource={connection.resource}
+            />
+          );
+        })}
       </div>
-    );
-  }
-);
+
+      <Snackbar open={Boolean(error)} message={errorMessage} />
+    </div>
+  );
+};
+
+export default ServiceConnectionsList;
--- packages/amplication-client/src/ServiceConnections/ServiceConnectionsPage.tsx
+++ packages/amplication-client/src/ServiceConnections/ServiceConnectionsPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { match } from "react-router-dom";
 import PageContent from "../Layout/PageContent";
 import { AppRouteProps } from "../routes/routesUtil";
-import { ServiceConnectionsList } from "./ServiceConnectionsList";
+import ServiceConnectionsList from "./ServiceConnectionsList";
 
 type Props = AppRouteProps & {
   match: match<{
