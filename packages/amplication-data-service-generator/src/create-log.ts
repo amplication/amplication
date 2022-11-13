9ff49f4234ad1f50eb07dcc8d9316c71e279bc3c
Ariel Weinberger <weinberger.ariel@gmail.com>
2022-11-13 15:03:28 +0100

    Fix formatting

--- packages/amplication-data-service-generator/src/create-log.ts
+++ packages/amplication-data-service-generator/src/create-log.ts
@@ -5,9 +5,12 @@ export const createLog = async (log: LogEntry): Promise<void> => {
   if (!process.env.REMOTE_ENV) {
     return;
   }
-  
-  await axios.post(new URL('build-logger/create-log', process.env.BUILD_MANAGER_URL).href, {
-    buildId: process.env.BUILD_ID,
-    ...log,
-  });
+
+  await axios.post(
+    new URL("build-logger/create-log", process.env.BUILD_MANAGER_URL).href,
+    {
+      buildId: process.env.BUILD_ID,
+      ...log,
+    }
+  );
 };
