71b480126b3d2d5e7c5131f2d43fcb02a1550544
Mor Hagbi <mor@amplication.com>
2022-11-21 10:42:04 +0200

    pr => remove unused Modules

--- packages/amplication-server/src/core/topic/topic.module.ts
+++ packages/amplication-server/src/core/topic/topic.module.ts
@@ -3,18 +3,11 @@ import { TopicService } from "./topic.service";
 import { TopicResolver } from "./topic.resolver";
 import { BlockModule } from "../block/block.module";
 import { PermissionsModule } from "../permissions/permissions.module";
-import { ServiceTopicsService } from "../serviceTopics/serviceTopics.service";
 import { ServiceTopicsModule } from "../serviceTopics/serviceTopics.module";
-import { ResourceModule } from "../resource/resource.module";
 
 @Module({
-  imports: [
-    BlockModule,
-    PermissionsModule,
-    ServiceTopicsModule,
-    ResourceModule,
-  ],
-  providers: [TopicService, TopicResolver, ServiceTopicsService],
-  exports: [TopicService, TopicResolver, ServiceTopicsService],
+  imports: [BlockModule, PermissionsModule, ServiceTopicsModule],
+  providers: [TopicService, TopicResolver],
+  exports: [TopicService, TopicResolver],
 })
 export class TopicModule {}
