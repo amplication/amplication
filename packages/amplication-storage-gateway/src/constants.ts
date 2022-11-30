3afdd206da8a3170ab6c8eade83685cccb080c7c
tupe12334 <ofek.gabay.he@gmail.com>
2022-11-30 17:28:22 +0200

    fix: storage

--- packages/amplication-storage-gateway/src/constants.ts
+++ packages/amplication-storage-gateway/src/constants.ts
@@ -6,8 +6,12 @@ export const BUILD_ARTIFACTS_BASE_FOLDER = "BUILD_ARTIFACTS_BASE_FOLDER";
 export const BUILD_ARTIFACTS_CODE_FOLDER = "BUILD_ARTIFACTS_CODE_FOLDER";
 
 export const CHECK_USER_ACCESS_TOPIC = "CHECK_USER_ACCESS_TOPIC";
-export const DOT_AMPLICATION_FOLDER = join(process.cwd(), ".amplication");
-console.log(DOT_AMPLICATION_FOLDER);
+export const DOT_AMPLICATION_FOLDER = join(
+  process.cwd(),
+  "..",
+  "..",
+  ".amplication"
+);
 
 export const DEFAULT_BUILDS_FOLDER = join(
   DOT_AMPLICATION_FOLDER,
