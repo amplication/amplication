d91ac15bf6edd85ecc7c59462dbc87c1fcb7e6f7
Amit Barletz <amit@amplication.com>
2022-12-05 18:57:56 +0200

    feat(prisma-db) change the output of prisma client

--- packages/amplication-prisma-db/src/index.ts
+++ packages/amplication-prisma-db/src/index.ts
@@ -1,4 +1,4 @@
-export * from "@amplication/prisma-clients/amplication-prisma-db";
+export * from "./generated-prisma-client";
 export * from "./prisma.service";
 export * from "./prisma.module";
 export * from "prisma";
