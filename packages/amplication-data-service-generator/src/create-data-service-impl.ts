6bb03fa382a5b486270eecafd5c92c7ca0e0aee9
barshimi <barshimi@gmail.com>
2022-12-06 10:57:23 +0200

    throw error at the end of process

--- packages/amplication-data-service-generator/src/create-data-service-impl.ts
+++ packages/amplication-data-service-generator/src/create-data-service-impl.ts
@@ -68,6 +68,6 @@ export async function createDataServiceImpl(
     });
     logger.error((error as Error).message);
     logger.error((error as Error).stack);
-    return [];
+    throw new Error((error as Error).message);
   }
 }
