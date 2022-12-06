1e200bf871c828064fa41916ab8ccf1c5a9fcfdc
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2022-12-06 11:14:01 +0000

    feat(dsg): add logs before and after request

--- packages/amplication-data-service-generator/src/util/httpClient.ts
+++ packages/amplication-data-service-generator/src/util/httpClient.ts
@@ -1,9 +1,20 @@
 import http from "http";
 import https from "https";
 import axios from "axios";
+import { defaultLogger as logger } from "../server/logging";
 
 const httpAgent = new http.Agent({ keepAlive: true });
 const httpsAgent = new https.Agent({ keepAlive: true });
+
+axios.interceptors.request.use((request) => {
+  logger.info("Starting Request", JSON.stringify(request, null, 2));
+  return request;
+});
+
+axios.interceptors.response.use((response) => {
+  logger.info("Response:", JSON.stringify(response, null, 2));
+  return response;
+});
 
 export const httpClient = axios.create({
   httpAgent,
