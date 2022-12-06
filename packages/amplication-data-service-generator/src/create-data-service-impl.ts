be875dc3aed9a55ceb96610621d649ab1b480da1
barshimi <barshimi@gmail.com>
2022-12-06 10:39:27 +0200

    remove data from log

--- packages/amplication-data-service-generator/src/create-data-service-impl.ts
+++ packages/amplication-data-service-generator/src/create-data-service-impl.ts
@@ -55,7 +55,6 @@ export async function createDataServiceImpl(
     await createLog({
       level: "info",
       message: "Failed to run createDataServiceImpl",
-      data: JSON.stringify(dSGResourceData),
     });
     logger.error((error as Error).stack);
     return [];
