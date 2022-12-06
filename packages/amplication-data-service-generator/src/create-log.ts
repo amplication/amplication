7e29e4fb4b21667f5f74cc5033f1658411e490a4
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2022-12-06 10:44:41 +0000

    fix(dsg): replace axios client with new httpClient in create log and execution status report requests

--- packages/amplication-data-service-generator/src/create-log.ts
+++ packages/amplication-data-service-generator/src/create-log.ts
@@ -1,18 +1,23 @@
-import axios from "axios";
+import { httpClient } from "./util/httpClient";
+import { defaultLogger as logger } from "./server/logging";
 import { LogEntry } from "winston";
 
 export const createLog = async (log: LogEntry): Promise<void> => {
-  if (process.env.REMOTE_ENV !== "true") {
-    console.log("Running locally, skipping log reporting");
-    return;
-  }
-
-  console.log("Sending log to build manager");
-  await axios.post(
-    new URL("build-logger/create-log", process.env.BUILD_MANAGER_URL).href,
-    {
-      buildId: process.env.BUILD_ID,
-      ...log,
+  try {
+    if (process.env.REMOTE_ENV !== "true") {
+      logger.info("Running locally, skipping log reporting");
+      return;
     }
-  );
+
+    logger.info("Sending log to build manager");
+    await httpClient.post(
+      new URL("build-logger/create-log", process.env.BUILD_MANAGER_URL).href,
+      {
+        buildId: process.env.BUILD_ID,
+        ...log,
+      }
+    );
+  } catch (error) {
+    logger.error("Failed to send log to build manager", error);
+  }
 };
