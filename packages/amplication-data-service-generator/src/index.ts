2e856026cd85829efeba4454c49bef95e80d3448
tupe12334 <ofek.gabay.he@gmail.com>
2022-12-11 17:15:25 +0200

    from app to lib dsg

--- packages/amplication-data-service-generator/src/index.ts
+++ packages/amplication-data-service-generator/src/index.ts
@@ -1,2 +1,4 @@
 export * from "@amplication/code-gen-types";
 export { createDataServiceImpl } from "./create-data-service-impl";
+export { defaultLogger } from "./server/logging";
+export { httpClient } from "./util/http-client";
