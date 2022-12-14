10d46b154e36f1b38c02299e60415f0ab970a0cd
goingdust <dustinluck@gmail.com>
2022-12-14 17:54:40 -0500

    Write NotFoundPage spec

--- packages/amplication-client/src/404/NotFoundPage.tsx
+++ packages/amplication-client/src/404/NotFoundPage.tsx
@@ -10,7 +10,12 @@ const NotFound = () => {
   return (
     <PageContent pageTitle={pageTitle} className={CLASS_NAME}>
       <h1>
-        <img src={warning} alt="warning" className="warning" />
+        <img
+          src={warning}
+          alt="warning"
+          className="warning"
+          data-testid="warning"
+        />
         404 | Not Found
       </h1>
       <h2>Oops! It looks like this page doesn't exist.</h2>
