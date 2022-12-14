09d3697c410754a7d710abb1077024650c6e172d
tupe12334 <ofek.gabay.he@gmail.com>
2022-12-14 13:57:58 +0200

    remove old code

--- libs/util/code-gen-utils/src/lib/util-code-gen-utils.spec.ts
+++
@@ -1,7 +0,0 @@
-import { utilCodeGenUtils } from "./util-code-gen-utils";
-
-describe("utilCodeGenUtils", () => {
-  it("should work", () => {
-    expect(utilCodeGenUtils()).toEqual("util-code-gen-utils");
-  });
-});
--- libs/util/code-gen-utils/src/lib/util-code-gen-utils.ts
+++
@@ -1,3 +0,0 @@
-export function utilCodeGenUtils(): string {
-  return "util-code-gen-utils";
-}
