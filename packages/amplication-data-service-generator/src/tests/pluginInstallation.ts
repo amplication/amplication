83943fc503ec8429efd7d0e586d3d2f4c43e4fad
Mor Hagbi <mor@amplication.com>
2022-12-15 18:43:46 +0200

    Update snapshot

--- packages/amplication-data-service-generator/src/tests/pluginInstallation.ts
+++ packages/amplication-data-service-generator/src/tests/pluginInstallation.ts
@@ -1,3 +1,11 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
 
-export const installedPlugins: PluginInstallation[] = [];
+export const installedPlugins: PluginInstallation[] = [
+  {
+    id: "auth",
+    enabled: true,
+    version: "0.0.1",
+    pluginId: "pluginId",
+    npm: "@amplication/plugin-auth",
+  },
+];
