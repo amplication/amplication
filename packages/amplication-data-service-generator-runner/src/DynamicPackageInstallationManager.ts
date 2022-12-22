d73236564e809006776aa7755b1efb1cb0999fdc
tupe12334 <ofek.gabay.he@gmail.com>
2022-12-22 10:23:51 +0200

    download files

--- packages/amplication-data-service-generator-runner/src/DynamicPackageInstallationManager.ts
+++ packages/amplication-data-service-generator-runner/src/DynamicPackageInstallationManager.ts
@@ -1,13 +1,32 @@
 import { packument } from "pacote";
 import { valid } from "semver";
+import { createWriteStream } from "fs";
+import axios from "axios";
 
 export class DynamicPackageInstallationManager {
+  constructor(private pluginInstallationPath: string) {}
   async install(packageName: string, version: string) {
     const validVersion = valid(version);
-    if (validVersion === null) {
-      throw new Error("Invalid version"); //TODO error class
-    }
-    const tarball = this.packageTarball(packageName, validVersion);
+    this.validateOrThrowVersion(version);
+
+    const tarball = await this.packageTarball(packageName, validVersion);
+    await this.downloadTarball(tarball);
+  }
+
+  async downloadTarball(tarball: string): Promise<void> {
+    const writer = createWriteStream(this.pluginInstallationPath);
+
+    const response = await axios(tarball, {
+      method: "GET",
+      responseType: "stream",
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
   }
 
   async packageTarball(packageName: string, version: string) {
@@ -20,5 +39,12 @@ export class DynamicPackageInstallationManager {
     }
 
     return latest.dist.tarball;
+  }
+
+  validateOrThrowVersion(version: string): Promise<void> {
+    // if (validVersion === null) {
+    //   throw new Error("Invalid version"); //TODO error class
+    // }
+    return;
   }
 }
--- packages/amplication-data-service-generator-runner/src/dynamic-package-installation.ts
+++ packages/amplication-data-service-generator-runner/src/dynamic-package-installation.ts
@@ -1,11 +1,14 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
+import { join } from "path";
 import { DynamicPackageInstallationManager } from "./DynamicPackageInstallationManager";
 
 export async function dynamicPackagesInstallations(
   packages: PluginInstallation[]
 ): Promise<void> {
   console.info("Installing dynamic packages");
-  const manager = new DynamicPackageInstallationManager();
+  const manager = new DynamicPackageInstallationManager(
+    join(__dirname, "..", "amplication_modules")
+  );
 
   for (const plugin of packages) {
     const { npm, version } = plugin;
