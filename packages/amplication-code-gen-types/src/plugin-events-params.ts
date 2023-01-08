0cd6cef9d1cf220355fe398cdb816618f869434b
Amit Barletz <amit@amplication.com>
2023-01-08 17:25:09 +0200

    fix(code-gen-types) add server and admin gitignore

--- packages/amplication-code-gen-types/src/plugin-events-params.ts
+++ packages/amplication-code-gen-types/src/plugin-events-params.ts
@@ -88,6 +88,9 @@ export interface CreateServerDotEnvParams extends EventParams {
   envVariables: VariableDictionary;
 }
 
+export interface CreateServerGitIgnoreParams extends EventParams {}
+export interface CreateAdminGitIgnoreParams extends EventParams {}
+
 export interface CreateServerDockerComposeParams extends EventParams {
   fileContent: string;
   updateProperties: { [key: string]: any }[];
