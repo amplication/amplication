9be430e8eb3dea2cbad2f012d237dda5e7656383
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-01-14 16:13:43 +0000

    fix(server): refactor the queue service to wait for a valid kafka connection before emit. Related to https://github.com/amplication/amplication/pull/4948 (#4949)

--- packages/amplication-server/src/core/queue/queue.service.ts
+++ packages/amplication-server/src/core/queue/queue.service.ts
@@ -1,22 +1,20 @@
-import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { ClientKafka } from "@nestjs/microservices";
 
 export const QUEUE_SERVICE_NAME = "QUEUE_SERVICE";
 
 @Injectable()
-export class QueueService implements OnModuleInit {
+export class QueueService {
   generatePullRequestTopic: string;
   constructor(
     @Inject(QUEUE_SERVICE_NAME)
     private readonly kafkaClient: ClientKafka
   ) {}
 
-  async onModuleInit() {
+  async emitMessage(topic: string, message: string): Promise<void> {
     // Explicitly wait for kafka client to connect. https://github.com/nestjs/nest/issues/10449
     await this.kafkaClient.connect();
-  }
 
-  emitMessage(topic: string, message: string): void {
     this.kafkaClient.emit(topic, message);
   }
 }
