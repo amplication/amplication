366105633edf2ab7fc29b029db7734f9ee8ea6d8
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-01-14 15:33:00 +0000

    fix(server): issue where first kafka event emitted fails silently

--- packages/amplication-server/src/core/queue/queue.service.ts
+++ packages/amplication-server/src/core/queue/queue.service.ts
@@ -1,15 +1,20 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
 import { ClientKafka } from "@nestjs/microservices";
 
 export const QUEUE_SERVICE_NAME = "QUEUE_SERVICE";
 
 @Injectable()
-export class QueueService {
+export class QueueService implements OnModuleInit {
   generatePullRequestTopic: string;
   constructor(
     @Inject(QUEUE_SERVICE_NAME)
     private readonly kafkaClient: ClientKafka
   ) {}
+
+  async onModuleInit() {
+    // Explicitly wait for kafka client to connect. https://github.com/nestjs/nest/issues/10449
+    await this.kafkaClient.connect();
+  }
 
   emitMessage(topic: string, message: string): void {
     this.kafkaClient.emit(topic, message);
