493daf498b4442e53b679a152ef59f6eb2c214cb
Mor Hagbi <mor@amplication.com>
2023-01-18 20:53:33 +0200

    feat(dsg): add auth to default plugins list

--- libs/data-service-generator/src/prepare-context.ts
+++ libs/data-service-generator/src/prepare-context.ts
@@ -26,6 +26,8 @@ import { resolveTopicNames } from "./util/message-broker";
 export const POSTGRESQL_PLUGIN_ID = "db-postgres";
 export const MYSQL_PLUGIN_ID = "db-mysql";
 export const POSTGRESQL_NPM = "@amplication/plugin-db-postgres";
+export const AUTH_PLUGIN_ID = "auth-service";
+export const AUTH_NPM = "@amplication/plugin-auth";
 
 const defaultPlugins: {
   categoryPluginIds: string[];
@@ -37,6 +39,16 @@ const defaultPlugins: {
       id: "placeholder-id",
       pluginId: POSTGRESQL_PLUGIN_ID,
       npm: POSTGRESQL_NPM,
+      enabled: true,
+      version: "latest",
+    },
+  },
+  {
+    categoryPluginIds: [AUTH_PLUGIN_ID],
+    defaultCategoryPlugin: {
+      id: "placeholder-id",
+      pluginId: AUTH_PLUGIN_ID,
+      npm: AUTH_NPM,
       enabled: true,
       version: "latest",
     },
