7ba0a477da408b51e604d45dbd8cc41e97f06a8c
yuval-hazaz <hazaz.yuval@gmail.com>
2023-01-19 16:17:21 +0200

    test(dsg):update snapshot

--- libs/data-service-generator/src/tests/pluginInstallation.ts
+++ libs/data-service-generator/src/tests/pluginInstallation.ts
@@ -1,12 +1,3 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
 import { plugins } from "./constants/example-plugins";
-export const installedPlugins: PluginInstallation[] = [
-  plugins.postgresPlugin,
-  {
-    id: "auth-api",
-    enabled: true,
-    version: "0.0.1",
-    pluginId: "pluginId",
-    npm: "@amplication/plugin-auth",
-  },
-];
+export const installedPlugins: PluginInstallation[] = [plugins.postgresPlugin];
