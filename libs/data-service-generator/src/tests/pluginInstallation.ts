eb161c79df0076b9489491984626ab39d9fefc23
Mor Hagbi <mor@amplication.com>
2023-01-26 17:43:38 +0200

    feat(dsg): add auth plugin to tests

--- libs/data-service-generator/src/tests/pluginInstallation.ts
+++ libs/data-service-generator/src/tests/pluginInstallation.ts
@@ -1,3 +1,13 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
 import { plugins } from "./constants/example-plugins";
-export const installedPlugins: PluginInstallation[] = [plugins.postgresPlugin];
+export const installedPlugins: PluginInstallation[] = [
+  plugins.postgresPlugin,
+  {
+    id: "clb3p3t7z0098jn015na6ork3",
+    npm: "@amplication/plugin-auth",
+    enabled: true,
+    version: "latest",
+    pluginId: "auth-api",
+    settings: {},
+  },
+];
