475df320b628129936dbfd87f39b33ef9f62b686
Amit Barletz <amit@amplication.com>
2023-01-27 00:40:30 +0200

    refactor(server): fix test

--- packages/amplication-server/src/core/git/__mocks__/Github.ts
+++ packages/amplication-server/src/core/git/__mocks__/Github.ts
@@ -4,9 +4,9 @@ import { TEST_GIT_REPO } from "./GitRepo";
 import { TEST_GIT_REPOS } from "./GitRepos";
 
 export const MOCK_GITHUB_SERVICE = mock<GithubService>({});
-MOCK_GITHUB_SERVICE.getOrganizationRepos.mockReturnValue(
+MOCK_GITHUB_SERVICE.getRepositories.mockReturnValue(
   Promise.resolve(TEST_GIT_REPOS)
 );
-MOCK_GITHUB_SERVICE.createOrganizationRepository.mockReturnValue(
+MOCK_GITHUB_SERVICE.createRepository.mockReturnValue(
   Promise.resolve(TEST_GIT_REPO)
 );
