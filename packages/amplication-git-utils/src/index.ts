193386ad4048f91d5e5edde73599df2fa5ccbbf4
Amit Barletz <amit@amplication.com>
2023-01-27 00:06:40 +0200

    refactor(git-utils): update exports in indexts

--- packages/amplication-git-utils/src/index.ts
+++ packages/amplication-git-utils/src/index.ts
@@ -1,5 +1,3 @@
 export { GitModule } from "./git/git.module";
-export { GitService } from "./git/git.service";
-export { GithubService } from "./git/github.service";
-export { GitServiceFactory } from "./git/git-service-factory";
+export { GitFactory } from "./git/git-factory";
 export * from "./types";
