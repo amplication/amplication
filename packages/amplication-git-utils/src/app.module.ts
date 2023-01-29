58561a44a040a699ea3c02450f03a5afbbb9a24f
Amit Barletz <amit@amplication.com>
2023-01-29 14:29:17 +0200

    refactor(git-utils): delete git module and update import and exports files

--- packages/amplication-git-utils/src/app.module.ts
+++ packages/amplication-git-utils/src/app.module.ts
@@ -1,11 +1,14 @@
+import { AmplicationLoggerModule } from "@amplication/nest-logger-module";
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { GitModule } from "./git/git.module";
+import { SERVICE_NAME } from "./git/git.constants";
 
 @Global()
 @Module({
   imports: [
-    GitModule,
+    AmplicationLoggerModule.register({
+      metadata: { service: SERVICE_NAME },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [".env.local", ".env"],
--- packages/amplication-git-utils/src/index.ts
+++ packages/amplication-git-utils/src/index.ts
@@ -1,3 +1,2 @@
-export { GitModule } from "./git/git.module";
 export { GitClientService } from "./git/git.service";
 export * from "./types";
