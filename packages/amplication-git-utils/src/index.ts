137161fcafef4aa4b891c7292f89567252b06960
Amit Barletz <amit@amplication.com>
2023-01-29 12:24:51 +0200

    refactor(git-utils): change file name back

--- packages/amplication-git-utils/src/index.ts
+++ packages/amplication-git-utils/src/index.ts
@@ -1,3 +1,3 @@
 export { GitModule } from "./git/git.module";
-export { GitClientService } from "./git/git-client.service";
+export { GitClientService } from "./git/git.service";
 export * from "./types";
