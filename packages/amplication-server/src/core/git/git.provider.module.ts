ec31d5a673f481273cf6d820e21b167e1883fd51
Amit Barletz <amit@amplication.com>
2023-01-29 14:30:21 +0200

    refactor(server): update after deletion of git module

--- packages/amplication-server/src/core/git/git.provider.module.ts
+++ packages/amplication-server/src/core/git/git.provider.module.ts
@@ -3,15 +3,9 @@ import { PrismaService, PrismaModule } from "../../prisma";
 import { ResourceModule } from "../resource/resource.module";
 import { PermissionsModule } from "../permissions/permissions.module";
 import { GitResolver } from "./git.resolver";
-import { GitModule, GitClientService } from "@amplication/git-utils";
 import { GitProviderService } from "./git.provider.service";
 @Module({
-  imports: [
-    PermissionsModule,
-    forwardRef(() => ResourceModule),
-    PrismaModule,
-    GitModule,
-  ],
-  providers: [GitProviderService, GitResolver, PrismaService, GitClientService],
+  imports: [PermissionsModule, forwardRef(() => ResourceModule), PrismaModule],
+  providers: [GitProviderService, GitResolver, PrismaService],
 })
 export class GitProviderModule {}
