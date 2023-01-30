9817806b9fde4677e024cef83301aa26335be06a
Amit Barletz <amit@amplication.com>
2023-01-30 11:33:05 +0200

    refactor(git-utils): rename variable

--- packages/amplication-git-utils/src/git/git-factory.ts
+++ packages/amplication-git-utils/src/git/git-factory.ts
@@ -3,10 +3,10 @@ import { EnumGitProvider, GitProvider, GitProviderArgs } from "../types";
 import { GithubService } from "./github.service";
 
 export class GitFactory {
-  public static getProvider(options: GitProviderArgs): GitProvider {
-    switch (options.provider) {
+  public static getProvider(gitProviderArgs: GitProviderArgs): GitProvider {
+    switch (gitProviderArgs.provider) {
       case EnumGitProvider.Github:
-        return new GithubService(options);
+        return new GithubService(gitProviderArgs);
       default:
         throw new Error(INVALID_SOURCE_CONTROL_ERROR_MESSAGE);
     }
