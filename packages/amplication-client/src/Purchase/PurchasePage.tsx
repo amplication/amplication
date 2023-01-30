34dea34908f1fa5354faf8dc81f4aa970870a326
Spid3rrr <bra.ami20@hotmail.fr>
2023-01-30 20:32:36 +0100

    Change page title when showing purchase model

--- packages/amplication-client/src/Purchase/PurchasePage.tsx
+++ packages/amplication-client/src/Purchase/PurchasePage.tsx
@@ -15,6 +15,7 @@ import { AppContext } from "../context/appContext";
 import { PromoBanner } from "./PromoBanner";
 import { ApolloError, useMutation } from "@apollo/client";
 import { PROVISION_SUBSCRIPTION } from "../Workspaces/queries/workspaceQueries";
+import PageContent from "../Layout/PageContent";
 
 export type DType = {
   provisionSubscription: models.ProvisionSubscriptionResult;
@@ -81,117 +82,120 @@ const PurchasePage = (props) => {
     });
 
   return (
-    <Modal open fullScreen>
-      <div className={CLASS_NAME}>
-        <div className={`${CLASS_NAME}__layout`}>
-          <Button
-            className={`${CLASS_NAME}__layout__btn`}
-            buttonStyle={EnumButtonStyle.Outline}
-            icon={"arrow_left"}
-            iconPosition={EnumIconPosition.Left}
-            onClick={backUrl}
-          >
-            Back
-          </Button>
-        </div>
-        <div className={`${CLASS_NAME}__header`}>
-          Pick the perfect plan for your needs
-        </div>
-        <PromoBanner />
-        <Paywall
-          textOverrides={{
-            entitlementsTitle: (plan) => {
-              return plan.basePlan
-                ? `Everything in ${plan.basePlan.displayName} plan, plus:`
-                : `All core backend functionality:`;
-            },
-            planCTAButton: {
-              startNew: provisionSubscriptionLoading
-                ? "...Loading"
-                : "Upgrade now",
-              upgrade: provisionSubscriptionLoading
-                ? "...Loading"
-                : "Upgrade now",
-              custom: "Contact us",
-            },
-            price: {
-              free: {
-                price: "$0",
-                unit: "",
+    <PageContent pageTitle="Pricing & Plans">
+      <Modal open fullScreen>
+        <div className={CLASS_NAME}>
+          <div className={`${CLASS_NAME}__layout`}>
+            <Button
+              className={`${CLASS_NAME}__layout__btn`}
+              buttonStyle={EnumButtonStyle.Outline}
+              icon={"arrow_left"}
+              iconPosition={EnumIconPosition.Left}
+              onClick={backUrl}
+            >
+              Back
+            </Button>
+          </div>
+          <div className={`${CLASS_NAME}__header`}>
+            Pick the perfect plan for your needs
+          </div>
+          <PromoBanner />
+          <Paywall
+            textOverrides={{
+              entitlementsTitle: (plan) => {
+                return plan.basePlan
+                  ? `Everything in ${plan.basePlan.displayName} plan, plus:`
+                  : `All core backend functionality:`;
               },
-              custom: "Contact Us",
-              priceNotSet: "Price not set",
-            },
-          }}
-          onBillingPeriodChange={(billingPeriod: BillingPeriod) => {
-            trackEvent({
-              eventName: AnalyticsEventNames.PricingPageChangeBillingCycle,
-              action: billingPeriod,
-            });
-          }}
-          onPlanSelected={async ({
-            plan,
-            intentionType,
-            selectedBillingPeriod,
-          }) => {
-            trackEvent({
-              eventName: AnalyticsEventNames.PricingPageCTAClick,
-              currentPlan: plan.basePlan.displayName,
-              type: plan.displayName,
-              action: intentionType,
-              Billing: selectedBillingPeriod,
-            });
-            selectedPlanAction[plan.id](
-              props,
-              currentWorkspace,
-              selectedBillingPeriod,
+              planCTAButton: {
+                startNew: provisionSubscriptionLoading
+                  ? "...Loading"
+                  : "Upgrade now",
+                upgrade: provisionSubscriptionLoading
+                  ? "...Loading"
+                  : "Upgrade now",
+                custom: "Contact us",
+              },
+              price: {
+                free: {
+                  price: "$0",
+                  unit: "",
+                },
+                custom: "Contact Us",
+                priceNotSet: "Price not set",
+              },
+            }}
+            onBillingPeriodChange={(billingPeriod: BillingPeriod) => {
+              trackEvent({
+                eventName: AnalyticsEventNames.PricingPageChangeBillingCycle,
+                action: billingPeriod,
+              });
+            }}
+            onPlanSelected={async ({
+              plan,
               intentionType,
-              provisionSubscription
-            );
-          }}
-        />
-        <div className={`${CLASS_NAME}__contact`}>
-          <div className={`${CLASS_NAME}__contact__content`}>
-            <div className={`${CLASS_NAME}__contact__content__header`}>
-              Building an open-source project?
+              selectedBillingPeriod,
+            }) => {
+              trackEvent({
+                eventName: AnalyticsEventNames.PricingPageCTAClick,
+                currentPlan: plan.basePlan.displayName,
+                type: plan.displayName,
+                action: intentionType,
+                Billing: selectedBillingPeriod,
+              });
+              selectedPlanAction[plan.id](
+                props,
+                currentWorkspace,
+                selectedBillingPeriod,
+                intentionType,
+                provisionSubscription
+              );
+            }}
+          />
+          <div className={`${CLASS_NAME}__contact`}>
+            <div className={`${CLASS_NAME}__contact__content`}>
+              <div className={`${CLASS_NAME}__contact__content__header`}>
+                Building an open-source project?
+              </div>
+              <div className={`${CLASS_NAME}__contact__content__description`}>
+                Let us know if there is anything we can support you with. We
+                will do our best to help you improve your project for the
+                community!
+              </div>
             </div>
-            <div className={`${CLASS_NAME}__contact__content__description`}>
-              Let us know if there is anything we can support you with. We will
-              do our best to help you improve your project for the community!
+            <Button buttonStyle={EnumButtonStyle.Primary}>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className={`${CLASS_NAME}__contact_pro_btn`}
+                href="mailto:sales@amplication.com?subject=Pro plan for Open Source project"
+              >
+                Contact us
+              </a>
+            </Button>
+          </div>
+          <div className={`${CLASS_NAME}__footer`}>
+            <div className={`${CLASS_NAME}__footer__copyright`}>
+              ©2022 amplication
+            </div>
+            <div className={`${CLASS_NAME}__footer__links`}>
+              <a
+                href="https://amplication.com/privacy-policy"
+                className={`${CLASS_NAME}__footer__links__privacy`}
+              >
+                Privacy Policy
+              </a>
+              <a
+                href="https://amplication.com/terms"
+                className={`${CLASS_NAME}__footer__links__terms`}
+              >
+                Terms & Conditions
+              </a>
             </div>
           </div>
-          <Button buttonStyle={EnumButtonStyle.Primary}>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className={`${CLASS_NAME}__contact_pro_btn`}
-              href="mailto:sales@amplication.com?subject=Pro plan for Open Source project"
-            >
-              Contact us
-            </a>
-          </Button>
         </div>
-        <div className={`${CLASS_NAME}__footer`}>
-          <div className={`${CLASS_NAME}__footer__copyright`}>
-            ©2022 amplication
-          </div>
-          <div className={`${CLASS_NAME}__footer__links`}>
-            <a
-              href="https://amplication.com/privacy-policy"
-              className={`${CLASS_NAME}__footer__links__privacy`}
-            >
-              Privacy Policy
-            </a>
-            <a
-              href="https://amplication.com/terms"
-              className={`${CLASS_NAME}__footer__links__terms`}
-            >
-              Terms & Conditions
-            </a>
-          </div>
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+    </PageContent>
   );
 };
 
