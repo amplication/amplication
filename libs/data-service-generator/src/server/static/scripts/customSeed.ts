e848d1ed9535fedafb32cfe00a18855f8a6199b2
Mor Hagbi <mor@amplication.com>
2023-02-08 00:43:53 +0200

    feat(dsg):  remove from customSeed unrelevant code

--- libs/data-service-generator/src/server/static/scripts/customSeed.ts
+++ libs/data-service-generator/src/server/static/scripts/customSeed.ts
@@ -2,16 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 export async function customSeed() {
   const client = new PrismaClient();
-  const username = "admin";
-
-  //replace this sample code to populate your database
-  //with data that is required for your service to start
-  await client.user.update({
-    where: { username: username },
-    data: {
-      username,
-    },
-  });
 
   client.$disconnect();
 }
