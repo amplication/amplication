85a9fe2bd63aba7543373edc8754a2732f590e2b
Mor Hagbi <mor@amplication.com>
2023-02-08 01:45:30 +0200

    feat(dsg):updateinstalledPlugins tests

--- libs/data-service-generator/src/tests/pluginInstallation.ts
+++ libs/data-service-generator/src/tests/pluginInstallation.ts
@@ -1,13 +1,3 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
 import { plugins } from "./constants/example-plugins";
-export const installedPlugins: PluginInstallation[] = [
-  plugins.postgresPlugin,
-  {
-    id: "clb3p3t7z0098jn015na6ork3",
-    npm: "@amplication/plugin-auth",
-    enabled: true,
-    version: "latest",
-    pluginId: "auth-api",
-    settings: {},
-  },
-];
+export const installedPlugins: PluginInstallation[] = [plugins.postgresPlugin];
