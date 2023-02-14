bc2f23e5f289cb7cd15dd6433da855bcd2899a51
Mor Hagbi <mor@amplication.com>
2023-02-14 18:43:10 +0200

    fix(dsg): remove auth-core plugin from test files

--- packages/data-service-generator/src/tests/pluginInstallation.ts
+++ packages/data-service-generator/src/tests/pluginInstallation.ts
@@ -1,12 +1,3 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
 import { plugins } from "./constants/example-plugins";
-export const installedPlugins: PluginInstallation[] = [
-  plugins.postgresPlugin,
-  {
-    id: "placeholder-id",
-    pluginId: "auth-core",
-    npm: "@amplication/plugin-auth-core",
-    enabled: true,
-    version: "1.0.1",
-  },
-];
+export const installedPlugins: PluginInstallation[] = [plugins.postgresPlugin];
