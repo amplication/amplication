6f173d7f02019ec3795c2998ed7e7dead5c28d13
Lokeswaran Aruljothi <74011196+a-loke@users.noreply.github.com>
2023-02-14 12:42:27 +0530

    fix(server): add logic to check minimum value is less than maximum value and tests

--- packages/amplication-server/src/core/entity/entity.service.ts
+++ packages/amplication-server/src/core/entity/entity.service.ts
@@ -5,6 +5,7 @@ import {
   Injectable,
   NotFoundException,
   ConflictException,
+  BadRequestException,
 } from "@nestjs/common";
 import { DataConflictError } from "../../errors/DataConflictError";
 import { Prisma, PrismaService } from "../../prisma";
@@ -124,6 +125,9 @@ export type EntityPendingChange = {
 const NAME_REGEX = /^(?![0-9])[a-zA-Z0-9$_]+$/;
 export const NAME_VALIDATION_ERROR_MESSAGE =
   "Name must only contain letters, numbers, the dollar sign, or the underscore character and must not start with a number";
+
+export const NUMBER_WITH_INVALID_MINIMUM_VALUE =
+  "Minimum value can not be greater than or equal to, the Maximum value";
 
 export const DELETE_ONE_USER_ENTITY_ERROR_MESSAGE = `The 'user' entity is a reserved entity and it cannot be deleted`;
 
@@ -2118,6 +2122,14 @@ export class EntityService {
         "The base properties of the ID field cannot be edited"
       );
     }
+    const { minimumValue, maximumValue } = args?.data
+      ?.properties as unknown as {
+      minimumValue: number;
+      maximumValue: number;
+    };
+    if (minimumValue && minimumValue >= maximumValue) {
+      throw new BadRequestException(NUMBER_WITH_INVALID_MINIMUM_VALUE);
+    }
 
     // Delete related field in case field data type is changed from lookup
     const shouldDeleteRelated =
@@ -2221,7 +2233,9 @@ export class EntityService {
             },
           });
         }
-
+        console.log("====================================");
+        console.log(updatedField);
+        console.log("====================================");
         return updatedField;
       }
     );
