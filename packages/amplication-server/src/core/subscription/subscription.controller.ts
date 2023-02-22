15e6733069d367e00360fe251b9b85ce8d3fabc2
Eugene Tseitlin <eugene@amplication.com>
2023-02-22 16:46:08 +0100

    moved logic to service

--- packages/amplication-server/src/core/subscription/subscription.controller.ts
+++ packages/amplication-server/src/core/subscription/subscription.controller.ts
@@ -1,16 +1,7 @@
 import { Body, Controller, Post, Headers } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { Env } from "../../env";
-import {
-  EnumEventType,
-  SegmentAnalyticsService,
-} from "../../services/segmentAnalytics/segmentAnalytics.service";
-import { BillingService } from "../billing/billing.service";
-import { EnumSubscriptionPlan } from "./dto";
-import { CreateSubscriptionInput } from "./dto/CreateSubscriptionInput";
-import { SubscriptionData } from "./dto/SubscriptionData";
 import { UpdateStatusDto } from "./dto/UpdateStatusDto";
-import { UpdateSubscriptionInput } from "./dto/UpdateSubscriptionInput";
 import { SubscriptionService } from "./subscription.service";
 
 @Controller("subscriptions")
@@ -18,9 +9,7 @@ export class SubscriptionController {
   private readonly stiggWebhooksSecret: string;
 
   constructor(
-    private readonly analyticsService: SegmentAnalyticsService,
     private readonly subscriptionService: SubscriptionService,
-    private readonly billingService: BillingService,
     private readonly configService: ConfigService
   ) {
     this.stiggWebhooksSecret = this.configService.get(
@@ -37,62 +26,8 @@ export class SubscriptionController {
       throw new Error("Invalid stigg-webhooks-secret");
     }
 
-    switch (updateStatusDto.type) {
-      case "subscription.created": {
-        const createSubscriptionInput =
-          this.mapUpdateStatusDtoToCreateSubscriptionInput(updateStatusDto);
-        await this.subscriptionService.create(
-          updateStatusDto.id,
-          createSubscriptionInput
-        );
-        const userId = updateStatusDto.metadata?.userId;
-        if (
-          createSubscriptionInput.plan !== EnumSubscriptionPlan.Free &&
-          userId
-        ) {
-          await this.analyticsService.track({
-            userId: userId,
-            properties: {
-              workspaceId: updateStatusDto.customer.id,
-            },
-            event: EnumEventType.WorkspacePlanUpgradeCompleted,
-          });
-        }
-        break;
-      }
-      case "subscription.updated":
-      case "subscription.expired":
-      case "subscription.canceled": {
-        const updateSubscriptionInput =
-          this.mapUpdateStatusDtoToUpdateSubscriptionInput(updateStatusDto);
-        await this.subscriptionService.update(
-          updateStatusDto.id,
-          updateSubscriptionInput
-        );
-        break;
-      }
-    }
-  }
-
-  mapUpdateStatusDtoToCreateSubscriptionInput(
-    updateStatusDto: UpdateStatusDto
-  ): CreateSubscriptionInput {
-    return {
-      workspaceId: updateStatusDto.customer.id,
-      status: this.billingService.mapSubscriptionStatus(updateStatusDto.status),
-      plan: this.billingService.mapSubscriptionPlan(updateStatusDto.plan.id),
-      subscriptionData: new SubscriptionData(),
-    };
-  }
-
-  mapUpdateStatusDtoToUpdateSubscriptionInput(
-    updateStatusDto: UpdateStatusDto
-  ): UpdateSubscriptionInput {
-    return {
-      workspaceId: updateStatusDto.customer.id,
-      status: this.billingService.mapSubscriptionStatus(updateStatusDto.status),
-      plan: this.billingService.mapSubscriptionPlan(updateStatusDto.plan.id),
-      subscriptionData: new SubscriptionData(),
-    };
+    await this.subscriptionService.handleUpdateSubscriptionStatusEvent(
+      updateStatusDto
+    );
   }
 }
