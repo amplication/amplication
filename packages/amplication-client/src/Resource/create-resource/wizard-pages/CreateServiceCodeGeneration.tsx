d9ec67676b8ced0d3039a35327f8309ad06e10ce
Eugene Tseitlin <eugene@amplication.com>
2023-03-10 17:58:57 +0100

    added create service code generation component

--- packages/amplication-client/src/Resource/create-resource/wizard-pages/CreateServiceCodeGeneration.tsx
+++ packages/amplication-client/src/Resource/create-resource/wizard-pages/CreateServiceCodeGeneration.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { match } from "react-router-dom";
 import "../CreateServiceWizard.scss";
+import "./CreateServiceCodeGeneration.scss";
 import { AppRouteProps } from "../../../routes/routesUtil";
+import ActionLog from "../../../VersionControl/ActionLog";
+import { Action } from "../../../models";
+
+import CodeGenerationCompleted from "../../../../src/assets/images/code-generation-completed.svg";
+import { Button } from "@amplication/design-system";
 
 type Props = AppRouteProps & {
   match: match<{
@@ -10,12 +16,58 @@ type Props = AppRouteProps & {
   }>;
 };
 
-const CreateServiceCodeGeneration: React.FC<Props> = ({ moduleClass }) => {
+const className = "create-service-code-generation";
+
+const CreateServiceCodeGeneration = ({ moduleClass }) => {
+  const actionLog = {
+    action: {
+      createdAt: new Date(),
+      id: "",
+      steps: [],
+    },
+    title: "Generating service",
+    versionNumber: "1.0.0",
+  };
+
+  const completed = true;
+
   return (
-    <div className={`${moduleClass}__service_box`}>
-      <div className={`${moduleClass}__code_gen_title`}>
+    <div className={className}>
+      <div className={`${className}__title`}>
         <h2>All set! We’re currently generating your service.</h2>
         <h3>It should only take a few seconds to finish. Don't go away!</h3>
+      </div>
+      <div className={`${className}__status`}>
+        {!completed ? (
+          <ActionLog
+            action={actionLog?.action}
+            title={actionLog?.title || ""}
+            versionNumber={actionLog?.versionNumber || ""}
+          />
+        ) : (
+          <div className={`${className}__status__completed`}>
+            <img
+              className={`${className}__status__completed__image`}
+              src={CodeGenerationCompleted}
+              alt=""
+            />
+
+            <div className={`${className}__status__completed__description`}>
+              <div
+                className={`${className}__status__completed__description__header`}
+              >
+                The code for your service is ready on
+              </div>
+              <div
+                className={`${className}__status__completed__description__link`}
+              >
+                https://github.com/yuval/myservice/
+              </div>
+              <div />
+            </div>
+            <Button>View my code</Button>
+          </div>
+        )}
       </div>
     </div>
   );
