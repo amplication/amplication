02a86e887618062f7d8ef4241451ca9a4f97da1a
Amit Barletz <amit@amplication.com>
2023-03-13 16:49:35 +0200

    feat(git-utils): env variables

--- packages/amplication-git-utils/src/providers/bitbucket/bitbucket.service.ts
+++ packages/amplication-git-utils/src/providers/bitbucket/bitbucket.service.ts
@@ -26,6 +26,7 @@ import {
   PullRequest,
   GitProviderArgs,
   PaginatedGitGroup,
+  BitBucketConfiguration,
 } from "../../types";
 import { NotImplementedError } from "../../utils/custom-error";
 import {
@@ -44,9 +45,10 @@ export class BitBucketService implements GitProvider {
 
   constructor(
     private readonly gitProviderArgs: GitProviderArgs,
+    private readonly providerConfiguration: BitBucketConfiguration,
     private readonly logger: ILogger
   ) {
-    const { clientId, clientSecret } = gitProviderArgs.providerProperties;
+    const { clientId, clientSecret } = providerConfiguration;
     if (!clientId || !clientSecret) {
       this.logger.error("Missing Bitbucket configuration");
       throw new Error("Missing Bitbucket configuration");
@@ -118,7 +120,7 @@ export class BitBucketService implements GitProvider {
     this.logger.info("BitBucketService completeOAuth2Flow");
 
     return {
-      providerProperties: {
+      providerOrganizationProperties: {
         ...oAuthData,
         ...currentUserData,
       },
@@ -128,7 +130,7 @@ export class BitBucketService implements GitProvider {
 
   async getGitGroups(): Promise<PaginatedGitGroup> {
     const { accessToken, refreshToken } =
-      this.gitProviderArgs.providerProperties;
+      this.gitProviderArgs.providerOrganizationProperties;
     const paginatedWorkspaceMembership = await currentUserWorkspacesRequest(
       accessToken,
       this.clientId,
--- packages/amplication-git-utils/src/providers/github/github.service.ts
+++ packages/amplication-git-utils/src/providers/github/github.service.ts
@@ -38,6 +38,7 @@ import {
   OAuth2FlowResponse,
   PaginatedGitGroup,
   GitProviderArgs,
+  GitHubConfiguration,
 } from "../../types";
 import { ConverterUtil } from "../../utils/convert-to-number";
 import { NotImplementedError } from "../../utils/custom-error";
@@ -58,27 +59,27 @@ export class GithubService implements GitProvider {
   public readonly domain = "github.com";
   constructor(
     private readonly gitProviderArgs: GitProviderArgs,
+    private readonly providerConfiguration: GitHubConfiguration,
     private readonly logger: ILogger
   ) {
     const {
-      GITHUB_APP_INSTALLATION_URL,
-      GITHUB_APP_APP_ID,
-      GITHUB_APP_PRIVATE_KEY,
-    } = process.env;
-    if (!GITHUB_APP_INSTALLATION_URL) {
-      throw new Error("GITHUB_APP_INSTALLATION_URL is not defined");
-    }
-    this.gitInstallationUrl = GITHUB_APP_INSTALLATION_URL;
-    if (!GITHUB_APP_APP_ID) {
-      throw new Error("GITHUB_APP_APP_ID is not defined");
-    }
-    this.appId = GITHUB_APP_APP_ID;
-    if (!GITHUB_APP_PRIVATE_KEY) {
-      throw new Error("GITHUB_APP_PRIVATE_KEY is not defined");
-    }
-    this.privateKey = GITHUB_APP_PRIVATE_KEY;
+      appId,
+      privateKey: envPrivateKey,
+      installationUrl,
+    } = providerConfiguration;
+    this.gitInstallationUrl = installationUrl;
 
-    this.installationId = gitProviderArgs.providerProperties.installationId;
+    this.appId = appId;
+
+    this.privateKey = envPrivateKey;
+
+    this.installationId =
+      gitProviderArgs.providerOrganizationProperties.installationId;
+
+    if (!appId || !envPrivateKey || !installationUrl) {
+      this.logger.error("Missing Github configuration");
+      throw new Error("Missing Github configuration");
+    }
 
     const privateKey = this.getFormattedPrivateKey(this.privateKey);
     this.app = new App({
