90541cb3900d94fca50fb1197d30fed6852654b9
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-03-14 12:42:26 +0000

    fix(dsg): generate-local-code flow

--- packages/data-service-generator/src/generate-code.ts
+++ packages/data-service-generator/src/generate-code.ts
@@ -1,12 +1,36 @@
 import { DSGResourceData, Module } from "@amplication/code-gen-types";
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, readFile, writeFile } from "fs/promises";
 import { dirname, join } from "path";
 import { createDataService } from "./create-data-service";
 import { dynamicPackagesInstallations } from "./dynamic-package-installation";
-import { logger } from "./logger";
+import { BuildManagerNotifier } from "./notify-build-manager";
+import { defaultLogger as logger } from "./server/logging";
 import { prepareDefaultPlugins } from "./utils/dynamic-installation/defaultPlugins";
 
 export const AMPLICATION_MODULES = "amplication_modules";
+
+const readInputJson = async (filePath: string): Promise<DSGResourceData> => {
+  const file = await readFile(filePath, "utf8");
+  const resourceData: DSGResourceData = JSON.parse(file);
+  return resourceData;
+};
+
+const writeModules = async (
+  modules: Module[],
+  destination: string
+): Promise<void> => {
+  logger.info("Creating base directory");
+  await mkdir(destination, { recursive: true });
+  logger.info(`Writing modules to ${destination} ...`);
+  await Promise.all(
+    modules.map(async (module) => {
+      const filePath = join(destination, module.path);
+      await mkdir(dirname(filePath), { recursive: true });
+      await writeFile(filePath, module.code);
+    })
+  );
+  logger.info(`Successfully wrote modules to ${destination}`);
+};
 
 export const generateCodeByResourceData = async (
   resourceData: DSGResourceData,
@@ -28,19 +52,29 @@ export const generateCodeByResourceData = async (
   logger.info("Code generation completed successfully");
 };
 
-async function writeModules(
-  modules: Module[],
-  destination: string
-): Promise<void> {
-  logger.info("Creating base directory");
-  await mkdir(destination, { recursive: true });
-  logger.info(`Writing modules to ${destination} ...`);
-  await Promise.all(
-    modules.map(async (module) => {
-      const filePath = join(destination, module.path);
-      await mkdir(dirname(filePath), { recursive: true });
-      await writeFile(filePath, module.code);
-    })
-  );
-  logger.info(`Successfully wrote modules to ${destination}`);
-}
+export const generateCode = async (): Promise<void> => {
+  const buildSpecPath = process.env.BUILD_SPEC_PATH;
+  const buildOutputPath = process.env.BUILD_OUTPUT_PATH;
+
+  if (!buildSpecPath) {
+    throw new Error("SOURCE is not defined");
+  }
+  if (!buildOutputPath) {
+    throw new Error("DESTINATION is not defined");
+  }
+
+  const buildManagerNotifier = new BuildManagerNotifier({
+    buildManagerUrl: process.env.BUILD_MANAGER_URL,
+    resourceId: process.env.RESOURCE_ID,
+    buildId: process.env.BUILD_ID,
+  });
+
+  try {
+    const resourceData = await readInputJson(buildSpecPath);
+    await generateCodeByResourceData(resourceData, buildOutputPath);
+    await buildManagerNotifier.success();
+  } catch (error) {
+    logger.error(error);
+    await buildManagerNotifier.failure();
+  }
+};
--- packages/data-service-generator/src/main.ts
+++ packages/data-service-generator/src/main.ts
@@ -1,57 +1,9 @@
-import { DSGResourceData } from "@amplication/code-gen-types";
-import { readFile } from "fs/promises";
-import { httpClient } from "./utils/http-client";
-import { generateCodeByResourceData } from "./generate-code";
+import { generateCode } from "./generate-code";
+import { defaultLogger as logger } from "./server/logging";
 
-const buildSpecPath = process.env.BUILD_SPEC_PATH;
-const buildOutputPath = process.env.BUILD_OUTPUT_PATH;
-
-if (!buildSpecPath) {
-  throw new Error("SOURCE is not defined");
-}
-if (!buildOutputPath) {
-  throw new Error("DESTINATION is not defined");
-}
-
-generateCode(buildSpecPath, buildOutputPath).catch((err) => {
-  logger.error(err);
-  process.exit(1);
-});
-
-async function readInputJson(filePath: string): Promise<DSGResourceData> {
-  const file = await readFile(filePath, "utf8");
-  const resourceData: DSGResourceData = JSON.parse(file);
-  return resourceData;
-}
-
-export default async function generateCode(
-  source: string,
-  destination: string
-): Promise<void> {
-  try {
-    const resourceData = await readInputJson(source);
-    await generateCodeByResourceData(resourceData, destination);
-    await httpClient.post(
-      new URL(
-        "build-runner/code-generation-success",
-        process.env.BUILD_MANAGER_URL
-      ).href,
-      {
-        resourceId: process.env.RESOURCE_ID,
-        buildId: process.env.BUILD_ID,
-      }
-    );
-  } catch (err) {
+if (require.main === module) {
+  generateCode().catch(async (err) => {
     logger.error(err);
-    await httpClient.post(
-      new URL(
-        "build-runner/code-generation-failure",
-        process.env.BUILD_MANAGER_URL
-      ).href,
-      {
-        resourceId: process.env.RESOURCE_ID,
-        buildId: process.env.BUILD_ID,
-      }
-    );
-  }
+    process.exit(1);
+  });
 }
