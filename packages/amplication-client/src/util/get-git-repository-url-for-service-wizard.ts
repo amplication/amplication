10091cd79988e3d894961c726ad5fec4255baee9
Amit Barletz <amit@amplication.com>
2023-04-19 15:29:33 +0300

    refactor(client): imporove function

--- packages/amplication-client/src/util/get-git-repository-url-for-service-wizard.ts
+++ packages/amplication-client/src/util/get-git-repository-url-for-service-wizard.ts
@@ -5,14 +5,15 @@ const BITBUCKET_URL = "https://bitbucket.org";
 
 /**
  * @param provider - the git provider that the repository belongs to
- * @param gitRepositoryFullName - organizationName/repositoryName
+ * @param gitRepositoryFullName - organizationName/repositoryName. Can be null because during the creation of the service in the service wizard,
+ * the repository is not yet created
  * @returns the computed git repository url for the given provider and repository full name
  */
 
 export function getGitRepositoryUrlForServiceWizard(
   provider: EnumGitProvider,
-  gitRepositoryFullName: string | undefined
-) {
+  gitRepositoryFullName: string | null
+): string | null {
   const gitRepositoryUrlMap = {
     [EnumGitProvider.Github]: `${GITHUB_URL}/${gitRepositoryFullName}`,
     [EnumGitProvider.Bitbucket]: `${BITBUCKET_URL}/${gitRepositoryFullName}`,
