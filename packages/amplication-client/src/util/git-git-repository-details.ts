78570ad50078107e7513da44dc6dbc1f9394ee46
Amit Barletz <amit@amplication.com>
2023-05-05 19:02:11 +0300

    feat(client): refactor helper function

---
+++ packages/amplication-client/src/util/git-git-repository-details.ts
@@ -0,0 +1,36 @@
+import { EnumGitProvider } from "../models";
+
+const GITHUB_URL = "https://github.com";
+const BITBUCKET_URL = "https://bitbucket.org";
+
+/**
+ * @param provider - the git provider that the repository belongs to
+ * @param gitRepositoryFullName - organizationName/repositoryName For Github and groupName/repositoryName for Bitbucket.
+ * Can be null because during the creation of the service in the service wizard,
+ * the repository is not yet created
+ * @returns the computed git repository url for the given provider and repository full name
+ */
+
+export function getGitRepositoryDetails(
+  provider: EnumGitProvider,
+  organizationName: string | null,
+  groupName: string | null,
+  repositoryName: string | null
+): { repositoryFullName: string; repositoryUrl: string } | null {
+  const repositoryFullName =
+    provider === EnumGitProvider.Github
+      ? `${organizationName}/${repositoryName}`
+      : provider === EnumGitProvider.Bitbucket
+      ? `${groupName}/${repositoryName}`
+      : null;
+
+  const gitRepositoryUrlMap = {
+    [EnumGitProvider.Github]: `${GITHUB_URL}/${repositoryFullName}`,
+    [EnumGitProvider.Bitbucket]: `${BITBUCKET_URL}/${repositoryFullName}/src/master/`,
+  };
+
+  return {
+    repositoryFullName: repositoryFullName,
+    repositoryUrl: gitRepositoryUrlMap[provider],
+  };
+}
