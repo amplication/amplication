19785f21b8dbae72eea937cf9182811365162311
Amit Barletz <amit@amplication.com>
2023-06-23 17:32:55 +0300

    feat(server): update dtos

--- packages/amplication-server/src/core/prismaSchemaUtils/CreateEntitiesFromPrismaSchemaResponse.ts
+++ packages/amplication-server/src/core/prismaSchemaUtils/CreateEntitiesFromPrismaSchemaResponse.ts
@@ -1,12 +1,12 @@
 import { Field, ObjectType } from "@nestjs/graphql";
 import { Entity } from "../../models";
-import { ErrorMessage } from "./ErrorMessages";
+import { ActionLog } from "../action/dto";
 
 @ObjectType()
 export class CreateEntitiesFromPrismaSchemaResponse {
   @Field(() => [Entity], { nullable: false })
   entities!: Entity[];
 
-  @Field(() => [ErrorMessage], { nullable: true })
-  errors: ErrorMessage[] | null;
+  @Field(() => [ActionLog], { nullable: false })
+  log!: ActionLog[];
 }
