a30b0b8cded7dd047649797cb19535940a331d7e
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-06-29 13:14:05 +0100

    test(git): increase coverage threshold

--- libs/util/git/jest.config.ts
+++ libs/util/git/jest.config.ts
@@ -15,8 +15,8 @@ export default {
   coverageDirectory: "../../../coverage/libs/util/git",
   coverageThreshold: {
     global: {
-      branches: 31.25,
-      lines: 25,
+      branches: 80,
+      lines: 40,
     },
   },
 };
