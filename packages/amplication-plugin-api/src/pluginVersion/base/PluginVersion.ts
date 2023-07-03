e63c4649051e2511f9a615fad964c990a30ffeb9
mulygottlieb <muly@amplication.com>
2023-07-03 09:58:03 +0300

    fix(plugin-api): fix merge conflict and tsconfig baseUrl

--- packages/amplication-plugin-api/src/pluginVersion/base/PluginVersion.ts
+++ packages/amplication-plugin-api/src/pluginVersion/base/PluginVersion.ts
@@ -19,16 +19,6 @@ import { Type } from "class-transformer";
 @ObjectType()
 class PluginVersion {
   @ApiProperty({
-    required: false,
-  })
-  @IsJSONValue()
-  @IsOptional()
-  @Field(() => GraphQLJSON, {
-    nullable: true,
-  })
-  configurations!: JsonValue;
-
-  @ApiProperty({
     required: true,
   })
   @IsDate()
