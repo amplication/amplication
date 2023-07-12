4e3c33d96f1e6f172d2129f7bff28d3634407ba5
Amit Barletz <amit@amplication.com>
2023-07-12 14:53:02 +0300

    fix(server): remove unused method

--- packages/amplication-server/src/core/UserAction/userAction.service.ts
+++ packages/amplication-server/src/core/UserAction/userAction.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../../prisma";
-import { CreateUserActionArgs, UserAction } from "./dto";
 import { FindOneUserActionArgs } from "./dto/FindOneUserActionArgs";
+import { UserAction } from "./dto";
 
 @Injectable()
 export class UserActionService {
@@ -9,11 +9,5 @@ export class UserActionService {
 
   async findOne(args: FindOneUserActionArgs): Promise<UserAction | null> {
     return this.prisma.userAction.findUnique(args);
-  }
-
-  async create(args: CreateUserActionArgs): Promise<UserAction> {
-    return this.prisma.userAction.create({
-      data: args.data,
-    });
   }
 }
