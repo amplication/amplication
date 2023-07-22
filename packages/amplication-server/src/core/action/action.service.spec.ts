818c7e429fc26fd24fdf37d06b60f8e889963cf7
Amit Barletz <amit@amplication.com>
2023-07-22 11:27:33 +0300

    test(server): add tests for action service

--- packages/amplication-server/src/core/action/action.service.spec.ts
+++ packages/amplication-server/src/core/action/action.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { ActionService, SELECT_ID } from "./action.service";
+import { ACTION_LOG_LEVEL, ActionService, SELECT_ID } from "./action.service";
 import { PrismaService } from "../../prisma/prisma.service";
 import { Action } from "./dto/Action";
 import { ActionStep } from "./dto/ActionStep";
@@ -8,6 +8,7 @@ import { FindOneActionArgs } from "./dto/FindOneActionArgs";
 import { EnumActionLogLevel } from "./dto";
 import { AmplicationLogger } from "@amplication/util/nestjs/logging";
 import { KafkaProducerService } from "@amplication/util/nestjs/kafka";
+import { UserActionLog } from "@amplication/schema-registry";
 
 const EXAMPLE_ACTION_ID = "exampleActionId";
 const EXAMPLE_ACTION_STEP_ID = "exampleActionStepId";
@@ -40,6 +41,12 @@ const prismaActionLogCreateMock = jest.fn();
 describe("ActionService", () => {
   let service: ActionService;
 
+  const mockServiceEmitMessage = jest
+    .fn()
+    .mockImplementation((topic: string, message: UserActionLog.KafkaEvent) =>
+      Promise.resolve()
+    );
+
   beforeEach(async () => {
     jest.clearAllMocks();
 
@@ -65,7 +72,7 @@ describe("ActionService", () => {
         {
           provide: KafkaProducerService,
           useClass: jest.fn(() => ({
-            emitMessage: jest.fn(),
+            emitMessage: mockServiceEmitMessage,
           })),
         },
         {
@@ -252,6 +259,212 @@ describe("ActionService", () => {
         },
       },
       select: SELECT_ID,
+    });
+  });
+
+  describe("onUserActionLog", () => {
+    it("should log the action and update action step status if completed", async () => {
+      const logByStepIdSpy = jest
+        .spyOn(service, "logByStepId")
+        .mockImplementation();
+      const updateActionStepStatusSpy = jest
+        .spyOn(service, "updateActionStepStatus")
+        .mockImplementation();
+
+      const mockLogEntry: UserActionLog.Value = {
+        stepId: "test-step",
+        message: "Test message",
+        level: "Info",
+        status: EnumActionStepStatus.Success,
+        isCompleted: true,
+      };
+
+      await service.onUserActionLog(mockLogEntry);
+
+      expect(logByStepIdSpy).toHaveBeenCalledWith(
+        mockLogEntry.stepId,
+        ACTION_LOG_LEVEL[mockLogEntry.level.toLowerCase()],
+        mockLogEntry.message
+      );
+
+      expect(updateActionStepStatusSpy).toHaveBeenCalledWith(
+        mockLogEntry.stepId,
+        mockLogEntry.status
+      );
+
+      // restore mocks (spyOn) to avoid test pollution
+      logByStepIdSpy.mockRestore();
+      updateActionStepStatusSpy.mockRestore();
+    });
+
+    it("should log the action and not update action step status if not completed", async () => {
+      const logByStepIdSpy = jest
+        .spyOn(service, "logByStepId")
+        .mockImplementation();
+      const updateActionStepStatusSpy = jest
+        .spyOn(service, "updateActionStepStatus")
+        .mockImplementation();
+
+      const mockLogEntry: UserActionLog.Value = {
+        stepId: "test-step",
+        message: "Test message",
+        level: "Info",
+        status: EnumActionStepStatus.Running,
+        isCompleted: false,
+      };
+
+      await service.onUserActionLog(mockLogEntry);
+
+      expect(logByStepIdSpy).toHaveBeenCalledWith(
+        mockLogEntry.stepId,
+        ACTION_LOG_LEVEL[mockLogEntry.level.toLowerCase()],
+        mockLogEntry.message
+      );
+
+      expect(updateActionStepStatusSpy).not.toHaveBeenCalled();
+
+      // restore mocks (spyOn) to avoid test pollution
+      logByStepIdSpy.mockRestore();
+      updateActionStepStatusSpy.mockRestore();
+    });
+
+    it("should log to the database before updating the action step status", async () => {
+      const logEntry: UserActionLog.Value = {
+        stepId: "test-step-id",
+        message: "test-message",
+        level: "Info",
+        status: EnumActionStepStatus.Success,
+        isCompleted: true,
+      };
+
+      const logByStepIdSpy = jest
+        .spyOn(service, "logByStepId")
+        .mockImplementation(() => Promise.resolve());
+
+      const updateActionStepStatusSpy = jest
+        .spyOn(service, "updateActionStepStatus")
+        .mockImplementation(() => Promise.resolve());
+
+      await service.onUserActionLog(logEntry);
+
+      // get call order and ensure both functions are called in the same order of their invocation
+      const logByStepIdCallOrder = logByStepIdSpy.mock.invocationCallOrder[0];
+      const updateActionStepStatusCallOrder =
+        updateActionStepStatusSpy.mock.invocationCallOrder[0];
+
+      // compare call order
+      expect(logByStepIdCallOrder).toBeLessThan(
+        updateActionStepStatusCallOrder
+      );
+
+      // restore mocks (spyOn) to avoid test pollution
+      logByStepIdSpy.mockRestore();
+      updateActionStepStatusSpy.mockRestore();
+    });
+  });
+
+  describe("Action Context functions", () => {
+    let step: ActionStep;
+    let userActionId: string;
+    let topicName: string;
+    let actionContext;
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+
+      step = {
+        id: "1",
+        name: "some-step-name",
+        message: "some-message",
+        status: EnumActionStepStatus.Success,
+        createdAt: new Date(),
+      };
+      userActionId = "user-action-id";
+      topicName = "topic-name"; // Replace with the actual topic name you're working with
+      actionContext = service.createActionContext(
+        userActionId,
+        step,
+        topicName
+      );
+    });
+
+    it("onEmitUserActionLog should call emitUserActionLog", async () => {
+      const message = "Test message";
+      const level = EnumActionLogLevel.Info;
+      const status = EnumActionStepStatus.Running;
+      const isStepCompleted = false;
+
+      const expectedKafkaMessage: UserActionLog.KafkaEvent =
+        service.createKafkaMessageForUserActionLog(userActionId, step.id)(
+          message,
+          level,
+          status,
+          isStepCompleted
+        );
+
+      await actionContext.onEmitUserActionLog(
+        message,
+        level,
+        status,
+        isStepCompleted
+      );
+
+      expect(mockServiceEmitMessage).toHaveBeenCalledWith(
+        topicName,
+        expectedKafkaMessage
+      );
+    });
+
+    it("onCompleteWithLog should call emitUserActionLog and updateActionStepStatus", async () => {
+      const message = "Test message";
+      const level = EnumActionLogLevel.Info;
+      const status = EnumActionStepStatus.Success;
+      const isStepCompleted = true;
+
+      const emitUserActionLogSpy = jest.spyOn(service, "emitUserActionLog");
+      const updateActionStepStatusSpy = jest.spyOn(
+        service,
+        "updateActionStepStatus"
+      );
+
+      await actionContext.onCompleteWithLog(
+        message,
+        level,
+        status,
+        isStepCompleted
+      );
+
+      expect(emitUserActionLogSpy).toHaveBeenCalledTimes(1);
+      expect(updateActionStepStatusSpy).toHaveBeenCalledWith(step.id, status);
+    });
+
+    it("createKafkaMessageForUserActionLog should return a Kafka message", () => {
+      const userActionId = "user-action-id";
+      const stepId = "step-id";
+      const message = "test message";
+      const level = EnumActionLogLevel.Info;
+      const status = EnumActionStepStatus.Running;
+      const isStepCompleted = false;
+
+      const kafkaMessage: UserActionLog.KafkaEvent = {
+        key: {
+          userActionId,
+        },
+        value: {
+          stepId,
+          message,
+          level,
+          status,
+          isCompleted: isStepCompleted,
+        },
+      };
+
+      const result = service.createKafkaMessageForUserActionLog(
+        userActionId,
+        stepId
+      )(message, level, status, isStepCompleted);
+
+      expect(result).toEqual(kafkaMessage);
     });
   });
 });
