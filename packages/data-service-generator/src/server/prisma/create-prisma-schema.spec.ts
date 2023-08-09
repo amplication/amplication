5218eef97ba97aaa6f59fca0858db7663e49136b
Amit Barletz <amit@amplication.com>
2023-08-09 19:02:02 +0300

    test(dsg): fix tests

--- packages/data-service-generator/src/server/prisma/create-prisma-schema.spec.ts
+++ packages/data-service-generator/src/server/prisma/create-prisma-schema.spec.ts
@@ -241,7 +241,7 @@ describe("createPrismaFields", () => {
     [
       "WholeNumber",
       EnumDataType.WholeNumber,
-      {},
+      { dataType: "INT" },
       PrismaSchemaDSL.createScalarField(
         EXAMPLE_ENTITY_FIELD_NAME,
         PrismaSchemaDSLTypes.ScalarType.Int,
@@ -263,7 +263,7 @@ describe("createPrismaFields", () => {
     [
       "DecimalNumber",
       EnumDataType.DecimalNumber,
-      {},
+      { dataType: "FLOAT" },
       PrismaSchemaDSL.createScalarField(
         EXAMPLE_ENTITY_FIELD_NAME,
         PrismaSchemaDSLTypes.ScalarType.Float,
@@ -367,7 +367,7 @@ describe("createPrismaFields", () => {
     [
       "Id",
       EnumDataType.Id,
-      {},
+      { idType: "CUID" },
       PrismaSchemaDSL.createScalarField(
         EXAMPLE_ENTITY_FIELD_NAME,
         PrismaSchemaDSLTypes.ScalarType.String,
