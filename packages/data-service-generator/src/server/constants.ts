6c46059fe86ef8c1783dfe23e54afac057b712b9
Amit Barletz <amit@amplication.com>
2023-08-24 11:53:56 +0300

    feat(dsg): update constants

--- packages/data-service-generator/src/server/constants.ts
+++ packages/data-service-generator/src/server/constants.ts
@@ -12,3 +12,8 @@ export const ENV_VARIABLES: VariableDictionary = [
 
 export const DOCKER_COMPOSE_FILE_NAME = "docker-compose.template.yml";
 export const DOCKER_COMPOSE_DEV_FILE_NAME = "docker-compose.dev.template.yml";
+export const GRAPHQL_BIGINT_FILE_NAME = "GraphQLBigInt.ts";
+export const BIGINT_FILTER_FILE_NAME = "BigIntFilter.ts";
+export const BIGINT_NULLABLE_FILTER_FILE_NAME = "BigIntNullableFilter.ts";
+export const DECIMAL_FILTER_FILE_NAME = "DecimalFilter.ts";
+export const DECIMAL_NULLABLE_FILTER_FILE_NAME = "DecimalNullableFilter.ts";
