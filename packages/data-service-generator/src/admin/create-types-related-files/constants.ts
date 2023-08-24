2d1e29afa9d597d078b0a7a301b37e15499f929a
Amit Barletz <amit@amplication.com>
2023-08-24 11:49:45 +0300

    feat(dsg): create types related files in the client only if needed

---
+++ packages/data-service-generator/src/admin/create-types-related-files/constants.ts
@@ -0,0 +1,4 @@
+export const BIGINT_FILTER_FILE_NAME = "BigIntFilter.ts";
+export const BIGINT_NULLABLE_FILTER_FILE_NAME = "BigIntNullableFilter.ts";
+export const DECIMAL_FILTER_FILE_NAME = "DecimalFilter.ts";
+export const DECIMAL_NULLABLE_FILTER_FILE_NAME = "DecimalNullableFilter.ts";
