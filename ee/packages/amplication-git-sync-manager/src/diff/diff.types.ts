df6d1e076f1bb8e43f87e33dd928ed11fcc3933f
Mor Hagbi <mor@amplication.com>
2023-08-31 16:53:52 +0300

    feat(ee:git-sync-manager): remove amplication from the service name

--- ee/packages/amplication-git-sync-manager/src/diff/diff.types.ts
+++
@@ -1,4 +0,0 @@
-import { File } from "@amplication/util/git";
-import { Difference } from "dir-compare";
-
-export type DiffVisitorFn = (diff: Difference) => File | null;
--- ee/packages/amplication-git-sync-manager/src/errors/MissingBuildFiles.ts
+++
@@ -1,5 +0,0 @@
-export class MissingBuildFiles extends Error {
-  constructor(buildId: string) {
-    super(`Build path for build: ${buildId} does not exist`);
-  }
-}
