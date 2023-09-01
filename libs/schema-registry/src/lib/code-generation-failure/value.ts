eb0da7e32fc759717882d747559149ada2925319
Amit Barletz <amit@amplication.com>
2023-09-01 14:40:12 +0300

    fix(schema-registry): add jsdoc comment

--- libs/schema-registry/src/lib/code-generation-failure/value.ts
+++ libs/schema-registry/src/lib/code-generation-failure/value.ts
@@ -4,6 +4,10 @@ export class Value {
   @IsString()
   buildId!: string;
 
+  /**
+   * @codeGeneratorVersion a calculation between the actual version and the selectionStrategy
+   * @example actual version: 1.2.3, selectionStrategy: LATEST_MINOR => 1.3.0
+   */
   @IsString()
   codeGeneratorVersion!: string;
 
