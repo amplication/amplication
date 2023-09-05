29f09326a24d331b029df4a779c586d2954f35cd
Mor Hagbi <mor@amplication.com>
2023-09-05 17:21:30 +0300

    chore(gpt-gateway): add support nx config and fix issues

--- packages/amplication-gpt-gateway/jest.config.ts
+++ packages/amplication-gpt-gateway/jest.config.ts
@@ -13,10 +13,4 @@ export default {
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory: "../../coverage/packages/amplication-gpt-gateway",
-  coverageThreshold: {
-    global: {
-      branches: 47,
-      lines: 27,
-    },
-  },
 };
