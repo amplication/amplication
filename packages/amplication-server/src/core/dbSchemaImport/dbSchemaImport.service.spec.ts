290cbaab117c3d3bf821f1f40bb92419b0cd539a
barshimi <barshimi@gmail.com>
2023-09-06 16:27:22 +0300

    types issues

--- packages/amplication-server/src/core/dbSchemaImport/dbSchemaImport.service.spec.ts
+++ packages/amplication-server/src/core/dbSchemaImport/dbSchemaImport.service.spec.ts
@@ -12,12 +12,10 @@ import { EntityService } from "../entity/entity.service";
 import { UserService } from "../user/user.service";
 import { UserActionService } from "../userAction/userAction.service";
 import { ActionService } from "../action/action.service";
-import { Env } from "../../env";
 import { KAFKA_TOPICS } from "@amplication/schema-registry";
 
 describe("DbSchemaImportService", () => {
   let service: DBSchemaImportService;
-  let configService: ConfigService;
   let kafkaProducerService: KafkaProducerService;
 
   const mockCreateDBSchemaImportArgs: CreateDBSchemaImportArgs = {
@@ -125,7 +123,6 @@ describe("DbSchemaImportService", () => {
     }).compile();
 
     service = module.get<DBSchemaImportService>(DBSchemaImportService);
-    configService = module.get<ConfigService>(ConfigService);
     kafkaProducerService =
       module.get<KafkaProducerService>(KafkaProducerService);
   });
