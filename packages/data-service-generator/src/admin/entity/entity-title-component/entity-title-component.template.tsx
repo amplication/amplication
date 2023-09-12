453a92ea4072630bcbf4b720eddf5d9e2eeefc28
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-09-12 15:22:05 +0530

    fix: type error in entity-title

--- packages/data-service-generator/src/admin/entity/entity-title-component/entity-title-component.template.tsx
+++ packages/data-service-generator/src/admin/entity/entity-title-component/entity-title-component.template.tsx
@@ -7,5 +7,7 @@ declare const ENTITY_TITLE_FIELD_NAME = "title";
 export const ENTITY_TITLE_FIELD_NAME_ID = ENTITY_TITLE_FIELD_NAME;
 
 export const ENTITY_TITLE = (record: ENTITY): string => {
-  return record.ENTITY_TITLE_FIELD || String(record.id);
+  return typeof record[ENTITY_TITLE_FIELD_NAME] === "string"
+    ? record[ENTITY_TITLE_FIELD_NAME]
+    : record[ENTITY_TITLE_FIELD_NAME].toString();
 };
