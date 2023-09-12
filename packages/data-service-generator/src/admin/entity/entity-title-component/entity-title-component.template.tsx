f50f12167e65f1b5305d45725fb45ec29099412b
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-09-12 16:55:04 +0530

    fix: change type check function

--- packages/data-service-generator/src/admin/entity/entity-title-component/entity-title-component.template.tsx
+++ packages/data-service-generator/src/admin/entity/entity-title-component/entity-title-component.template.tsx
@@ -7,8 +7,5 @@ declare const ENTITY_TITLE_FIELD_NAME = "title";
 export const ENTITY_TITLE_FIELD_NAME_ID = ENTITY_TITLE_FIELD_NAME;
 
 export const ENTITY_TITLE = (record: ENTITY): string => {
-  return Object.prototype.toString.call(record.ENTITY_TITLE_FIELD_NAME) ===
-    "[object String]"
-    ? record.ENTITY_TITLE_FIELD_NAME
-    : record.ENTITY_TITLE_FIELD_NAME.toString();
+  return record.ENTITY_TITLE_FIELD?.toString() || String(record.id);
 };
