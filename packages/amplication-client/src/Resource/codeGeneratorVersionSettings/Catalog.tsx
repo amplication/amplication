be311a177ad93c9e53be083d0aa878c4873305bc
Amit Barletz <amit@amplication.com>
2023-09-13 10:25:45 +0300

    fix(client): catalog lint and cleanup

--- packages/amplication-client/src/Resource/codeGeneratorVersionSettings/Catalog.tsx
+++ packages/amplication-client/src/Resource/codeGeneratorVersionSettings/Catalog.tsx
@@ -1,6 +1,4 @@
 import { PanelCollapsible } from "@amplication/ui/design-system";
-import { Icon } from "libs/ui/design-system/src/lib/components/Icon/Icon";
-import { useState } from "react";
 
 const CLASS_NAME = "catalog";
 
@@ -10,12 +8,6 @@ type CatalogProps = {
 };
 
 export const DSGCatalog: React.FC<CatalogProps> = ({ name, changelog }) => {
-  const [open, setOpen] = useState(false);
-
-  const handleOnCLick = () => {
-    setOpen(!open);
-  };
-
   return (
     <div className={CLASS_NAME}>
       <PanelCollapsible headerContent={name}>
