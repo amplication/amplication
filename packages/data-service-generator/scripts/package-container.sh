aaf8661b1e944e5bfddac8f676d4d623fa3a7e92
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-09-18 13:54:15 +0100

    chore(dsg): fix and improve package:container script to detect the correct target environment

--- packages/data-service-generator/scripts/package-container.sh
+++ packages/data-service-generator/scripts/package-container.sh
@@ -1,31 +1,31 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 IMAGE_REPO='439403303254.dkr.ecr.us-east-1.amazonaws.com/data-service-generator'
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$SCRIPT_DIR/../../../
 
-cd $SCRIPT_DIR/../../../
+cd $ROOT_DIR
 
-if [[ -z "$INPUT_TAGS" ]]; then
-    echo "Local environment detected"
-
-    echo "INPUT_TAGS not set, skipping container build"
-    npx nx internal:package:container data-service-generator
-
-elif [[ $INPUT_TAGS == *":next"* ]] || [[ $INPUT_TAGS == *":master"* ]]; then
+if [[ $INPUT_TAGS == *":next"* ]] || [[ $INPUT_TAGS == *":master"* ]]; then
     echo "Sandbox / Staging deployment detected"
     # check if INPUT_TAGS contains a `next` or `master` tag
     echo "Skipping container build for next or master branch"
     npx nx internal:package:container data-service-generator --prod
+    exit 0
     
-else
+elif [[ $PRODUCTION_TAGS = "true" ]]; then
     echo "Production deployment detected"
     echo "Overriding INPUT_TAGS with data-service-generator package version"
 
     PACKAGE_VERSION=$(cat $SCRIPT_DIR/../package.json | jq -r '.version')
     # prefix with `v` to match other container image tags
     IMAGE_TAG="v$PACKAGE_VERSION"
-    INPUT_TAGS="$IMAGE_REPO:$IMAGE_TAG,$IMAGE_REPO:sha-$GIT_SHA"
+    export INPUT_TAGS="$IMAGE_REPO:$IMAGE_TAG,$IMAGE_REPO:sha-$GIT_SHA"
     npx nx internal:package:container data-service-generator --prod
+    exit 0
 fi
 
+echo "Local environment detected"
 
+echo "INPUT_TAGS not set, skipping container build"
+npx nx internal:package:container data-service-generator
