a1e4c0d3178072cff904684d097338ca7b331824
yuval-hazaz <hazaz.yuval@gmail.com>
2023-09-24 11:48:50 +0300

    style(design-system):use mui for breadcrumbs

--- libs/ui/design-system/src/lib/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ libs/ui/design-system/src/lib/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -10,11 +10,9 @@ export default {
 export const Default = (props: any) => {
   return (
     <Breadcrumbs>
-      <Breadcrumbs.Item href="">Page 1</Breadcrumbs.Item>
-      <Breadcrumbs.Item href="">Page 2</Breadcrumbs.Item>
-      <Breadcrumbs.Item selected href="">
-        Page 3
-      </Breadcrumbs.Item>
+      <Breadcrumbs.Item to="">Page 1</Breadcrumbs.Item>
+      <Breadcrumbs.Item to="">Page 2</Breadcrumbs.Item>
+      <Breadcrumbs.Item to="">Page 3</Breadcrumbs.Item>
     </Breadcrumbs>
   );
 };
