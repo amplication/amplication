88fe135b68c1a9093d1a8f8447312fdda315b2ef
yuval-hazaz <hazaz.yuval@gmail.com>
2023-09-24 11:18:04 +0300

    style(design-system):fix selected tab style

--- libs/ui/design-system/src/lib/components/Tabs/Tabs.tsx
+++ libs/ui/design-system/src/lib/components/Tabs/Tabs.tsx
@@ -4,14 +4,23 @@ import {
   Tabs as MuiTabs,
   TabsProps as MuiTabsProps,
 } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Tabs.scss";
+import { useState } from "react";
 
 export type Props = MuiTabsProps;
 
 export const Tabs = ({ children }: Props) => {
+  const [value, setValue] = useState(0);
+
   return (
-    <MuiTabs value={false} className="amp-tabs">
+    <MuiTabs
+      value={value}
+      className="amp-tabs"
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+    >
       {children}
     </MuiTabs>
   );
@@ -29,7 +38,8 @@ function Tab(props: TabProps) {
   return (
     <MuiTab
       className="amp-tab"
-      component={NavLink}
+      component={Link}
+      classes={{ selected: "amp-tab--selected" }}
       to={to}
       disableRipple
       {...rest}
