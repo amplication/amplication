12667197eb72044197fab16022d1b67c94facd77
yuval-hazaz <hazaz.yuval@gmail.com>
2023-09-25 18:33:47 +0300

    feat(client):variable page width

--- packages/amplication-client/src/Layout/PageContent.tsx
+++ packages/amplication-client/src/Layout/PageContent.tsx
@@ -3,12 +3,19 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import "./PageContent.scss";
 
+export enum EnumPageWidth {
+  Default = "default",
+  Full = "full",
+  Wide = "wide",
+}
+
 type Props = {
   children: React.ReactNode;
   sideContent?: React.ReactNode;
   headerContent?: React.ReactNode;
   className?: string;
   pageTitle?: string;
+  pageWidth?: EnumPageWidth;
 };
 
 const CLASS_NAME = "amp-page-content";
@@ -19,13 +26,20 @@ function PageContent({
   className,
   pageTitle,
   headerContent,
+  pageWidth = EnumPageWidth.Default,
 }: Props) {
   return (
     <>
       <Helmet>
         <title>{`Amplication${pageTitle ? ` | ${pageTitle}` : ""}`}</title>
       </Helmet>
-      <div className={classNames(CLASS_NAME, className)}>
+      <div
+        className={classNames(
+          CLASS_NAME,
+          `${CLASS_NAME}--width-${pageWidth}`,
+          className
+        )}
+      >
         <div className={`${CLASS_NAME}__header`}>{headerContent}</div>
         <div className={`${CLASS_NAME}__body`}>
           {sideContent && (
