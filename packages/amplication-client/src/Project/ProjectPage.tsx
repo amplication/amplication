46cb88a3fe91efc5521ec7ad793b6a6c0ca2d49c
yuval-hazaz <hazaz.yuval@gmail.com>
2023-09-25 12:11:29 +0300

    feat(client):show pending changes count

--- packages/amplication-client/src/Project/ProjectPage.tsx
+++ packages/amplication-client/src/Project/ProjectPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import ResourceList from "../Workspaces/ResourceList";
 import { AppRouteProps } from "../routes/routesUtil";
 import { match } from "react-router-dom";
@@ -8,6 +8,7 @@ import PageLayout from "../Layout/PageLayout";
 import { AppContext } from "../context/appContext";
 import useTabRoutes from "../Layout/useTabRoutes";
 import { TabItem } from "@amplication/ui/design-system";
+import PendingChange from "../VersionControl/PendingChange";
 
 type Props = AppRouteProps & {
   match: match<{
@@ -15,6 +16,7 @@ type Props = AppRouteProps & {
     project: string;
   }>;
 };
+const OVERVIEW = "Overview";
 
 const ProjectPage: React.FC<Props> = ({
   innerRoutes,
@@ -23,15 +25,32 @@ const ProjectPage: React.FC<Props> = ({
   tabRoutes,
   tabRoutesDef,
 }) => {
-  const { currentProject } = useContext(AppContext);
+  const { currentProject, pendingChanges } = useContext(AppContext);
 
   useBreadcrumbs(currentProject?.name, match.url);
   const { tabs, currentRouteIsTab } = useTabRoutes(tabRoutesDef);
 
-  const tabItems: TabItem[] = [
-    { name: "Overview", to: match.url, exact: true },
-    ...tabs,
-  ];
+  const tabItems: TabItem[] = useMemo(() => {
+    const tabsWithPendingChanges = tabs.map((tab) => {
+      if (tab.name === "Pending Changes") {
+        return {
+          ...tab,
+          indicatorValue: pendingChanges?.length
+            ? pendingChanges.length
+            : undefined,
+        };
+      } else return tab;
+    });
+
+    return [
+      {
+        name: OVERVIEW,
+        to: match.url,
+        exact: true,
+      },
+      ...(tabsWithPendingChanges || []),
+    ];
+  }, [tabs, pendingChanges]);
 
   return match.isExact || currentRouteIsTab ? (
     <>
