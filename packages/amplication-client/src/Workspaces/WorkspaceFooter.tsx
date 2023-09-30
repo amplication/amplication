9ef2ef11b473ee94d6900fe161357f9abd8c89ce
yuval-hazaz <hazaz.yuval@gmail.com>
2023-09-30 17:06:38 +0300

    style(client):footer

--- packages/amplication-client/src/Workspaces/WorkspaceFooter.tsx
+++ packages/amplication-client/src/Workspaces/WorkspaceFooter.tsx
@@ -1,16 +1,23 @@
-import { Icon, SkeletonWrapper } from "@amplication/ui/design-system";
-import { isEmpty } from "lodash";
+import {
+  EnumGapSize,
+  EnumItemsAlign,
+  EnumTextColor,
+  EnumTextStyle,
+  FlexItem,
+  Icon,
+  SkeletonWrapper,
+  Text,
+} from "@amplication/ui/design-system";
 import React, { useContext, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { ClickableId } from "../Components/ClickableId";
-import { AppContext } from "../context/appContext";
 import GitRepoDetails from "../Resource/git/GitRepoDetails";
-import { AnalyticsEventNames } from "../util/analytics-events.types";
-import { PUSH_TO_GIT_STEP_NAME } from "../VersionControl/BuildSteps";
-import "./WorkspaceFooter.scss";
-import { Commit } from "../models";
 import { gitProviderIconMap } from "../Resource/git/git-provider-icon-map";
-import { gitProviderName } from "../Resource/git/gitProviderDisplayName";
+import useBuildGitUrl from "../VersionControl/useBuildGitUrl";
+import { AppContext } from "../context/appContext";
+import { Commit } from "../models";
+import { AnalyticsEventNames } from "../util/analytics-events.types";
+import "./WorkspaceFooter.scss";
 
 const CLASS_NAME = "workspace-footer";
 
@@ -57,22 +64,7 @@ const WorkspaceFooter: React.FC<{ lastCommit: Commit }> = ({ lastCommit }) => {
     />
   );
 
-  const gitUrl =
-    useMemo(() => {
-      if (!lastResourceBuild?.action?.steps?.length) {
-        return gitRepositoryUrl;
-      }
-      const gitStep = lastResourceBuild?.action.steps.find(
-        (step) =>
-          step.name === PUSH_TO_GIT_STEP_NAME(gitRepositoryOrganizationProvider)
-      );
-
-      const log = gitStep?.logs?.find(
-        (log) => !isEmpty(log.meta) && !isEmpty(log.meta.githubUrl)
-      );
-      // if there is "lastResourceBuild" link to the last PR
-      return lastResourceBuild ? log?.meta?.githubUrl : gitRepositoryUrl;
-    }, [gitRepositoryUrl, lastResourceBuild]) || gitRepositoryUrl;
+  const gitUrl = useBuildGitUrl(lastResourceBuild);
 
   return (
     currentProject && (
@@ -80,39 +72,54 @@ const WorkspaceFooter: React.FC<{ lastCommit: Commit }> = ({ lastCommit }) => {
         <>
           <div className={`${CLASS_NAME}__left`}>
             {gitRepositoryFullName && gitRepositoryOrganizationProvider ? (
-              <div className={`${CLASS_NAME}__git-connection`}>
+              <FlexItem
+                itemsAlign={EnumItemsAlign.Center}
+                gap={EnumGapSize.Small}
+              >
                 <Icon
                   icon={gitProviderIconMap[gitRepositoryOrganizationProvider]}
                   size="small"
-                  className={`${CLASS_NAME}__git-icon`}
+                  color={EnumTextColor.Black20}
                 />
-                <GitRepoDetails gitRepositoryFullName={gitRepositoryFullName} />
+
                 <a
                   className={`${CLASS_NAME}__git-link`}
-                  href={gitUrl}
+                  href={gitUrl || gitRepositoryUrl}
                   target={
                     gitRepositoryOrganizationProvider?.toLocaleLowerCase() ||
                     "_blank"
                   }
                 >
-                  {`Open With ${gitProviderName[gitRepositoryOrganizationProvider]}`}
+                  <Text textStyle={EnumTextStyle.Description}>
+                    <GitRepoDetails
+                      gitRepositoryFullName={gitRepositoryFullName}
+                    />
+                  </Text>
                 </a>
-              </div>
+              </FlexItem>
             ) : (
-              <div className={`${CLASS_NAME}__git-disconnected`}>
+              <FlexItem
+                itemsAlign={EnumItemsAlign.Center}
+                gap={EnumGapSize.Small}
+              >
                 <Icon
                   icon="pending_changes"
                   size="small"
-                  className={`${CLASS_NAME}__git-icon`}
+                  color={EnumTextColor.ThemeRed}
                 />
                 <Link
                   className={`${CLASS_NAME}__connect-to-git`}
                   title={`Connect to git`}
                   to={`/${currentWorkspace?.id}/${currentProject?.id}/${projectConfigurationResource?.id}/git-sync`}
                 >
-                  Connect to git
+                  <Text
+                    textStyle={EnumTextStyle.Description}
+                    textColor={EnumTextColor.ThemeRed}
+                  >
+                    Connect to git
+                  </Text>
                 </Link>
-              </div>
+              </FlexItem>
             )}
           </div>
           <div className={`${CLASS_NAME}__right`}>
