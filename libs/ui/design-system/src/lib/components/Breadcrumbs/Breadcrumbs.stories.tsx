fab0060b5fb531331be6c409dc154364e2533fcf
yuval-hazaz <hazaz.yuval@gmail.com>
2023-10-01 20:29:30 +0300

    fix(design-system):add router to story

--- libs/ui/design-system/src/lib/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ libs/ui/design-system/src/lib/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Meta } from "@storybook/react";
 import Breadcrumbs from "./Breadcrumbs";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default {
   title: "Breadcrumbs",
@@ -9,10 +10,12 @@ export default {
 
 export const Default = (props: any) => {
   return (
-    <Breadcrumbs>
-      <Breadcrumbs.Item to="">Page 1</Breadcrumbs.Item>
-      <Breadcrumbs.Item to="">Page 2</Breadcrumbs.Item>
-      <Breadcrumbs.Item to="">Page 3</Breadcrumbs.Item>
-    </Breadcrumbs>
+    <Router>
+      <Breadcrumbs>
+        <Breadcrumbs.Item to="">Page 1</Breadcrumbs.Item>
+        <Breadcrumbs.Item to="">Page 2</Breadcrumbs.Item>
+        <Breadcrumbs.Item to="">Page 3</Breadcrumbs.Item>
+      </Breadcrumbs>
+    </Router>
   );
 };
