e0afd229991466335f6c5b873dab6ff270216bd1
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-04 13:56:35 +0100

    chore: update to nx 16.9.1 with related migrations

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
 import type { StorybookConfig } from "@storybook/core-common";
 
 export const rootMain: StorybookConfig = {
-  stories: [],
-  addons: ["@storybook/addon-essentials"],
+  addons: [],
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
   //   // Return the altered config
   //   return config;
   // },
+};
+
+export const framework = {
+  name: "@storybook/react-vite",
+  options: {},
+};
+
+export const docs = {
+  autodocs: true,
 };
