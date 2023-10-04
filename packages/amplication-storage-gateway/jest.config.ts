36035bbcfdeda8dff29726bc04e8c51bb7158834
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-04 17:06:23 +0100

    test: remove fallback to old jest snapshot in projects that don't have snapshots

--- packages/amplication-storage-gateway/jest.config.ts
+++ packages/amplication-storage-gateway/jest.config.ts
@@ -20,14 +20,4 @@ export default {
       lines: 9,
     },
   },
-  /* TODO: Update to latest Jest snapshotFormat
-   * By default Nx has kept the older style of Jest Snapshot formats
-   * to prevent breaking of any existing tests with snapshots.
-   * It's recommend you update to the latest format.
-   * You can do this by removing snapshotFormat property
-   * and running tests with --update-snapshot flag.
-   * Example: From within the project directory, run "nx test --update-snapshot"
-   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
-   */
-  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
