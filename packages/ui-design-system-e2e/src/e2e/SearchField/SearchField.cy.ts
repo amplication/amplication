e61c4ad549cb8928f264b4181bcd69fd5d2a5b99
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-04 16:56:35 +0100

    chore(design-system-e2e): removed unused design system cypress tests

--- packages/ui-design-system-e2e/src/e2e/SearchField/SearchField.cy.ts
+++
@@ -1,7 +0,0 @@
-describe("ui-design-system: SearchField component", () => {
-  beforeEach(() => cy.visit("/iframe.html?id=searchfield"));
-
-  it("should render the component", () => {
-    cy.get("[id=root]").should("not.be.empty");
-  });
-});
