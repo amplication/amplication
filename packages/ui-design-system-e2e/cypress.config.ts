d11a4e21946caad0763109f177257a9a4b5cfc0d
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-05 14:58:29 +0100

    chore(design-system-e2e): update from nx v16.9.1 migrations run

--- packages/ui-design-system-e2e/cypress.config.ts
+++ packages/ui-design-system-e2e/cypress.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "cypress";
-import { nxE2EStorybookPreset } from "@nrwl/storybook/presets/cypress";
+import { nxE2EStorybookPreset } from "@nx/storybook/presets/cypress";
 
 export default defineConfig({
-  e2e: nxE2EStorybookPreset(__dirname),
+  e2e: {
+    ...nxE2EStorybookPreset(__dirname),
+    /**
+     * TODO(@nx/cypress): In Cypress v12,the testIsolation option is turned on by default.
+     * This can cause tests to start breaking where not indended.
+     * You should consider enabling this once you verify tests do not depend on each other
+     * More Info: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
+     **/
+    testIsolation: false,
+  },
 });
