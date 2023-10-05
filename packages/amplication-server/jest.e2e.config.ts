e1e82fd88c9977d13924a674b339f6dbbde6e40b
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-05 14:57:22 +0100

    chore(server): update from nx v16.9.1 migrations run

--- packages/amplication-server/jest.e2e.config.ts
+++ packages/amplication-server/jest.e2e.config.ts
@@ -2,17 +2,28 @@
 export default {
   displayName: "amplication-server",
   preset: "../../jest.preset.js",
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
-  },
+  globals: {},
   testEnvironment: "node",
   transform: {
-    "^.+\\.[tj]s$": "ts-jest",
+    "^.+\\.[tj]s$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory: "../../coverage/packages/amplication-server",
   testMatch: ["**/e2e/**/*.spec.ts"],
   collectCoverage: false,
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
