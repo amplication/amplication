83bce5a83788131aa9da98bcd9c17facb5bcca15
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-05 14:56:59 +0100

    chore(git-pull-request-service): update from nx v16.9.1 migrations run

--- packages/amplication-git-pull-request-service/jest.config.ts
+++ packages/amplication-git-pull-request-service/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: "amplication-git-pull-request-service",
   preset: "../../jest.preset.js",
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
-  },
+  globals: {},
   transform: {
-    "^.+\\.[tj]s$": "ts-jest",
+    "^.+\\.[tj]s$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory:
@@ -19,4 +20,14 @@ export default {
       lines: 25,
     },
   },
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
