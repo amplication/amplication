e5e25deb15a4dca0c1cc78dc71a801ca5228f528
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-10-05 14:58:01 +0100

    chore(local-dsg-controller): update from nx v16.9.1 migrations run

--- packages/local-data-service-generator-controller/jest.config.ts
+++ packages/local-data-service-generator-controller/jest.config.ts
@@ -2,16 +2,27 @@
 export default {
   displayName: "local-data-service-generator-controller",
   preset: "../../jest.preset.js",
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.spec.json",
-    },
-  },
+  globals: {},
   testEnvironment: "node",
   transform: {
-    "^.+\\.[tj]s$": "ts-jest",
+    "^.+\\.[tj]s$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory:
     "../../coverage/packages/local-data-service-generator-controller",
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
