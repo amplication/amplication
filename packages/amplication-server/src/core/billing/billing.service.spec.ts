b181ebfbf0be86eaa2b88ccf00816fb3c07f505f
Amit Barletz <amit@amplication.com>
2023-10-17 10:04:02 +0300

    test(server): add test

--- packages/amplication-server/src/core/billing/billing.service.spec.ts
+++ packages/amplication-server/src/core/billing/billing.service.spec.ts
@@ -75,6 +75,100 @@ describe("BillingService", () => {
     );
   });
 
+  it("should throw exceptions on number of projects if the workspace has no entitlement to bypass code generation limitation and the current project is out of the limitation based on the number of allowed projects and its creation date", async () => {
+    const workspaceId = "id";
+    const projectsPerWorkspaceLimit = 1;
+
+    const spyOnServiceGetBooleanEntitlement = jest
+      .spyOn(service, "getBooleanEntitlement")
+      .mockResolvedValue({
+        hasAccess: false,
+      } as BooleanEntitlement);
+
+    const spyOnServiceGetMeteredEntitlement = jest
+      .spyOn(service, "getMeteredEntitlement")
+      .mockResolvedValue({
+        hasAccess: false,
+        usageLimit: projectsPerWorkspaceLimit,
+      } as MeteredEntitlement);
+
+    const user: User = {
+      id: "user-id",
+      account: {
+        id: "account-id",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        email: "email",
+        firstName: "first-name",
+        lastName: "last-name",
+        password: "password",
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isOwner: true,
+    };
+
+    const projects: Project[] = [
+      {
+        id: "project-id-1",
+        name: "project-1",
+        workspaceId: workspaceId,
+        useDemoRepo: false,
+        createdAt: new Date("2023-01-01"),
+        updatedAt: new Date(),
+      },
+      {
+        id: "project-id-2",
+        name: "project-2",
+        workspaceId: workspaceId,
+        useDemoRepo: false,
+        createdAt: new Date("2023-01-02"),
+        updatedAt: new Date(),
+      },
+    ];
+
+    await expect(
+      service.validateSubscriptionPlanLimitationsForWorkspace(
+        workspaceId,
+        user,
+        "project-id-2",
+        projects
+      )
+    ).rejects.toThrow(
+      new ValidationError("LimitationError: Allowed projects per workspace: 1")
+    );
+
+    expect(spyOnServiceGetBooleanEntitlement).toHaveBeenCalledTimes(1);
+    expect(spyOnServiceGetBooleanEntitlement).toHaveBeenCalledWith(
+      workspaceId,
+      BillingFeature.IgnoreValidationCodeGeneration
+    );
+    await expect(
+      service.getBooleanEntitlement(
+        workspaceId,
+        BillingFeature.IgnoreValidationCodeGeneration
+      )
+    ).resolves.toEqual(
+      expect.objectContaining({
+        hasAccess: false,
+      })
+    );
+
+    expect(spyOnServiceGetMeteredEntitlement).toHaveBeenCalledTimes(1);
+    expect(spyOnServiceGetMeteredEntitlement).toHaveBeenNthCalledWith(
+      1,
+      workspaceId,
+      BillingFeature.Projects
+    );
+    await expect(
+      service.getMeteredEntitlement(workspaceId, BillingFeature.Projects)
+    ).resolves.toEqual(
+      expect.objectContaining({
+        hasAccess: false,
+      })
+    );
+  });
+
   it("should throw exceptions on number of services if the workspace has no entitlement to bypass code generation limitation", async () => {
     const workspaceId = "id";
     const projectId = "project-id-1";
