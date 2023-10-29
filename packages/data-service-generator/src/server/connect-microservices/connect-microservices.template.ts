916ab95bcae036870fa16c18e60e499fa0d135d9
Mor Hagbi <mor@amplication.com>
2023-10-29 22:16:59 +0200

    fix(dsg): delete unused import from microservices template

--- packages/data-service-generator/src/server/connect-microservices/connect-microservices.template.ts
+++ packages/data-service-generator/src/server/connect-microservices/connect-microservices.template.ts
@@ -1,6 +1,5 @@
 import { INestApplication } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { MicroserviceOptions } from "@nestjs/microservices";
 
 export async function connectMicroservices(app: INestApplication) {
   const configService = app.get(ConfigService);
