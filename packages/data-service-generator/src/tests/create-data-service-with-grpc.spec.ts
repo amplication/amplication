0106602ddb9a544b94d8892bf9acaa55db2da831
Mor Hagbi <mor@amplication.com>
2023-10-30 18:51:54 +0200

    fix(dsg): add snapshot test to grpc-controller

---
+++ packages/data-service-generator/src/tests/create-data-service-with-grpc.spec.ts
@@ -0,0 +1,67 @@
+import entities from "./entities";
+import roles from "./roles";
+import { AppInfo } from "@amplication/code-gen-types";
+import { appInfo, MODULE_EXTENSIONS_TO_SNAPSHOT } from "./appInfo";
+import { EnumResourceType } from "../models";
+import { createDataService } from "../create-data-service";
+import { MockedLogger } from "@amplication/util/logging/test-utils";
+import { join } from "path";
+import { AMPLICATION_MODULES } from "../generate-code";
+
+const newAppInfo: AppInfo = {
+  ...appInfo,
+  settings: {
+    ...appInfo.settings,
+    serverSettings: {
+      generateGraphQL: false,
+      generateRestApi: true,
+      serverPath: "",
+    },
+  },
+};
+
+jest.setTimeout(100000);
+
+describe("createDataService", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  describe("when graphql is disabled", () => {
+    test("creates app as expected", async () => {
+      const modules = await createDataService(
+        {
+          entities,
+          buildId: "example_build_id",
+          roles,
+          resourceInfo: newAppInfo,
+          resourceType: EnumResourceType.Service,
+          pluginInstallations: [
+            {
+              id: "clb3p3ov800cplc01a8f4uwje",
+              npm: "@amplication/transport-grpc",
+              enabled: true,
+              version: "0.0.1-beta.130",
+              pluginId: "transport-grpc",
+              configurations: {
+                generateGRPC: "true",
+              },
+            },
+          ],
+        },
+        MockedLogger,
+        join(__dirname, "../../", AMPLICATION_MODULES)
+      );
+      const modulesToSnapshot = modules
+        .modules()
+        .filter((module) =>
+          MODULE_EXTENSIONS_TO_SNAPSHOT.some((extension) =>
+            module.path.endsWith(extension)
+          )
+        );
+      const pathToCode = Object.fromEntries(
+        modulesToSnapshot.map((module) => [module.path, module.code])
+      );
+      expect(pathToCode).toMatchSnapshot();
+    });
+  });
+});
