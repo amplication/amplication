945f4a2f63d5a730d236b4c8108acf73a9398451
Amit Barletz <amit@amplication.com>
2023-11-08 13:32:06 +0200

    refactor(build-manager): remove unused import

--- packages/amplication-build-manager/src/build-runner/build-runner.module.ts
+++ packages/amplication-build-manager/src/build-runner/build-runner.module.ts
@@ -3,11 +3,10 @@ import { Module } from "@nestjs/common";
 import { BuildRunnerController } from "./build-runner.controller";
 import { BuildRunnerService } from "./build-runner.service";
 import { CodeGeneratorService } from "../code-generator/code-generator-catalog.service";
-import { TarService } from "./tar.service";
 
 @Module({
   imports: [KafkaModule],
   controllers: [BuildRunnerController],
-  providers: [BuildRunnerService, CodeGeneratorService, TarService],
+  providers: [BuildRunnerService, CodeGeneratorService],
 })
 export class BuildRunnerModule {}
