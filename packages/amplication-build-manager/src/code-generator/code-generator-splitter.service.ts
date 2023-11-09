cccd9e29101a6a6ef42a7bceb818ec46e552f011
Amit Barletz <amit@amplication.com>
2023-11-09 19:33:08 +0200

    refactor(build-manager): update extractBuildId to return the string as is if there is nothing to do

--- packages/amplication-build-manager/src/code-generator/code-generator-splitter.service.ts
+++ packages/amplication-build-manager/src/code-generator/code-generator-splitter.service.ts
@@ -169,14 +169,19 @@ export class CodeGeneratorSplitterService {
    * This function extracts the buildId from the buildId with suffix.
    * It's needed because the buildId is used as a key in the Kafka messages and and as a folder name in the artifacts and when it
    * interacts with the data-service-generator and the server it needs to be without the suffix.
-   * @param buildIdWithSuffix the buildId with suffix which in this case could be "-server" or "-admin-ui"
+   * if the buildId doesn't contain the suffix it will return the whole string
+   * @param buildId the buildId with suffix which in this case could be "-server" or "-admin-ui"
    * @returns return the substring before the first hyphen or the whole string if there is no hyphen
    */
-  extractBuildId(buildIdWithSuffix: string): string {
+  extractBuildId(buildId: string): string {
     const regexPattern = `-(?:${EnumDomainName.Server}|${EnumDomainName.AdminUI})$`;
     const regex = new RegExp(regexPattern);
 
-    return buildIdWithSuffix.replace(regex, "");
+    if (!regex.test(buildId)) {
+      return buildId;
+    }
+
+    return buildId.replace(regex, "");
   }
 
   extractDomainName(buildIdWithSuffix: string): string {
