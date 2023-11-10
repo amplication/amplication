29400aa90abf7e3002657ec1eac0b2779b322325
Amit Barletz <amit@amplication.com>
2023-11-10 22:57:17 +0200

    test(build-manager): fix build runner controller test

--- packages/amplication-build-manager/src/build-runner/build-runner.controller.spec.ts
+++ packages/amplication-build-manager/src/build-runner/build-runner.controller.spec.ts
@@ -1,35 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { ConfigService } from "@nestjs/config";
-import { KafkaProducerService } from "@amplication/util/nestjs/kafka";
-import {
-  CodeGenerationFailure,
-  CodeGenerationRequest,
-  CodeGenerationSuccess,
-  KAFKA_TOPICS,
-} from "@amplication/schema-registry";
+import { CodeGenerationRequest } from "@amplication/schema-registry";
 import { CodeGeneratorVersionStrategy } from "@amplication/code-gen-types/models";
-import { AmplicationLogger } from "@amplication/util/nestjs/logging";
 import { MockedAmplicationLoggerProvider } from "@amplication/util/nestjs/logging/test-utils";
 
-import { Env } from "../env";
 import { BuildRunnerController } from "./build-runner.controller";
 import { BuildRunnerService } from "./build-runner.service";
-import { CodeGeneratorService } from "../code-generator/code-generator-catalog.service";
 import { CodeGenerationSuccessDto } from "./dto/CodeGenerationSuccess";
 import { CodeGenerationFailureDto } from "./dto/CodeGenerationFailure";
 import { AppInfo } from "@amplication/code-gen-types";
-import { CodeGeneratorSplitterService } from "../code-generator/code-generator-splitter.service";
+import { EnumJobStatus } from "../types";
 
 describe("BuildRunnerController", () => {
   let controller: BuildRunnerController;
-  let loggerService: AmplicationLogger;
-  let codeGeneratorSplitterService: CodeGeneratorSplitterService;
-
-  const mockRunnerServiceCopyFromJobToArtifact = jest.fn();
-  const mockRunnerServiceGetCodeGeneratorVersion = jest.fn();
-  const mockRunnerServiceRunJobs = jest.fn();
-  const mockCodeGeneratorServiceGetCodeGeneratorVersion = jest.fn();
-  const mockKafkaServiceEmitMessage = jest.fn();
+  let buildRunnerService: BuildRunnerService;
 
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -39,285 +22,87 @@ describe("BuildRunnerController", () => {
       controllers: [BuildRunnerController],
       providers: [
         {
-          provide: KafkaProducerService,
-          useClass: jest.fn(() => ({
-            emitMessage: mockKafkaServiceEmitMessage,
-          })),
-        },
-        {
           provide: BuildRunnerService,
           useClass: jest.fn(() => ({
-            copyFromJobToArtifact: mockRunnerServiceCopyFromJobToArtifact,
-            getCodeGeneratorVersion: mockRunnerServiceGetCodeGeneratorVersion,
-            runJobs: mockRunnerServiceRunJobs,
+            runBuilds: jest.fn(),
+            processBuildResult: jest.fn(),
           })),
-        },
-        {
-          provide: CodeGeneratorService,
-          useClass: jest.fn(() => ({
-            getCodeGeneratorVersion:
-              mockCodeGeneratorServiceGetCodeGeneratorVersion,
-          })),
-        },
-        {
-          provide: ConfigService,
-          useValue: {
-            get: (variable) => {
-              switch (variable) {
-                case KAFKA_TOPICS.CODE_GENERATION_SUCCESS_TOPIC:
-                  return "code_generation_success_topic";
-                case KAFKA_TOPICS.CODE_GENERATION_FAILURE_TOPIC:
-                  return "code_generation_failure_topic";
-                case Env.DSG_RUNNER_URL:
-                  return "http://runner.url/";
-                case Env.DSG_CATALOG_SERVICE_URL:
-                  return "http://catalog.url/";
-                default:
-                  return "";
-              }
-            },
-          },
-        },
-        {
-          provide: CodeGeneratorSplitterService,
-          useValue: {
-            extractBuildId: jest.fn(),
-          },
         },
         MockedAmplicationLoggerProvider,
       ],
     }).compile();
 
     controller = module.get<BuildRunnerController>(BuildRunnerController);
-    loggerService = module.get<AmplicationLogger>(AmplicationLogger);
-    codeGeneratorSplitterService = module.get<CodeGeneratorSplitterService>(
-      CodeGeneratorSplitterService
-    );
+    buildRunnerService = module.get<BuildRunnerService>(BuildRunnerService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 
-  it("On code generation success, copy file and/or directories from `jobPath` to `artifactPath` and emit Kafka success event", async () => {
-    const buildId = "buildId";
-    const spyOnCodeGeneratorSplitterServiceExtractBuildId = jest
-      .spyOn(codeGeneratorSplitterService, "extractBuildId")
-      .mockReturnValue(buildId);
-
-    const codeGenerationSuccessDTOMock: CodeGenerationSuccessDto = {
-      resourceId: "resourceId",
-      buildId: buildId,
-    };
-
-    const expectedCodeGeneratorVersion = "v1.2.2";
-    mockRunnerServiceGetCodeGeneratorVersion.mockResolvedValue(
-      expectedCodeGeneratorVersion
-    );
-
-    const kafkaSuccessEventMock: CodeGenerationSuccess.KafkaEvent = {
-      key: null,
-      value: {
-        buildId: codeGenerationSuccessDTOMock.buildId,
-        codeGeneratorVersion: expectedCodeGeneratorVersion,
-      },
-    };
-
-    mockRunnerServiceCopyFromJobToArtifact.mockResolvedValue(undefined);
-    mockKafkaServiceEmitMessage.mockResolvedValue(undefined);
-
-    await controller.onCodeGenerationSuccess(codeGenerationSuccessDTOMock);
-
-    expect(mockRunnerServiceCopyFromJobToArtifact).toBeCalledWith(
-      codeGenerationSuccessDTOMock.resourceId,
-      codeGenerationSuccessDTOMock.buildId
-    );
-    await expect(
-      mockRunnerServiceCopyFromJobToArtifact()
-    ).resolves.not.toThrow();
-
-    expect(mockKafkaServiceEmitMessage).toBeCalledWith(
-      KAFKA_TOPICS.CODE_GENERATION_SUCCESS_TOPIC,
-      kafkaSuccessEventMock
-    );
-
-    expect(spyOnCodeGeneratorSplitterServiceExtractBuildId).toBeCalledTimes(1);
-    await expect(mockKafkaServiceEmitMessage()).resolves.not.toThrow();
-  });
-
-  it("On code generation success with unhandled exception thrown, log `error.message` with log level `error` and emit Kafka failure event", async () => {
-    const buildId = "buildId";
-    const spyOnCodeGeneratorSplitterServiceExtractBuildId = jest
-      .spyOn(codeGeneratorSplitterService, "extractBuildId")
-      .mockReturnValue(buildId);
-
-    const errorMock = new Error("Test error");
-    const codeGenerationSuccessDTOMock: CodeGenerationSuccessDto = {
-      resourceId: "resourceId",
-      buildId: buildId,
-    };
-    const expectedCodeGeneratorVersion = "v1.2.2";
-    mockRunnerServiceGetCodeGeneratorVersion.mockResolvedValue(
-      expectedCodeGeneratorVersion
-    );
-
-    const kafkaFailureEventMock: CodeGenerationFailure.KafkaEvent = {
-      key: null,
-      value: <CodeGenerationFailure.Value>{
-        buildId: codeGenerationSuccessDTOMock.buildId,
-        error: errorMock,
-        codeGeneratorVersion: expectedCodeGeneratorVersion,
-      },
-    } as unknown as CodeGenerationFailure.KafkaEvent;
-
-    mockRunnerServiceCopyFromJobToArtifact.mockRejectedValue(errorMock);
-    mockKafkaServiceEmitMessage.mockResolvedValue(undefined);
-
-    await controller.onCodeGenerationSuccess(codeGenerationSuccessDTOMock);
-
-    expect(mockRunnerServiceCopyFromJobToArtifact).toBeCalledWith(
-      codeGenerationSuccessDTOMock.resourceId,
-      codeGenerationSuccessDTOMock.buildId
-    );
-    await expect(
-      async () => await mockRunnerServiceCopyFromJobToArtifact()
-    ).rejects.toThrow(errorMock);
-
-    expect(mockKafkaServiceEmitMessage).toBeCalledWith(
-      KAFKA_TOPICS.CODE_GENERATION_FAILURE_TOPIC,
-      kafkaFailureEventMock
-    );
-
-    expect(spyOnCodeGeneratorSplitterServiceExtractBuildId).toBeCalledTimes(1);
-    await expect(mockKafkaServiceEmitMessage()).resolves.not.toThrow();
-  });
-
-  it("On code generation failure, log `error.message` with log level `error` and emit Kafka failure event", async () => {
-    const errorMock = new Error("Test error");
-    const buildId = "buildId";
-    const spyOnCodeGeneratorSplitterServiceExtractBuildId = jest
-      .spyOn(codeGeneratorSplitterService, "extractBuildId")
-      .mockReturnValue(buildId);
-    const codeGenerationFailureDTOMock: CodeGenerationFailureDto = {
-      resourceId: "resourceId",
-      buildId: buildId,
-      error: errorMock,
-    };
-    const expectedCodeGeneratorVersion = "v1.2.2";
-    mockRunnerServiceGetCodeGeneratorVersion.mockResolvedValue(
-      expectedCodeGeneratorVersion
-    );
-
-    const kafkaFailureEventMock: CodeGenerationFailure.KafkaEvent = {
-      key: null,
-      value: <CodeGenerationFailure.Value>{
-        buildId: codeGenerationFailureDTOMock.buildId,
-        error: errorMock,
-        codeGeneratorVersion: expectedCodeGeneratorVersion,
-      },
-    } as unknown as CodeGenerationFailure.KafkaEvent;
-
-    mockKafkaServiceEmitMessage.mockResolvedValue(undefined);
-
-    await controller.onCodeGenerationFailure(codeGenerationFailureDTOMock);
-
-    expect(mockKafkaServiceEmitMessage).toBeCalledWith(
-      KAFKA_TOPICS.CODE_GENERATION_FAILURE_TOPIC,
-      kafkaFailureEventMock
-    );
-
-    expect(spyOnCodeGeneratorSplitterServiceExtractBuildId).toBeCalledTimes(1);
-    await expect(mockKafkaServiceEmitMessage()).resolves.not.toThrow();
-  });
-
-  describe("on code generation request", () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
+  describe("onCodeGenerationSuccess", () => {
+    it("should call processBuildResult with Success", async () => {
+      const dto: CodeGenerationSuccessDto = {
+        buildId: "buildId",
+        resourceId: "resourceId",
+      };
+      const spyOnProcessBuildResult = jest.spyOn(
+        buildRunnerService,
+        "processBuildResult"
+      );
+      await controller.onCodeGenerationSuccess(dto);
+      expect(spyOnProcessBuildResult).toHaveBeenCalledWith(
+        dto.resourceId,
+        dto.buildId,
+        EnumJobStatus.Success
+      );
     });
-    it("should successfully trigger DSG jobs", async () => {
-      const expectedCodeGeneratorVersion = "v1.2.2";
-      const expectedResourceId = "resourceId";
-      const expectedBuildId = "buildId";
+  });
+
+  describe("onCodeGenerationFailure", () => {
+    it("should call processBuildResult with Failure", async () => {
+      const dto: CodeGenerationFailureDto = {
+        buildId: "buildId",
+        resourceId: "resourceId",
+        error: new Error("Test Error"),
+      };
+      const spyOnProcessBuildResult = jest.spyOn(
+        buildRunnerService,
+        "processBuildResult"
+      );
+      await controller.onCodeGenerationFailure(dto);
+      expect(spyOnProcessBuildResult).toHaveBeenCalledWith(
+        dto.resourceId,
+        dto.buildId,
+        EnumJobStatus.Failure,
+        dto.error
+      );
+    });
+  });
+
+  describe("onCodeGenerationRequest", () => {
+    it("should call runBuilds", async () => {
       const codeGenerationRequestDTOMock: CodeGenerationRequest.Value = {
-        resourceId: expectedResourceId,
-        buildId: expectedBuildId,
+        resourceId: "resourceId",
+        buildId: "buildId",
         dsgResourceData: {
           resourceType: "Service",
           buildId: "12345",
           pluginInstallations: [],
           resourceInfo: {
             codeGeneratorVersionOptions: {
-              version: expectedCodeGeneratorVersion,
+              version: "2.0.0",
               selectionStrategy: CodeGeneratorVersionStrategy.Specific,
             },
           } as unknown as AppInfo,
         },
       };
-
-      mockCodeGeneratorServiceGetCodeGeneratorVersion.mockResolvedValue(
-        expectedCodeGeneratorVersion
-      );
-
-      mockRunnerServiceRunJobs.mockResolvedValue(undefined);
+      const spyOnRunBuilds = jest.spyOn(buildRunnerService, "runBuilds");
       await controller.onCodeGenerationRequest(codeGenerationRequestDTOMock);
-      expect(mockRunnerServiceRunJobs).toBeCalledWith(
-        expectedResourceId,
-        expectedBuildId,
-        codeGenerationRequestDTOMock.dsgResourceData,
-        expectedCodeGeneratorVersion
-      );
-    });
-
-    it("should emit code generation failure event when the trigger DSG jobs fails", async () => {
-      //const errorMock = new Error("Test error");
-      const expectedCodeGeneratorVersion = "v1.2.2";
-      const expectedResourceId = "resourceId";
-      const expectedBuildId = "buildId";
-      const codeGenerationRequestDTOMock: CodeGenerationRequest.Value = {
-        resourceId: expectedResourceId,
-        buildId: expectedBuildId,
-        dsgResourceData: {
-          resourceType: "Service",
-          buildId: expectedBuildId,
-          pluginInstallations: [],
-          resourceInfo: {
-            codeGeneratorVersionOptions: {
-              version: expectedCodeGeneratorVersion,
-              selectionStrategy: CodeGeneratorVersionStrategy.Specific,
-            },
-          } as unknown as AppInfo,
-        },
-      };
-      mockCodeGeneratorServiceGetCodeGeneratorVersion.mockResolvedValue(
-        expectedCodeGeneratorVersion
-      );
-      const kafkaFailureEventMock: CodeGenerationFailure.KafkaEvent = {
-        key: null,
-        value: <CodeGenerationFailure.Value>{
-          buildId: codeGenerationRequestDTOMock.buildId,
-          error: new Error("Test error"),
-          codeGeneratorVersion: expectedCodeGeneratorVersion,
-        },
-      } as unknown as CodeGenerationFailure.KafkaEvent;
-
-      mockRunnerServiceRunJobs.mockRejectedValue(new Error("Test error"));
-
-      await controller.onCodeGenerationRequest(codeGenerationRequestDTOMock);
-
-      expect(mockRunnerServiceRunJobs).toBeCalledWith(
-        expectedResourceId,
-        expectedBuildId,
-        codeGenerationRequestDTOMock.dsgResourceData,
-        expectedCodeGeneratorVersion
-      );
-
-      expect(loggerService.error).toBeCalledWith(
-        "Test error",
-        new Error("Test error")
-      );
-      expect(mockKafkaServiceEmitMessage).toBeCalledWith(
-        KAFKA_TOPICS.CODE_GENERATION_FAILURE_TOPIC,
-        kafkaFailureEventMock
+      expect(spyOnRunBuilds).toHaveBeenCalledWith(
+        codeGenerationRequestDTOMock.resourceId,
+        codeGenerationRequestDTOMock.buildId,
+        codeGenerationRequestDTOMock.dsgResourceData
       );
     });
   });
