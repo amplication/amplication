3f2d0d922bb9b0c934ca6c480bfc19efa0e80be4
Amit Barletz <amit@amplication.com>
2023-11-16 14:32:34 +0200

    fix(build-manager): redis config tls

--- packages/amplication-build-manager/src/env.ts
+++ packages/amplication-build-manager/src/env.ts
@@ -11,7 +11,6 @@ export class Env {
   static readonly REDIS_TLS_CA = "REDIS_TLS_CA";
   static readonly REDIS_TLS_CERT = "REDIS_TLS_CA";
   static readonly REDIS_TLS_KEY = "REDIS_TLS_CA";
-  static readonly REDIS_TLS_ENABLED = "REDIS_TLS_ENABLED";
 
   static readonly BUILD_ARTIFACTS_BASE_FOLDER = "BUILD_ARTIFACTS_BASE_FOLDER";
   static readonly BUILD_ARTIFACTS_CODE_FOLDER = "BUILD_ARTIFACTS_CODE_FOLDER";
