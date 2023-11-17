9f417b51067a35c8b1917027fb149bcc5e92815a
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-11-17 14:50:44 +0000

    chore(plugin-api): fixing lint

--- packages/amplication-plugin-api/src/connectMicroservices.ts
+++ packages/amplication-plugin-api/src/connectMicroservices.ts
@@ -1,6 +1,5 @@
 import { INestApplication } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 
 export async function connectMicroservices(app: INestApplication) {
-  const configService = app.get(ConfigService);
+  //
 }
