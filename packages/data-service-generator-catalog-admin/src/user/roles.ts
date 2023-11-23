1d4b483ac6e976bdfb845e0c41187268ca8c58aa
xonx <119700621+xonx4l@users.noreply.github.com>
2023-11-23 09:54:06 +0530

    Update roles.ts
    issue-:#1313

--- packages/data-service-generator-catalog-admin/src/user/roles.ts
+++ packages/data-service-generator-catalog-admin/src/user/roles.ts
@@ -1,6 +1,35 @@
-export const ROLES = [
+
+export interface Role {
+  name: string;
+  displayName: string;
+}
+
+export const ROLES: Role[] = [
   {
     name: "user",
     displayName: "User",
   },
 ];
+
+type RoleUpdateCallback = (updatedRoles: Role[]) => void;
+
+let roleUpdateCallback: RoleUpdateCallback | null = null;
+
+export const setRoleUpdateCallback = (callback: RoleUpdateCallback) => {
+  roleUpdateCallback = callback;
+};
+
+export const updateRole = (oldName: string, newName: string) => {
+  const roleIndex = ROLES.findIndex((role) => role.name === oldName);
+
+  if (roleIndex !== -1) {
+    // Update the role name
+    ROLES[roleIndex].name = newName;
+
+    // Notify about the role update
+    if (roleUpdateCallback) {
+      roleUpdateCallback(ROLES);
+    }
+  }
+};
+
