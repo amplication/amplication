f89d60a0bb7bd3453c584944c1ea7ca0ef83bd5b
xonx <119700621+xonx4l@users.noreply.github.com>
2023-11-30 09:45:10 +0530

    Update Catalog.tsx
    issue-:#7021

--- packages/amplication-client/src/Resource/codeGeneratorVersionSettings/Catalog.tsx
+++ packages/amplication-client/src/Resource/codeGeneratorVersionSettings/Catalog.tsx
@@ -1,19 +1,17 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import { EnumTextStyle, ListItem, Text } from "@amplication/ui/design-system";
 
 type CatalogProps = {
   name: string;
-  changelog: string;
+  changelog: string; 
 };
 
 export const DSGCatalog: React.FC<CatalogProps> = ({ name, changelog }) => {
-  const changelogParagraphs = changelog.split("\n");
-  const changelogText = changelogParagraphs.map((paragraph, index) => (
-    <p key={index}>{paragraph}</p>
-  ));
   return (
     <ListItem>
       <Text textStyle={EnumTextStyle.Normal}>{name}</Text>
-      <Text textStyle={EnumTextStyle.Tag}>{changelogText}</Text>
+      <ReactMarkdown source={changelog} />
     </ListItem>
   );
 };
