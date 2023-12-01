db9b155cd40427dadf3a6c212cecdefdf8fb7b3a
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-12-01 12:45:34 +0000

    chore(ui-design-system): add new button progress component

--- libs/ui/design-system/src/lib/components/ButtonProgress/ButtonProgress.tsx
+++ libs/ui/design-system/src/lib/components/ButtonProgress/ButtonProgress.tsx
@@ -1,38 +1,89 @@
 import React from "react";
 
-import { Icon, IconSize } from "../Icon/Icon";
 import { ButtonGroup as PrimerButtonGroup } from "@primer/react";
+import type { ButtonProps as PrimerButtonProps } from "@primer/react/deprecated";
 
 import "./ButtonProgress.scss";
-import { Button, EnumButtonStyle } from "../..";
-import type { Props as ButtonProps } from "../Button/Button";
+import { Button, EnumButtonStyle } from "../Button/Button";
 import classNames from "classnames";
+import { CircularProgress, circularProgressClasses } from "@mui/material";
+import type { CircularProgressProps } from "@mui/material";
+import { Box } from "@mui/material";
+import { margin } from "@mui/system";
+import {
+  EnumFlexDirection,
+  EnumGapSize,
+  EnumItemsAlign,
+  FlexItem,
+} from "../FlexItem/FlexItem";
+import { EnumFormStyle } from "../Form/Form";
 
-export interface Props extends ButtonProps {
+export interface Props
+  extends Pick<PrimerButtonProps, "className" | "children" | "onClick"> {
   /** Progress percentage 0-100 */
   progress: number;
-  color?: string;
   yellowColorThreshold: number;
   redColorThreshold: number;
+  leftValue: string;
   rest?: any;
 }
 
 enum EnumButtonProgressStyle {
-  Default = "default",
+  Primary = "primary",
   Warning = "warning",
   Danger = "danger",
 }
 
+const Pie = (props: CircularProgressProps) => {
+  return (
+    <Box
+      className={props.className}
+      sx={{
+        position: "relative",
+        display: "flex",
+      }}
+    >
+      <CircularProgress
+        {...props}
+        variant="determinate"
+        className=".amp-button-progress--background"
+        value={100}
+        thickness={3}
+        sx={{
+          color: "#686f8c",
+        }}
+      />
+      <CircularProgress
+        {...props}
+        variant="determinate"
+        value={props.value}
+        thickness={3}
+        sx={{
+          color: "inherit",
+          position: "absolute",
+        }}
+      />
+    </Box>
+  );
+};
+
 export const ButtonProgress = (props: Props) => {
-  const {
+  let {
     yellowColorThreshold,
     redColorThreshold,
     progress,
-    splitValue,
+    leftValue,
     className,
     children,
     rest,
   } = props;
+
+  if (progress < 0) {
+    progress = 0;
+  }
+  if (progress > 100) {
+    progress = 100;
+  }
 
   let buttonProgressStyle: EnumButtonProgressStyle;
   if (progress <= redColorThreshold) {
@@ -40,7 +91,7 @@ export const ButtonProgress = (props: Props) => {
   } else if (progress <= yellowColorThreshold) {
     buttonProgressStyle = EnumButtonProgressStyle.Warning;
   } else {
-    buttonProgressStyle = EnumButtonProgressStyle.Default;
+    buttonProgressStyle = EnumButtonProgressStyle.Primary;
   }
 
   return (
@@ -53,20 +104,33 @@ export const ButtonProgress = (props: Props) => {
       <Button
         className={classNames(
           "amp-button-progress",
-          `amp-button-progress--left`,
           className,
+          `amp-button-progress--left`,
           `amp-button-progress--${buttonProgressStyle}--left`
         )}
         buttonStyle={EnumButtonStyle.Outline}
         {...rest}
       >
-        {splitValue}
+        <FlexItem
+          direction={EnumFlexDirection.Row}
+          itemsAlign={EnumItemsAlign.Center}
+          gap={EnumGapSize.Small}
+        >
+          <Pie
+            value={progress}
+            size="14px"
+            className={classNames(
+              `amp-button-progress-pie--${buttonProgressStyle}`
+            )}
+          />
+          {leftValue}
+        </FlexItem>
       </Button>
       <Button
         className={classNames(
           "amp-button-progress",
-          "amp-button-progress--right",
           className,
+          "amp-button-progress--right",
           `amp-button-progress--${buttonProgressStyle}--right`
         )}
         {...rest}
