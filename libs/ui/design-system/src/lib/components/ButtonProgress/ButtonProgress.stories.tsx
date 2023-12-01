31353cee2de9e2dae59e33c90e1088021eff265c
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-12-01 12:45:50 +0000

    chore(ui-design-system): add story for button progress component

--- libs/ui/design-system/src/lib/components/ButtonProgress/ButtonProgress.stories.tsx
+++ libs/ui/design-system/src/lib/components/ButtonProgress/ButtonProgress.stories.tsx
@@ -1,108 +1,58 @@
 import React from "react";
 import { Meta } from "@storybook/react";
-import {
-  Button,
-  EnumButtonState,
-  EnumButtonStyle,
-  EnumIconPosition,
-} from "./ButtonProgress";
+import { ButtonProgress, Props } from "./ButtonProgress";
 import { EnumFlexDirection, FlexItem } from "../FlexItem/FlexItem";
 
 export default {
-  title: "Button",
-  component: Button,
+  title: "ButtonProgress",
+  component: ButtonProgress,
 } as Meta;
 
 export const Default = {
-  render: (args: any) => {
-    return <Button {...args}>Default Button</Button>;
+  render: (args: Props) => {
+    return (
+      <ButtonProgress
+        {...args}
+        progress={args.progress || 100}
+        leftValue={args.leftValue || "some progress"}
+      >
+        ButtonProgress
+      </ButtonProgress>
+    );
   },
 };
 
-export const Primary = {
-  render: (args: any) => {
+export const All = {
+  render: (args: Props) => {
     return (
       <>
         <FlexItem direction={EnumFlexDirection.Column}>
-          <Button buttonStyle={EnumButtonStyle.Primary}>Primary </Button>
-          <Button buttonStyle={EnumButtonStyle.Primary} disabled={true}>
-            Primary Disabled
-          </Button>
-          <Button
-            buttonStyle={EnumButtonStyle.Primary}
-            buttonState={EnumButtonState.Danger}
+          <ButtonProgress
+            progress={100}
+            yellowColorThreshold={50}
+            redColorThreshold={1}
+            leftValue="some progress"
           >
-            Primary Danger
-          </Button>
-          <Button icon="trash_2" buttonStyle={EnumButtonStyle.Primary}>
-            Primary With Icon Right
-          </Button>
-          <Button
-            icon="trash_2"
-            iconPosition={EnumIconPosition.Left}
-            buttonStyle={EnumButtonStyle.Primary}
+            default
+          </ButtonProgress>
+          <ButtonProgress
+            progress={50}
+            leftValue="some progress"
+            yellowColorThreshold={50}
+            redColorThreshold={1}
           >
-            Primary With Icon Right
-          </Button>
+            warning
+          </ButtonProgress>
+          <ButtonProgress
+            progress={0}
+            leftValue="some progress"
+            yellowColorThreshold={50}
+            redColorThreshold={1}
+          >
+            danger
+          </ButtonProgress>
         </FlexItem>
       </>
-    );
-  },
-};
-
-export const Outline = {
-  render: (args: any) => {
-    return (
-      <FlexItem direction={EnumFlexDirection.Column}>
-        <Button buttonStyle={EnumButtonStyle.Outline}>Outline </Button>
-        <Button buttonStyle={EnumButtonStyle.Outline} disabled={true}>
-          Outline Disabled
-        </Button>
-        <Button
-          buttonStyle={EnumButtonStyle.Outline}
-          buttonState={EnumButtonState.Danger}
-        >
-          Outline Danger
-        </Button>
-        <Button icon="trash_2" buttonStyle={EnumButtonStyle.Outline}>
-          Outline With Icon Right
-        </Button>
-        <Button
-          icon="trash_2"
-          iconPosition={EnumIconPosition.Left}
-          buttonStyle={EnumButtonStyle.Outline}
-        >
-          Outline With Icon Left
-        </Button>
-      </FlexItem>
-    );
-  },
-};
-export const Text = {
-  render: (args: any) => {
-    return (
-      <FlexItem direction={EnumFlexDirection.Column}>
-        <Button buttonStyle={EnumButtonStyle.Text}>Text </Button>
-        <Button buttonStyle={EnumButtonStyle.Text} disabled={true}>
-          Text Disabled
-        </Button>
-        <Button
-          buttonStyle={EnumButtonStyle.Text}
-          buttonState={EnumButtonState.Danger}
-        >
-          Text Danger
-        </Button>
-        <Button icon="trash_2" buttonStyle={EnumButtonStyle.Text}>
-          Text With Icon Right
-        </Button>
-        <Button
-          icon="trash_2"
-          iconPosition={EnumIconPosition.Left}
-          buttonStyle={EnumButtonStyle.Text}
-        >
-          Text With Icon Left
-        </Button>
-      </FlexItem>
     );
   },
 };
