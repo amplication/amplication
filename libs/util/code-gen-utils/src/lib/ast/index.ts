211f6af1ca592dd2b6e300e52621dc6e3ecc8758
Mor Hagbi <mor@amplication.com>
2023-12-06 18:25:39 +0200

    refactor(dsg): pass custom-actions ast function to the DSG

--- libs/util/code-gen-utils/src/lib/ast/index.ts
+++ libs/util/code-gen-utils/src/lib/ast/index.ts
@@ -4,4 +4,3 @@ export * from "./remove-typescript-ignore-comments";
 export * from "./remove-typescript-interface-declares";
 export * from "./remove-typescript-variable-declares";
 export * from "./remove-imports-typescript-ignore-comments";
-export * from "./remove-custom-actions-functions";
