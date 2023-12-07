0ff6c1bbaf02ba2f24b39780a2049d6bb84fe0cf
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-12-07 11:28:27 +0000

    chore(client): remove redundant file

--- packages/amplication-client/src/util/BillingPlan.ts
+++
@@ -1,6 +0,0 @@
-export enum BillingPlan {
-  Free = "plan-amplication-free",
-  Pro = "plan-amplication-pro",
-  Enterprise = "plan-amplication-enterprise",
-  ProWithTrial = "plan-amplication-pro-with-trial",
-}
