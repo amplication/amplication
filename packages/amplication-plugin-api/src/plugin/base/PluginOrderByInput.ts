5e381435aaeab2193b5f090da104228e12dc0d76
Muly Gottlieb <muly@amplication.com>
2023-12-13 15:46:33 +0200

    Update PluginOrderByInput.ts

--- packages/amplication-plugin-api/src/plugin/base/PluginOrderByInput.ts
+++ packages/amplication-plugin-api/src/plugin/base/PluginOrderByInput.ts
@@ -28,17 +28,6 @@ class PluginOrderByInput {
   @Field(() => SortOrder, {
     nullable: true,
   })
-  categories?: SortOrder;
-
-  @ApiProperty({
-    required: false,
-    enum: ["asc", "desc"],
-  })
-  @IsOptional()
-  @IsEnum(SortOrder)
-  @Field(() => SortOrder, {
-    nullable: true,
-  })
   createdAt?: SortOrder;
 
   @ApiProperty({
