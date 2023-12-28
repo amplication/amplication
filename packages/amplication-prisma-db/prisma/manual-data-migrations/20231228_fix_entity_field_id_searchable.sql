7558fb2c4baacc6af4c286dbfdef8b61b1a4aa43
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2023-12-28 11:22:04 +0100

    refactor(db): improve fix searchable db data migration performance

--- packages/amplication-prisma-db/prisma/manual-data-migrations/20231228_fix_entity_field_id_searchable.sql
+++ packages/amplication-prisma-db/prisma/manual-data-migrations/20231228_fix_entity_field_id_searchable.sql
@@ -1,19 +1,21 @@
-BEGIN;
+DO $$ 
+DECLARE 
+    batch_size INT := 500; -- Adjust the batch size as needed
+    total_rows INT;
+BEGIN
+    SELECT COUNT(*) INTO total_rows FROM "public"."EntityField" WHERE "dataType" = 'Id';
 
-SELECT COUNT(*)
-FROM "public"."EntityField" 
-WHERE  "EntityField"."dataType" = 'Id'
-AND "EntityField".searchable = 't' ;
+    FOR i IN 0..ceil(total_rows / batch_size) - 1 LOOP
+        UPDATE "public"."EntityField" 
+        SET searchable = 't', "updatedAt" = NOW()
+        WHERE ctid IN (
+            SELECT ctid
+            FROM "public"."EntityField"
+            WHERE "dataType" = 'Id'
+            AND searchable <> 't'
+            LIMIT batch_size OFFSET i * batch_size
+        );
 
-UPDATE "public"."EntityField" SET searchable = 't', "updatedAt" = NOW()
-    WHERE "dataType" = 'Id';
-
-SAVEPOINT my_savepoint;
-
-SELECT COUNT(*)
-FROM "public"."EntityField" 
-WHERE  "EntityField"."dataType" = 'Id'AND "EntityField".searchable <> 't' ;
-
--- ROLLBACK TO my_savepoint;
-
--- COMMIT;
+        COMMIT; -- Commit after each batch to release locks
+    END LOOP;
+END $$;
