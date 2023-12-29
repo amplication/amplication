4d9f06bb6f290139bbaebdc9375a71e358f40e27
Amit Barletz <amit@amplication.com>
2023-12-29 12:09:34 +0200

    fix(server): remove unused function

--- packages/amplication-server/src/core/account/account.service.ts
+++ packages/amplication-server/src/core/account/account.service.ts
@@ -44,35 +44,6 @@ export class AccountService {
     return account;
   }
 
-  async createPreviewAccount(
-    args: Prisma.AccountCreateArgs,
-    identityProvider: string
-  ): Promise<Account> {
-    const account = await this.prisma.account.create(args);
-
-    const userData: IdentifyData = {
-      userId: account.id,
-      createdAt: account.createdAt,
-      email: account.email,
-      firstName: account.firstName,
-      lastName: account.lastName,
-    };
-
-    await this.analytics.identify(userData);
-    //we send the userData again to prevent race condition
-    await this.analytics.track({
-      userId: account.id,
-      event: EnumEventType.Signup,
-      properties: {
-        identityProvider,
-      },
-      context: {
-        traits: userData,
-      },
-    });
-    return account;
-  }
-
   findAccount(args: Prisma.AccountFindUniqueArgs): Promise<Account> {
     return this.prisma.account.findUnique(args);
   }
