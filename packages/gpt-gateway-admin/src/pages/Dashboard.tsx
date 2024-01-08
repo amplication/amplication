77cdf9fd7f7e83f1895256a86b6a2ee7ab481482
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-01-08 17:12:25 +0000

    chore(gpt-gateway-admin): expose auth information in dashboard for convinence

--- packages/gpt-gateway-admin/src/pages/Dashboard.tsx
+++ packages/gpt-gateway-admin/src/pages/Dashboard.tsx
@@ -2,11 +2,26 @@ import * as React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Title } from "react-admin";
-const Dashboard = () => (
-  <Card>
-    <Title title="Welcome to the administration" />
-    <CardContent>Welcome</CardContent>
-  </Card>
-);
+import { useEffect, useState } from "react";
+import { width } from "@mui/system";
+
+const Dashboard = () => {
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("credentials")?.split(" ")[1];
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+  return (
+    <Card>
+      <Title title="Welcome to the administration" />
+      <CardContent>Welcome</CardContent>
+      <CardContent>
+        <small>{token}</small>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Dashboard;
