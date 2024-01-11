ad1b511814aec8f83bef2f792c20ee607f9a678e
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-01-11 11:29:59 +0000

    chore(schema-registry): fix lint error with --fix

--- libs/schema-registry/src/lib/user-action/index.ts
+++ libs/schema-registry/src/lib/user-action/index.ts
@@ -1,6 +1,6 @@
-import { DecodedKafkaMessage } from "@amplication/util/kafka";
 import { Key } from "./key";
 import { Value, UserActionType } from "./value";
+import { DecodedKafkaMessage } from "@amplication/util/kafka";
 
 interface KafkaEvent extends DecodedKafkaMessage {
   key: Key;
