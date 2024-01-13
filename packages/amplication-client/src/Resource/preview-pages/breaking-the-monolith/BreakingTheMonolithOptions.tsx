1b546b24c385a1ff77f75239fd1905b79be85a08
Amit Barletz <amit@amplication.com>
2024-01-13 19:40:48 +0200

    feat(client): update breaking the monolith options component with the new mutation and logic

--- packages/amplication-client/src/Resource/preview-pages/breaking-the-monolith/BreakingTheMonolithOptions.tsx
+++ packages/amplication-client/src/Resource/preview-pages/breaking-the-monolith/BreakingTheMonolithOptions.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { match, useHistory } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { CircularProgress } from "@mui/material";
 import {
   Button,
   EnumContentAlign,
@@ -11,12 +14,19 @@ import {
   Panel,
   Text,
 } from "@amplication/ui/design-system";
-import { MonolithOptions, monolithOptions } from "./monolith-options";
-import "./BreakingTheMonolithOptions.scss";
-import { match, useHistory } from "react-router-dom";
+import * as models from "../../../models";
+import { MonolithOption, monolithOptions } from "./monolith-options";
+import useUserActionWatchStatus from "../../../Entity/ImportPrismaSchema/useUserActionWatchStatus";
 import { AppRouteProps } from "../../../routes/routesUtil";
+import { CREATE_ENTITIES_FROM_PREDEFINED_SCHEMA } from "../../../Entity/ImportPrismaSchema/queries";
+
+import "./BreakingTheMonolithOptions.scss";
 
 const CLASS_NAME = "breaking-the-monolith-options";
+
+type TData = {
+  createEntitiesFromPredefinedSchema: models.UserAction;
+};
 
 type Props = AppRouteProps & {
   match: match<{
@@ -27,99 +37,127 @@ type Props = AppRouteProps & {
 };
 
 const BreakingTheMonolithOptions: React.FC<Props> = ({ match }) => {
-  const history = useHistory();
   const {
     workspace: workspaceId,
     project: projectId,
     resource: resourceId,
   } = match.params;
 
-  const convertPathToFileToFile = (selectedMonolith: MonolithOptions): File => {
-    const file = new File(
-      [selectedMonolith.pathToPrismaSchema],
-      selectedMonolith.title
-    );
-    return file;
-  };
+  const history = useHistory();
+  const [userAction, setUserAction] = React.useState<models.UserAction>(null);
+  const { data: userActionData } = useUserActionWatchStatus(userAction);
 
-  const handleBreakClicked = (selectedMonolithToBreak: MonolithOptions) => {
-    console.log("break clicked");
-    const file = convertPathToFileToFile(selectedMonolithToBreak);
-    history.push(
-      `/${workspaceId}/${projectId}/${resourceId}/entities/import-schema`,
-      { file: file }
-    );
+  const [createEntitiesFormPredefinedSchema] = useMutation<TData>(
+    CREATE_ENTITIES_FROM_PREDEFINED_SCHEMA,
+    {
+      onCompleted: (data) => {
+        setUserAction(data.createEntitiesFromPredefinedSchema);
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (!userActionData || !userActionData.userAction) return;
+    if (
+      userActionData.userAction.status === models.EnumUserActionStatus.Completed
+    ) {
+      history.push(`/${workspaceId}/${projectId}/${resourceId}/entities`); // TODO: change to the architecture page
+    }
+  }, [userActionData, history, workspaceId, projectId, resourceId]);
+
+  const handleBreakClicked = (selectedMonolithToBreak: MonolithOption) => {
+    createEntitiesFormPredefinedSchema({
+      variables: {
+        data: {
+          schemaName: selectedMonolithToBreak.name,
+          resource: {
+            connect: {
+              id: resourceId,
+            },
+          },
+        },
+      },
+    }).catch(console.error);
   };
 
   return (
-    <FlexItem
-      className={CLASS_NAME}
-      direction={EnumFlexDirection.Column}
-      itemsAlign={EnumItemsAlign.Center}
-    >
-      <Text textStyle={EnumTextStyle.H2}>
-        Select the monolith you want to break
-      </Text>
-      <Text
-        textStyle={EnumTextStyle.Normal}
-        textColor={EnumTextColor.Black20}
-        textAlign={EnumTextAlign.Center}
-      >
-        <div>
-          To illustrate how Amplication can transform legacy systems into a
-          micro-services architecture,
-        </div>
-        <div>
-          choose an open-source monolith, represented its its database schema
-        </div>
-      </Text>
+    <>
+      {userActionData?.userAction &&
+        userActionData?.userAction?.status !==
+          models.EnumUserActionStatus.Completed && (
+          <div className={`${CLASS_NAME}__overlay`}>
+            <CircularProgress />
+          </div>
+        )}
       <FlexItem
-        className={`${CLASS_NAME}__monolith_options`}
-        contentAlign={EnumContentAlign.Center}
+        className={CLASS_NAME}
+        direction={EnumFlexDirection.Column}
+        itemsAlign={EnumItemsAlign.Center}
       >
-        {monolithOptions.map((option, index) => (
-          <Panel key={index}>
-            <FlexItem direction={EnumFlexDirection.Column}>
-              <FlexItem>
-                <Text textStyle={EnumTextStyle.H3}>{option.title}</Text>
-                <Button
-                  onClick={() => handleBreakClicked(option)}
-                  className={`${CLASS_NAME}__action-button`}
+        <Text textStyle={EnumTextStyle.H2}>
+          Select the monolith you want to break
+        </Text>
+        <Text
+          textStyle={EnumTextStyle.Normal}
+          textColor={EnumTextColor.Black20}
+          textAlign={EnumTextAlign.Center}
+        >
+          <div>
+            To illustrate how Amplication can transform legacy systems into a
+            micro-services architecture,
+          </div>
+          <div>
+            choose an open-source monolith, represented its its database schema
+          </div>
+        </Text>
+        <FlexItem
+          className={`${CLASS_NAME}__monolith_options`}
+          contentAlign={EnumContentAlign.Center}
+        >
+          {monolithOptions.map((option, index) => (
+            <Panel key={index}>
+              <FlexItem direction={EnumFlexDirection.Column}>
+                <FlexItem>
+                  <Text textStyle={EnumTextStyle.H3}>{option.displayName}</Text>
+                  <Button
+                    onClick={() => handleBreakClicked(option)}
+                    className={`${CLASS_NAME}__action-button`}
+                  >
+                    Break
+                  </Button>
+                </FlexItem>
+                <Text
+                  textStyle={EnumTextStyle.Description}
+                  textColor={EnumTextColor.Black20}
                 >
-                  Break
-                </Button>
+                  {option.description}
+                </Text>
+                <Text textStyle={EnumTextStyle.Label}>
+                  <a
+                    className={`${CLASS_NAME}__link`}
+                    href={option.linkToRepository}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Take me to the Github repository
+                  </a>
+                </Text>
+                <Text textStyle={EnumTextStyle.Label}>
+                  <a
+                    className={`${CLASS_NAME}__link`}
+                    href={option.linkToPrismaSchema}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Show me the schema
+                  </a>
+                </Text>
               </FlexItem>
-              <Text
-                textStyle={EnumTextStyle.Description}
-                textColor={EnumTextColor.Black20}
-              >
-                {option.description}
-              </Text>
-              <Text textStyle={EnumTextStyle.Label}>
-                <a
-                  className={`${CLASS_NAME}__link`}
-                  href={option.linkToRepository}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Take me to the Github repository
-                </a>
-              </Text>
-              <Text textStyle={EnumTextStyle.Label}>
-                <a
-                  className={`${CLASS_NAME}__link`}
-                  href={option.linkToPrismaSchema}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Show me the schema
-                </a>
-              </Text>
-            </FlexItem>
-          </Panel>
-        ))}
+            </Panel>
+          ))}
+        </FlexItem>
       </FlexItem>
-    </FlexItem>
+    </>
   );
 };
 
