50ed0400816885139f9888a6ff599e42d355a51a
Amit Barletz <amit@amplication.com>
2024-01-13 19:39:30 +0200

    feat(client): use new mutation and types

--- packages/amplication-client/src/Resource/preview-pages/breaking-the-monolith/monolith-options.ts
+++ packages/amplication-client/src/Resource/preview-pages/breaking-the-monolith/monolith-options.ts
@@ -1,14 +1,18 @@
-export type MonolithOptions = {
-  title: string;
+import { EnumSchemaNames } from "../../../models";
+
+export type MonolithOption = {
+  name: EnumSchemaNames;
+  displayName: string;
   description: string;
   linkToRepository: string;
   linkToPrismaSchema: string;
   pathToPrismaSchema: string;
 };
 
-export const monolithOptions: MonolithOptions[] = [
+export const monolithOptions: MonolithOption[] = [
   {
-    title: "cal.com",
+    name: EnumSchemaNames.CalDotCom,
+    displayName: "Cal.com",
     description: "Scheduling infrastructure for absolutely everyone.",
     linkToRepository: "https://github.com/calcom/cal.com",
     linkToPrismaSchema:
