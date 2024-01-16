fc26e441a8d2e04ece22eb026b24d7240fdcdfeb
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-01-16 15:14:30 +0000

    chore(server):  update prompt input and output types

--- packages/amplication-server/src/core/ai/prompt-manager.service.ts
+++ packages/amplication-server/src/core/ai/prompt-manager.service.ts
@@ -15,33 +15,21 @@ export class PromptManagerService {
       return acc;
     });
 
-    const resourceDetails = {
-      resourceName: resource.name,
-      resourceDisplayName: resource.name,
-      entities: resource.entities.map((entity) => {
+    const prompt: BreakTheMonolithPromptInput = {
+      dataModels: resource.entities.map((entity) => {
         return {
           name: entity.name,
-          displayName: entity.displayName,
           fields: entity.versions[0].fields.map((field) => {
             return {
               name: field.name,
-              displayName: field.displayName,
               dataType:
                 field.dataType == EnumDataType.Lookup
                   ? entityIdNameMap[field.properties["relatedEntityId"]]
                   : field.dataType,
-              relatedDataModel:
-                field.dataType == EnumDataType.Lookup
-                  ? field.properties["relatedEntityId"]
-                  : undefined,
             };
           }),
         };
       }),
-    };
-
-    const prompt: BreakTheMonolithPromptInput = {
-      dataModels: resourceDetails.entities,
     };
 
     return JSON.stringify(prompt);
