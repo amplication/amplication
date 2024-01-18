a31c6018de65b28d1d228dc92efe0a68d146243b
Amit Barletz <amit@amplication.com>
2024-01-18 18:07:31 +0200

    refacotr(client): success state component

--- packages/amplication-client/src/Entity/ImportPrismaSchema/ImportSchemaSuccess.tsx
+++ packages/amplication-client/src/Entity/ImportPrismaSchema/ImportSchemaSuccess.tsx
@@ -1,11 +1,10 @@
 import { EnumContentAlign, FlexItem } from "@amplication/ui/design-system";
-import { UploadSchemaStateProps } from "./uploadSchemaState";
 import { LabelDescriptionSelector } from "../../Resource/create-resource/wizard-pages/LabelDescriptionSelector";
 import { useCallback, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { AppContext } from "../../context/appContext";
 
-export const ImportSchemaSuccess = ({ className }: UploadSchemaStateProps) => {
+export const ImportSchemaSuccess = () => {
   const { currentWorkspace, currentProject, currentResource } =
     useContext(AppContext);
   const history = useHistory();
@@ -20,15 +19,8 @@ export const ImportSchemaSuccess = ({ className }: UploadSchemaStateProps) => {
   }, []);
 
   return (
-    <div className={`${className}__header`}>
-      <h2>Schema import completed successfully!</h2>
-      <div className={`${className}__message`}>
-        Ready to take the next step? Choose your action.
-      </div>
-      <FlexItem
-        contentAlign={EnumContentAlign.Center}
-        className={`${className}__import-success`}
-      >
+    <div>
+      <FlexItem contentAlign={EnumContentAlign.Center}>
         <LabelDescriptionSelector
           name="Go to Entities Screen"
           image=""
