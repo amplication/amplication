50a6d2c091e48e94e674207ff538a5fc278a49cd
Amit Barletz <amit@amplication.com>
2024-01-26 16:44:07 +0200

    feat(client): break the monolith component and loader

--- packages/amplication-client/src/Resource/break-the-monolith/BreakTheMonolith.tsx
+++ packages/amplication-client/src/Resource/break-the-monolith/BreakTheMonolith.tsx
@@ -1,23 +1,103 @@
-import { CircularProgress } from "@amplication/ui/design-system";
-import React, { useContext } from "react";
-import { AppContext } from "../../context/appContext";
+import {
+  Button,
+  EnumFlexDirection,
+  EnumListStyle,
+  EnumTextColor,
+  EnumTextStyle,
+  FlexItem,
+  Icon,
+  List,
+  ListItem,
+  Text,
+} from "@amplication/ui/design-system";
 import { useBtmService } from "./hooks/useBtmService";
+import { BtmLoader } from "./BtmLoader";
+import "./BreakTheMonolith.scss";
 
-const BreakTheMonolith: React.FC = () => {
-  const { currentResource } = useContext(AppContext);
-  const {
-    btmResult: data,
-    loading,
-    error,
-  } = useBtmService({
-    resourceId: currentResource?.id,
+const CLASS_NAME = "break-the-monolith";
+
+type Props = {
+  resourceId: string;
+  handleConfirmSuggestion: () => void;
+  openInModal?: boolean;
+};
+
+const BreakTheMonolith: React.FC<Props> = ({
+  resourceId,
+  openInModal = false,
+  handleConfirmSuggestion,
+}) => {
+  const { btmResult, loading, error } = useBtmService({
+    resourceId,
   });
 
   return (
-    <div>
-      {loading && <CircularProgress />}
-      <div>Status :{data.status}</div>
-      <div>{JSON.stringify(data)}</div>
+    <div className={CLASS_NAME}>
+      {!btmResult?.data && loading ? (
+        <div className={`${CLASS_NAME}__loader`}>
+          <BtmLoader />
+        </div>
+      ) : (
+        <>
+          <div className={`${CLASS_NAME}__content`}>
+            <FlexItem direction={EnumFlexDirection.Column}>
+              <Text textStyle={EnumTextStyle.H3}>
+                Microservices Suggestion by Amplication AI
+              </Text>
+
+              <Text
+                textStyle={EnumTextStyle.H4}
+                textColor={EnumTextColor.Black20}
+              >
+                Our AI has mapped out a sleek microservices architecture for
+                you.
+              </Text>
+              <Text
+                textStyle={EnumTextStyle.Tag}
+                textColor={EnumTextColor.Black20}
+              >
+                Check out the suggested breakdown and start streamlining your
+                system.
+              </Text>
+            </FlexItem>
+            <div className={`${CLASS_NAME}__services`}>
+              {btmResult?.data?.microservices.map((item) => (
+                <List
+                  className={`${CLASS_NAME}__services__service`}
+                  listStyle={EnumListStyle.Dark}
+                  headerContent={
+                    <FlexItem
+                      className={`${CLASS_NAME}__services__service__header`}
+                      start={<Icon icon="settings"></Icon>}
+                    >
+                      {item.name}
+                    </FlexItem>
+                  }
+                >
+                  <ListItem
+                    className={`${CLASS_NAME}__services__service__entities`}
+                  >
+                    {item.dataModels.map((entity) => (
+                      <Text
+                        className={`${CLASS_NAME}__services__service__entities__entity`}
+                        textStyle={EnumTextStyle.Subtle}
+                      >
+                        {entity.name}
+                      </Text>
+                    ))}
+                  </ListItem>
+                </List>
+              ))}
+            </div>
+          </div>
+          <Button
+            className={`${CLASS_NAME}__continue_button`}
+            onClick={handleConfirmSuggestion}
+          >
+            Continue
+          </Button>
+        </>
+      )}
     </div>
   );
 };
---
+++ packages/amplication-client/src/Resource/break-the-monolith/BtmLoader.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  EnumFlexDirection,
+  EnumItemsAlign,
+  FlexItem,
+  Text,
+  Loader,
+  AnimationType,
+  EnumTextStyle,
+  EnumTextAlign,
+  EnumTextColor,
+} from "@amplication/ui/design-system";
+
+const CLASS_NAME = "btm-loader";
+
+export const BtmLoader: React.FC = () => {
+  return (
+    <div className={`${CLASS_NAME}__overlay`}>
+      <FlexItem
+        direction={EnumFlexDirection.Column}
+        itemsAlign={EnumItemsAlign.Center}
+      >
+        <Text textStyle={EnumTextStyle.H3}>
+          Experience the Microservices Marvel using Amplication AI.
+        </Text>
+        <Text
+          textColor={EnumTextColor.Black20}
+          textAlign={EnumTextAlign.Center}
+        >
+          Our AI-driven magic is currently at work, suggesting how to elevate
+          your service and its entities into a thriving microservices wonderland
+        </Text>
+        <Loader animationType={AnimationType.Tiny} />
+      </FlexItem>
+    </div>
+  );
+};
