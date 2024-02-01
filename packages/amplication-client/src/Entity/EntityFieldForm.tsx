6ee9dd232cae9578ba047d939dc242c6aeeb6722
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-02-01 09:45:43 +0000

    chore(client): remove lint ignore comments

--- packages/amplication-client/src/Entity/EntityFieldForm.tsx
+++ packages/amplication-client/src/Entity/EntityFieldForm.tsx
@@ -96,18 +96,18 @@ const EntityFieldForm = ({
         );
         //validate the field dynamic properties
         const schema = getSchemaForDataType(values.dataType);
-        // eslint-disable-next-line @typescript-eslint/ban-types
-        const propertiesError = validate<Object>(values.properties, schema);
+        const propertiesError = validate<{
+          relatedEntityId?: string;
+          allowMultipleSelection?: string;
+        }>(values.properties, schema);
 
         // Ignore related field ID error
         if ("relatedFieldId" in propertiesError) {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
           delete propertiesError.relatedFieldId;
         }
 
         if (!isEmpty(propertiesError)) {
-          errors.properties = propertiesError as any; // TODO: remove eslint rules and fix this
+          errors.properties = propertiesError;
         }
 
         return errors;
