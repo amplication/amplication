ffc972169125243582a39f174bce26a5599e91c1
Amit Barletz <amit@amplication.com>
2024-02-06 18:42:09 +0200

    refactor(client): handle new schemes

--- packages/amplication-client/src/Resource/preview-pages/breaking-the-monolith/monolith-options.ts
+++ packages/amplication-client/src/Resource/preview-pages/breaking-the-monolith/monolith-options.ts
@@ -11,7 +11,27 @@ export const monolithOptions: MonolithOption[] = [
   {
     name: EnumSchemaNames.CalDotCom,
     displayName: "Cal.com",
-    description: "Scheduling infrastructure for absolutely everyone.",
+    description: "The open-source Calendly successor.",
     linkToRepository: "https://github.com/calcom/cal.com",
+  },
+  {
+    name: EnumSchemaNames.NextCrmApp,
+    displayName: "Next CRM App",
+    description:
+      "NextCRM is a CRM build on top of the Next.JS 14 using TypeScript, great UI library shadcn, Prisma and MongoDB as a database",
+    linkToRepository: "https://github.com/pdovhomilja/nextcrm-app",
+  },
+  {
+    name: EnumSchemaNames.Abby,
+    displayName: "Abby",
+    description: "an open source feature management and A/B testing platform",
+    linkToRepository: "https://github.com/tryabby/abby",
+  },
+  {
+    name: EnumSchemaNames.WebStudio,
+    displayName: "Web Studio",
+    description:
+      "Webstudio is an Open Source Visual Development Platform for developers, designers, and cross-functional teams.",
+    linkToRepository: "https://github.com/webstudio-is/webstudio",
   },
 ];
