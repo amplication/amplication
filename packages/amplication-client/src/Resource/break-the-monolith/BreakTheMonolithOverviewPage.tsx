2f8afa1e2a063edb943b7aa9de9939395a0fd596
Amit Barletz <amit@amplication.com>
2024-02-09 18:40:14 +0200

    feat(client): btm overview style

--- packages/amplication-client/src/Resource/break-the-monolith/BreakTheMonolithOverviewPage.tsx
+++ packages/amplication-client/src/Resource/break-the-monolith/BreakTheMonolithOverviewPage.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { match } from "react-router-dom";
-import { Modal } from "@amplication/ui/design-system";
+import {
+  Text,
+  EnumFlexDirection,
+  EnumItemsAlign,
+  EnumTextStyle,
+  FlexItem,
+  Modal,
+  EnumTextColor,
+  EnumTextWeight,
+  Button,
+} from "@amplication/ui/design-system";
 import { AppRouteProps } from "../../routes/routesUtil";
 import ModelOrganizer from "../../Project/ArchitectureConsole/ModelOrganizer";
+
+import "./BreakTheMonolithOverviewPage.scss";
+const CLASS_NAME = "break-the-monolith-overview-page";
 
 type Props = AppRouteProps & {
   match: match<{
@@ -19,9 +32,46 @@ const BreakTheMonolithOptionsPage: React.FC<Props> = ({ match }) => {
     resource: resourceId,
   } = match.params;
 
+  const handleBreakTheMonolithClicked = useCallback(() => {
+    // call ai
+    // prepare data for useModelOrganizer
+    // redirect to architecture page in redesign mode
+  }, []);
+
   return (
     <Modal open fullScreen>
-      <ModelOrganizer />
+      <FlexItem
+        className={CLASS_NAME}
+        direction={EnumFlexDirection.Column}
+        itemsAlign={EnumItemsAlign.Center}
+      >
+        <div className={`${CLASS_NAME}__instructions`}>
+          <Text
+            textStyle={EnumTextStyle.H2}
+            textWeight={EnumTextWeight.Bold}
+            className={`${CLASS_NAME}__title`}
+          >
+            Breaking the chosen monolith
+          </Text>
+          <Text textColor={EnumTextColor.Black20}>
+            Explore the entities of the selected open-source monolith. When
+            ready to transform into microservices architecture, simply press the
+            button.
+          </Text>
+          <Text textColor={EnumTextColor.Black20}>
+            Note that Amplication can then generate the code for the
+            architecture change, and push the code to your git provider" Button:
+            "Break the monolith.
+          </Text>
+          <Button
+            className={`${CLASS_NAME}__break-btn`}
+            onClick={handleBreakTheMonolithClicked}
+          >
+            Break the monolith
+          </Button>
+        </div>
+        <ModelOrganizer />
+      </FlexItem>
     </Modal>
   );
 };
