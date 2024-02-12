033a13dbe3495d5fc53544b09e66d710ad77b14b
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2024-02-12 09:56:36 +0000

    chore(server): fix subscription upgrade tracking event

--- packages/amplication-server/src/core/subscription/subscription.service.ts
+++ packages/amplication-server/src/core/subscription/subscription.service.ts
@@ -84,12 +84,18 @@ export class SubscriptionService {
     workspaceId: string
   ): Promise<void> {
     if (plan !== EnumSubscriptionPlan.Free && userId) {
-      await this.analyticsService.trackWithContext({
-        properties: {
-          workspaceId: workspaceId,
-          $groups: { groupWorkspace: workspaceId },
+      await this.analyticsService.trackManual({
+        user: {
+          accountId: userId,
+          workspaceId,
         },
-        event: EnumEventType.WorkspacePlanUpgradeCompleted,
+        data: {
+          properties: {
+            workspaceId: workspaceId,
+            $groups: { groupWorkspace: workspaceId },
+          },
+          event: EnumEventType.WorkspacePlanUpgradeCompleted,
+        },
       });
     }
   }
