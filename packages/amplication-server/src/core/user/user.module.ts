73bffb391a82b2e218a6173115a40114280770b7
mulygottlieb <muly@amplication.com>
2024-02-13 15:26:03 +0200

    chore(server): fix tests

--- packages/amplication-server/src/core/user/user.module.ts
+++ packages/amplication-server/src/core/user/user.module.ts
@@ -6,9 +6,16 @@ import { PermissionsModule } from "../permissions/permissions.module";
 import { KafkaModule } from "@amplication/util/nestjs/kafka";
 import { BillingModule } from "../billing/billing.module";
 import { UserController } from "./user.controller";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [PrismaModule, PermissionsModule, KafkaModule, BillingModule],
+  imports: [
+    PrismaModule,
+    PermissionsModule,
+    KafkaModule,
+    BillingModule,
+    ConfigModule,
+  ],
   controllers: [UserController],
   providers: [UserService, UserResolver],
   exports: [UserService, UserResolver],
