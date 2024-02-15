82914e3e113a076bf525256f7ef0cd93477f4697
yuval-hazaz <hazaz.yuval@gmail.com>
2024-02-15 09:45:33 +0200

    feat(client):remove fake data

--- packages/amplication-client/src/Resource/break-the-monolith/hooks/useBtmService.ts
+++ packages/amplication-client/src/Resource/break-the-monolith/hooks/useBtmService.ts
@@ -22,7 +22,6 @@ export type TBreakServiceToMicroservicesResult = {
 
 type BtmProps = {
   resourceId: string;
-  useFakeData?: boolean;
 };
 
 export const useBtmService = ({ resourceId }: BtmProps) => {
