687fbd1bbe59e94e346cb3828ced7689d99f1938
yuval-hazaz <hazaz.yuval@gmail.com>
2024-02-17 10:23:24 +0200

    refactor(client):use normal loading for the design system

--- packages/amplication-client/src/User/SignupPreviewAccount.tsx
+++ packages/amplication-client/src/User/SignupPreviewAccount.tsx
@@ -2,16 +2,7 @@ import { formatError } from "../util/error";
 import "./Signup.scss";
 import useSignupPreviewAccount from "./hooks/useSignupPreviewAccount";
 import { EnumPreviewAccountType } from "../models";
-import { lazy } from "react";
-
-//use specific import path to prevent inclusion of all the design-system CSS in the main bundle
-import { AnimationType } from "@amplication/ui/design-system/components/Loader/Loader";
-
-//use lazy loading imports to prevent inclusion of the components CSS in the main bundle
-const FullScreenLoader = lazy(
-  () =>
-    import("@amplication/ui/design-system/components/Loader/FullScreenLoader")
-);
+import { AnimationType, FullScreenLoader } from "@amplication/ui/design-system";
 
 const SignupPreviewAccount = () => {
   const queryString = window.location.search;
