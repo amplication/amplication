d42fd2f66e75fd6d6e6ffc702bc9ccf377c884c3
yuval-hazaz <hazaz.yuval@gmail.com>
2024-02-18 18:03:07 +0200

    fix(client):revert panel changes for clickable footer

--- libs/ui/design-system/src/lib/components/Panel/Panel.stories.tsx
+++ libs/ui/design-system/src/lib/components/Panel/Panel.stories.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Meta } from "@storybook/react";
 import { Panel, EnumPanelStyle, PanelHeader } from "./Panel";
 import { Button, EnumButtonStyle } from "../Button/Button";
-import { EnumTextColor, Text } from "../Text/Text";
-import { FlexItem } from "../FlexItem/FlexItem";
+import { EnumTextColor } from "../Text/Text";
 
 export default {
   title: "Panel",
@@ -107,39 +106,6 @@ export const WithContentHeader = {
   render: (props: any) => {
     return (
       <Panel {...props}>
-        <PanelHeader>
-          <h3>Panel Title</h3>
-          <Button buttonStyle={EnumButtonStyle.Text} icon="edit" />
-        </PanelHeader>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </Panel>
-    );
-  },
-};
-
-export const WithNonClickableFooter = {
-  render: (props: any) => {
-    return (
-      <Panel
-        {...props}
-        nonClickableFooter={
-          <FlexItem
-            end={
-              <Text textColor={EnumTextColor.ThemeTurquoise}>
-                Non-Clickable Footer
-              </Text>
-            }
-          ></FlexItem>
-        }
-      >
         <PanelHeader>
           <h3>Panel Title</h3>
           <Button buttonStyle={EnumButtonStyle.Text} icon="edit" />
