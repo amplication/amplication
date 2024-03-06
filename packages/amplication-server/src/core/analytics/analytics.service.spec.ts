73e09996df3cec016bbfe92a5a8b08f5a339dac0
Amit Barletz <amit@amplication.com>
2024-03-06 17:13:43 +0200

    test(server): add tests

---
+++ packages/amplication-server/src/core/analytics/analytics.service.spec.ts
@@ -0,0 +1,193 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { MockedAmplicationLoggerProvider } from "@amplication/util/nestjs/logging/test-utils";
+import { AnalyticsService } from "./analytics.service";
+import { Project, Resource, Workspace } from "../../models";
+import { Build } from "../build/dto/Build";
+import { PrismaService } from "../../prisma";
+
+const buildFindManyMock = jest.fn();
+
+describe("AnalyticsService", () => {
+  let service: AnalyticsService;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AnalyticsService,
+        MockedAmplicationLoggerProvider,
+        {
+          provide: PrismaService,
+          useValue: {
+            build: {
+              findMany: buildFindManyMock,
+            },
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<AnalyticsService>(AnalyticsService);
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  xit("should count the lines of code added/updated for a given build", () => {
+    // Not implemented
+  });
+
+  describe("when calculating project analytics", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.restoreAllMocks();
+    });
+
+    const now = new Date();
+
+    const buildMock: Build = {
+      id: "build-id",
+      createdAt: now,
+      resourceId: "resource-id",
+      userId: "user-id",
+      version: "",
+      actionId: "",
+      commitId: "",
+    };
+
+    const resourceMock: Resource = {
+      id: "resource-id",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      name: "resource-name",
+      description: "resource-description",
+      project: {
+        id: "project-id",
+        workspaceId: "workspace-id",
+      } as unknown as Project,
+      builds: [buildMock],
+      resourceType: "Service",
+      gitRepositoryOverride: false,
+      licensed: false,
+    };
+    const projectMock: Project = {
+      id: "project-id",
+      name: "project-name",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      useDemoRepo: false,
+      licensed: true,
+      workspaceId: "workspace-id",
+      resources: [resourceMock],
+    };
+    const workspaceMock: Workspace = {
+      id: "workspace-id",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      name: "workspace-name",
+      users: [],
+      projects: [projectMock],
+      allowLLMFeatures: false,
+    };
+    describe("when calculating a specific project analytics", () => {
+      it("should get the number of builds for a given project in a specific time frame", async () => {
+        buildFindManyMock.mockResolvedValueOnce([buildMock]);
+        const result = await service.countProjectBuilds({
+          workspaceId: workspaceMock.id,
+          startDate: new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+          ),
+          endDate: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+          projectId: projectMock.id,
+        });
+
+        expect(buildFindManyMock).toBeCalledWith({
+          where: {
+            createdAt: {
+              gte: new Date(
+                now.getFullYear(),
+                now.getMonth() - 1,
+                now.getDate()
+              ),
+              lte: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+            },
+            resource: {
+              project: {
+                id: projectMock.id,
+                workspaceId: workspaceMock.id,
+              },
+            },
+          },
+        });
+
+        expect(result).toBeDefined();
+      });
+
+      xit("should get the number of entities created/updated for a given project in a specific time frame", () => {
+        // Not implemented
+      });
+
+      xit("should get the number of APIs created/updated for a given project in a specific time frame", () => {
+        // Not implemented
+      });
+
+      xit("should get the number of Plugins created/updated for a given project in a specific time frame", () => {
+        // Not implemented
+      });
+    });
+    describe("when calculating the total project analytics", () => {
+      it("should get the number of builds in a specific time frame", async () => {
+        buildFindManyMock.mockResolvedValueOnce([buildMock]);
+        const result = await service.countProjectBuilds({
+          workspaceId: workspaceMock.id,
+          startDate: new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate() - 14
+          ),
+          endDate: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+        });
+
+        expect(buildFindManyMock).toBeCalledWith({
+          where: {
+            createdAt: {
+              gte: new Date(
+                now.getFullYear(),
+                now.getMonth(),
+                now.getDate() - 14
+              ),
+              lte: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+            },
+            resource: {
+              project: {
+                id: undefined,
+                workspaceId: workspaceMock.id,
+              },
+            },
+          },
+        });
+
+        expect(result).toBeDefined();
+      });
+
+      xit("should get the number of entities created/updated in a specific time frame", () => {
+        // Not implemented
+      });
+
+      xit("should get the number of APIs created/updated in a specific time frame", () => {
+        // Not implemented
+      });
+
+      xit("should get the number of Plugins created/updated in a specific time frame", () => {
+        // Not implemented
+      });
+    });
+  });
+});
