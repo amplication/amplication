a8aa50b38cc486e41769494814360822ee234365
yuval-hazaz <hazaz.yuval@gmail.com>
2024-03-09 16:41:08 +0200

    feat(client):plugin categories

--- packages/amplication-client/src/Resource/hooks/categoriesQueries.ts
+++ packages/amplication-client/src/Resource/hooks/categoriesQueries.ts
@@ -3,8 +3,11 @@ import { gql } from "@apollo/client";
 export const GET_CATEGORIES = gql`
   query {
     categories(orderBy: { rank: Asc }) {
+      id
       name
       rank
+      description
+      icon
     }
   }
 `;
