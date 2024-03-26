1ed09d5b0dd8fc9ac619cf8668396b47a68ad510
Mor Hagbi <mor@amplication.com>
2024-03-26 19:03:42 +0200

    refactor(DSG): delete prisma from service.template constructor

--- packages/data-service-generator/src/server/custom-module/custom-service/service.template.ts
+++ packages/data-service-generator/src/server/custom-module/custom-service/service.template.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class SERVICE {
-  constructor(protected readonly prisma: PrismaService) {}
+  constructor() {}
 }
