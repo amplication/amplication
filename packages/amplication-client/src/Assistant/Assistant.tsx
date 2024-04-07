0440ea365a7c4590ce7852eae0b244aaba00c7c9
yuval-hazaz <hazaz.yuval@gmail.com>
2024-04-07 19:02:33 +0300

    style(client):assistant width

--- packages/amplication-client/src/Assistant/Assistant.tsx
+++ packages/amplication-client/src/Assistant/Assistant.tsx
@@ -1,4 +1,11 @@
-import { TextField } from "@amplication/ui/design-system";
+import {
+  EnumTextColor,
+  EnumTextStyle,
+  Icon,
+  Text,
+  TextField,
+  Tooltip,
+} from "@amplication/ui/design-system";
 import { Form, Formik } from "formik";
 import { useCallback, useState } from "react";
 import { GlobalHotKeys } from "react-hotkeys";
@@ -16,6 +23,7 @@ type SendMessageType = models.SendAssistantMessageInput;
 const INITIAL_VALUES: SendMessageType = {
   message: "",
 };
+const DIRECTION = "sw";
 
 const CLASS_NAME = "assistant";
 
@@ -23,10 +31,30 @@ const keyMap = {
   SUBMIT: CROSS_OS_CTRL_ENTER,
 };
 
+const WIDTH_STATE_DEFAULT = "default";
+const WIDTH_STATE_WIDE = "wide";
+
+const WIDTH_STATE_SETTINGS: Record<
+  string,
+  { icon: string; tooltip: string; nextState: string }
+> = {
+  [WIDTH_STATE_DEFAULT]: {
+    icon: "chevrons_right",
+    tooltip: "Increase Size",
+    nextState: WIDTH_STATE_WIDE,
+  },
+  [WIDTH_STATE_WIDE]: {
+    icon: "chevrons_left",
+    tooltip: "Restore Size",
+    nextState: WIDTH_STATE_DEFAULT,
+  },
+};
+
 const Assistant = () => {
   const { trackEvent } = useTracking();
 
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
+  const [widthState, setWidthState] = useState(WIDTH_STATE_DEFAULT);
 
   const {
     sendMessage,
@@ -44,55 +72,107 @@ const Assistant = () => {
   );
 
   return (
-    <div className={classNames(CLASS_NAME, { [`${CLASS_NAME}--open`]: open })}>
-      <div className={`${CLASS_NAME}__messages`}>
-        {messages.map((message) => (
-          <AssistantMessage
-            key={message.id}
-            message={message}
-            onOptionClick={sendMessage}
-          />
-        ))}
-        {loading && (
-          <div className={`${CLASS_NAME}__message`}>
-            <ReactMarkdown>Thinking...</ReactMarkdown>
-          </div>
+    <>
+      {!open && (
+        <div className={`${CLASS_NAME}__handle`} onClick={() => setOpen(true)}>
+          <Icon icon="ai" color={EnumTextColor.White} size="large" />
+          <Text className={`${CLASS_NAME}__title`} textStyle={EnumTextStyle.H4}>
+            Jovu
+          </Text>
+        </div>
+      )}
+      <div
+        className={classNames(
+          CLASS_NAME,
+          { [`${CLASS_NAME}--close`]: !open },
+          `${CLASS_NAME}--${widthState}`
         )}
-        {error && <div className={`${CLASS_NAME}__error`}>{error.message}</div>}
-      </div>
+      >
+        <div className={`${CLASS_NAME}__header`}>
+          <Icon icon="ai" color={EnumTextColor.White} size="large" />
+          <Text className={`${CLASS_NAME}__title`} textStyle={EnumTextStyle.H4}>
+            Jovu
+          </Text>
+          <Tooltip
+            aria-label={WIDTH_STATE_SETTINGS[widthState].tooltip}
+            direction={DIRECTION}
+            noDelay
+          >
+            <Button
+              buttonStyle={EnumButtonStyle.Text}
+              onClick={() => {
+                setWidthState(WIDTH_STATE_SETTINGS[widthState].nextState);
+              }}
+            >
+              <Icon
+                icon={WIDTH_STATE_SETTINGS[widthState].icon}
+                size="small"
+              ></Icon>
+            </Button>
+          </Tooltip>
+          <Tooltip aria-label="Hide" direction={DIRECTION} noDelay>
+            <Button
+              buttonStyle={EnumButtonStyle.Text}
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              <Icon icon={"close"} size="small"></Icon>
+            </Button>
+          </Tooltip>
+        </div>
 
-      <div className={`${CLASS_NAME}__chat_input`}>
-        <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
-          {(formik) => {
-            const handlers = {
-              SUBMIT: formik.submitForm,
-            };
-            return (
-              <Form>
-                <GlobalHotKeys keyMap={keyMap} handlers={handlers} />
-                <TextField
-                  textarea
-                  name="message"
-                  label="How can I help you?"
-                  disabled={loading}
-                  autoFocus
-                  autoComplete="off"
-                  hideLabel
-                  rows={2}
-                />
-                <Button
-                  type="submit"
-                  buttonStyle={EnumButtonStyle.Primary}
-                  disabled={loading}
-                >
-                  Send
-                </Button>
-              </Form>
-            );
-          }}
-        </Formik>
+        <div className={`${CLASS_NAME}__messages`}>
+          {messages.map((message) => (
+            <AssistantMessage
+              key={message.id}
+              message={message}
+              onOptionClick={sendMessage}
+            />
+          ))}
+          {loading && (
+            <div className={`${CLASS_NAME}__message`}>
+              <ReactMarkdown>Thinking...</ReactMarkdown>
+            </div>
+          )}
+          {error && (
+            <div className={`${CLASS_NAME}__error`}>{error.message}</div>
+          )}
+        </div>
+
+        <div className={`${CLASS_NAME}__chat_input`}>
+          <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
+            {(formik) => {
+              const handlers = {
+                SUBMIT: formik.submitForm,
+              };
+              return (
+                <Form>
+                  <GlobalHotKeys keyMap={keyMap} handlers={handlers} />
+                  <TextField
+                    textarea
+                    name="message"
+                    label="How can I help you?"
+                    disabled={loading}
+                    autoFocus
+                    autoComplete="off"
+                    hideLabel
+                    rows={2}
+                  />
+                  <Button
+                    type="submit"
+                    buttonStyle={EnumButtonStyle.Primary}
+                    disabled={loading}
+                  >
+                    Send
+                  </Button>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
