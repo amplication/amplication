cc058972b13c262abcd0a4964153fa199ce0b1de
yuval-hazaz <hazaz.yuval@gmail.com>
2024-04-30 18:30:16 +0300

    style(client):dotnet page

--- packages/amplication-client/src/VersionControl/DotNetPromoteStartupOrEnterpriseIOption.tsx
+++ packages/amplication-client/src/VersionControl/DotNetPromoteStartupOrEnterpriseIOption.tsx
@@ -1,29 +1,67 @@
 import "./DotNetPromoteOptions.scss";
-import { EnumTextColor, Text } from "@amplication/ui/design-system";
+import {
+  Button,
+  EnumButtonStyle,
+  EnumFlexDirection,
+  EnumGapSize,
+  EnumItemsAlign,
+  EnumPanelStyle,
+  EnumTextAlign,
+  EnumTextColor,
+  EnumTextStyle,
+  FlexItem,
+  Icon,
+  Panel,
+  Text,
+} from "@amplication/ui/design-system";
 
 type Props = {
   contactLink: string;
 };
 
+const CLASS_NAME = "dotnet-promote-options";
+
 export const DotNetPromoteStartupOrEnterpriseIOption = ({
   contactLink,
 }: Props) => {
   return (
-    <div>
-      To explore our new product capabilities, including lightning-fast backend
-      code generation, and gain early access, please schedule a demo with us.
-      <br />
-      Unfortunately, we encounter limited demo slots due to high demand.
-      <br />
-      Apologies for any inconvenience.
-      <br />
-      <br />
-      We look forward to showcasing how Amplication can accelerate your
-      startup's development journey
-      <br />
+    <FlexItem
+      direction={EnumFlexDirection.Column}
+      itemsAlign={EnumItemsAlign.Center}
+      gap={EnumGapSize.Large}
+    >
+      <Text textStyle={EnumTextStyle.Normal} textAlign={EnumTextAlign.Center}>
+        To explore our new product capabilities, including lightning-fast
+        backend code generation, and gain early access, please schedule a demo
+        with us.
+      </Text>
+      <Text
+        textStyle={EnumTextStyle.Normal}
+        textColor={EnumTextColor.White}
+        textAlign={EnumTextAlign.Center}
+      >
+        We look forward to showcasing how Amplication can accelerate your
+        startup's development journey
+      </Text>
+
       <a href={contactLink} target="blank">
-        <Text textColor={EnumTextColor.ThemeTurquoise}>{"Contact us"}</Text>{" "}
+        <Button
+          buttonStyle={EnumButtonStyle.Primary}
+          className={`${CLASS_NAME}__demo`}
+        >
+          <Icon icon="calendar" size="small" />
+          {"Schedule a demo"}
+        </Button>
       </a>
-    </div>
+      <Panel panelStyle={EnumPanelStyle.Bordered}>
+        <Text
+          textStyle={EnumTextStyle.Description}
+          textColor={EnumTextColor.ThemeOrange}
+        >
+          Unfortunately, we encounter limited demo slots due to high demand.
+          Apologies for any inconvenience.
+        </Text>
+      </Panel>
+    </FlexItem>
   );
 };
