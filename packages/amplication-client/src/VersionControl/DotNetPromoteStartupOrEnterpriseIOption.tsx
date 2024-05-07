435d34b19bb2cb86774cbe36f3ed74ea9cf67e39
mulygottlieb <muly@amplication.com>
2024-05-07 19:44:20 +0300

    fix(client): fix dotnet promotion text for enterprise

--- packages/amplication-client/src/VersionControl/DotNetPromoteStartupOrEnterpriseIOption.tsx
+++ packages/amplication-client/src/VersionControl/DotNetPromoteStartupOrEnterpriseIOption.tsx
@@ -43,14 +43,25 @@ export const DotNetPromoteStartupOrEnterpriseIOption = ({
         backend code generation, and gain early access, please schedule a demo
         with us.
       </Text>
-      <Text
-        textStyle={EnumTextStyle.Normal}
-        textColor={EnumTextColor.White}
-        textAlign={EnumTextAlign.Center}
-      >
-        We look forward to showcasing how Amplication can accelerate your
-        startup's development journey
-      </Text>
+      {type === "startup" ? (
+        <Text
+          textStyle={EnumTextStyle.Normal}
+          textColor={EnumTextColor.White}
+          textAlign={EnumTextAlign.Center}
+        >
+          We look forward to showcasing how Amplication can accelerate your
+          startup's development journey
+        </Text>
+      ) : (
+        <Text
+          textStyle={EnumTextStyle.Normal}
+          textColor={EnumTextColor.White}
+          textAlign={EnumTextAlign.Center}
+        >
+          We look forward to showcasing how Amplication can accelerate your
+          enterprise's development journey
+        </Text>
+      )}
 
       <a href={contactLink} target="blank">
         <Button
