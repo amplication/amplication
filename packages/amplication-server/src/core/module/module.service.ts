ed880ec3c08483f926460ff93e8884228f0518ac
yuval-hazaz <hazaz.yuval@gmail.com>
2024-05-14 18:36:19 +0300

    chore(server):remove redundant event

--- packages/amplication-server/src/core/module/module.service.ts
+++ packages/amplication-server/src/core/module/module.service.ts
@@ -73,18 +73,18 @@ export class ModuleService extends BlockTypeService<
       },
     };
 
-    if (user) {
-      const subscription = await this.billingService.getSubscription(
-        user.workspace?.id
-      );
+    // if (user) {
+    //   const subscription = await this.billingService.getSubscription(
+    //     user.workspace?.id
+    //   );
 
-      await this.analytics.trackWithContext({
-        properties: {
-          planType: subscription.subscriptionPlan,
-        },
-        event: EnumEventType.SearchAPIs,
-      });
-    }
+    //   await this.analytics.trackWithContext({
+    //     properties: {
+    //       planType: subscription.subscriptionPlan,
+    //     },
+    //     event: EnumEventType.SearchAPIs,
+    //   });
+    // }
 
     return super.findMany(prismaArgs);
   }
