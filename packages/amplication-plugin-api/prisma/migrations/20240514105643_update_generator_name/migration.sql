d5c9e972d39ca0858642d8faea809763d5792ab1
yuval-hazaz <hazaz.yuval@gmail.com>
2024-06-02 13:53:00 +0300

    feat(plugin-api):create migration for codeGeneratorName

--- packages/amplication-plugin-api/prisma/migrations/20240514105643_update_generator_name/migration.sql
+++ packages/amplication-plugin-api/prisma/migrations/20240514105643_update_generator_name/migration.sql
@@ -5,8 +5,8 @@
 
 */
 -- AlterTable
-ALTER TABLE "Plugin" ALTER COLUMN "codeGeneratorName" SET NULL;
-UPDATE "Plugin" SET "codeGeneratorName" = NULL
+-- ALTER TABLE "Plugin" ALTER COLUMN "codeGeneratorName" SET NULL;
+-- UPDATE "Plugin" SET "codeGeneratorName" = NULL;
 
 ALTER TABLE "Plugin" DROP COLUMN "codeGeneratorName",
 ADD COLUMN     "codeGeneratorNames" JSONB DEFAULT '["data-service-generator"]';
