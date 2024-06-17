180975c2baa39057561cb11a25343cf1cc957fb9
Amit Barletz <amit@amplication.com>
2024-06-17 14:56:27 +0300

    feat(server): update the commit create input with the commit strategy and resource ids to commit and build

--- packages/amplication-server/src/core/resource/dto/CommitCreateInput.ts
+++ packages/amplication-server/src/core/resource/dto/CommitCreateInput.ts
@@ -1,5 +1,6 @@
 import { Field, InputType } from "@nestjs/graphql";
 import { WhereParentIdInput } from "../../../dto";
+import { EnumCommitStrategy } from "./EnumCommitStrategy";
 
 @InputType({
   isAbstract: true,
@@ -22,6 +23,23 @@ export class CommitCreateInput {
       "It will bypass the limitations of the plan (if any). It will only work for limitation that support commit bypass.",
   })
   bypassLimitations? = false;
+
+  @Field(() => EnumCommitStrategy, {
+    nullable: true,
+    description:
+      "The strategy to use when committing the changes. If not provided, the default strategy will be used.",
+    defaultValue: EnumCommitStrategy.All,
+  })
+  commitStrategy?: EnumCommitStrategy;
+
+  @Field(() => [String], {
+    nullable: true,
+    description: `The resources to commit. By default, it contains all the project resources. 
+      If the commit strategy is AllWithPendingChanges, the server will . 
+      If the commit strategy is Specific, it will be an array with one element. 
+      For on boarding only the newly created service will be committed.`,
+  })
+  resourceIds?: string[];
 
   /**do not expose to GraphQL - This field should be injected from context  */
   user!: WhereParentIdInput;
