3c9f406a838b87ce1e3d2d5dedcdfde6b1354f3e
yuval-hazaz <hazaz.yuval@gmail.com>
2024-11-23 07:51:03 +0100

    feat(client):exclude the project resources from the catalog

--- packages/amplication-client/src/Catalog/ResourceTypeFilter.tsx
+++ packages/amplication-client/src/Catalog/ResourceTypeFilter.tsx
@@ -3,15 +3,18 @@ import {
   DataGridRenderFilterProps,
 } from "@amplication/ui/design-system";
 import { resourceThemeMap } from "../Resource/constants";
+import { EnumResourceType } from "../models";
 
-const OPTIONS = Object.keys(resourceThemeMap).map((key) => {
-  const theme = resourceThemeMap[key];
-  return {
-    value: key,
-    label: theme.name,
-    color: theme.color,
-  };
-});
+const OPTIONS = Object.keys(resourceThemeMap)
+  .filter((type) => type !== EnumResourceType.ProjectConfiguration)
+  .map((key) => {
+    const theme = resourceThemeMap[key];
+    return {
+      value: key,
+      label: theme.name,
+      color: theme.color,
+    };
+  });
 
 export const ResourceTypeFilter = ({
   selectedValue,
