6c2dbb122f4ff6833ed07249b5a7cc3bc7b24d34
yuval-hazaz <hazaz.yuval@gmail.com>
2024-12-06 18:52:45 +0200

    fix(server):add missing type

--- packages/amplication-server/src/core/resourceSettings/dto/ResourceSettingsUpdateInput.ts
+++ packages/amplication-server/src/core/resourceSettings/dto/ResourceSettingsUpdateInput.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from "@nestjs/graphql";
 import { BlockUpdateInput } from "../../block/dto/BlockUpdateInput";
 import { GraphQLJSONObject } from "graphql-type-json";
-import { JsonValue } from "type-fest";
+import type { JsonValue } from "type-fest";
 
 @InputType({
   isAbstract: true,
