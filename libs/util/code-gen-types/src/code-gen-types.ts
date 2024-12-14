7c02931ba844d7fb0911fb954d1a14cd03444b20
yuval-hazaz <hazaz.yuval@gmail.com>
2024-12-14 09:50:01 +0200

    chore(dsg-types):release version 3.1.0 with resource (catalog) properties

--- libs/util/code-gen-types/src/code-gen-types.ts
+++ libs/util/code-gen-types/src/code-gen-types.ts
@@ -4,6 +4,7 @@ import { Lookup, MultiSelectOptionSet, OptionSet } from "./types";
 import { DSGResourceData } from "./dsg-resource-data";
 import { BuildLogger } from "./build-logger";
 import { FileMap, IFile } from "./files";
+import { JsonValue } from "type-fest";
 
 export {
   EnumDataType,
@@ -38,6 +39,7 @@ export type AppInfo = {
   settings: ServiceSettings;
   codeGeneratorVersionOptions: models.CodeGeneratorVersionOptionsInput;
   codeGeneratorName?: string;
+  properties?: JsonValue;
 };
 
 export type Role = Omit<
