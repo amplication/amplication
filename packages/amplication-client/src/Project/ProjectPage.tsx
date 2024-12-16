497d71b1f56617dd7a4b92a032792f76c0e9783c
yuval-hazaz <hazaz.yuval@gmail.com>
2024-12-16 08:32:09 +0200

    feat(client):project selector

--- packages/amplication-client/src/Project/ProjectPage.tsx
+++ packages/amplication-client/src/Project/ProjectPage.tsx
@@ -8,7 +8,6 @@ import React, {
 } from "react";
 import { match } from "react-router-dom";
 import PageLayout from "../Layout/PageLayout";
-import useBreadcrumbs from "../Layout/useBreadcrumbs";
 import useTabRoutes from "../Layout/useTabRoutes";
 import ResourceList from "../Workspaces/ResourceList";
 import { AppContext } from "../context/appContext";
@@ -32,7 +31,7 @@ const ProjectPage: React.FC<Props> = ({
   tabRoutes,
   tabRoutesDef,
 }) => {
-  const { currentProject, pendingChanges } = useContext(AppContext);
+  const { pendingChanges } = useContext(AppContext);
   const [fromPreviewUserDialog, setFromPreviewUserDialog] =
     useState<boolean>(false);
 
@@ -49,8 +48,6 @@ const ProjectPage: React.FC<Props> = ({
   const handleFromPreviewUserDialogDismiss = useCallback(() => {
     setFromPreviewUserDialog(false);
   }, [setFromPreviewUserDialog]);
-
-  useBreadcrumbs(currentProject?.name, match.url);
 
   const { tabs, currentRouteIsTab } = useTabRoutes(tabRoutesDef);
 
