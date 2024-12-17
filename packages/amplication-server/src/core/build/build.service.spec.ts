59cb1f8b1be42733beeeb4a9f723615f495e15e9
yuval-hazaz <hazaz.yuval@gmail.com>
2024-12-17 13:15:57 +0200

    feat(server):download plugins from multiple repos

--- packages/amplication-server/src/core/build/build.service.spec.ts
+++ packages/amplication-server/src/core/build/build.service.spec.ts
@@ -586,17 +586,21 @@ describe("BuildService", () => {
             resourceId: EXAMPLE_RESOURCE_ID,
           },
           value: {
-            ...EXAMPLE_GIT_SETTINGS,
             buildId: EXAMPLE_BUILD_ID,
             resourceId: EXAMPLE_RESOURCE_ID,
-            pluginsToDownload: [
+            repositoryPlugins: [
               {
-                pluginId: EXAMPLE_PRIVATE_PLUGIN_ID,
-                pluginVersion: "1.1.0",
-              },
-              {
-                pluginId: EXAMPLE_PRIVATE_PLUGIN_ID_2,
-                pluginVersion: "1.0.0",
+                ...EXAMPLE_GIT_SETTINGS,
+                pluginsToDownload: [
+                  {
+                    pluginId: EXAMPLE_PRIVATE_PLUGIN_ID,
+                    pluginVersion: "1.1.0",
+                  },
+                  {
+                    pluginId: EXAMPLE_PRIVATE_PLUGIN_ID_2,
+                    pluginVersion: "1.0.0",
+                  },
+                ],
               },
             ],
           },
