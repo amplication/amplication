fd044857a11ddcab53330ca779a0d3243e496da3
yuval-hazaz <hazaz.yuval@gmail.com>
2025-01-03 12:17:44 +0200

    test(server):add coverage for permissions service

--- packages/amplication-server/src/core/permissions/permissions.service.spec.ts
+++ packages/amplication-server/src/core/permissions/permissions.service.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PermissionsService } from "./permissions.service";
 import { PrismaService } from "../../prisma/prisma.service";
-import { Account, Workspace } from "../../models";
+import { Account, Resource, Workspace } from "../../models";
 import { AuthorizableOriginParameter } from "../../enums/AuthorizableOriginParameter";
 import { AuthUser } from "../auth/types";
 import { MockedAmplicationLoggerProvider } from "@amplication/util/nestjs/logging/test-utils";
+import { RolesPermissions } from "@amplication/util-roles-types";
+import { EnumResourceType } from "../../prisma";
+import { TeamAssignment } from "../../models/TeamAssignment";
 
 const UNEXPECTED_ORIGIN_ID = "unexpectedOriginId";
 
@@ -12,6 +15,7 @@ const EXAMPLE_WORKSPACE_ID = "exampleWorkspaceId";
 const EXAMPLE_WORKSPACE_NAME = "exampleWorkspaceName";
 
 const EXAMPLE_RESOURCE_ID = "exampleResourceId";
+const EXAMPLE_PROJECT_ID = "exampleProjectId";
 
 const EXAMPLE_RESOURCE_ROLE_ID = "exampleResourceRoleId";
 
@@ -48,15 +52,63 @@ const EXAMPLE_USER: AuthUser = {
   account: EXAMPLE_ACCOUNT,
 };
 
-const prismaResourceCountMock = jest.fn(() => {
-  return EXAMPLE_COUNT;
-});
+const EXAMPLE_RESOURCE: Resource = {
+  id: EXAMPLE_RESOURCE_ID,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  name: "exampleName",
+  codeGeneratorName: "NodeJS",
+  description: "",
+  resourceType: EnumResourceType.Service,
+  gitRepositoryOverride: false,
+  licensed: true,
+};
 
-const prismaResourceRoleFindFirstMock = jest.fn(() => {
-  return {
-    id: EXAMPLE_RESOURCE_ROLE_ID,
-  };
-});
+const EXAMPLE_TEAM_ASSIGNMENT: TeamAssignment = {
+  id: "exampleTeamAssignmentId",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  teamId: "exampleTeamId",
+  resourceId: "exampleResourceId",
+  roles: [
+    {
+      id: EXAMPLE_RESOURCE_ROLE_ID,
+      key: "exampleResourceRoleKey",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      name: "exampleResourceRoleName",
+      permissions: ["resource.create"],
+    },
+  ],
+};
+
+const prismaMock = {
+  teamAssignment: {
+    findMany: jest.fn(() => {
+      return [EXAMPLE_TEAM_ASSIGNMENT];
+    }),
+  },
+  resource: {
+    count: jest.fn(() => {
+      return EXAMPLE_COUNT;
+    }),
+    findFirst: jest.fn(() => {
+      return EXAMPLE_RESOURCE;
+    }),
+  },
+  resourceRole: {
+    findFirst: jest.fn(() => {
+      return {
+        id: EXAMPLE_RESOURCE_ROLE_ID,
+      };
+    }),
+  },
+  project: {
+    count: jest.fn(() => {
+      return EXAMPLE_COUNT;
+    }),
+  },
+};
 
 describe("PermissionsService", () => {
   let service: PermissionsService;
@@ -69,14 +121,7 @@ describe("PermissionsService", () => {
         MockedAmplicationLoggerProvider,
         {
           provide: PrismaService,
-          useClass: jest.fn().mockImplementation(() => ({
-            resource: {
-              count: prismaResourceCountMock,
-            },
-            resourceRole: {
-              findFirst: prismaResourceRoleFindFirstMock,
-            },
-          })),
+          useClass: jest.fn().mockImplementation(() => prismaMock),
         },
       ],
     }).compile();
@@ -132,8 +177,8 @@ describe("PermissionsService", () => {
         undefined
       )
     ).toEqual(true);
-    expect(prismaResourceCountMock).toHaveBeenCalledTimes(1);
-    expect(prismaResourceCountMock).toHaveBeenCalledWith(countArgs);
+    expect(prismaMock.resource.count).toHaveBeenCalledTimes(1);
+    expect(prismaMock.resource.count).toHaveBeenCalledWith(countArgs);
   });
 
   it("should return true if originType is an authorized instance of AuthorizableOriginParameter", async () => {
@@ -166,8 +211,8 @@ describe("PermissionsService", () => {
         undefined
       )
     ).toEqual(true);
-    expect(prismaResourceRoleFindFirstMock).toHaveBeenCalledTimes(1);
-    expect(prismaResourceRoleFindFirstMock).toHaveBeenCalledWith(countArgs);
+    expect(prismaMock.resourceRole.findFirst).toHaveBeenCalledTimes(1);
+    expect(prismaMock.resourceRole.findFirst).toHaveBeenCalledWith(countArgs);
   });
 
   it("should return false when originType is an unauthorized origin id", async () => {
@@ -184,5 +229,133 @@ describe("PermissionsService", () => {
         undefined
       )
     ).toEqual(false);
+  });
+
+  it("should return false when no originId is provided and originType is not None", async () => {
+    const args = {
+      user: EXAMPLE_USER,
+      originType: AuthorizableOriginParameter.WorkspaceId,
+      originId: "",
+    };
+    expect(
+      await service.validateAccess(
+        args.user,
+        args.originType,
+        args.originId,
+        undefined
+      )
+    ).toEqual(false);
+  });
+
+  it("should return false when validation function returns false", async () => {
+    const args = {
+      user: EXAMPLE_USER,
+      originType: AuthorizableOriginParameter.WorkspaceId,
+      originId: "AnotherWorkspaceId",
+    };
+    expect(
+      await service.validateAccess(
+        args.user,
+        args.originType,
+        args.originId,
+        undefined
+      )
+    ).toEqual(false);
+  });
+
+  it("should return true if user has wildcard permission", async () => {
+    const userWithWildcardPermission = {
+      ...EXAMPLE_USER,
+      permissions: ["*"] as RolesPermissions[],
+    };
+    const args = {
+      user: userWithWildcardPermission,
+      originType: AuthorizableOriginParameter.WorkspaceId,
+      originId: EXAMPLE_WORKSPACE_ID,
+    };
+    expect(
+      await service.validateAccess(args.user, args.originType, args.originId, [
+        "git.org.create",
+      ])
+    ).toEqual(true);
+  });
+
+  it("should return false if user does not have required permissions", async () => {
+    const userWithWildcardPermission = {
+      ...EXAMPLE_USER,
+      permissions: ["resource.create"] as RolesPermissions[],
+    };
+    const args = {
+      user: userWithWildcardPermission,
+      originType: AuthorizableOriginParameter.WorkspaceId,
+      originId: EXAMPLE_WORKSPACE_ID,
+    };
+    expect(
+      await service.validateAccess(args.user, args.originType, args.originId, [
+        "git.org.create",
+      ])
+    ).toEqual(false);
+  });
+
+  it("should return true if no specific permissions are required", async () => {
+    const args = {
+      user: EXAMPLE_USER,
+      originType: AuthorizableOriginParameter.WorkspaceId,
+      originId: EXAMPLE_WORKSPACE_ID,
+    };
+    expect(
+      await service.validateAccess(
+        args.user,
+        args.originType,
+        args.originId,
+        []
+      )
+    ).toEqual(true);
+  });
+
+  it("should return true if user has required permissions on team level", async () => {
+    const userWithOtherPermissions = {
+      ...EXAMPLE_USER,
+      permissions: ["git.org.create"] as RolesPermissions[],
+    };
+    const args = {
+      user: userWithOtherPermissions,
+      originType: AuthorizableOriginParameter.ProjectId,
+      originId: EXAMPLE_PROJECT_ID,
+      requiredPermissions: ["git.org.create"] as RolesPermissions[],
+    };
+    expect(
+      await service.validateAccess(
+        args.user,
+        args.originType,
+        args.originId,
+        args.requiredPermissions
+      )
+    ).toEqual(true);
+  });
+
+  it("should return true if user has required permissions on resource level", async () => {
+    const userWithPermissions = {
+      ...EXAMPLE_USER,
+      permissions: [],
+    };
+    const args = {
+      user: userWithPermissions,
+      originType: AuthorizableOriginParameter.ResourceId,
+      originId: EXAMPLE_RESOURCE_ID,
+      requiredPermissions: ["resource.create"] as RolesPermissions[],
+    };
+    prismaMock.resource.count.mockReturnValueOnce(1);
+    prismaMock.resourceRole.findFirst.mockReturnValueOnce({
+      id: EXAMPLE_RESOURCE_ROLE_ID,
+    });
+    expect(
+      await service.validateAccess(
+        args.user,
+        args.originType,
+        args.originId,
+        args.requiredPermissions
+      )
+    ).toEqual(true);
   });
 });
