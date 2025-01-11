9864de8bf46616c537a6a2803132a06441ac8949
yuval-hazaz <hazaz.yuval@gmail.com>
2025-01-11 20:14:40 +0200

    test(server):update include phrase

--- packages/amplication-server/src/core/commit/commit.service.spec.ts
+++ packages/amplication-server/src/core/commit/commit.service.spec.ts
@@ -62,8 +62,8 @@ describe("CommitService", () => {
   it("should find one Commit", async () => {
     const args = { where: { id: EXAMPLE_COMMIT_ID } };
     expect(await service.findOne(args)).toEqual(EXAMPLE_COMMIT);
-    expect(prismaCommitFindOneMock).toBeCalledTimes(1);
-    expect(prismaCommitFindOneMock).toBeCalledWith(args);
+    expect(prismaCommitFindOneMock).toHaveBeenCalledTimes(1);
+    expect(prismaCommitFindOneMock).toHaveBeenCalledWith(args);
   });
 
   it("should find many Commits", async () => {
@@ -78,8 +78,8 @@ describe("CommitService", () => {
       RESOURCE_TYPE_GROUP_TO_RESOURCE_TYPE[resourceTypeGroup];
 
     expect(await service.findMany(args)).toEqual([EXAMPLE_COMMIT]);
-    expect(prismaCommitFindManyMock).toBeCalledTimes(1);
-    expect(prismaCommitFindManyMock).toBeCalledWith({
+    expect(prismaCommitFindManyMock).toHaveBeenCalledTimes(1);
+    expect(prismaCommitFindManyMock).toHaveBeenCalledWith({
       ...args,
       include: {
         builds: {
