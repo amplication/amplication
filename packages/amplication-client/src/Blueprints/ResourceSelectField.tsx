5dbe098d4203820d16a2ecfb62358f47809e7bcb
yuval-hazaz <hazaz.yuval@gmail.com>
2025-03-09 14:48:57 +0200

    feat(client):remove legacy code

--- packages/amplication-client/src/Blueprints/ResourceSelectField.tsx
+++ packages/amplication-client/src/Blueprints/ResourceSelectField.tsx
@@ -11,28 +11,13 @@ type Props = Omit<SelectFieldProps, "options"> & {
 const ResourceSelectField = (props: Props) => {
   const { blueprintId, ...rest } = props;
 
-  const {
-    blueprintsMap: { blueprintsMapById },
-  } = useAppContext();
-
   const { catalog, setFilter } = useCatalog({ initialPageSize: 200 });
 
   useEffect(() => {
-    const blueprint = Object.values(blueprintsMapById).find(
-      (blueprint) => blueprint.id === blueprintId
-    );
-
-    if (blueprint?.key === "LEGACY_SERVICE") {
-      //LEGACY_SERVICE is a unique key for services that were created before the introduction of blueprints
-      setFilter({
-        resourceType: EnumResourceType.Service,
-      });
-    } else {
-      setFilter({
-        blueprintId: [blueprintId],
-      });
-    }
-  }, [blueprintId, blueprintsMapById, setFilter]);
+    setFilter({
+      blueprintId: [blueprintId],
+    });
+  }, [blueprintId, setFilter]);
 
   const options = useMemo(() => {
     return catalog?.map((catalog) => ({
